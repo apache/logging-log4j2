afbfed498b27052201cae37c2dc7c83dc2ff8aa2
Gary D. Gregory <ggregory@apache.org>
2012-07-22 12:42:25 +0000

    Add missing blank line.
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/log4j2/trunk@1364281 13f79535-47bb-0310-9956-ffa450edef68

--- api/src/main/java/org/apache/logging/log4j/message/ObjectMessage.java
+++ api/src/main/java/org/apache/logging/log4j/message/ObjectMessage.java
@@ -20,6 +20,7 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
+
 /**
  * Handles messages that contain an Object.
  */
