8c3e0617ea0dbc1cec49bfe8bb4bd235f13708c0
Ralph Goers <rgoers@apache.org>
2012-08-10 07:02:22 +0000

    Add support for formatting using String.format()
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/log4j2/trunk@1371582 13f79535-47bb-0310-9956-ffa450edef68

---
+++ api/src/main/java/org/apache/logging/log4j/message/StringFormattedMessage.java
@@ -0,0 +1,171 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache license, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the license for the specific language governing permissions and
+ * limitations under the license.
+ */
+package org.apache.logging.log4j.message;
+
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.status.StatusLogger;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.IllegalFormatException;
+
+/**
+ * Handles messages that consist of a format string conforming to java.util.Formatter
+ */
+public class StringFormattedMessage implements Message, Serializable {
+
+    private static final Logger LOGGER = StatusLogger.getLogger();
+
+    private static final long serialVersionUID = -665975803997290697L;
+
+    private static final int HASHVAL = 31;
+
+    private String messagePattern;
+    private transient Object[] argArray;
+    private String[] stringArgs;
+    private transient String formattedMessage;
+
+    /**
+     * Create the StringFormattedMessage.
+     */
+    public StringFormattedMessage() {
+        this(null, null, null);
+    }
+
+
+    public StringFormattedMessage(String messagePattern, Object... arguments) {
+        this.messagePattern = messagePattern;
+        this.argArray = arguments;
+    }
+
+    /**
+     * Return the formatted message.
+     * @return the formatted message.
+     */
+    public String getFormattedMessage() {
+        if (formattedMessage == null) {
+            formattedMessage = formatMessage(messagePattern, argArray);
+        }
+        return formattedMessage;
+    }
+
+    /**
+     * Returns the message pattern.
+     * @return the message pattern.
+     */
+    public String getMessageFormat() {
+        return messagePattern;
+    }
+
+    /**
+     * Set the message pattern.
+     * @param messagePattern The message pattern.
+     */
+    public void setMessageFormat(String messagePattern) {
+        this.messagePattern = messagePattern;
+        this.formattedMessage = null;
+    }
+
+    /**
+     * Returns the message parameters.
+     * @return the message parameters.
+     */
+    public Object[] getParameters() {
+        if (argArray != null) {
+            return argArray;
+        }
+        return stringArgs;
+    }
+
+    /**
+     * Sets the parameters for the message.
+     * @param parameters The parameters.
+     */
+    public void setParameters(Object[] parameters) {
+        this.argArray = parameters;
+        this.formattedMessage = null;
+    }
+
+    protected String formatMessage(String msgPattern, Object... args) {
+        try {
+            return String.format(msgPattern, args);
+        } catch (IllegalFormatException ife) {
+            LOGGER.error("Unable to format msg: " + msgPattern, ife);
+            return msgPattern;
+        }
+    }
+
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        StringFormattedMessage that = (StringFormattedMessage) o;
+
+        if (messagePattern != null ? !messagePattern.equals(that.messagePattern) : that.messagePattern != null) {
+            return false;
+        }
+        if (!Arrays.equals(stringArgs, that.stringArgs)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public int hashCode() {
+        int result = messagePattern != null ? messagePattern.hashCode() : 0;
+        result = HASHVAL * result + (stringArgs != null ? Arrays.hashCode(stringArgs) : 0);
+        return result;
+    }
+
+
+    public String toString() {
+        return "StringFormatMessage[messagePattern=" + messagePattern + ", args=" +
+            Arrays.toString(argArray) +  "]";
+    }
+
+    private void writeObject(ObjectOutputStream out) throws IOException {
+        out.defaultWriteObject();
+        getFormattedMessage();
+        out.writeUTF(formattedMessage);
+        out.writeUTF(messagePattern);
+        out.writeInt(argArray.length);
+        stringArgs = new String[argArray.length];
+        int i = 0;
+        for (Object obj: argArray) {
+            stringArgs[i] = obj.toString();
+            ++i;
+        }
+    }
+
+    private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
+        in.defaultReadObject();
+        formattedMessage = in.readUTF();
+        messagePattern = in.readUTF();
+        int length = in.readInt();
+        stringArgs = new String[length];
+        for (int i=0; i < length; ++i) {
+            stringArgs[i] = in.readUTF();
+        }
+    }
+}
---
+++ api/src/test/java/org/apache/logging/log4j/message/StringFormattedMessageTest.java
@@ -0,0 +1,97 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache license, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the license for the specific language governing permissions and
+ * limitations under the license.
+ */
+package org.apache.logging.log4j.message;
+
+import org.apache.logging.log4j.Timer;
+import org.junit.AfterClass;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ *
+ */
+public class StringFormattedMessageTest {
+
+    private static final int LOOP_CNT = 500;
+    String[] array = new String[LOOP_CNT];
+    private static long stringTime = 0;
+    private static long paramTime = 0;
+
+    @AfterClass
+    public static void after() {
+        if (stringTime > paramTime) {
+            System.out.println(String.format("Parameterized is %1$.2f times faster than StringFormat.",
+                ((float) stringTime / paramTime)));
+        } else if (stringTime > paramTime) {
+            System.out.println(String.format("Parameterized is %1$.2f times slower than StringFormat.",
+                ((float) paramTime / stringTime)));
+        } else {
+            System.out.println("The speed of Parameterized and StringFormat are the same");
+        }
+    }
+
+    @Test
+    public void testNoArgs() {
+        String testMsg = "Test message %1s";
+        StringFormattedMessage msg = new StringFormattedMessage(testMsg, null);
+        String result = msg.getFormattedMessage();
+        String expected = "Test message null";
+        assertEquals(expected, result);
+        Object[] array = null;
+        msg = new StringFormattedMessage(testMsg, array, null);
+        result = msg.getFormattedMessage();
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testOneArg() {
+        String testMsg = "Test message %1s";
+        StringFormattedMessage msg = new StringFormattedMessage(testMsg, "Apache");
+        String result = msg.getFormattedMessage();
+        String expected = "Test message Apache";
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testStringPerf() {
+        String testMsg = "Test message %1s %2s";
+        Timer timer = new Timer("StringFormat", LOOP_CNT);
+        timer.start();
+        for (int i = 0; i < LOOP_CNT; ++i) {
+            StringFormattedMessage msg = new StringFormattedMessage(testMsg, "Apache", "Log4j");
+            array[i] = msg.getFormattedMessage();
+        }
+        timer.stop();
+        stringTime = timer.getElapsedNanoTime();
+        System.out.println(timer.toString());
+    }
+
+    @Test
+    public void testParameterizedPerf() {
+        String testMsg = "Test message {} {}";
+        Timer timer = new Timer("Parameterized", LOOP_CNT);
+        timer.start();
+        for (int i = 0; i < LOOP_CNT; ++i) {
+            ParameterizedMessage msg = new ParameterizedMessage(testMsg, "Apache", "Log4j");
+            array[i] = msg.getFormattedMessage();
+        }
+        timer.stop();
+        paramTime = timer.getElapsedNanoTime();
+        System.out.println(timer.toString());
+    }
+}
