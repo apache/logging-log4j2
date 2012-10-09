b6648baf13701e4fe1580a5e48afdfe3deb8789d
Gary D. Gregory <ggregory@apache.org>
2012-10-09 20:09:09 +0000

    Make ivar private final.
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/log4j2/trunk@1396279 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/test/java/org/apache/logging/log4j/core/util/AbsoluteDateAndTimeFilter.java
+++ core/src/test/java/org/apache/logging/log4j/core/util/AbsoluteDateAndTimeFilter.java
@@ -19,9 +19,8 @@ package org.apache.logging.log4j.core.util;
 
 import org.apache.oro.text.perl.Perl5Util;
 
-
 public class AbsoluteDateAndTimeFilter implements Filter {
-  Perl5Util util = new Perl5Util();
+  private final Perl5Util util = new Perl5Util();
 
   public String filter(String in) {
     String pat = "/" + Filter.ABSOLUTE_DATE_AND_TIME_PAT + "/";
