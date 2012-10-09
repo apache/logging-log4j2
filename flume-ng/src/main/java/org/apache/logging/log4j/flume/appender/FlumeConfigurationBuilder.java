96e9ad1b13f763127236feb1a17cfc0435ce4dca
Gary D. Gregory <ggregory@apache.org>
2012-10-09 11:32:07 +0000

    Remove unused imports.
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/log4j2/trunk@1395971 13f79535-47bb-0310-9956-ffa450edef68

--- flume-ng/src/main/java/org/apache/logging/log4j/flume/appender/FlumeConfigurationBuilder.java
+++ flume-ng/src/main/java/org/apache/logging/log4j/flume/appender/FlumeConfigurationBuilder.java
@@ -56,9 +56,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
-import java.util.SortedMap;
-import java.util.SortedSet;
-import java.util.TreeMap;
 import java.util.TreeSet;
 
 /**
