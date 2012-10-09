b8b5f53867593127b3793cfaa7a12e387da156b7
Gary D. Gregory <ggregory@apache.org>
2012-10-09 12:36:19 +0000

    Remove unused import.
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/log4j2/trunk@1395999 13f79535-47bb-0310-9956-ffa450edef68

--- api/src/test/java/org/apache/logging/log4j/LevelTest.java
+++ api/src/test/java/org/apache/logging/log4j/LevelTest.java
@@ -18,7 +18,6 @@ package org.apache.logging.log4j;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
