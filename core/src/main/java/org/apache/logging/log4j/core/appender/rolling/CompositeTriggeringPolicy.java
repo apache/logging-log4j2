4f8d36f5ecb16d9dbce3c40f712b02a87616e99e
Gary D. Gregory <ggregory@apache.org>
2012-10-09 21:51:44 +0000

    Use varargs.
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/log4j2/trunk@1396356 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/main/java/org/apache/logging/log4j/core/appender/rolling/CompositeTriggeringPolicy.java
+++ core/src/main/java/org/apache/logging/log4j/core/appender/rolling/CompositeTriggeringPolicy.java
@@ -29,7 +29,7 @@ public final class CompositeTriggeringPolicy implements TriggeringPolicy {
 
     private final TriggeringPolicy[] policies;
 
-    private CompositeTriggeringPolicy(TriggeringPolicy[] policies) {
+    private CompositeTriggeringPolicy(TriggeringPolicy... policies) {
         this.policies = policies;
     }
 
