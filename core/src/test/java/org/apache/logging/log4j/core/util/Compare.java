33d5238665366a26d7961289a5313f9e5f00b972
Gary D. Gregory <ggregory@apache.org>
2012-10-09 20:11:02 +0000

    Organize imports.
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/log4j2/trunk@1396284 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/test/java/org/apache/logging/log4j/core/util/Compare.java
+++ core/src/test/java/org/apache/logging/log4j/core/util/Compare.java
@@ -18,14 +18,13 @@
 package org.apache.logging.log4j.core.util;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.*;
-import java.net.URL;
-
 
 public class Compare {
     static final int B1_NULL = -1;
