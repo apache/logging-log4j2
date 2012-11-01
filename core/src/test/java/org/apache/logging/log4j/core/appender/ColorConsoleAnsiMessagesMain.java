4a4d3a93cd9e75fcfb8be442a33ee67ee9bce804
Gary D. Gregory <ggregory@apache.org>
2012-11-01 02:11:49 +0000

    Shows how to use ANSI escape codes to color messages. Each message is printed to the console in color, but the rest of the log entry (time stamp for example) is in the default color for that console.
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/log4j2/trunk@1404450 13f79535-47bb-0310-9956-ffa450edef68

---
+++ core/src/test/java/org/apache/logging/log4j/core/appender/ColorConsoleAnsiMessagesMain.java
@@ -0,0 +1,34 @@
+package org.apache.logging.log4j.core.appender;
+
+import java.io.IOException;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.LoggerContext;
+import org.apache.logging.log4j.core.config.Configurator;
+
+/**
+ * Shows how to use ANSI escape codes to color messages. Each message is printed to the console in color, but the rest of the log entry
+ * (time stamp for example) is in the default color for that console.
+ */
+public class ColorConsoleAnsiMessagesMain {
+
+    private static final Logger LOG = LogManager.getLogger(ColorConsoleAnsiMessagesMain.class);
+
+    public static void main(String[] args) {
+        LoggerContext ctx = Configurator.initialize(ColorConsoleAnsiMessagesMain.class.getName(), null,
+                "target/test-classes/log4j2-console.xml");
+        try {
+            LOG.fatal("\u001b[1;35mFatal message.\u001b[0m");
+            LOG.error("\u001b[1;31mError message.\u001b[0m");
+            LOG.warn("\u001b[0;33mWarning message.\u001b[0m");
+            LOG.info("\u001b[0;32mInformation message.\u001b[0m");
+            LOG.debug("\u001b[0;36mDebug message.\u001b[0m");
+            LOG.trace("\u001b[1;30mTrace message.\u001b[0m");
+            LOG.error("\u001b[1;31mError message.\u001b[0m", new IOException("test"));
+        } finally {
+            Configurator.shutdown(ctx);
+        }
+    }
+
+}
