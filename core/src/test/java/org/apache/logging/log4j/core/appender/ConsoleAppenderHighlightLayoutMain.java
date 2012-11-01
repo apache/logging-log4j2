147eccdc279372796b79098afda115d01597dce0
Gary D. Gregory <ggregory@apache.org>
2012-11-01 13:16:46 +0000

    Shows how to use ANSI escape codes to color messages.
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/log4j2/trunk@1404578 13f79535-47bb-0310-9956-ffa450edef68

---
+++ core/src/test/java/org/apache/logging/log4j/core/appender/ConsoleAppenderHighlightLayoutMain.java
@@ -0,0 +1,30 @@
+package org.apache.logging.log4j.core.appender;
+
+import java.io.IOException;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.LoggerContext;
+import org.apache.logging.log4j.core.config.Configurator;
+
+public class ConsoleAppenderHighlightLayoutMain {
+
+    private static final Logger LOG = LogManager.getLogger(ConsoleAppenderHighlightLayoutMain.class);
+
+    public static void main(String[] args) {
+        LoggerContext ctx = Configurator.initialize(ConsoleAppenderAnsiMessagesMain.class.getName(), null,
+                "target/test-classes/log4j2-console-highlight.xml");
+        try {
+            LOG.fatal("Fatal message.");
+            LOG.error("Error message.");
+            LOG.warn("Warning message.");
+            LOG.info("Information message.");
+            LOG.debug("Debug message.");
+            LOG.trace("Trace message.");
+            LOG.error("Error message.", new IOException("test"));
+        } finally {
+            Configurator.shutdown(ctx);
+        }
+    }
+
+}
