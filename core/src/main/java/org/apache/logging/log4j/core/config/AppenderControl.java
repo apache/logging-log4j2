65161d4018b476d079e7ef929f16bdd889ffa7ab
Gary D. Gregory <ggregory@apache.org>
2012-11-02 22:31:09 +0000

    The expression of type Appender is already an instance of type LifeCycle.
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/log4j2/trunk@1405194 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/main/java/org/apache/logging/log4j/core/config/AppenderControl.java
+++ core/src/main/java/org/apache/logging/log4j/core/config/AppenderControl.java
@@ -19,7 +19,6 @@ package org.apache.logging.log4j.core.config;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.core.Appender;
 import org.apache.logging.log4j.core.Filter;
-import org.apache.logging.log4j.core.LifeCycle;
 import org.apache.logging.log4j.core.LogEvent;
 import org.apache.logging.log4j.core.appender.AppenderRuntimeException;
 import org.apache.logging.log4j.core.filter.Filterable;
