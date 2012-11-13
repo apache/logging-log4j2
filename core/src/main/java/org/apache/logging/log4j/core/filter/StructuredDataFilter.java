c2251174cb949e2a005f1f5e26287915dab4ca66
Gary D. Gregory <ggregory@apache.org>
2012-11-13 02:16:14 +0000

    Remove unused import.
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/log4j2/trunk@1408539 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/main/java/org/apache/logging/log4j/core/filter/StructuredDataFilter.java
+++ core/src/main/java/org/apache/logging/log4j/core/filter/StructuredDataFilter.java
@@ -30,7 +30,6 @@ import org.apache.logging.log4j.message.StructuredDataMessage;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
 
 /**
