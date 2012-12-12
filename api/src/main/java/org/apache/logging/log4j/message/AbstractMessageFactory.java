44b6eb14e75c2d3211e13d7a59d5737556bb6753
Gary D. Gregory <ggregory@apache.org>
2012-12-12 15:30:27 +0000

    Javadoc.
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/log4j2/trunk@1420767 13f79535-47bb-0310-9956-ffa450edef68

--- api/src/main/java/org/apache/logging/log4j/message/AbstractMessageFactory.java
+++ api/src/main/java/org/apache/logging/log4j/message/AbstractMessageFactory.java
@@ -1,26 +1,36 @@
 package org.apache.logging.log4j.message;
 
 /**
+ * Provides an abstract superclass for MessageFactory implementations with default implementations.
+ * <p>
+ * This class is immutable.
+ * </p>
  * 
  * @version $Id$
  */
 public abstract class AbstractMessageFactory implements MessageFactory {
 
-    /* (non-Javadoc)
+    /*
+     * (non-Javadoc)
+     * 
      * @see org.apache.logging.log4j.message.MessageFactory#newMessage(java.lang.Object)
      */
     public Message newMessage(final Object message) {
         return new ObjectMessage(message);
     }
 
-    /* (non-Javadoc)
+    /*
+     * (non-Javadoc)
+     * 
      * @see org.apache.logging.log4j.message.MessageFactory#newMessage(java.lang.String)
      */
     public Message newMessage(final String message) {
         return new SimpleMessage(message);
     }
 
-    /* (non-Javadoc)
+    /*
+     * (non-Javadoc)
+     * 
      * @see org.apache.logging.log4j.message.MessageFactory#newMessage(java.lang.String, java.lang.Object)
      */
     public abstract Message newMessage(String message, Object... params);
