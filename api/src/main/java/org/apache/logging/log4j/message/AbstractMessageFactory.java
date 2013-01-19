53c5fa729e4051f3244ba95bfc04fca376cd4221
Ralph Goers <rgoers@apache.org>
2013-01-19 05:52:55 +0000

    Add missing license headers
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/log4j2/trunk@1435494 13f79535-47bb-0310-9956-ffa450edef68

--- api/src/main/java/org/apache/logging/log4j/message/AbstractMessageFactory.java
+++ api/src/main/java/org/apache/logging/log4j/message/AbstractMessageFactory.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache license, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the license for the specific language governing permissions and
+ * limitations under the license.
+ */
 package org.apache.logging.log4j.message;
 
 /**
@@ -5,14 +21,14 @@ package org.apache.logging.log4j.message;
  * <p>
  * This class is immutable.
  * </p>
- * 
+ *
  * @version $Id$
  */
 public abstract class AbstractMessageFactory implements MessageFactory {
 
     /*
      * (non-Javadoc)
-     * 
+     *
      * @see org.apache.logging.log4j.message.MessageFactory#newMessage(java.lang.Object)
      */
     public Message newMessage(final Object message) {
@@ -21,7 +37,7 @@ public abstract class AbstractMessageFactory implements MessageFactory {
 
     /*
      * (non-Javadoc)
-     * 
+     *
      * @see org.apache.logging.log4j.message.MessageFactory#newMessage(java.lang.String)
      */
     public Message newMessage(final String message) {
@@ -30,7 +46,7 @@ public abstract class AbstractMessageFactory implements MessageFactory {
 
     /*
      * (non-Javadoc)
-     * 
+     *
      * @see org.apache.logging.log4j.message.MessageFactory#newMessage(java.lang.String, java.lang.Object)
      */
     public abstract Message newMessage(String message, Object... params);
