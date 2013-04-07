def9908fc399b9fa1f57b219449ad0b8e464c42a
Ralph Goers <rgoers@apache.org>
2013-04-07 22:07:42 +0000

    LOG4J2-195 - Move files generated by async tests to target directory. Add missing license headers
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/log4j2/trunk@1465483 13f79535-47bb-0310-9956-ffa450edef68

--- log4j-async/src/test/java/org/apache/logging/log4j/async/AsyncLoggerContextSelectorTest.java
+++ log4j-async/src/test/java/org/apache/logging/log4j/async/AsyncLoggerContextSelectorTest.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache license, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the license for the specific language governing permissions and
+ * limitations under the license.
+ */
 package org.apache.logging.log4j.async;
 
 import static org.junit.Assert.*;
@@ -13,7 +29,7 @@ public class AsyncLoggerContextSelectorTest {
     public void testContextReturnsAsyncLoggerContext() {
         AsyncLoggerContextSelector selector = new AsyncLoggerContextSelector();
         LoggerContext context = selector.getContext(null, null, false);
-        
+
         assertTrue(context instanceof AsyncLoggerContext);
     }
 
@@ -21,7 +37,7 @@ public class AsyncLoggerContextSelectorTest {
     public void testContext2ReturnsAsyncLoggerContext() {
         AsyncLoggerContextSelector selector = new AsyncLoggerContextSelector();
         LoggerContext context = selector.getContext(null, null, false, null);
-        
+
         assertTrue(context instanceof AsyncLoggerContext);
     }
 
@@ -29,7 +45,7 @@ public class AsyncLoggerContextSelectorTest {
     public void testLoggerContextsReturnsAsyncLoggerContext() {
         AsyncLoggerContextSelector selector = new AsyncLoggerContextSelector();
         List<LoggerContext> list = selector.getLoggerContexts();
-        
+
         assertEquals(1, list.size());
         assertTrue(list.get(0) instanceof AsyncLoggerContext);
     }
--- log4j-async/src/test/java/org/apache/logging/log4j/async/appender/FastRollingFileAppenderLocationTest.java
+++ log4j-async/src/test/java/org/apache/logging/log4j/async/appender/FastRollingFileAppenderLocationTest.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache license, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the license for the specific language governing permissions and
+ * limitations under the license.
+ */
 package org.apache.logging.log4j.async.appender;
 
 import static org.junit.Assert.*;
@@ -23,7 +39,7 @@ public class FastRollingFileAppenderLocationTest {
 
     @Test
     public void testLocationIncluded() throws Exception {
-        File f = new File("FastRollingFileAppenderLocationTest.log");
+        File f = new File("target", "FastRollingFileAppenderLocationTest.log");
         // System.out.println(f.getAbsolutePath());
         f.delete();
         Logger log = LogManager.getLogger("com.foo.Bar");
