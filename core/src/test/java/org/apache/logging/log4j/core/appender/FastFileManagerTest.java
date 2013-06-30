b09fbe4901714e8288132dd9836b5f59cba056e1
Remko Popma <rpopma@apache.org>
2013-06-30 00:40:33 +0000

    Fixes for LOG4J2-295, LOG4J2-292 and LOG4J2-267 Fast(Rolling)FileAppender bugs; added JUnit tests.
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/log4j2/trunk@1498044 13f79535-47bb-0310-9956-ffa450edef68

---
+++ core/src/test/java/org/apache/logging/log4j/core/appender/FastFileManagerTest.java
@@ -0,0 +1,105 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache license, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the license for the specific language governing permissions and
+ * limitations under the license.
+ */
+package org.apache.logging.log4j.core.appender;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.RandomAccessFile;
+
+import org.junit.Test;
+
+/**
+ * Tests the FastFileManager class.
+ */
+public class FastFileManagerTest {
+
+    /**
+     * Test method for
+     * {@link org.apache.logging.log4j.core.appender.FastFileManager#write(byte[], int, int)}
+     * .
+     */
+    @Test
+    public void testWrite_multiplesOfBufferSize() throws IOException {
+        File file = File.createTempFile("log4j2", "test");
+        file.deleteOnExit();
+        RandomAccessFile raf = new RandomAccessFile(file, "rw");
+        OutputStream os = new FastFileManager.DummyOutputStream();
+        FastFileManager manager = new FastFileManager(raf, file.getName(), os,
+                false, null, null);
+
+        int size = FastFileManager.DEFAULT_BUFFER_SIZE * 3;
+        byte[] data = new byte[size];
+        manager.write(data); // no buffer overflow exception
+
+        // buffer is full but not flushed yet
+        assertEquals(FastFileManager.DEFAULT_BUFFER_SIZE * 2, raf.length());
+    }
+
+    /**
+     * Test method for
+     * {@link org.apache.logging.log4j.core.appender.FastFileManager#write(byte[], int, int)}
+     * .
+     */
+    @Test
+    public void testWrite_dataExceedingBufferSize() throws IOException {
+        File file = File.createTempFile("log4j2", "test");
+        file.deleteOnExit();
+        RandomAccessFile raf = new RandomAccessFile(file, "rw");
+        OutputStream os = new FastFileManager.DummyOutputStream();
+        FastFileManager manager = new FastFileManager(raf, file.getName(), os,
+                false, null, null);
+
+        int size = FastFileManager.DEFAULT_BUFFER_SIZE * 3 + 1;
+        byte[] data = new byte[size];
+        manager.write(data); // no exception
+        assertEquals(FastFileManager.DEFAULT_BUFFER_SIZE * 3, raf.length());
+
+        manager.flush();
+        assertEquals(size, raf.length()); // all data written to file now
+    }
+
+    @Test
+    public void testAppendDoesNotOverwriteExistingFile() throws IOException {
+        boolean isAppend = true;
+        File file = File.createTempFile("log4j2", "test");
+        file.deleteOnExit();
+        assertEquals(0, file.length());
+
+        byte[] bytes = new byte[4 * 1024];
+
+        // create existing file
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream(file);
+            fos.write(bytes, 0, bytes.length);
+            fos.flush();
+        } finally {
+            fos.close();
+        }
+        assertEquals("all flushed to disk", bytes.length, file.length());
+
+        FastFileManager manager = FastFileManager.getFileManager(
+                file.getAbsolutePath(), isAppend, true, null, null);
+        manager.write(bytes, 0, bytes.length);
+        int expected = bytes.length * 2;
+        assertEquals("appended, not overwritten", expected, file.length());
+    }
+}
