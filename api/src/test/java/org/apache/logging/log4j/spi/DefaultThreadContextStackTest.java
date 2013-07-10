3e1a4d2564e6deef06649abe76f17f17ef73ec6d
Remko Popma <rpopma@apache.org>
2013-07-10 15:49:03 +0000

    LOG4J2-302: Added toString methods to ThreadContextStack/Map implementation classes
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/log4j2/trunk@1501806 13f79535-47bb-0310-9956-ffa450edef68

--- api/src/test/java/org/apache/logging/log4j/spi/DefaultThreadContextStackTest.java
+++ api/src/test/java/org/apache/logging/log4j/spi/DefaultThreadContextStackTest.java
@@ -317,4 +317,18 @@ public class DefaultThreadContextStackTest {
         assertTrue(stack.contains("msg3"));
     }
 
+    @Test
+    public void testToStringShowsListContents() {
+        final DefaultThreadContextStack stack = new DefaultThreadContextStack(true);
+        stack.clear();
+        assertEquals("[]", stack.toString());
+        
+        stack.push("msg1");
+        stack.add("msg2");
+        stack.push("msg3");
+        assertEquals("[msg1, msg2, msg3]", stack.toString());
+        
+        stack.retainAll(Arrays.asList("msg1", "msg3"));
+        assertEquals("[msg1, msg3]", stack.toString());
+    }
 }
--- api/src/test/java/org/apache/logging/log4j/spi/MutableThreadContextStackTest.java
+++ api/src/test/java/org/apache/logging/log4j/spi/MutableThreadContextStackTest.java
@@ -320,4 +320,18 @@ public class MutableThreadContextStackTest {
         assertFalse(stack.contains("msg2"));
         assertTrue(stack.contains("msg3"));
     }
+
+    @Test
+    public void testToStringShowsListContents() {
+        final MutableThreadContextStack stack = new MutableThreadContextStack(new ArrayList<String>());
+        assertEquals("[]", stack.toString());
+        
+        stack.push("msg1");
+        stack.add("msg2");
+        stack.push("msg3");
+        assertEquals("[msg1, msg2, msg3]", stack.toString());
+        
+        stack.retainAll(Arrays.asList("msg1", "msg3"));
+        assertEquals("[msg1, msg3]", stack.toString());
+    }
 }
