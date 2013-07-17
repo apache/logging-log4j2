cc2a2fa9efcc5484f0950ebb25841e07a5a3f5ae
Gary D. Gregory <ggregory@apache.org>
2013-07-17 16:08:03 +0000

    [LOG4J2-312] XML layout improvements (compact vs. pretty, namespace, namespace prefix, root element).
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/log4j2/trunk@1504184 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/test/java/org/apache/logging/log4j/core/appender/XmlFileAppenderTest.java
+++ core/src/test/java/org/apache/logging/log4j/core/appender/XmlFileAppenderTest.java
@@ -54,31 +54,27 @@ public class XmlFileAppenderTest {
         String line1;
         String line2;
         String line3;
-        String line4;
         try {
             line1 = reader.readLine();
             line2 = reader.readLine();
             line3 = reader.readLine();
-            line4 = reader.readLine();
         } finally {
             reader.close();
             f.delete();
         }
         assertNotNull("line1", line1);
-        final String msg1 = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
-        assertTrue("line1 incorrect: [" + line1 + "], does not contain: [" + msg1 + "]", line1.equals(msg1));
         
-        assertNotNull("line2", line2);
-        final String msg2 = "<log4j:eventSet xmlns:log4j=\"http://logging.apache.org/log4j/\">";
-        assertTrue("line2 incorrect: [" + line2 + "], does not contain: [" + msg2 + "]", line2.equals(msg2));
-        
-        assertNotNull("line3", line3);
-        final String msg3 = "<log4j:event ";
-        assertTrue("line3 incorrect: [" + line3 + "], does not contain: [" + msg3 + "]", line3.contains(msg3));
+        assertNotNull("line1", line1);
+        final String msg1 = "<log4j:event ";
+        assertTrue("line1 incorrect: [" + line1 + "], does not contain: [" + msg1 + "]", line1.contains(msg1));
 
-        assertNotNull("line4", line4);
-        final String msg4 = logMsg;
-        assertTrue("line4 incorrect: [" + line4 + "], does not contain: [" + msg4 + "]", line4.contains(msg4));
+        assertNotNull("line2", line2);
+        final String msg2 = logMsg;
+        assertTrue("line2 incorrect: [" + line2 + "], does not contain: [" + msg2 + "]", line2.contains(msg2));
+
+        assertNotNull("line3", line3);
+        final String msg3 = "</log4j:event>";
+        assertTrue("line3 incorrect: [" + line3 + "], does not contain: [" + msg3 + "]", line3.contains(msg3));
 
         final String location = "testFlushAtEndOfBatch";
         assertTrue("no location", !line1.contains(location));
