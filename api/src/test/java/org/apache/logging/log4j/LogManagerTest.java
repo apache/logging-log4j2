970ab0e02abf5510339fb559e0aa1b8ce8dab277
Gary D. Gregory <ggregory@apache.org>
2013-07-29 18:49:26 +0000

    Fix compiler warning (generics).
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/log4j2/trunk@1508175 13f79535-47bb-0310-9956-ffa450edef68

--- api/src/test/java/org/apache/logging/log4j/LogManagerTest.java
+++ api/src/test/java/org/apache/logging/log4j/LogManagerTest.java
@@ -35,10 +35,10 @@ public class LogManagerTest {
         logger = LogManager.getLogger(ParameterizedMessageFactory.INSTANCE);
         assertNotNull("No Logger returned", logger);
         assertTrue("Incorrect Logger name: " + logger.getName(),LogManagerTest.class.getName().equals(logger.getName()));
-        logger = LogManager.getLogger((Class) null);
+        logger = LogManager.getLogger((Class<?>) null);
         assertNotNull("No Logger returned", logger);
         assertTrue("Incorrect Logger name: " + logger.getName(),LogManagerTest.class.getName().equals(logger.getName()));
-        logger = LogManager.getLogger((Class) null, ParameterizedMessageFactory.INSTANCE);
+        logger = LogManager.getLogger((Class<?>) null, ParameterizedMessageFactory.INSTANCE);
         assertNotNull("No Logger returned", logger);
         assertTrue("Incorrect Logger name: " + logger.getName(),LogManagerTest.class.getName().equals(logger.getName()));
         logger = LogManager.getLogger((String) null);
