ec6a2991bae45290b7c254982cf65ec5bf6faa28
Ralph Goers <rgoers@apache.org>
2013-08-13 06:02:12 +0000

    LOG4J2-342 - Ignore xml:base attributes.
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/log4j2/trunk@1513360 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/main/java/org/apache/logging/log4j/core/config/XMLConfiguration.java
+++ core/src/main/java/org/apache/logging/log4j/core/config/XMLConfiguration.java
@@ -368,6 +368,9 @@ public class XMLConfiguration extends BaseConfiguration implements Reconfigurabl
             final org.w3c.dom.Node w3cNode = attrs.item(i);
             if (w3cNode instanceof Attr) {
                 final Attr attr = (Attr) w3cNode;
+                if (attr.getName().equals("xml:base")) {
+                    continue;
+                }
                 attributes.put(attr.getName(), attr.getValue());
             }
         }
