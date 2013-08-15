b73cdc0fab065863d918b4f1adfb8aba77128d23
Gary D. Gregory <ggregory@apache.org>
2013-08-15 13:28:34 +0000

    Format (up to 120 line length).
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/log4j2/trunk@1514259 13f79535-47bb-0310-9956-ffa450edef68

--- api/src/main/java/org/apache/logging/log4j/spi/ThreadContextStack.java
+++ api/src/main/java/org/apache/logging/log4j/spi/ThreadContextStack.java
@@ -23,6 +23,5 @@ import org.apache.logging.log4j.ThreadContext;
 /**
  *
  */
-public interface ThreadContextStack extends ThreadContext.ContextStack,
-        Collection<String> {
+public interface ThreadContextStack extends ThreadContext.ContextStack, Collection<String> {
 }
