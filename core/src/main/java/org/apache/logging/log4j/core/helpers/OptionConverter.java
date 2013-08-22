88f22c07e599b1518097ce35d0e1ab5f82296d63
Gary D. Gregory <ggregory@apache.org>
2013-08-22 15:41:27 +0000

    Make reading this nested code easier: Statements unnecessarily nested within else clause.
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/log4j2/trunk@1516490 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/main/java/org/apache/logging/log4j/core/helpers/OptionConverter.java
+++ core/src/main/java/org/apache/logging/log4j/core/helpers/OptionConverter.java
@@ -292,39 +292,37 @@ public final class OptionConverter {
                 // no more variables
                 if (i == 0) { // this is a simple string
                     return val;
-                } else { // add the tail string which contails no variables and return the result.
-                    sbuf.append(val.substring(i, val.length()));
-                    return sbuf.toString();
                 }
-            } else {
-                sbuf.append(val.substring(i, j));
-                k = val.indexOf(DELIM_STOP, j);
-                if (k == -1) {
-                    throw new IllegalArgumentException('"' + val +
-                        "\" has no closing brace. Opening brace at position " + j
-                        + '.');
-                } else {
-                    j += DELIM_START_LEN;
-                    final String key = val.substring(j, k);
-                    // first try in System properties
-                    String replacement = PropertiesUtil.getProperties().getStringProperty(key, null);
-                    // then try props parameter
-                    if (replacement == null && props != null) {
-                        replacement = props.getProperty(key);
-                    }
-
-                    if (replacement != null) {
-                        // Do variable substitution on the replacement string
-                        // such that we can solve "Hello ${x2}" as "Hello p1"
-                        // the where the properties are
-                        // x1=p1
-                        // x2=${x1}
-                        final String recursiveReplacement = substVars(replacement, props);
-                        sbuf.append(recursiveReplacement);
-                    }
-                    i = k + DELIM_STOP_LEN;
-                }
+                // add the tail string which contails no variables and return the result.
+                sbuf.append(val.substring(i, val.length()));
+                return sbuf.toString();
             }
+            sbuf.append(val.substring(i, j));
+            k = val.indexOf(DELIM_STOP, j);
+            if (k == -1) {
+                throw new IllegalArgumentException('"' + val +
+                    "\" has no closing brace. Opening brace at position " + j
+                    + '.');
+            }
+            j += DELIM_START_LEN;
+            final String key = val.substring(j, k);
+            // first try in System properties
+            String replacement = PropertiesUtil.getProperties().getStringProperty(key, null);
+            // then try props parameter
+            if (replacement == null && props != null) {
+                replacement = props.getProperty(key);
+            }
+
+            if (replacement != null) {
+                // Do variable substitution on the replacement string
+                // such that we can solve "Hello ${x2}" as "Hello p1"
+                // the where the properties are
+                // x1=p1
+                // x2=${x1}
+                final String recursiveReplacement = substVars(replacement, props);
+                sbuf.append(recursiveReplacement);
+            }
+            i = k + DELIM_STOP_LEN;
         }
     }
 }
