3b39d3de00bdbecaeb7352717231d9db62655f83
Gary D. Gregory <ggregory@apache.org>
2013-10-09 17:41:16 +0000

    Javadoc: Use the active voice.
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/log4j2/trunk@1530727 13f79535-47bb-0310-9956-ffa450edef68

--- log4j-core/src/main/java/org/apache/logging/log4j/core/lookup/SystemPropertiesLookup.java
+++ log4j-core/src/main/java/org/apache/logging/log4j/core/lookup/SystemPropertiesLookup.java
@@ -26,7 +26,7 @@ import org.apache.logging.log4j.core.config.plugins.Plugin;
 public class SystemPropertiesLookup implements StrLookup {
 
     /**
-     * Lookup the value for the key.
+     * Looks up the value for the key.
      * @param key  the key to be looked up, may be null
      * @return The value for the key.
      */
@@ -40,7 +40,7 @@ public class SystemPropertiesLookup implements StrLookup {
     }
 
     /**
-     * Lookup the value for the key using the data in the LogEvent.
+     * Looks up the value for the key using the data in the LogEvent.
      * @param event The current LogEvent.
      * @param key  the key to be looked up, may be null
      * @return The value associated with the key.
