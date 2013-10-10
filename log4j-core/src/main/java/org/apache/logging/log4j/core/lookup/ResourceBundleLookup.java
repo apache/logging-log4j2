b60743bc808ec42fd100269a4357d1948e251e31
Gary D. Gregory <ggregory@apache.org>
2013-10-10 18:10:23 +0000

    [LOG4J2-420] Create a lookup for resource bundle substitution.
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/log4j2/trunk@1531064 13f79535-47bb-0310-9956-ffa450edef68

---
+++ log4j-core/src/main/java/org/apache/logging/log4j/core/lookup/ResourceBundleLookup.java
@@ -0,0 +1,78 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache license, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the license for the specific language governing permissions and
+ * limitations under the license.
+ */
+package org.apache.logging.log4j.core.lookup;
+
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+
+import org.apache.logging.log4j.core.LogEvent;
+import org.apache.logging.log4j.core.config.plugins.Plugin;
+
+/**
+ * Looks up keys from resource bundles.
+ */
+@Plugin(name = "bundle", category = "Lookup")
+public class ResourceBundleLookup implements StrLookup {
+
+    /**
+     * Looks up the value for the key in the format "BundleName:BundleKey".
+     * 
+     * For example: "com.domain.messages:MyKey".
+     * 
+     * @param key
+     *            the key to be looked up, may be null
+     * @return The value for the key.
+     */
+    @Override
+    public String lookup(final String key) {
+        if (key == null) {
+            return null;
+        }
+        String[] keys = key.split(":");
+        int keyLen = keys.length;
+        if (keyLen != 2) {
+            // throw new IllegalArgumentException("Bad key format " + key + ", format is BundleName:Value");
+            // log?
+            return null;
+        }
+        String bundleName = keys[0];
+        String bundleKey = keys[1];
+        try {
+            // The ResourceBundle class caches bundles, no need to cache here.
+            return ResourceBundle.getBundle(bundleName).getString(bundleKey);
+        } catch (MissingResourceException e) {
+            // log?
+            return null;
+        }
+    }
+
+    /**
+     * Looks up the value for the key in the format "BundleName:BundleKey".
+     * 
+     * For example: "com.domain.messages:MyKey".
+     * 
+     * @param event
+     *            The current LogEvent.
+     * @param key
+     *            the key to be looked up, may be null
+     * @return The value associated with the key.
+     */
+    @Override
+    public String lookup(final LogEvent event, final String key) {
+        return lookup(key);
+    }
+}
