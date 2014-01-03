b86ff20253405e66e38aa5772cb138e0d95939d7
rpopma <rpopma@unknown>
2014-01-03 12:48:46 +0000

    LOG4J2-467: Added option to toggle Thread name caching in AsyncLogger
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/log4j2/trunk@1555076 13f79535-47bb-0310-9956-ffa450edef68

---
+++ log4j-core/src/test/java/org/apache/logging/log4j/core/async/AsyncLoggerTestUncachedThreadName.java
@@ -0,0 +1,76 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache license, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the license for the specific language governing permissions and
+ * limitations under the license.
+ */
+package org.apache.logging.log4j.core.async;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.LifeCycle;
+import org.apache.logging.log4j.core.config.ConfigurationFactory;
+import org.apache.logging.log4j.core.helpers.Constants;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class AsyncLoggerTestUncachedThreadName {
+
+    @BeforeClass
+    public static void beforeClass() {
+        System.setProperty("AsyncLogger.ThreadNameStrategy", "UNCACHED");
+        System.setProperty(Constants.LOG4J_CONTEXT_SELECTOR,
+                AsyncLoggerContextSelector.class.getName());
+        System.setProperty(ConfigurationFactory.CONFIGURATION_FILE_PROPERTY,
+                "AsyncLoggerTest.xml");
+    }
+
+    @AfterClass
+    public static void afterClass() {
+        System.setProperty(Constants.LOG4J_CONTEXT_SELECTOR, "");
+    }
+
+    @Test
+    public void testAsyncLogUsesCurrentThreadName() throws Exception {
+        final File f = new File("target", "AsyncLoggerTest.log");
+        // System.out.println(f.getAbsolutePath());
+        f.delete();
+        final Logger log = LogManager.getLogger("com.foo.Bar");
+        final String msg = "Async logger msg";
+        log.info(msg);
+        Thread.currentThread().setName("MODIFIED-THREADNAME");
+        log.info(msg);
+        ((LifeCycle) LogManager.getContext()).stop(); // stop async thread
+
+        final BufferedReader reader = new BufferedReader(new FileReader(f));
+        final String line1 = reader.readLine();
+        final String line2 = reader.readLine();
+        // System.out.println(line1);
+        // System.out.println(line2);
+        reader.close();
+        f.delete();
+        assertNotNull("line1", line1);
+        assertNotNull("line2", line2);
+        assertTrue("line1", line1.endsWith(" INFO c.f.Bar [main]   Async logger msg "));
+        assertTrue("line2", line2.endsWith(" INFO c.f.Bar [MODIFIED-THREADNAME]   Async logger msg "));
+    }
+
+}
---
+++ log4j-core/src/test/java/org/apache/logging/log4j/core/async/AsyncLoggerThreadNameStrategyTest.java
@@ -0,0 +1,69 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache license, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the license for the specific language governing permissions and
+ * limitations under the license.
+ */
+package org.apache.logging.log4j.core.async;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AsyncLoggerThreadNameStrategyTest {
+
+    @Test
+    public void testDefaultThreadNameIsCached() throws Exception {
+        AsyncLogger.ThreadNameStrategy tns = AsyncLogger.ThreadNameStrategy.create();
+        assertSame(AsyncLogger.ThreadNameStrategy.CACHED, tns);
+    }
+
+    @Test
+    public void testUseCachedThreadNameIfInvalidConfig() throws Exception {
+        System.setProperty("AsyncLogger.ThreadNameStrategy", "\\%%InValid ");
+        AsyncLogger.ThreadNameStrategy tns = AsyncLogger.ThreadNameStrategy.create();
+        assertSame(AsyncLogger.ThreadNameStrategy.CACHED, tns);
+    }
+
+    @Test
+    public void testUseUncachedThreadNameIfConfigured() throws Exception {
+        System.setProperty("AsyncLogger.ThreadNameStrategy", "UNCACHED");
+        AsyncLogger.ThreadNameStrategy tns = AsyncLogger.ThreadNameStrategy.create();
+        assertSame(AsyncLogger.ThreadNameStrategy.UNCACHED, tns);
+    }
+
+    @Test
+    public void testUncachedThreadNameStrategyReturnsCurrentThreadName() throws Exception {
+        AsyncLogger.Info info = new AsyncLogger.Info(null, "original");
+        final String name1 = "MODIFIED-THREADNAME1";
+        Thread.currentThread().setName(name1);
+        assertEquals(name1, AsyncLogger.ThreadNameStrategy.UNCACHED.getThreadName(info));
+
+        final String name2 = "OTHER-THREADNAME2";
+        Thread.currentThread().setName(name2);
+        assertEquals(name2, AsyncLogger.ThreadNameStrategy.UNCACHED.getThreadName(info));
+    }
+
+    @Test
+    public void testCachedThreadNameStrategyReturnsCachedThreadName() throws Exception {
+        final String original = "Original-ThreadName";
+        Thread.currentThread().setName(original);
+        AsyncLogger.Info info = new AsyncLogger.Info(null, original);
+        assertEquals(original, AsyncLogger.ThreadNameStrategy.CACHED.getThreadName(info));
+
+        final String name2 = "OTHER-THREADNAME2";
+        Thread.currentThread().setName(name2);
+        assertEquals(original, AsyncLogger.ThreadNameStrategy.CACHED.getThreadName(info));
+    }
+
+}
