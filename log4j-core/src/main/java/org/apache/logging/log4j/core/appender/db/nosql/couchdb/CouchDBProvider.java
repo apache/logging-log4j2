8e8b6d4ff34f5fbc608749ee4ac160795432d21b
Christian Grobmeier <grobmeier@apache.org>
2014-02-07 21:46:25 +0000

    moved package to couchdb to match product name better
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/log4j2/trunk@1565824 13f79535-47bb-0310-9956-ffa450edef68

--- log4j-core/src/main/java/org/apache/logging/log4j/core/appender/db/nosql/couchdb/CouchDBProvider.java
+++ log4j-core/src/main/java/org/apache/logging/log4j/core/appender/db/nosql/couchdb/CouchDBProvider.java
@@ -14,7 +14,7 @@
  * See the license for the specific language governing permissions and
  * limitations under the license.
  */
-package org.apache.logging.log4j.core.appender.db.nosql.couch;
+package org.apache.logging.log4j.core.appender.db.nosql.couchdb;
 
 import java.lang.reflect.Method;
 
