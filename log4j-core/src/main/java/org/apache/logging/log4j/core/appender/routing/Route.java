42a67db908485fcb693a2ddb22e1497d96b6831c
Christian Grobmeier <grobmeier@apache.org>
2014-02-08 09:23:41 +0000

    removed empty loop
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/log4j2/trunk@1565943 13f79535-47bb-0310-9956-ffa450edef68

--- log4j-core/src/main/java/org/apache/logging/log4j/core/appender/routing/Route.java
+++ log4j-core/src/main/java/org/apache/logging/log4j/core/appender/routing/Route.java
@@ -98,9 +98,6 @@ public final class Route {
             @PluginAttribute("key") final String key,
             @PluginNode final Node node) {
         if (node != null && node.hasChildren()) {
-            for (final Node child : node.getChildren()) {
-
-            }
             if (appenderRef != null) {
                 LOGGER.error("A route cannot be configured with an appender reference and an appender definition");
                 return null;
