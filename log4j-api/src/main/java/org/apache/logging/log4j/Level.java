029f727b5514ecb4a179906ded12ce8f0660b471
Gary D. Gregory <ggregory@apache.org>
2014-02-09 03:13:44 +0000

    Format.
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/log4j2/trunk@1566191 13f79535-47bb-0310-9956-ffa450edef68

--- log4j-api/src/main/java/org/apache/logging/log4j/Level.java
+++ log4j-api/src/main/java/org/apache/logging/log4j/Level.java
@@ -51,30 +51,37 @@ public final class Level implements Comparable<Level>, Serializable {
      * No events will be logged.
      */
     public static Level OFF;
+    
     /**
      * A severe error that will prevent the application from continuing.
      */
     public static Level FATAL;
+    
     /**
      * An error in the application, possibly recoverable.
      */
     public static Level ERROR;
+    
     /**
      * An event that might possible lead to an error.
      */
     public static Level WARN;
+    
     /**
      * An event for informational purposes.
      */
     public static Level INFO;
+    
     /**
      * A general debugging event.
      */
     public static Level DEBUG;
+    
     /**
      * A fine-grained debug message, typically capturing the flow through the application.
      */
     public static Level TRACE;
+    
     /**
      * All events should be logged.
      */
