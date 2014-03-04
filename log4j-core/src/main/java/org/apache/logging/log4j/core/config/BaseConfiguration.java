5e185c20aeea40b7bf6da575083e87c0c9821e0f
Gary D. Gregory <ggregory@apache.org>
2014-03-04 18:57:41 +0000

    <action issue="LOG4J2-561" dev="ggregory" type="update" due-to="vibin">   Allow spaces around commas in Configuration's package attribute. </action>
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/log4j2/trunk@1574176 13f79535-47bb-0310-9956-ffa450edef68

--- log4j-core/src/main/java/org/apache/logging/log4j/core/config/BaseConfiguration.java
+++ log4j-core/src/main/java/org/apache/logging/log4j/core/config/BaseConfiguration.java
@@ -70,6 +70,9 @@ import org.apache.logging.log4j.util.PropertiesUtil;
  * The Base Configuration. Many configuration implementations will extend this class.
  */
 public class BaseConfiguration extends AbstractFilterable implements Configuration {
+    
+    static final String PACKAGE_SEPARATOR = "\\s*,\\s*";
+
     /**
      * Allow subclasses access to the status logger without creating another instance.
      */
