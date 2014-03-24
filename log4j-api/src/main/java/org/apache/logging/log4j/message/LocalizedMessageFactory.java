da0aaece67a38c44dedcc3eb3179ac2e6588ae68
Gary D. Gregory <ggregory@apache.org>
2014-03-24 06:30:53 +0000

    Better internal names.
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/log4j2/trunk@1580771 13f79535-47bb-0310-9956-ffa450edef68

--- log4j-api/src/main/java/org/apache/logging/log4j/message/LocalizedMessageFactory.java
+++ log4j-api/src/main/java/org/apache/logging/log4j/message/LocalizedMessageFactory.java
@@ -66,17 +66,17 @@ public class LocalizedMessageFactory extends AbstractMessageFactory {
     /**
      * Creates {@link org.apache.logging.log4j.message.StringFormattedMessage} instances.
      *
-     * @param message The message format String.
-     * @param params The parameters for the message.
+     * @param key The key String, used as a message if the key is absent.
+     * @param params The parameters for the message at the given key.
      * @return The Message.
      *
      * @see org.apache.logging.log4j.message.MessageFactory#newMessage(String, Object...)
      */
     @Override
-    public Message newMessage(final String message, final Object... params) {
+    public Message newMessage(final String key, final Object... params) {
         if (resourceBundle == null) {
-            return new LocalizedMessage(baseName,  message, params);
+            return new LocalizedMessage(baseName,  key, params);
         }
-        return new LocalizedMessage(resourceBundle, message, params);
+        return new LocalizedMessage(resourceBundle, key, params);
     }
 }
