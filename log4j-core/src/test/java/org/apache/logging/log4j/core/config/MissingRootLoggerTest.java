8410102b77483a95be9bf85392c0c599cc5257cd
mattsicker <mattsicker@unknown>
2014-03-26 04:48:33 +0000

    Update MissingRootLoggerTest to use test rule.
      - Added documentation about writing unit tests.
      - Added documentation for the MissingRootLoggerTest.
      - Updated said test with some better assert messages.
    
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/log4j2/trunk@1581693 13f79535-47bb-0310-9956-ffa450edef68

--- log4j-core/src/test/java/org/apache/logging/log4j/core/config/MissingRootLoggerTest.java
+++ log4j-core/src/test/java/org/apache/logging/log4j/core/config/MissingRootLoggerTest.java
@@ -16,31 +16,32 @@
  */
 package org.apache.logging.log4j.core.config;
 
-import java.util.Map;
-
 import org.apache.logging.log4j.Level;
-import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.core.Appender;
 import org.apache.logging.log4j.core.LoggerContext;
-import org.apache.logging.log4j.core.config.plugins.PluginManager;
+import org.apache.logging.log4j.junit.InitialLoggerContext;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import java.util.Map;
+
+import static org.junit.Assert.*;
 
 @RunWith(JUnit4.class)
 public class MissingRootLoggerTest {
 
+    @Rule
+    public InitialLoggerContext context = new InitialLoggerContext("missingRootLogger.xml");
 
     @Test
     public void testMissingRootLogger() throws Exception {
-        PluginManager.addPackage("org.apache.logging.log4j.test.appender");
-        final LoggerContext ctx = Configurator.initialize("Test1", "missingRootLogger.xml");
-        final Logger logger = LogManager.getLogger("sample.Logger1");
+        final LoggerContext ctx = context.getContext();
+        final Logger logger = ctx.getLogger("sample.Logger1");
+        assertTrue("Logger should have the INFO level enabled", logger.isInfoEnabled());
+        assertFalse("Logger should have the DEBUG level disabled", logger.isDebugEnabled());
         final Configuration config = ctx.getConfiguration();
         assertNotNull("Config not null", config);
 //        final String MISSINGROOT = "MissingRootTest";
@@ -48,32 +49,29 @@ public class MissingRootLoggerTest {
 //                MISSINGROOT.equals(config.getName()));
         final Map<String, Appender> map = config.getAppenders();
         assertNotNull("Appenders not null", map);
-        assertEquals("Appenders Size", 2, map.size());
+        assertEquals("There should only be two appenders", 2, map.size());
         assertTrue("Contains List", map.containsKey("List"));
         assertTrue("Contains Console", map.containsKey("Console"));
 
         final Map<String, LoggerConfig> loggerMap = config.getLoggers();
         assertNotNull("loggerMap not null", loggerMap);
-        assertEquals("loggerMap Size", 1, loggerMap.size());
+        assertEquals("There should only be one configured logger", 1, loggerMap.size());
         // only the sample logger, no root logger in loggerMap!
         assertTrue("contains key=sample", loggerMap.containsKey("sample"));
 
         final LoggerConfig sample = loggerMap.get("sample");
         final Map<String, Appender> sampleAppenders = sample.getAppenders();
-        assertEquals("sampleAppenders Size", 1, sampleAppenders.size());
+        assertEquals("The sample logger should only have one appender", 1, sampleAppenders.size());
         // sample only has List appender, not Console!
-        assertTrue("sample has appender List", sampleAppenders.containsKey("List"));
+        assertTrue("The sample appender should be a ListAppender", sampleAppenders.containsKey("List"));
 
         final AbstractConfiguration baseConfig = (AbstractConfiguration) config;
         final LoggerConfig root = baseConfig.getRootLogger();
         final Map<String, Appender> rootAppenders = root.getAppenders();
-        assertEquals("rootAppenders Size", 1, rootAppenders.size());
+        assertEquals("The root logger should only have one appender", 1, rootAppenders.size());
         // root only has Console appender!
-        assertTrue("root has appender Console", rootAppenders.containsKey("Console"));
+        assertTrue("The root appender should be a ConsoleAppender", rootAppenders.containsKey("Console"));
         assertEquals(Level.ERROR, root.getLevel());
-
-        logger.isDebugEnabled();
-        Configurator.shutdown(ctx);
     }
 
 }
