ecbf9b6aafcf0caef92b04c99a64fd0e500f8633
mattsicker <mattsicker@unknown>
2014-04-07 22:06:29 +0000

    Refactor plugin cache to its own class.
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/log4j2/trunk@1585603 13f79535-47bb-0310-9956-ffa450edef68

--- log4j-plugin-processor/src/test/java/org/apache/logging/log4j/core/config/plugins/processor/PluginProcessorTest.java
+++ log4j-plugin-processor/src/test/java/org/apache/logging/log4j/core/config/plugins/processor/PluginProcessorTest.java
@@ -17,11 +17,8 @@
 
 package org.apache.logging.log4j.core.config.plugins.processor;
 
-import java.io.BufferedInputStream;
-import java.io.DataInputStream;
 import java.net.URL;
 import java.util.Enumeration;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import org.apache.logging.log4j.core.config.plugins.Plugin;
 import org.apache.logging.log4j.core.config.plugins.PluginAliases;
@@ -30,63 +27,35 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 @RunWith(JUnit4.class)
 public class PluginProcessorTest {
 
     private static final String CACHE_FILE = "org/apache/logging/log4j/core/config/plugins/Log4j2Plugins.dat";
 
-    private static ConcurrentMap<String, ConcurrentMap<String, PluginEntry>> pluginCategories;
+    private static final PluginCache pluginCache = new PluginCache();
 
     private final Plugin p = FakePlugin.class.getAnnotation(Plugin.class);
 
     @BeforeClass
     public static void setUpClass() throws Exception {
-        // TODO: refactor PluginManager.decode() into this module?
-        pluginCategories = new ConcurrentHashMap<String, ConcurrentMap<String, PluginEntry>>();
         final Enumeration<URL> resources = PluginProcessor.class.getClassLoader().getResources(CACHE_FILE);
-        while (resources.hasMoreElements()) {
-            final URL url = resources.nextElement();
-            final DataInputStream in = new DataInputStream(new BufferedInputStream(url.openStream()));
-            try {
-                final int count = in.readInt();
-                for (int i = 0; i < count; i++) {
-                    final String category = in.readUTF();
-                    pluginCategories.putIfAbsent(category, new ConcurrentHashMap<String, PluginEntry>());
-                    final ConcurrentMap<String, PluginEntry> m = pluginCategories.get(category);
-                    final int entries = in.readInt();
-                    for (int j = 0; j < entries; j++) {
-                        final PluginEntry entry = new PluginEntry();
-                        entry.setKey(in.readUTF());
-                        entry.setClassName(in.readUTF());
-                        entry.setName(in.readUTF());
-                        entry.setPrintable(in.readBoolean());
-                        entry.setDefer(in.readBoolean());
-                        entry.setCategory(category);
-                        m.putIfAbsent(entry.getKey(), entry);
-                    }
-                    pluginCategories.putIfAbsent(category, m);
-                }
-            } finally {
-                in.close();
-            }
-        }
+        pluginCache.loadCacheFiles(resources);
     }
 
     @Test
     public void testTestCategoryFound() throws Exception {
         assertNotNull("No plugin annotation on FakePlugin.", p);
-        final ConcurrentMap<String, PluginEntry> testCategory = pluginCategories.get(p.category());
-        assertNotEquals("No plugins were found.", 0, pluginCategories.size());
+        final ConcurrentMap<String, PluginEntry> testCategory = pluginCache.getCategory(p.category());
+        assertNotEquals("No plugins were found.", 0, pluginCache.size());
         assertNotNull("The category '" + p.category() + "' was not found.", testCategory);
+        assertFalse(testCategory.isEmpty());
     }
 
     @Test
     public void testFakePluginFoundWithCorrectInformation() throws Exception {
-        final PluginEntry fake = pluginCategories.get(p.category()).get(p.name().toLowerCase());
+        final PluginEntry fake = pluginCache.getCategory(p.category()).get(p.name().toLowerCase());
         verifyFakePluginEntry(p.name(), fake);
     }
 
@@ -94,7 +63,7 @@ public class PluginProcessorTest {
     public void testFakePluginAliasesContainSameInformation() throws Exception {
         final PluginAliases aliases = FakePlugin.class.getAnnotation(PluginAliases.class);
         for (final String alias : aliases.value()) {
-            final PluginEntry fake = pluginCategories.get(p.category()).get(alias.toLowerCase());
+            final PluginEntry fake = pluginCache.getCategory(p.category()).get(alias.toLowerCase());
             verifyFakePluginEntry(alias, fake);
         }
     }
@@ -112,7 +81,7 @@ public class PluginProcessorTest {
     @Test
     public void testNestedPlugin() throws Exception {
         final Plugin p = FakePlugin.Nested.class.getAnnotation(Plugin.class);
-        final PluginEntry nested = pluginCategories.get(p.category()).get(p.name().toLowerCase());
+        final PluginEntry nested = pluginCache.getCategory(p.category()).get(p.name().toLowerCase());
         assertNotNull(nested);
         assertEquals(p.name().toLowerCase(), nested.getKey());
         assertEquals(FakePlugin.Nested.class.getName(), nested.getClassName());
