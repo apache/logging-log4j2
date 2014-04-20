c9290a1e3fc9cdc15f9470b773623d6d35cf243b
mattsicker <mattsicker@unknown>
2014-04-20 17:57:31 +0000

    Move the rest of the plugin annotations.
      - All the @PluginFoo annotations are now housed in the 
      log4j-plugin-processor module.
      - log4j-core already depends on this :)
    
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/log4j2/trunk@1588809 13f79535-47bb-0310-9956-ffa450edef68

--- log4j-plugin-processor/src/main/java/org/apache/logging/log4j/core/config/plugins/SensitivePluginAttribute.java
+++ log4j-plugin-processor/src/main/java/org/apache/logging/log4j/core/config/plugins/SensitivePluginAttribute.java
@@ -1,12 +1,12 @@
 /*
  * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
+ * contributor license agreements. See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
+ * the License. You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -27,7 +27,7 @@ import java.lang.annotation.Target;
  * and instead should be hashed.
  *
  * @see org.apache.logging.log4j.core.helpers.NameUtil#md5(String) MD5
- * @see PluginAttribute
+ * @see org.apache.logging.log4j.core.config.plugins.PluginAttribute
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.PARAMETER)
