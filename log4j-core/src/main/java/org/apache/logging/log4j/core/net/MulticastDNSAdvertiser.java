09ed00ee2790ec3a66ba8957f5583898ad497d1c
Matt Sicker <mattsicker@apache.org>
2014-04-25 23:38:07 +0000

    Remove unused import.
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/log4j2/trunk@1590192 13f79535-47bb-0310-9956-ffa450edef68

--- log4j-core/src/main/java/org/apache/logging/log4j/core/net/MulticastDNSAdvertiser.java
+++ log4j-core/src/main/java/org/apache/logging/log4j/core/net/MulticastDNSAdvertiser.java
@@ -16,7 +16,6 @@
  */
 package org.apache.logging.log4j.core.net;
 
-import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.HashMap;
