e98671d5b80cf772323cb22f12e069a4cec68be4
Matt Sicker <mattsicker@apache.org>
2014-04-27 01:26:52 +0000

    Update documentation.
      - Make the hasCategory method make more sense as well.
      - Added warning message to PluginManager.main.
    
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/log4j2/trunk@1590335 13f79535-47bb-0310-9956-ffa450edef68

--- log4j-core/src/main/java/org/apache/logging/log4j/core/config/plugins/util/PluginRegistry.java
+++ log4j-core/src/main/java/org/apache/logging/log4j/core/config/plugins/util/PluginRegistry.java
@@ -32,6 +32,15 @@ public class PluginRegistry<T extends Serializable> {
     private final ConcurrentMap<String, ConcurrentMap<String, T>> categories =
         new ConcurrentHashMap<String, ConcurrentMap<String, T>>();
 
+    /**
+     * Gets or creates a plugin category if not already available. Category names are case-insensitive. The
+     * ConcurrentMap that is returned should also be treated as a case-insensitive plugin map where names should be
+     * converted to lowercase before retrieval or storage.
+     *
+     * @param category the plugin category to look up or create.
+     * @return the plugin map for the given category name.
+     * @throws IllegalArgumentException if the argument is {@code null}
+     */
     public ConcurrentMap<String, T> getCategory(final String category) {
         if (category == null) {
             throw new IllegalArgumentException("Category name cannot be null.");
@@ -41,22 +50,52 @@ public class PluginRegistry<T extends Serializable> {
         return categories.get(key);
     }
 
+    /**
+     * Returns the number of plugin categories currently available. This is primarily useful for serialization.
+     *
+     * @return the number of plugin categories.
+     */
     public int getCategoryCount() {
         return categories.size();
     }
 
+    /**
+     * Indicates whether or not any plugin categories have been registered. Note that this does not necessarily
+     * indicate if any plugins are registered as categories may be empty.
+     *
+     * @return {@code true} if there any categories registered.
+     */
     public boolean isEmpty() {
         return categories.isEmpty();
     }
 
+    /**
+     * Resets the registry to an empty state.
+     */
     public void clear() {
         categories.clear();
     }
 
-    public boolean hasCategory(final String key) {
-        return categories.containsKey(key);
+    /**
+     * Indicates whether or not the given category name is registered and has plugins in that category.
+     *
+     * @param category the plugin category name to check.
+     * @return {@code true} if the category exists and has plugins registered.
+     * @throws IllegalArgumentException if the argument is {@code null}
+     */
+    public boolean hasCategory(final String category) {
+        if (category == null) {
+            throw new IllegalArgumentException("Category name cannot be null.");
+        }
+        final String key = category.toLowerCase();
+        return categories.containsKey(key) && !categories.get(key).isEmpty();
     }
 
+    /**
+     * Gets an entry set for iterating over the registered plugin categories.
+     *
+     * @return an entry set of the registered plugin categories.
+     */
     public Set<Map.Entry<String, ConcurrentMap<String, T>>> getCategories() {
         return categories.entrySet();
     }
