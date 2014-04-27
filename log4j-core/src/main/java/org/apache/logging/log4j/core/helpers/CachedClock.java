d40418af2e70680a45c3dfa62bd579de74f053df
Matt Sicker <mattsicker@apache.org>
2014-04-27 18:31:20 +0000

    Convert anonymous thread to runnable.
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/log4j2/trunk@1590447 13f79535-47bb-0310-9956-ffa450edef68

--- log4j-core/src/main/java/org/apache/logging/log4j/core/helpers/CachedClock.java
+++ log4j-core/src/main/java/org/apache/logging/log4j/core/helpers/CachedClock.java
@@ -31,20 +31,20 @@ public final class CachedClock implements Clock {
     private static CachedClock instance = new CachedClock();
     private volatile long millis = System.currentTimeMillis();
     private volatile short count = 0;
-    private final Thread updater = new Thread("Clock Updater Thread") {
-        @Override
-        public void run() {
-            while (true) {
-                final long time = System.currentTimeMillis();
-                millis = time;
-
-                // avoid explicit dependency on sun.misc.Util
-                LockSupport.parkNanos(1000 * 1000);
-            }
-        }
-    };
 
     private CachedClock() {
+        final Thread updater = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    final long time = System.currentTimeMillis();
+                    millis = time;
+
+                    // avoid explicit dependency on sun.misc.Util
+                    LockSupport.parkNanos(1000 * 1000);
+                }
+            }
+        }, "Clock Updater Thread");
         updater.setDaemon(true);
         updater.start();
     }
