7a9602d5ef1f42d67d72f83b2ac80d25cfdcf6d6
Matt Sicker <mattsicker@apache.org>
2014-04-27 19:45:58 +0000

    Add serialVersionUID.
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/log4j2/trunk@1590473 13f79535-47bb-0310-9956-ffa450edef68

--- log4j-slf4j-impl/src/main/java/org/apache/logging/slf4j/Log4jMarker.java
+++ log4j-slf4j-impl/src/main/java/org/apache/logging/slf4j/Log4jMarker.java
@@ -30,6 +30,8 @@ import java.util.List;
  */
 public class Log4jMarker implements Marker {
 
+    public static final long serialVersionUID = 1590472L;
+
     private final IMarkerFactory factory = StaticMarkerBinder.SINGLETON.getMarkerFactory();
 
     private final org.apache.logging.log4j.Marker marker;
