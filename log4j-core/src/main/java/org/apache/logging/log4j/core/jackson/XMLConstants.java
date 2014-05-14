1cdf795fcfc98c6bfd3d98379d6de924991308ad
Gary D. Gregory <ggregory@apache.org>
2014-05-14 21:28:16 +0000

    Better XML representation of array and array items.
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/log4j2/trunk@1594728 13f79535-47bb-0310-9956-ffa450edef68

--- log4j-core/src/main/java/org/apache/logging/log4j/core/jackson/XMLConstants.java
+++ log4j-core/src/main/java/org/apache/logging/log4j/core/jackson/XMLConstants.java
@@ -23,13 +23,16 @@ public final class XMLConstants {
     public static final String ELT_CAUSE = "Cause";
     public static final String ELT_CONTEXT_MAP = "ContextMap";
     public static final String ELT_CONTEXT_STACK = "ContextStack";
+    public static final String ELT_CONTEXT_STACK_ITEM = "ContextStackItem";
     public static final String ELT_EVENT = "Event";
     public static final String ELT_EXTENDED_STACK_TRACE = "ExtendedStackTrace";
+    public static final String ELT_EXTENDED_STACK_TRACE_ITEM = "ExtendedStackTraceItem";
     public static final String ELT_MARKER = "Marker";
     public static final String ELT_MESSAGE = "Message";
     public static final String ELT_PARENTS = "Parents";
     public static final String ELT_SOURCE = "Source";
     public static final String ELT_SUPPRESSED = "Suppressed";
+    public static final String ELT_SUPPRESSED_ITEM = "SuppressedItem";
     public static final String ELT_THROWN = "Thrown";
     public static final String XML_NAMESPACE = "http://logging.apache.org/log4j/2.0/events";
 }
