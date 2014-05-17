70e9e2d095502d0460279dd5adc0781fdaf02993
rpopma <rpopma@unknown>
2014-05-17 12:08:04 +0000

    reduce sleep time to speed up project build time (by about 10 seconds)
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/log4j2/trunk@1595448 13f79535-47bb-0310-9956-ffa450edef68

--- log4j-core/src/test/java/org/apache/logging/log4j/core/LoggerTest.java
+++ log4j-core/src/test/java/org/apache/logging/log4j/core/LoggerTest.java
@@ -229,11 +229,12 @@ public class LoggerTest {
 
     @Test
     public void testReconfiguration() throws Exception {
+        final int MONITOR_INTERVAL_SECONDS = 1;
         final File file = new File("target/test-classes/" + CONFIG);
         final long orig = file.lastModified();
         final long newTime = orig + 10000;
         file.setLastModified(newTime);
-        Thread.sleep(6000);
+        Thread.sleep((MONITOR_INTERVAL_SECONDS + 1) * 1000);
         for (int i = 0; i < 17; ++i) {
             logger.debug("Reconfigure");
         }
