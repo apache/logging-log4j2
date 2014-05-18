7d54b8367a7be08081471ded8c7f1c67631e195b
mattsicker <mattsicker@unknown>
2014-05-18 01:48:51 +0000

    Add more tests to PerformanceTests category.
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/log4j2/trunk@1595558 13f79535-47bb-0310-9956-ffa450edef68

--- log4j-core/src/test/java/org/apache/logging/log4j/core/SimplePerfTest.java
+++ log4j-core/src/test/java/org/apache/logging/log4j/core/SimplePerfTest.java
@@ -22,14 +22,17 @@ import java.util.Random;
 
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.categories.PerformanceTests;
 import org.apache.logging.log4j.core.config.Configuration;
 import org.apache.logging.log4j.core.config.DefaultConfiguration;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 
 /**
  *
  */
+@Category(PerformanceTests.class)
 public class SimplePerfTest {
 
     private static org.apache.logging.log4j.Logger logger = LogManager.getLogger(SimplePerfTest.class.getName());
--- log4j-core/src/test/java/org/apache/logging/log4j/core/ThreadedPerfTest.java
+++ log4j-core/src/test/java/org/apache/logging/log4j/core/ThreadedPerfTest.java
@@ -21,11 +21,14 @@ import java.util.concurrent.Executors;
 
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.categories.PerformanceTests;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 
 /**
  *
  */
+@Category(PerformanceTests.class)
 public class ThreadedPerfTest {
 
     private static org.apache.logging.log4j.Logger logger = LogManager.getLogger(ThreadedPerfTest.class.getName());
