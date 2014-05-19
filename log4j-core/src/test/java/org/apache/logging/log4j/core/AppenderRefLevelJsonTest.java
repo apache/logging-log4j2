de7669ccaf7d95bc1dc4d6f8da302e71b50724e4
Matt Sicker <mattsicker@apache.org>
2014-05-19 01:59:31 +0000

    Refactor test to use InitialLoggerContext rule.
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/log4j2/trunk@1595720 13f79535-47bb-0310-9956-ffa450edef68

--- log4j-core/src/test/java/org/apache/logging/log4j/core/AppenderRefLevelJsonTest.java
+++ log4j-core/src/test/java/org/apache/logging/log4j/core/AppenderRefLevelJsonTest.java
@@ -16,22 +16,18 @@
  */
 package org.apache.logging.log4j.core;
 
-import static org.junit.Assert.assertTrue;
-
 import java.util.List;
-import java.util.Map;
 
-import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Marker;
 import org.apache.logging.log4j.MarkerManager;
-import org.apache.logging.log4j.core.config.Configuration;
-import org.apache.logging.log4j.core.config.ConfigurationFactory;
-import org.apache.logging.log4j.status.StatusLogger;
+import org.apache.logging.log4j.junit.InitialLoggerContext;
 import org.apache.logging.log4j.test.appender.ListAppender;
-import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.ClassRule;
 import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 /**
  *
@@ -39,35 +35,21 @@ import org.junit.Test;
 public class AppenderRefLevelJsonTest {
 
     private static final String CONFIG = "log4j-reference-level.json";
-    private static Configuration config;
     private static ListAppender app1;
     private static ListAppender app2;
-    private static LoggerContext ctx;
+
+    @ClassRule
+    public static InitialLoggerContext context = new InitialLoggerContext(CONFIG);
 
     @BeforeClass
     public static void setupClass() {
-        System.setProperty(ConfigurationFactory.CONFIGURATION_FILE_PROPERTY, CONFIG);
-        ctx = (LoggerContext) LogManager.getContext(false);
-        config = ctx.getConfiguration();
-        for (final Map.Entry<String, Appender> entry : config.getAppenders().entrySet()) {
-            if (entry.getKey().equals("LIST1")) {
-                app1 = (ListAppender) entry.getValue();
-            } else if (entry.getKey().equals("LIST2")) {
-                app2 = (ListAppender) entry.getValue();
-            }
-        }
+        app1 = (ListAppender) context.getAppender("LIST1");
+        app2 = (ListAppender) context.getAppender("LIST2");
     }
 
-    @AfterClass
-    public static void cleanupClass() {
-        System.clearProperty(ConfigurationFactory.CONFIGURATION_FILE_PROPERTY);
-        ctx.reconfigure();
-        StatusLogger.getLogger().reset();
-    }
-
-    org.apache.logging.log4j.Logger logger1 = LogManager.getLogger("org.apache.logging.log4j.test1");
-    org.apache.logging.log4j.Logger logger2 = LogManager.getLogger("org.apache.logging.log4j.test2");
-    org.apache.logging.log4j.Logger logger3 = LogManager.getLogger("org.apache.logging.log4j.test3");
+    org.apache.logging.log4j.Logger logger1 = context.getLogger("org.apache.logging.log4j.test1");
+    org.apache.logging.log4j.Logger logger2 = context.getLogger("org.apache.logging.log4j.test2");
+    org.apache.logging.log4j.Logger logger3 = context.getLogger("org.apache.logging.log4j.test3");
     Marker testMarker = MarkerManager.getMarker("TEST");
 
     @Before
@@ -85,9 +67,9 @@ public class AppenderRefLevelJsonTest {
         logger1.warn("warn Message");
         logger1.exit();
         List<LogEvent> events = app1.getEvents();
-        assertTrue("Incorrect number of events. Expected 6, actual " + events.size(), events.size() == 6);
+        assertEquals("Incorrect number of events. Expected 6, actual " + events.size(), 6, events.size());
         events = app2.getEvents();
-        assertTrue("Incorrect number of events. Expected 1, actual " + events.size(), events.size() == 1);
+        assertEquals("Incorrect number of events. Expected 1, actual " + events.size(), 1, events.size());
     }
 
     @Test
@@ -99,9 +81,9 @@ public class AppenderRefLevelJsonTest {
         logger2.warn("warn Message");
         logger2.exit();
         List<LogEvent> events = app1.getEvents();
-        assertTrue("Incorrect number of events. Expected 2, actual " + events.size(), events.size() == 2);
+        assertEquals("Incorrect number of events. Expected 2, actual " + events.size(), 2, events.size());
         events = app2.getEvents();
-        assertTrue("Incorrect number of events. Expected 4, actual " + events.size(), events.size() == 4);
+        assertEquals("Incorrect number of events. Expected 4, actual " + events.size(), 4, events.size());
     }
 
     @Test
@@ -113,7 +95,7 @@ public class AppenderRefLevelJsonTest {
         logger3.warn("warn Message");
         logger3.exit();
         final List<LogEvent> events = app1.getEvents();
-        assertTrue("Incorrect number of events. Expected 4, actual " + events.size(), events.size() == 4);
+        assertEquals("Incorrect number of events. Expected 4, actual " + events.size(), 4, events.size());
     }
 }
 
