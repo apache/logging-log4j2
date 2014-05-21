101093e8720ebf8780d09ed42da4f25e0aed62d1
Matt Sicker <mattsicker@apache.org>
2014-05-21 03:49:59 +0000

    Refactor test to use InitialLoggerContext.
      - Removed * static import on Level.
      - Made Expected a static inner class.
      - Assertion statement clarifications.
    
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/log4j2/trunk@1596453 13f79535-47bb-0310-9956-ffa450edef68

--- log4j-core/src/test/java/org/apache/logging/log4j/core/LevelTest.java
+++ log4j-core/src/test/java/org/apache/logging/log4j/core/LevelTest.java
@@ -17,25 +17,20 @@
 package org.apache.logging.log4j.core;
 
 import java.util.List;
-import java.util.Map;
 
 import org.apache.logging.log4j.Level;
-import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.Marker;
 import org.apache.logging.log4j.MarkerManager;
-import org.apache.logging.log4j.core.config.Configuration;
-import org.apache.logging.log4j.core.config.ConfigurationFactory;
+import org.apache.logging.log4j.junit.InitialLoggerContext;
 import org.apache.logging.log4j.message.Message;
 import org.apache.logging.log4j.message.ObjectMessage;
-import org.apache.logging.log4j.status.StatusLogger;
 import org.apache.logging.log4j.test.appender.ListAppender;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
+import org.junit.ClassRule;
 import org.junit.Test;
 
-import static org.apache.logging.log4j.Level.*;
-
+import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.*;
 
 /**
@@ -44,7 +39,6 @@ import static org.junit.Assert.*;
 public class LevelTest {
 
     private static final String CONFIG = "log4j-Level.xml";
-    private static LoggerContext ctx;
     private static ListAppender listAll;
     private static ListAppender listTrace;
     private static ListAppender listDebug;
@@ -53,57 +47,22 @@ public class LevelTest {
     private static ListAppender listError;
     private static ListAppender listFatal;
 
-    @BeforeClass
-    public static void setupClass() {
-        System.setProperty(ConfigurationFactory.CONFIGURATION_FILE_PROPERTY, CONFIG);
-        ctx = (LoggerContext) LogManager.getContext(false);
-    }
-
-    @AfterClass
-    public static void cleanupClass() {
-        System.clearProperty(ConfigurationFactory.CONFIGURATION_FILE_PROPERTY);
-        ctx.reconfigure();
-        StatusLogger.getLogger().reset();
-    }
+    @ClassRule
+    public static InitialLoggerContext context = new InitialLoggerContext(CONFIG);
 
     @Before
     public void before() {
-        Configuration config = ctx.getConfiguration();
-        for (final Map.Entry<String, Appender> entry : config.getAppenders().entrySet()) {
-            if (entry.getKey().equals("ListAll")) {
-                listAll = (ListAppender) entry.getValue();
-            } else if (entry.getKey().equals("ListTrace")) {
-                listTrace = (ListAppender) entry.getValue();
-            } else if (entry.getKey().equals("ListDebug")) {
-                listDebug = (ListAppender) entry.getValue();
-            } else if (entry.getKey().equals("ListInfo")) {
-                listInfo = (ListAppender) entry.getValue();
-            } else if (entry.getKey().equals("ListWarn")) {
-                listWarn = (ListAppender) entry.getValue();
-            } else if (entry.getKey().equals("ListError")) {
-                listError = (ListAppender) entry.getValue();
-            } else if (entry.getKey().equals("ListFatal")) {
-                listFatal = (ListAppender) entry.getValue();
-            }
-        }
-        assertNotNull("No listAll Appender", listAll);
-        assertNotNull("No listTrace Appender", listTrace);
-        assertNotNull("No listDebug Appender", listDebug);
-        assertNotNull("No listInfo Appender", listInfo);
-        assertNotNull("No listWarn Appender", listWarn);
-        assertNotNull("No listError Appender", listError);
-        assertNotNull("No listFatal Appender", listFatal);
-        listAll.clear();
-        listTrace.clear();
-        listDebug.clear();
-        listInfo.clear();
-        listWarn.clear();
-        listError.clear();
-        listFatal.clear();
+        listAll = context.getListAppender("ListAll").clear();
+        listTrace = context.getListAppender("ListTrace").clear();
+        listDebug = context.getListAppender("ListDebug").clear();
+        listInfo = context.getListAppender("ListInfo").clear();
+        listWarn = context.getListAppender("ListWarn").clear();
+        listError = context.getListAppender("ListError").clear();
+        listFatal = context.getListAppender("ListFatal").clear();
     }
 
     // Helper class
-    class Expected {
+    private static class Expected {
         final ListAppender appender;
         final int expectedEventCount;
         final String expectedInitialEventLevel;
@@ -122,12 +81,12 @@ public class LevelTest {
         Marker marker = MarkerManager.getMarker("marker");
         Message msg = new ObjectMessage("msg");
         Throwable t = new Throwable("test");
-        Level[] levels = new Level[] { TRACE, DEBUG, INFO, WARN, ERROR, FATAL };
+        Level[] levels = new Level[] { Level.TRACE, Level.DEBUG, Level.INFO, Level.WARN, Level.ERROR, Level.FATAL };
         String[] names = new String[] { "levelTest", "levelTest.Trace", "levelTest.Debug", "levelTest.Info",
                 "levelTest.Warn", "levelTest.Error", "levelTest.Fatal" };
         for (Level level : levels) {
             for (String name : names) {
-                org.apache.logging.log4j.Logger logger = LogManager.getLogger(name);
+                final Logger logger = context.getLogger(name);
                 logger.log(level, msg); // Message
                 logger.log(level, 123); // Object
                 logger.log(level, name); // String
@@ -161,12 +120,11 @@ public class LevelTest {
             final String description = expected.description;
             final List<LogEvent> events = expected.appender.getEvents();
             assertNotNull(description + ": No events", events);
-            assertTrue(description + ": Incorrect number of events. Expected " + expected.expectedEventCount + " got "
-                    + events.size(), events.size() == expected.expectedEventCount);
-            LogEvent event = events.get(0);
-            assertTrue(
-                    description + ": Expected level " + expected.expectedInitialEventLevel + ", got" + event.getLevel(),
-                    event.getLevel().name().equals(expected.expectedInitialEventLevel));
+            assertThat(events, hasSize(expected.expectedEventCount));
+            final LogEvent event = events.get(0);
+            assertEquals(
+                description + ": Expected level " + expected.expectedInitialEventLevel + ", got" + event.getLevel(),
+                event.getLevel().name(), expected.expectedInitialEventLevel);
         }
     }
 }
