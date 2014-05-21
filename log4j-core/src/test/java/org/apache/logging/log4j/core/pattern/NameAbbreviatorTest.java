767554405e2d777c09ea2d9defb7d82b07f0e605
mattsicker <mattsicker@unknown>
2014-05-21 05:35:18 +0000

    Convert test to parameterized version.
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/log4j2/trunk@1596471 13f79535-47bb-0310-9956-ffa450edef68

--- log4j-core/src/test/java/org/apache/logging/log4j/core/pattern/NameAbbreviatorTest.java
+++ log4j-core/src/test/java/org/apache/logging/log4j/core/pattern/NameAbbreviatorTest.java
@@ -16,72 +16,50 @@
  */
 package org.apache.logging.log4j.core.pattern;
 
+import java.util.Arrays;
+import java.util.Collection;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
 import static org.junit.Assert.*;
 
 /**
  *
  */
+@RunWith(Parameterized.class)
 public class NameAbbreviatorTest {
 
-    @Test
-    public void testZero() {
-        final String str = this.getClass().getName();
-        final NameAbbreviator a = NameAbbreviator.getAbbreviator("0");
-        final String result = a.abbreviate(str);
-        assertEquals("NameAbbreviatorTest", result);
+    private final String pattern;
+    private final String expected;
+
+    public NameAbbreviatorTest(final String pattern, final String expected) {
+        this.pattern = pattern;
+        this.expected = expected;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(
+            new Object[][]{
+                // { pattern, expected }
+                { "0", "NameAbbreviatorTest" },
+                { "1", "NameAbbreviatorTest" },
+                { "2", "pattern.NameAbbreviatorTest" },
+                { "3", "core.pattern.NameAbbreviatorTest" },
+                { "1.", "o.a.l.l.c.p.NameAbbreviatorTest" },
+                { "1.1.~", "o.a.~.~.~.~.NameAbbreviatorTest" },
+                { ".", "......NameAbbreviatorTest" }
+            }
+        );
     }
 
     @Test
-    public void testNameAbbreviation() {
-        final String str = this.getClass().getName();
-        final NameAbbreviator a = NameAbbreviator.getAbbreviator("1");
-        final String result = a.abbreviate(str);
-        assertEquals("NameAbbreviatorTest", result);
+    public void testAbbreviatorPatterns() throws Exception {
+        final NameAbbreviator abbreviator = NameAbbreviator.getAbbreviator(this.pattern);
+        final String actual = abbreviator.abbreviate(this.getClass().getName());
+        assertEquals(expected, actual);
     }
 
-     @Test
-    public void testTwo() {
-        final String str = this.getClass().getName();
-        final NameAbbreviator a = NameAbbreviator.getAbbreviator("2");
-        final String result = a.abbreviate(str);
-        assertEquals("pattern.NameAbbreviatorTest", result);
-
-    }
-
-    @Test
-    public void testThree() throws Exception {
-        final String str = this.getClass().getName();
-        final NameAbbreviator a = NameAbbreviator.getAbbreviator("3");
-        final String result = a.abbreviate(str);
-        assertEquals("core.pattern.NameAbbreviatorTest", result);
-    }
-
-    @Test
-    public void testShortName() {
-        final String str = this.getClass().getName();
-        final NameAbbreviator a = NameAbbreviator.getAbbreviator("1.");
-        final String result = a.abbreviate(str);
-        assertEquals("o.a.l.l.c.p.NameAbbreviatorTest", result);
-
-    }
-
-    @Test
-    public void testSkipNames() {
-        final String str = this.getClass().getName();
-        final NameAbbreviator a = NameAbbreviator.getAbbreviator("1.1.~");
-        final String result = a.abbreviate(str);
-        assertEquals("o.a.~.~.~.~.NameAbbreviatorTest", result);
-
-    }
-
-     @Test
-    public void testZeroDot() {
-        final String str = this.getClass().getName();
-        final NameAbbreviator a = NameAbbreviator.getAbbreviator(".");
-        final String result = a.abbreviate(str);
-        assertEquals("......NameAbbreviatorTest", result);
-
-    }
 }
