353bbd59780c4a1bfd6a85bb55823533df07722d
Gary D. Gregory <ggregory@apache.org>
2014-05-24 07:28:20 +0000

    Finish the throwable proxy rework: - Go back to the design that had the class and package information for a Class in a standalone class, now called ExtendedClassInfo. - The ExtendedStackTraceElement now has a cleaner design: it holds a JRE StackTraceElement and a ExtendedClassInfo. - The ThrowableProxy can now go back to caching ExtendedClassInfo instead of ExtendedStackTraceElement, which did to track a stack trace correctly anyway because ExtendedStackTraceElement was reused instead of just the extra class info. - Rename StackTracePackageElementMixIn to ExtendedStackTraceElementMixIn. - I wonder if this fixes [LOG4J2-651] Log4j 2 throws ArrayIndexOutOfBoundsException. Perhaps this was already fixed.
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/log4j2/trunk@1597261 13f79535-47bb-0310-9956-ffa450edef68

---
+++ log4j-core/src/main/java/org/apache/logging/log4j/core/impl/ExtendedClassInfo.java
@@ -0,0 +1,116 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache license, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the license for the specific language governing permissions and
+ * limitations under the license.
+ */
+package org.apache.logging.log4j.core.impl;
+
+import java.io.Serializable;
+
+/**
+ * Class and package data used with a {@link StackTraceElement} in a {@link ExtendedStackTraceElement}.
+ */
+public final class ExtendedClassInfo implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private final boolean exact;
+
+    private final String location;
+
+    private final String version;
+
+    /**
+     * Constructs a new instance.
+     * 
+     * @param exact
+     * @param location
+     * @param version
+     */
+    public ExtendedClassInfo(final boolean exact, final String location, final String version) {
+        super();
+        this.exact = exact;
+        this.location = location;
+        this.version = version;
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (!(obj instanceof ExtendedClassInfo)) {
+            return false;
+        }
+        final ExtendedClassInfo other = (ExtendedClassInfo) obj;
+        if (this.exact != other.exact) {
+            return false;
+        }
+        if (this.location == null) {
+            if (other.location != null) {
+                return false;
+            }
+        } else if (!this.location.equals(other.location)) {
+            return false;
+        }
+        if (this.version == null) {
+            if (other.version != null) {
+                return false;
+            }
+        } else if (!this.version.equals(other.version)) {
+            return false;
+        }
+        return true;
+    }
+
+    public boolean getExact() {
+        return this.exact;
+    }
+
+    public String getLocation() {
+        return this.location;
+    }
+
+    public String getVersion() {
+        return this.version;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (this.exact ? 1231 : 1237);
+        result = prime * result + ((this.location == null) ? 0 : this.location.hashCode());
+        result = prime * result + ((this.version == null) ? 0 : this.version.hashCode());
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder();
+        if (!this.exact) {
+            sb.append('~');
+        }
+        sb.append('[');
+        sb.append(this.location);
+        sb.append(':');
+        sb.append(this.version);
+        sb.append(']');
+        return sb.toString();
+    }
+
+}
