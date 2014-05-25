8e3a6ff6d472ddf7d28b151f2767fbaee6c14186
Matt Sicker <mattsicker@apache.org>
2014-05-25 03:10:50 +0000

    Use default character set (UTF-8) if given null value in constructor.
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/log4j2/trunk@1597372 13f79535-47bb-0310-9956-ffa450edef68

--- log4j-core/src/main/java/org/apache/logging/log4j/core/layout/AbstractStringLayout.java
+++ log4j-core/src/main/java/org/apache/logging/log4j/core/layout/AbstractStringLayout.java
@@ -19,6 +19,7 @@ package org.apache.logging.log4j.core.layout;
 import java.nio.charset.Charset;
 
 import org.apache.logging.log4j.core.LogEvent;
+import org.apache.logging.log4j.core.util.Charsets;
 
 /**
  * Abstract base class for Layouts that result in a String.
@@ -31,7 +32,7 @@ public abstract class AbstractStringLayout extends AbstractLayout<String> {
     private final Charset charset;
 
     protected AbstractStringLayout(final Charset charset) {
-        this.charset = charset;
+        this.charset = charset == null ? Charsets.UTF_8 : charset;
     }
 
     /**
