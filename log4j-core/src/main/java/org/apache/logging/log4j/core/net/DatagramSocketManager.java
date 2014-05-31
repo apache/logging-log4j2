60e53886f3f851bc77491a3082504b664adbc6b2
Gary D. Gregory <ggregory@apache.org>
2014-05-31 05:22:17 +0000

    Fix DatagramOutputStream leak when an error occurs.
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/log4j2/trunk@1598816 13f79535-47bb-0310-9956-ffa450edef68

--- log4j-core/src/main/java/org/apache/logging/log4j/core/net/DatagramSocketManager.java
+++ log4j-core/src/main/java/org/apache/logging/log4j/core/net/DatagramSocketManager.java
@@ -105,14 +105,14 @@ public class DatagramSocketManager extends AbstractSocketManager {
         @Override
         public DatagramSocketManager createManager(final String name, final FactoryData data) {
             InetAddress inetAddress;
-            final OutputStream os = new DatagramOutputStream(data.host, data.port, data.layout.getHeader(),
-                data.layout.getFooter());
             try {
                 inetAddress = InetAddress.getByName(data.host);
             } catch (final UnknownHostException ex) {
                 LOGGER.error("Could not find address of " + data.host, ex);
                 return null;
             }
+            final OutputStream os = new DatagramOutputStream(data.host, data.port, data.layout.getHeader(),
+                    data.layout.getFooter());
             return new DatagramSocketManager(name, os, inetAddress, data.host, data.port, data.layout);
         }
     }
