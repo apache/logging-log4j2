5e84c66c0c7cd10a82784b0e264fee8d2ef2bd64
Gary D. Gregory <ggregory@apache.org>
2014-06-03 06:55:53 +0000

    Don't use an lvar called l.
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/log4j2/trunk@1599429 13f79535-47bb-0310-9956-ffa450edef68

--- log4j-core/src/test/java/org/apache/logging/log4j/core/net/jms/JmsTopicFailoverTest.java
+++ log4j-core/src/test/java/org/apache/logging/log4j/core/net/jms/JmsTopicFailoverTest.java
@@ -85,8 +85,8 @@ public class JmsTopicFailoverTest {
 
     private static void setupQueue() throws Exception {
         // MockContextFactory becomes the primary JNDI provider
-        final StatusConsoleListener l = new StatusConsoleListener(Level.ERROR);
-        StatusLogger.getLogger().registerListener(l);
+        final StatusConsoleListener listener = new StatusConsoleListener(Level.ERROR);
+        StatusLogger.getLogger().registerListener(listener);
         MockContextFactory.setAsInitial();
         context = new InitialContext();
         context.rebind(FACTORY_NAME, new TopicConnectionFactoryImpl() );
