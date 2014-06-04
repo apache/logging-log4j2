984b6f5a756e9c88db4f7179aef2871f3823b1f0
Remko Popma <rpopma@apache.org>
2014-06-04 15:23:25 +0000

    added benchmarks for Async Loggers and Async Appenders
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/log4j2/trunk@1600291 13f79535-47bb-0310-9956-ffa450edef68

---
+++ log4j-perf/src/main/java/org/apache/logging/log4j/perf/jmh/AsyncLoggersBenchmark.java
@@ -0,0 +1,103 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache license, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the license for the specific language governing permissions and
+ * limitations under the license.
+ */
+
+package org.apache.logging.log4j.perf.jmh;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.LifeCycle;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.GenerateMicroBenchmark;
+import org.openjdk.jmh.annotations.Level;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.TearDown;
+
+/**
+ * Tests Log4j2 Async Loggers performance.
+ */
+// ============================== HOW TO RUN THIS TEST: ====================================
+//
+// single thread:
+// java -jar log4j-perf/target/microbenchmarks.jar ".*Async.*Benchmark.*" -f 1 -wi 5 -i 5
+//
+// multiple threads (for example, 4 threads):
+// java -jar log4j-perf/target/microbenchmarks.jar ".*Async.*Benchmark.*" -f 1 -wi 5 -i 5 -t 4 -si true
+//
+// Usage help:
+// java -jar log4j-perf/target/microbenchmarks.jar -help
+//
+@State(Scope.Thread)
+public class AsyncLoggersBenchmark {
+    final static char[] CHARS = new char[500];
+    static {
+        Arrays.fill(CHARS, 'a');
+    }
+    final static String TEST = new String(CHARS);
+
+    Logger logger;
+
+    @Setup(Level.Trial)
+    public void up() {
+        new File("perftest.log").delete();
+        System.setProperty("log4j.configurationFile", "perf3PlainNoLoc.xml");
+         System.setProperty("Log4jContextSelector",
+         "org.apache.logging.log4j.core.async.AsyncLoggerContextSelector");
+        logger = LogManager.getLogger(getClass());
+    }
+
+    @TearDown(Level.Trial)
+    public void down() {
+        ((LifeCycle) LogManager.getContext(false)).stop();
+        new File("perftest.log").delete();
+    }
+
+    @GenerateMicroBenchmark
+    @BenchmarkMode(Mode.Throughput)
+    @OutputTimeUnit(TimeUnit.SECONDS)
+    public boolean throughputBaseline() {
+        return logger.isInfoEnabled();
+    }
+
+    @GenerateMicroBenchmark
+    @BenchmarkMode(Mode.Throughput)
+    @OutputTimeUnit(TimeUnit.SECONDS)
+    public void throughput() {
+        logger.info(TEST);
+    }
+
+    @GenerateMicroBenchmark
+    @BenchmarkMode(Mode.SampleTime)
+    @OutputTimeUnit(TimeUnit.NANOSECONDS)
+    public boolean latencyBaseline() {
+        return logger.isInfoEnabled();
+    }
+
+    @GenerateMicroBenchmark
+    @BenchmarkMode(Mode.SampleTime)
+    @OutputTimeUnit(TimeUnit.NANOSECONDS)
+    public void latency() {
+        logger.info(TEST);
+    }
+}
