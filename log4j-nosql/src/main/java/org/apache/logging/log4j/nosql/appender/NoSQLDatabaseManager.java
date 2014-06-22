c1789024136eb186a214fd1a824547502af57b7f
Matt Sicker <mattsicker@apache.org>
2014-06-22 18:33:14 +0000

    Don't close NoSQL clients on commit.
      - Related to LOG4J2-676.
      - Our NoSQL drivers are actually clients which means calling close() on them (or equivalent) is really more like shutting down the connection pool than closing an individual connection.
    
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/log4j2/trunk@1604636 13f79535-47bb-0310-9956-ffa450edef68

--- log4j-nosql/src/main/java/org/apache/logging/log4j/nosql/appender/NoSQLDatabaseManager.java
+++ log4j-nosql/src/main/java/org/apache/logging/log4j/nosql/appender/NoSQLDatabaseManager.java
@@ -51,6 +51,7 @@ public final class NoSQLDatabaseManager<W> extends AbstractDatabaseManager {
 
     @Override
     protected void shutdownInternal() {
+        // NoSQL doesn't use transactions, so all we need to do here is simply close the client
         Closer.closeSilent(this.connection);
     }
 
@@ -155,13 +156,10 @@ public final class NoSQLDatabaseManager<W> extends AbstractDatabaseManager {
 
     @Override
     protected void commitAndClose() {
-        try {
-            if (this.connection != null && !this.connection.isClosed()) {
-                this.connection.close();
-            }
-        } catch (Exception e) {
-            throw new AppenderLoggingException("Failed to commit and close NoSQL connection in manager.", e);
-        }
+        // all NoSQL drivers auto-commit (since NoSQL doesn't generally use the concept of transactions).
+        // also, all our NoSQL drivers use internal connection pooling and provide clients, not connections.
+        // thus, we should not be closing the client until shutdown as NoSQL is very different from SQL.
+        // see LOG4J2-591 and LOG4J2-676
     }
 
     private NoSQLObject<W>[] convertStackTrace(final StackTraceElement[] stackTrace) {
