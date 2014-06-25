fd13ccf912de1c684ee24c763569a713e3d9293d
Gary D. Gregory <ggregory@apache.org>
2014-06-25 17:32:36 +0000

    Remove unused imports.
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/log4j2/trunk@1605515 13f79535-47bb-0310-9956-ffa450edef68

--- log4j-nosql/src/test/java/org/apache/logging/log4j/nosql/appender/NoSqlAppenderTest.java
+++ log4j-nosql/src/test/java/org/apache/logging/log4j/nosql/appender/NoSqlAppenderTest.java
@@ -16,13 +16,14 @@
  */
 package org.apache.logging.log4j.nosql.appender;
 
-import org.junit.After;
-import org.junit.Before;
+import static org.easymock.EasyMock.createStrictMock;
+import static org.easymock.EasyMock.replay;
+import static org.easymock.EasyMock.verify;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
 import org.junit.Test;
-
-import static org.easymock.EasyMock.*;
-
-import static org.junit.Assert.*;
 
 public class NoSqlAppenderTest {
 
