ef245ced530e6cc8c1a035bf60e50f580fd7e0ea
Gary D. Gregory <ggregory@apache.org>
2014-06-25 04:27:22 +0000

    Use final.
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/log4j2/trunk@1605259 13f79535-47bb-0310-9956-ffa450edef68

--- log4j-jcl/src/main/java/org/apache/logging/log4j/jcl/Log4jLog.java
+++ log4j-jcl/src/main/java/org/apache/logging/log4j/jcl/Log4jLog.java
@@ -67,62 +67,62 @@ public class Log4jLog implements Log, Serializable {
     }
 
     @Override
-    public void trace(Object message) {
+    public void trace(final Object message) {
         logger.logIfEnabled(FQCN, Level.TRACE, null, message, null);
     }
 
     @Override
-    public void trace(Object message, Throwable t) {
+    public void trace(final Object message, final Throwable t) {
         logger.logIfEnabled(FQCN, Level.TRACE, null, message, t);
     }
 
     @Override
-    public void debug(Object message) {
+    public void debug(final Object message) {
         logger.logIfEnabled(FQCN, Level.DEBUG, null, message, null);
     }
 
     @Override
-    public void debug(Object message, Throwable t) {
+    public void debug(final Object message, final Throwable t) {
         logger.logIfEnabled(FQCN, Level.DEBUG, null, message, t);
     }
 
     @Override
-    public void info(Object message) {
+    public void info(final Object message) {
         logger.logIfEnabled(FQCN, Level.INFO, null, message, null);
     }
 
     @Override
-    public void info(Object message, Throwable t) {
+    public void info(final Object message, final Throwable t) {
         logger.logIfEnabled(FQCN, Level.INFO, null, message, t);
     }
 
     @Override
-    public void warn(Object message) {
+    public void warn(final Object message) {
         logger.logIfEnabled(FQCN, Level.WARN, null, message, null);
     }
 
     @Override
-    public void warn(Object message, Throwable t) {
+    public void warn(final Object message, final Throwable t) {
         logger.logIfEnabled(FQCN, Level.WARN, null, message, t);
     }
 
     @Override
-    public void error(Object message) {
+    public void error(final Object message) {
         logger.logIfEnabled(FQCN, Level.ERROR, null, message, null);
     }
 
     @Override
-    public void error(Object message, Throwable t) {
+    public void error(final Object message, final Throwable t) {
         logger.logIfEnabled(FQCN, Level.ERROR, null, message, t);
     }
 
     @Override
-    public void fatal(Object message) {
+    public void fatal(final Object message) {
         logger.logIfEnabled(FQCN, Level.FATAL, null, message, null);
     }
 
     @Override
-    public void fatal(Object message, Throwable t) {
+    public void fatal(final Object message, final Throwable t) {
         logger.logIfEnabled(FQCN, Level.FATAL, null, message, t);
     }
 }
