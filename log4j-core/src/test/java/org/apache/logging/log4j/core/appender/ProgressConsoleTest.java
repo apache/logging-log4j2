0216332ff3a56d1d262e17c3005d39720f5d2f2c
Gary D. Gregory <ggregory@apache.org>
2014-06-25 03:49:04 +0000

    Use a standard acronym to describe the start of escape sequences (See "Sequence elements" in https://en.wikipedia.org/wiki/ANSI_escape_code).
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/log4j2/trunk@1605246 13f79535-47bb-0310-9956-ffa450edef68

---
+++ log4j-core/src/test/java/org/apache/logging/log4j/core/appender/ProgressConsoleTest.java
@@ -0,0 +1,48 @@
+package org.apache.logging.log4j.core.appender;
+
+import java.text.DecimalFormat;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.LoggerContext;
+import org.apache.logging.log4j.core.config.Configurator;
+
+public class ProgressConsoleTest {
+
+    private static final Logger LOG = LogManager.getLogger(ProgressConsoleTest.class);
+
+    public static void main(final String[] args) {
+        final LoggerContext ctx = Configurator.initialize(ProgressConsoleTest.class.getName(),
+                "src/test/resources/log4j2-console-progress.xml");
+        //src/test/resources/log4j2-console-progress.xml
+        //target/test-classes/log4j2-progress-console.xml
+        try {
+            for (double i = 0; i <= 1; i = i + 0.05) {
+                updateProgress(i);
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        } finally {
+            Configurator.shutdown(ctx);
+        }
+    }
+
+    private static void updateProgress(final double progressPercentage) {
+        final int width = 50; // progress bar width in chars
+
+        String s = "[";
+        int i = 0;
+        for (; i <= (int) (progressPercentage * width); i++) {
+            s += ".";
+        }
+        for (; i < width; i++) {
+            s += " ";
+        }
+        s += "](" + (new DecimalFormat("#0.00")).format(progressPercentage * 100) + "%)\r";
+        LOG.info(s);
+    }
+}
