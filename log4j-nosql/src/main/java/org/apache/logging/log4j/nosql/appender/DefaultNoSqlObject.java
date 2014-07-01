a76d3d614658224b3a6e1869982121cbc77b4163
Matt Sicker <mattsicker@apache.org>
2014-07-01 00:27:25 +0000

    Refactor CouchDbObject into DefaultNoSqlObject.
      - Back when I was looking into the APIs for some other NoSQL drivers, I noticed that almost all of them use Map<String, Object> as their document type. Only MongoDB uses a more specific class. Therefore, I've renamed the CouchDbObject class due to its versatility in supporting more NoSQL drivers in the future.
      - Updated tests that referenced the old class.
    
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/log4j2/trunk@1606953 13f79535-47bb-0310-9956-ffa450edef68

--- log4j-nosql/src/main/java/org/apache/logging/log4j/nosql/appender/DefaultNoSqlObject.java
+++ log4j-nosql/src/main/java/org/apache/logging/log4j/nosql/appender/DefaultNoSqlObject.java
@@ -14,43 +14,45 @@
  * See the license for the specific language governing permissions and
  * limitations under the license.
  */
-package org.apache.logging.log4j.nosql.appender.couchdb;
+package org.apache.logging.log4j.nosql.appender;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-import org.apache.logging.log4j.nosql.appender.NoSqlObject;
-
 /**
- * The Apache CouchDB implementation of {@link NoSqlObject}.
+ * Default implementation of {@link org.apache.logging.log4j.nosql.appender.NoSqlObject}. Most NoSQL Java APIs tend
+ * to re-use the Map interface for storage and retrieval of the underlying JSON documents. Therefore, this
+ * implementation is provided for convenience.
  */
-public final class CouchDbObject implements NoSqlObject<Map<String, Object>> {
+public class DefaultNoSqlObject implements NoSqlObject<Map<String, Object>> {
+
     private final Map<String, Object> map;
 
-    public CouchDbObject() {
+    public DefaultNoSqlObject() {
         this.map = new HashMap<String, Object>();
     }
 
     @Override
-    public void set(final String field, final Object value) {
+    public void set(String field, Object value) {
         this.map.put(field, value);
     }
 
     @Override
-    public void set(final String field, final NoSqlObject<Map<String, Object>> value) {
+    public void set(String field, NoSqlObject<Map<String, Object>> value) {
         this.map.put(field, value.unwrap());
     }
 
     @Override
-    public void set(final String field, final Object[] values) {
+    public void set(String field, Object[] values) {
         this.map.put(field, Arrays.asList(values));
     }
 
     @Override
-    public void set(final String field, final NoSqlObject<Map<String, Object>>[] values) {
-        final ArrayList<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
+    public void set(String field, NoSqlObject<Map<String, Object>>[] values) {
+        final List<Map<String, Object>> list = new ArrayList<Map<String, Object>>(values.length);
         for (final NoSqlObject<Map<String, Object>> value : values) {
             list.add(value.unwrap());
         }
