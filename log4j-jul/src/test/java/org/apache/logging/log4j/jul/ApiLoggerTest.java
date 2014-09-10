2d1c0f87b11c06adedeed2039279e2574045e060
Matt Sicker <mattsicker@apache.org>
2014-09-10 01:41:43 -0500

    Refactor tests and update a few.

--- log4j-jul/src/test/java/org/apache/logging/log4j/jul/ApiLoggerTest.java
+++ log4j-jul/src/test/java/org/apache/logging/log4j/jul/ApiLoggerTest.java
@@ -17,12 +17,8 @@
 
 package org.apache.logging.log4j.jul;
 
-import java.util.List;
 import java.util.logging.Logger;
 
-import org.apache.logging.log4j.Level;
-import org.apache.logging.log4j.core.LogEvent;
-import org.apache.logging.log4j.core.impl.Log4jLogEvent;
 import org.apache.logging.log4j.test.appender.ListAppender;
 import org.junit.After;
 import org.junit.Before;
@@ -30,16 +26,9 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.hasSize;
-import static org.hamcrest.Matchers.instanceOf;
 import static org.junit.Assert.*;
 
-public class ApiLoggerTest {
-
-    public static final String LOGGER_NAME = "Test";
-    private Logger logger;
-    private ListAppender eventAppender;
-    private ListAppender stringAppender;
+public class ApiLoggerTest extends AbstractLoggerTest {
 
     @BeforeClass
     public static void setUpClass() {
@@ -59,71 +48,6 @@ public class ApiLoggerTest {
     public void tearDown() throws Exception {
         eventAppender.clear();
         stringAppender.clear();
-    }
-
-    @Test
-    public void testLog() throws Exception {
-        logger.info("Informative message here.");
-        final List<LogEvent> events = eventAppender.getEvents();
-        assertThat(events, hasSize(1));
-        final LogEvent event = events.get(0);
-        assertThat(event, instanceOf(Log4jLogEvent.class));
-        assertEquals(Level.INFO, event.getLevel());
-        assertEquals(LOGGER_NAME, event.getLoggerName());
-        assertEquals("Informative message here.", event.getMessage().getFormattedMessage());
-        assertEquals(Logger.class.getName(), event.getLoggerFqcn());
-    }
-
-    @Test
-    public void testLogWithCallingClass() throws Exception {
-        final Logger log = Logger.getLogger("Test.CallerClass");
-        log.config("Calling from LoggerTest");
-        final List<String> messages = stringAppender.getMessages();
-        assertThat(messages, hasSize(1));
-        final String message = messages.get(0);
-        assertEquals(getClass().getName(), message);
-    }
-
-    @Test
-    public void testLogUsingCustomLevel() throws Exception {
-        logger.log(CustomJdkLevel.TEST, "Test level");
-        final List<LogEvent> events = eventAppender.getEvents();
-        assertThat(events, hasSize(1));
-        final LogEvent event = events.get(0);
-        assertThat(event.getLevel(), equalTo(Level.INFO));
-        final String levelName = event.getContextMap().get(ApiLogger.LEVEL);
-        assertThat(levelName, equalTo(CustomJdkLevel.TEST.getName()));
-    }
-
-    @Test
-    public void testIsLoggable() throws Exception {
-        assertThat(logger.isLoggable(java.util.logging.Level.SEVERE), equalTo(true));
-        assertThat(logger.isLoggable(CustomJdkLevel.DEFCON_1), equalTo(true));
-    }
-
-    @Test
-    public void testGetName() throws Exception {
-        assertThat(logger.getName(), equalTo(LOGGER_NAME));
-    }
-
-    @Test
-    public void testGlobalLoggerName() throws Exception {
-        final Logger root = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
-        assertThat(root.getName(), equalTo(Logger.GLOBAL_LOGGER_NAME));
-    }
-
-    @Test
-    public void testGlobalLogger() throws Exception {
-        final Logger root = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
-        root.info("Test info message");
-        root.config("Test info message");
-        root.log(CustomJdkLevel.TEST, "Test info message");
-        final List<LogEvent> events = eventAppender.getEvents();
-        assertThat(events, hasSize(3));
-        for (final LogEvent event : events) {
-            final String message = event.getMessage().getFormattedMessage();
-            assertThat(message, equalTo("Test info message"));
-        }
     }
 
     @Test
--- log4j-jul/src/test/java/org/apache/logging/log4j/jul/CoreLoggerTest.java
+++ log4j-jul/src/test/java/org/apache/logging/log4j/jul/CoreLoggerTest.java
@@ -1,11 +1,25 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache license, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the license for the specific language governing permissions and
+ * limitations under the license.
+ */
+
 package org.apache.logging.log4j.jul;
 
-import java.util.List;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import org.apache.logging.log4j.Level;
-import org.apache.logging.log4j.core.LogEvent;
-import org.apache.logging.log4j.core.impl.Log4jLogEvent;
 import org.apache.logging.log4j.test.appender.ListAppender;
 import org.junit.After;
 import org.junit.Before;
@@ -13,16 +27,9 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.hasSize;
-import static org.hamcrest.Matchers.instanceOf;
 import static org.junit.Assert.*;
 
-public class CoreLoggerTest {
-
-    public static final String LOGGER_NAME = "Test";
-    private Logger logger;
-    private ListAppender eventAppender;
-    private ListAppender stringAppender;
+public class CoreLoggerTest extends AbstractLoggerTest {
 
     @BeforeClass
     public static void setUpClass() {
@@ -32,85 +39,32 @@ public class CoreLoggerTest {
     @Before
     public void setUp() throws Exception {
         logger = Logger.getLogger(LOGGER_NAME);
-        assertThat(logger.getLevel(), equalTo(java.util.logging.Level.FINE));
+        assertThat(logger.getLevel(), equalTo(Level.FINE));
         eventAppender = ListAppender.getListAppender("TestAppender");
         stringAppender = ListAppender.getListAppender("StringAppender");
     }
 
     @After
     public void tearDown() throws Exception {
-        eventAppender.clear();
-        stringAppender.clear();
-    }
-
-    @Test
-    public void testLog() throws Exception {
-        logger.info("Informative message here.");
-        final List<LogEvent> events = eventAppender.getEvents();
-        assertThat(events, hasSize(1));
-        final LogEvent event = events.get(0);
-        assertThat(event, instanceOf(Log4jLogEvent.class));
-        assertEquals(Level.INFO, event.getLevel());
-        assertEquals(LOGGER_NAME, event.getLoggerName());
-        assertEquals("Informative message here.", event.getMessage().getFormattedMessage());
-        assertEquals(Logger.class.getName(), event.getLoggerFqcn());
-    }
-
-    @Test
-    public void testLogWithCallingClass() throws Exception {
-        final Logger log = Logger.getLogger("Test.CallerClass");
-        log.config("Calling from LoggerTest");
-        final List<String> messages = stringAppender.getMessages();
-        assertThat(messages, hasSize(1));
-        final String message = messages.get(0);
-        assertEquals(getClass().getName(), message);
-    }
-
-    @Test
-    public void testLogUsingCustomLevel() throws Exception {
-        logger.log(CustomJdkLevel.TEST, "Test level");
-        final List<LogEvent> events = eventAppender.getEvents();
-        assertThat(events, hasSize(1));
-        final LogEvent event = events.get(0);
-        assertThat(event.getLevel(), equalTo(Level.INFO));
-        final String levelName = event.getContextMap().get(ApiLogger.LEVEL);
-        assertThat(levelName, equalTo(CustomJdkLevel.TEST.getName()));
+        if (eventAppender != null) {
+            eventAppender.clear();
+        }
+        if (stringAppender != null) {
+            stringAppender.clear();
+        }
     }
 
     @Test
     public void testSetLevel() throws Exception {
-        logger.setLevel(java.util.logging.Level.SEVERE);
-        assertThat(logger.getLevel(), equalTo(java.util.logging.Level.SEVERE));
-    }
-
-    @Test
-    public void testIsLoggable() throws Exception {
-        assertThat(logger.isLoggable(java.util.logging.Level.SEVERE), equalTo(true));
-        assertThat(logger.isLoggable(CustomJdkLevel.DEFCON_1), equalTo(true));
-    }
-
-    @Test
-    public void testGetName() throws Exception {
-        assertThat(logger.getName(), equalTo(LOGGER_NAME));
-    }
-
-    @Test
-    public void testGlobalLoggerName() throws Exception {
-        final Logger root = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
-        assertThat(root.getName(), equalTo(Logger.GLOBAL_LOGGER_NAME));
-    }
-
-    @Test
-    public void testGlobalLogger() throws Exception {
-        final Logger root = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
-        root.info("Test info message");
-        root.config("Test info message");
-        root.log(CustomJdkLevel.TEST, "Test info message");
-        final List<LogEvent> events = eventAppender.getEvents();
-        assertThat(events, hasSize(3));
-        for (final LogEvent event : events) {
-            final String message = event.getMessage().getFormattedMessage();
-            assertThat(message, equalTo("Test info message"));
-        }
+        final Logger childLogger = Logger.getLogger(LOGGER_NAME + ".Child");
+        assertThat(childLogger.getLevel(), equalTo(Level.FINE));
+        logger.setLevel(Level.SEVERE);
+        assertThat(childLogger.getLevel(), equalTo(Level.FINE));
+        assertThat(logger.getLevel(), equalTo(Level.SEVERE));
+        logger.setLevel(Level.FINER);
+        assertThat(logger.getLevel(), equalTo(Level.FINER));
+        logger.setLevel(Level.FINE);
+        assertThat(logger.getLevel(), equalTo(Level.FINE));
+        assertThat(childLogger.getLevel(), equalTo(Level.FINE));
     }
 }
