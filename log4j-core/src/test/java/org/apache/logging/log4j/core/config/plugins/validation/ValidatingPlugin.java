5533cae78ab9c0c88a8befd5b01c66b4b3e9e76f
Matt Sicker <mattsicker@apache.org>
2014-09-14 00:00:16 -0500

    Add tests for required annotation.
      - Part of LOG4J2-825.

---
+++ log4j-core/src/test/java/org/apache/logging/log4j/core/config/plugins/validation/ValidatingPlugin.java
@@ -0,0 +1,68 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache license, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the license for the specific language governing permissions and
+ * limitations under the license.
+ */
+package org.apache.logging.log4j.core.config.plugins.validation;
+
+import org.apache.logging.log4j.core.config.plugins.Plugin;
+import org.apache.logging.log4j.core.config.plugins.PluginBuilderAttribute;
+import org.apache.logging.log4j.core.config.plugins.PluginBuilderFactory;
+import org.apache.logging.log4j.core.config.plugins.PluginFactory;
+import org.apache.logging.log4j.core.config.plugins.validation.constraints.RequiresNonNull;
+import org.apache.logging.log4j.core.util.Assert;
+
+/**
+ *
+ */
+@Plugin(name = "Validator", category = "Test")
+public class ValidatingPlugin {
+
+    private final String name;
+
+    public ValidatingPlugin(final String name) {
+        this.name = Assert.requireNonNull(name, "name");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @PluginFactory
+    public static ValidatingPlugin newValidatingPlugin(@RequiresNonNull final String name) {
+        return new ValidatingPlugin(name);
+    }
+
+    @PluginBuilderFactory
+    public static Builder newBuilder() {
+        return new Builder();
+    }
+
+    public static class Builder implements org.apache.logging.log4j.core.util.Builder<ValidatingPlugin> {
+
+        @PluginBuilderAttribute
+        @RequiresNonNull
+        private String name;
+
+        public Builder setName(final String name) {
+            this.name = name;
+            return this;
+        }
+
+        @Override
+        public ValidatingPlugin build() {
+            return new ValidatingPlugin(name);
+        }
+    }
+}
---
+++ log4j-core/src/test/java/org/apache/logging/log4j/core/config/plugins/validation/validators/RequiresNonNullValidatorTest.java
@@ -0,0 +1,63 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache license, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the license for the specific language governing permissions and
+ * limitations under the license.
+ */
+package org.apache.logging.log4j.core.config.plugins.validation.validators;
+
+import org.apache.logging.log4j.core.config.Node;
+import org.apache.logging.log4j.core.config.NullConfiguration;
+import org.apache.logging.log4j.core.config.plugins.util.PluginBuilder;
+import org.apache.logging.log4j.core.config.plugins.util.PluginManager;
+import org.apache.logging.log4j.core.config.plugins.util.PluginType;
+import org.apache.logging.log4j.core.config.plugins.validation.ValidatingPlugin;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RequiresNonNullValidatorTest {
+
+    private PluginType<ValidatingPlugin> plugin;
+    private Node node;
+
+    @SuppressWarnings("unchecked")
+    @Before
+    public void setUp() throws Exception {
+        final PluginManager manager = new PluginManager("Test");
+        manager.collectPlugins();
+        plugin = (PluginType<ValidatingPlugin>) manager.getPluginType("Validator");
+        node = new Node(null, "Validator", plugin);
+    }
+
+    @Test
+    public void testNullDefaultValue() throws Exception {
+        final ValidatingPlugin validatingPlugin = new PluginBuilder<ValidatingPlugin>(plugin)
+            .withConfiguration(new NullConfiguration())
+            .withConfigurationNode(node)
+            .build();
+        assertNull(validatingPlugin);
+    }
+
+    @Test
+    public void testNonNullValue() throws Exception {
+        node.getAttributes().put("name", "foo");
+        final ValidatingPlugin validatingPlugin = new PluginBuilder<ValidatingPlugin>(plugin)
+            .withConfiguration(new NullConfiguration())
+            .withConfigurationNode(node)
+            .build();
+        assertNotNull(validatingPlugin);
+        assertEquals("foo", validatingPlugin.getName());
+    }
+}
