226a03120b0e6bf0c2fd8031e13e55633f73d943
Gary Gregory <garydgregory@gmail.com>
2014-09-19 18:19:52 -0400

    Fix compiler warning: exception not thrown.

--- log4j-flume-ng/src/test/java/org/apache/logging/log4j/flume/appender/FlumeEmbeddedAppenderTest.java
+++ log4j-flume-ng/src/test/java/org/apache/logging/log4j/flume/appender/FlumeEmbeddedAppenderTest.java
@@ -126,7 +126,7 @@ public class FlumeEmbeddedAppenderTest {
     }
 
     @Test
-    public void testLog4Event() throws InterruptedException, IOException {
+    public void testLog4Event() throws IOException {
 
         final StructuredDataMessage msg = new StructuredDataMessage("Test", "Test Log4j", "Test");
         EventLogger.logEvent(msg);
@@ -139,7 +139,7 @@ public class FlumeEmbeddedAppenderTest {
     }
 
     @Test
-    public void testMultiple() throws InterruptedException, IOException {
+    public void testMultiple() throws IOException {
 
         for (int i = 0; i < 10; ++i) {
             final StructuredDataMessage msg = new StructuredDataMessage("Test", "Test Multiple " + i, "Test");
@@ -196,7 +196,7 @@ public class FlumeEmbeddedAppenderTest {
     }
     /* Flume 1.4.0 does not support interceptors on the embedded agent
     @Test      */
-    public void testHeaderAddedByInterceptor() throws InterruptedException, IOException {
+    public void testHeaderAddedByInterceptor() {
 
         final StructuredDataMessage msg = new StructuredDataMessage("Test", "Test Log4j", "Test");
         EventLogger.logEvent(msg);
