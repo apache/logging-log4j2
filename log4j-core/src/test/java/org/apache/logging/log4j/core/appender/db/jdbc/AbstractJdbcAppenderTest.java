b1255cfd5ddfacd2bde7dd89604598fcbfcc6ec4
rpopma <rpopma@apache.org>
2014-09-20 17:25:08 +0900

    LOG4J2-840, LOG4J2-841: Migrate JdbcHyperSqlAppender and JdbcH2Appender performance test from JUnit to log4j-perf

--- log4j-core/src/test/java/org/apache/logging/log4j/core/appender/db/jdbc/AbstractJdbcAppenderTest.java
+++ log4j-core/src/test/java/org/apache/logging/log4j/core/appender/db/jdbc/AbstractJdbcAppenderTest.java
@@ -28,7 +28,6 @@ import javax.sql.DataSource;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.categories.PerformanceTests;
 import org.apache.logging.log4j.core.Appender;
 import org.apache.logging.log4j.core.LoggerContext;
 import org.apache.logging.log4j.core.config.ConfigurationFactory;
@@ -39,7 +38,6 @@ import org.easymock.IAnswer;
 import org.h2.util.IOUtils;
 import org.junit.After;
 import org.junit.Test;
-import org.junit.experimental.categories.Category;
 import org.mockejb.jndi.MockContextFactory;
 
 import static org.easymock.EasyMock.*;
@@ -218,42 +216,5 @@ public abstract class AbstractJdbcAppenderTest {
                 IOUtils.readStringAndClose(resultSet.getNClob("exception").getCharacterStream(), -1));
 
         assertFalse("There should not be three rows.", resultSet.next());
-    }
-
-    @Test
-    @Category(PerformanceTests.class)
-    public void testPerformanceOfAppenderWith1000Events() throws Exception {
-        this.setUp("fmLogEntry", "log4j2-" + this.databaseType + "-factory-method.xml");
-
-        final RuntimeException exception = new RuntimeException("Hello, world!");
-
-        final Logger logger = LogManager.getLogger(this.getClass().getName() +
-                ".testPerformanceOfAppenderWith1000Events");
-        logger.info("This is a warm-up message.");
-
-        System.out.println("Starting a performance test for JDBC Appender for " + this.databaseType + '.');
-
-        final long start = System.nanoTime();
-
-        for(int i = 0; i < 1000; i++) {
-            if (i % 25 == 0) {
-                logger.warn("This is an exception message.", exception);
-            } else {
-                logger.info("This is an info message.");
-            }
-        }
-
-        final long elapsed = System.nanoTime() - start;
-        final long elapsedMilli = elapsed / 1000000;
-
-        final Statement statement = this.connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,
-                ResultSet.CONCUR_READ_ONLY);
-        final ResultSet resultSet = statement.executeQuery("SELECT * FROM fmLogEntry ORDER BY id");
-
-        resultSet.last();
-        assertEquals("The number of records is not correct.", 1001, resultSet.getRow());
-
-        System.out.println("Wrote 1,000 log events in " + elapsed + " nanoseconds (" + elapsedMilli +
-                " milliseconds) for " + this.databaseType + '.');
     }
 }
---
+++ log4j-perf/src/main/java/org/apache/logging/log4j/perf/jmh/JdbcAppenderBenchmark.java
@@ -0,0 +1,204 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache license, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the license for the specific language governing permissions and
+ * limitations under the license.
+ */
+
+package org.apache.logging.log4j.perf.jmh;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.LoggerContext;
+import org.apache.logging.log4j.core.appender.db.jdbc.JdbcAppender;
+import org.apache.logging.log4j.core.config.ConfigurationFactory;
+import org.apache.logging.log4j.core.config.DefaultConfiguration;
+import org.apache.logging.log4j.core.util.Closer;
+import org.apache.logging.log4j.status.StatusLogger;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.GenerateMicroBenchmark;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.TearDown;
+import org.openjdk.jmh.logic.BlackHole;
+
+/**
+ * Tests the overhead of a number of JDBC Appenders.
+ */
+// ============================== HOW TO RUN THIS TEST: ====================================
+//
+// java -jar log4j-perf/target/microbenchmarks.jar ".*JdbcAppenderBenchmark.*" -f 1 -wi 5 -i 5
+//
+// Usage help:
+// java -jar log4j-perf/target/microbenchmarks.jar -help
+//
+@State(Scope.Thread)
+public class JdbcAppenderBenchmark {
+    private Logger loggerH2;
+    private Logger loggerHSQLDB;
+    private Connection connectionHSQLDB;
+    private Connection connectionH2;
+    private final RuntimeException exception = new RuntimeException("Hello, world!");
+
+    @Setup
+    public void setup() throws Exception {
+        connectionHSQLDB = getConnectionHSQLDB();
+        connectionH2 = getConnectionH2();
+        createTable(connectionHSQLDB, toCreateTableSqlStringHQLDB("fmLogEntry"));
+        createTable(connectionH2, toCreateTableSqlStringH2("fmLogEntry"));
+
+        System.setProperty(ConfigurationFactory.CONFIGURATION_FILE_PROPERTY, "log4j2-jdbc-appender.xml");
+        final LoggerContext context = (LoggerContext) LogManager.getContext(false);
+        if (context.getConfiguration() instanceof DefaultConfiguration) {
+            context.reconfigure();
+        }
+        StatusLogger.getLogger().reset();
+        loggerH2 = LogManager.getLogger("H2Logger");
+        loggerHSQLDB = LogManager.getLogger("HSQLDBLogger");
+    }
+
+    @BenchmarkMode(Mode.Throughput)
+    @OutputTimeUnit(TimeUnit.SECONDS)
+    @GenerateMicroBenchmark
+    public void testThroughputH2Message(final BlackHole bh) {
+        loggerH2.info("Test message");
+    }
+
+    @BenchmarkMode(Mode.Throughput)
+    @OutputTimeUnit(TimeUnit.SECONDS)
+    @GenerateMicroBenchmark
+    public void testThroughputH2Exception(final BlackHole bh) {
+        loggerH2.warn("Test message", exception);
+    }
+
+    @BenchmarkMode(Mode.SampleTime)
+    @OutputTimeUnit(TimeUnit.MICROSECONDS)
+    @GenerateMicroBenchmark
+    public void testResponseTimeH2Message(final BlackHole bh) {
+        loggerH2.info("Test message");
+    }
+
+    @BenchmarkMode(Mode.SampleTime)
+    @OutputTimeUnit(TimeUnit.MICROSECONDS)
+    @GenerateMicroBenchmark
+    public void testResponseTimeH2Exception(final BlackHole bh) {
+        loggerH2.warn("Test message", exception);
+    }
+
+    @BenchmarkMode(Mode.Throughput)
+    @OutputTimeUnit(TimeUnit.SECONDS)
+    @GenerateMicroBenchmark
+    public void testThroughputHSQLDBMessage(final BlackHole bh) {
+        loggerHSQLDB.info("Test message");
+    }
+
+    @BenchmarkMode(Mode.Throughput)
+    @OutputTimeUnit(TimeUnit.SECONDS)
+    @GenerateMicroBenchmark
+    public void testThroughputHSQLDBException(final BlackHole bh) {
+        loggerHSQLDB.warn("Test message", exception);
+    }
+
+    @BenchmarkMode(Mode.SampleTime)
+    @OutputTimeUnit(TimeUnit.MICROSECONDS)
+    @GenerateMicroBenchmark
+    public void testResponseTimeHSQLDBMessage(final BlackHole bh) {
+        loggerHSQLDB.info("Test message");
+    }
+
+    @BenchmarkMode(Mode.SampleTime)
+    @OutputTimeUnit(TimeUnit.MICROSECONDS)
+    @GenerateMicroBenchmark
+    public void testResponseTimeHSQLDBException(final BlackHole bh) {
+        loggerHSQLDB.warn("Test message", exception);
+    }
+
+    @TearDown
+    public void tearDown() throws SQLException {
+        final LoggerContext context = (LoggerContext) LogManager.getContext(false);
+        try {
+            ((JdbcAppender) context.getConfiguration().getAppender("H2Appender")).getManager().release();
+            ((JdbcAppender) context.getConfiguration().getAppender("HSQLDBAppender")).getManager().release();
+        } finally {
+            System.clearProperty(ConfigurationFactory.CONFIGURATION_FILE_PROPERTY);
+            // context.reconfigure();
+            // StatusLogger.getLogger().reset();
+
+            Statement statement = null;
+            try {
+                statement = connectionHSQLDB.createStatement();
+                statement.execute("SHUTDOWN");
+            } catch (SQLException ignore) {
+                // ignore
+            } finally {
+                Closer.closeSilently(statement);
+                Closer.closeSilently(connectionHSQLDB);
+            }
+            try {
+                statement = connectionH2.createStatement();
+                statement.execute("SHUTDOWN");
+            } catch (SQLException ignore) {
+                // ignore
+            } finally {
+                Closer.closeSilently(statement);
+                Closer.closeSilently(connectionH2);
+            }
+        }
+    }
+
+    private void createTable(Connection connection, String createSQL) throws SQLException {
+        final Statement statement = connection.createStatement();
+        try {
+            statement.executeUpdate(createSQL);
+        } finally {
+            statement.close();
+        }
+    }
+
+    private String toCreateTableSqlStringH2(final String tableName) {
+        return "CREATE TABLE " + tableName + " ( "
+                + "id INTEGER IDENTITY, eventDate DATETIME, literalColumn VARCHAR(255), level NVARCHAR(10), "
+                + "logger NVARCHAR(255), message VARCHAR(1024), exception NCLOB" + " )";
+    }
+
+    private String toCreateTableSqlStringHQLDB(final String tableName) {
+        return "CREATE TABLE " + tableName + " ( "
+                + "id INTEGER IDENTITY, eventDate DATETIME, literalColumn VARCHAR(255), level VARCHAR(10), "
+                + "logger VARCHAR(255), message VARCHAR(1024), exception CLOB" + " )";
+    }
+
+    /**
+     * Referred from log4j2-jdbc-appender.xml.
+     */
+    public static Connection getConnectionH2() throws Exception {
+        Class.forName("org.h2.Driver");
+        return DriverManager.getConnection("jdbc:h2:mem:Log4j", "sa", "");
+    }
+
+    /**
+     * Referred from log4j2-jdbc-appender.xml.
+     */
+    public static Connection getConnectionHSQLDB() throws Exception {
+        Class.forName("org.hsqldb.jdbcDriver");
+        return DriverManager.getConnection("jdbc:hsqldb:mem:Log4j", "sa", "");
+    }
+}
