099346490f397ee04d8d823ece66c52eb3102e4b
rpopma <rpopma@apache.org>
2014-09-27 23:23:24 +0900

    reverted for-each loop to normal for loop in performance-sensitive code

--- log4j-api/src/main/java/org/apache/logging/log4j/MarkerManager.java
+++ log4j-api/src/main/java/org/apache/logging/log4j/MarkerManager.java
@@ -323,7 +323,9 @@ public final class MarkerManager {
          */
         private static boolean contains(final Marker parent, final Marker... localParents) {
             //noinspection ForLoopReplaceableByForEach
-            for (final Marker marker : localParents) {
+            // performance tests showed a normal for loop is slightly faster than a for-each loop on some platforms
+            for (int i = 0, localParentsLength = localParents.length; i < localParentsLength; i++) {
+                final Marker marker = localParents[i];
                 if (marker == parent) {
                     return true;
                 }
