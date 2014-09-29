99871ce86741bd706f4a2f651827ce48c355569b
Gary Gregory <garydgregory@gmail.com>
2014-09-28 23:45:11 -0400

    Fix for Javadoc 8.

--- log4j-nosql/src/main/java/org/apache/logging/log4j/nosql/appender/NoSqlProvider.java
+++ log4j-nosql/src/main/java/org/apache/logging/log4j/nosql/appender/NoSqlProvider.java
@@ -23,14 +23,16 @@ package org.apache.logging.log4j.nosql.appender;
  * @param <C> Specifies which implementation of {@link NoSqlConnection} this provider provides.
  */
 public interface NoSqlProvider<C extends NoSqlConnection<?, ? extends NoSqlObject<?>>> {
+    
     /**
      * Obtains a connection from this provider. The concept of a connection in this case is not strictly an active
      * duplex UDP or TCP connection to the underlying database. It can be thought of more as a gateway, a path for
-     * inserting objects that may use a persistent connection or may use HTTP web service calls, etc.<br />
-     * <br />
+     * inserting objects that may use a persistent connection or may use HTTP web service calls, etc.
+     * <p>
      * Where applicable, this method should return a connection from the connection pool as opposed to opening a
      * brand new connection every time.
-     *
+     * </p>
+     * 
      * @return a connection that can be used to create and persist objects to this database.
      * @see NoSqlConnection
      */
