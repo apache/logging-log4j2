936cb5773dd8e0bc3324c706cae56b17161181e5
Matt Sicker <mattsicker@apache.org>
2014-09-30 16:48:14 -0500

    Update JCL unit tests.

--- log4j-jcl/src/test/java/org/apache/logging/log4j/jcl/LoggerTest.java
+++ log4j-jcl/src/test/java/org/apache/logging/log4j/jcl/LoggerTest.java
@@ -20,17 +20,14 @@ import java.util.List;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.core.Appender;
-import org.apache.logging.log4j.core.LoggerContext;
-import org.apache.logging.log4j.core.config.ConfigurationFactory;
 import org.apache.logging.log4j.core.util.Constants;
-import org.apache.logging.log4j.status.StatusLogger;
+import org.apache.logging.log4j.junit.InitialLoggerContext;
 import org.apache.logging.log4j.test.appender.ListAppender;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
+import org.junit.Rule;
 import org.junit.Test;
 
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
 import static org.junit.Assert.*;
 
 /**
@@ -40,25 +37,12 @@ public class LoggerTest {
 
     private static final String CONFIG = "log4j-test1.xml";
 
-    @BeforeClass
-    public static void setupClass() {
-        System.setProperty(ConfigurationFactory.CONFIGURATION_FILE_PROPERTY, CONFIG);
-        final LoggerContext ctx = (LoggerContext) LogManager.getContext(false);
-        ctx.getConfiguration();
-    }
-
-    @AfterClass
-    public static void cleanupClass() {
-        System.clearProperty(ConfigurationFactory.CONFIGURATION_FILE_PROPERTY);
-        final LoggerContext ctx = (LoggerContext) LogManager.getContext(false);
-        ctx.reconfigure();
-        StatusLogger.getLogger().reset();
-    }
-
-    Log logger = LogFactory.getLog("LoggerTest");
+    @Rule
+    public final InitialLoggerContext context = new InitialLoggerContext(CONFIG);
 
     @Test
     public void testLog() {
+        Log logger = LogFactory.getLog("LoggerTest");
         logger.debug("Test message");
         verify("List", "o.a.l.l.j.LoggerTest Test message MDC{}" + Constants.LINE_SEPARATOR);
         logger.debug("Exception: " , new NullPointerException("Test"));
@@ -70,15 +54,12 @@ public class LoggerTest {
     }
 
     private void verify(final String name, final String expected) {
-        final LoggerContext ctx = (LoggerContext) LogManager.getContext(false);
-        final Appender listApp = ctx.getConfiguration().getAppender(name);
-        assertNotNull("Missing Appender", listApp);
-        assertTrue("Not a ListAppender", listApp instanceof ListAppender);
-        final List<String> events = ((ListAppender) listApp).getMessages();
-        assertTrue("Incorrect number of messages. Expected 1 Actual " + events.size(), events.size()== 1);
+        final ListAppender listApp = context.getListAppender(name);
+        final List<String> events = listApp.getMessages();
+        assertThat(events, hasSize(1));
         final String actual = events.get(0);
-        assertEquals("Incorrect message. Expected " + expected + ". Actual " + actual, expected, actual);
-        ((ListAppender) listApp).clear();
+        assertThat(actual, equalTo(expected));
+        listApp.clear();
     }
 
 }
