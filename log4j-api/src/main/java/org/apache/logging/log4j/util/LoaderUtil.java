2f5c79d05290ced708e8d086c89be274c860e35e
Matt Sicker <mattsicker@apache.org>
2014-10-01 10:50:35 -0500

    Add equals and hashCode to LoaderUtil.UrlResource

--- log4j-api/src/main/java/org/apache/logging/log4j/util/LoaderUtil.java
+++ log4j-api/src/main/java/org/apache/logging/log4j/util/LoaderUtil.java
@@ -224,5 +224,33 @@ public final class LoaderUtil {
         public URL getUrl() {
             return url;
         }
+
+        @Override
+        public boolean equals(final Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+
+            final UrlResource that = (UrlResource) o;
+
+            if (classLoader != null ? !classLoader.equals(that.classLoader) : that.classLoader != null) {
+                return false;
+            }
+            if (url != null ? !url.equals(that.url) : that.url != null) {
+                return false;
+            }
+
+            return true;
+        }
+
+        @Override
+        public int hashCode() {
+            int result = classLoader != null ? classLoader.hashCode() : 0;
+            result = 31 * result + (url != null ? url.hashCode() : 0);
+            return result;
+        }
     }
 }
