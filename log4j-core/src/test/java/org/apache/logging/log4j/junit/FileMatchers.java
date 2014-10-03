5315e2a924d965e43f82bdf7d9516e532360490c
Matt Sicker <mattsicker@apache.org>
2014-10-03 02:20:21 -0500

    Add more FileMatchers.

--- log4j-core/src/test/java/org/apache/logging/log4j/junit/FileMatchers.java
+++ log4j-core/src/test/java/org/apache/logging/log4j/junit/FileMatchers.java
@@ -18,10 +18,14 @@ package org.apache.logging.log4j.junit;
 
 import java.io.File;
 
+import org.hamcrest.CustomMatcher;
 import org.hamcrest.FeatureMatcher;
 import org.hamcrest.Matcher;
 
 import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.hamcrest.number.OrderingComparison.greaterThan;
+import static org.hamcrest.number.OrderingComparison.lessThanOrEqualTo;
 
 /**
  * Hamcrest Matchers that operate on File objects.
@@ -31,7 +35,25 @@ import static org.hamcrest.core.Is.is;
 public final class FileMatchers {
 
     /**
-     * Returns a File Matcher that matches if the File exists.
+     * Decorating Matcher similar to {@code is()}, but for better grammar.
+     *
+     * @param matcher the Matcher to decorate.
+     * @param <T> the type expected by the Matcher.
+     * @return the decorated Matcher.
+     */
+    public static <T> Matcher<T> that(final Matcher<T> matcher) {
+        return new CustomMatcher<T>("that") {
+            @Override
+            public boolean matches(final Object item) {
+                return matcher.matches(item);
+            }
+        };
+    }
+
+    /**
+     * Matches if the File exists.
+     *
+     * @return the Matcher.
      */
     public static Matcher<File> exists() {
         return new FeatureMatcher<File, Boolean>(is(true), "file exists", "file exists") {
@@ -43,15 +65,111 @@ public final class FileMatchers {
     }
 
     /**
-     * Returns a File Matcher that matches a Long Matcher against the file length.
+     * Matches a Long Matcher against the file length.
      *
      * @param matcher the Matcher to use on the File length.
+     * @return the Matcher.
      */
     public static Matcher<File> hasLength(final Matcher<Long> matcher) {
         return new FeatureMatcher<File, Long>(matcher, "file with size", "file with size") {
             @Override
             protected Long featureValueOf(final File actual) {
                 return actual.length();
+            }
+        };
+    }
+
+    /**
+     * Matches a specific file length.
+     *
+     * @param length the file length to match
+     * @param <T>    the type of the length.
+     * @return the Matcher.
+     */
+    public static <T extends Number> Matcher<File> hasLength(final T length) {
+        return hasLength(equalTo(length.longValue()));
+    }
+
+    /**
+     * Matches a specific file length.
+     *
+     * @param length the file length to match
+     * @return the Matcher.
+     */
+    public static Matcher<File> hasLength(final long length) {
+        return hasLength(equalTo(length));
+    }
+
+    /**
+     * Matches a file with length equal to zero.
+     *
+     * @return the Matcher.
+     */
+    public static Matcher<File> isEmpty() {
+        return hasLength(0L);
+    }
+
+    /**
+     * Matches against a file's last modification time in milliseconds.
+     *
+     * @param matcher the Matcher to use on the File modification time.
+     * @return the Matcher.
+     */
+    public static Matcher<File> lastModified(final Matcher<Long> matcher) {
+        return new FeatureMatcher<File, Long>(matcher, "was last modified", "was last modified") {
+            @Override
+            protected Long featureValueOf(final File actual) {
+                return actual.lastModified();
+            }
+        };
+    }
+
+    /**
+     * Matches a time in millis before the current time.
+     *
+     * @return the Matcher.
+     */
+    public static Matcher<Long> beforeNow() {
+        return lessThanOrEqualTo(System.currentTimeMillis());
+    }
+
+    /**
+     * Matches the number of files in a directory.
+     *
+     * @param matcher the Matcher to use on the number of files.
+     * @return the Matcher.
+     */
+    public static Matcher<File> hasNumberOfFiles(final Matcher<Integer> matcher) {
+        return new FeatureMatcher<File, Integer>(matcher, "directory with number of files",
+            "directory with number of files") {
+            @Override
+            protected Integer featureValueOf(final File actual) {
+                final File[] files = actual.listFiles();
+                return files == null ? 0 : files.length;
+            }
+        };
+    }
+
+    /**
+     * Matches a directory with at least one file.
+     *
+     * @return the Matcher.
+     */
+    public static Matcher<File> hasFiles() {
+        return hasNumberOfFiles(greaterThan(0));
+    }
+
+    /**
+     * Matches a file name.
+     *
+     * @param matcher the Matcher to use on the file name.
+     * @return the Matcher.
+     */
+    public static Matcher<File> hasName(final Matcher<String> matcher) {
+        return new FeatureMatcher<File, String>(matcher, "file named", "file named") {
+            @Override
+            protected String featureValueOf(final File actual) {
+                return actual.getName();
             }
         };
     }
