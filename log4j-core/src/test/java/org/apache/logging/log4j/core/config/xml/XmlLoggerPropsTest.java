1ce4c812d916c73dfc991412dd37fe9d99b02dbf
Matt Sicker <mattsicker@apache.org>
2014-10-02 22:38:48 -0500

    Use ILC and literate assertions.

--- log4j-core/src/test/java/org/apache/logging/log4j/core/config/xml/XmlLoggerPropsTest.java
+++ log4j-core/src/test/java/org/apache/logging/log4j/core/config/xml/XmlLoggerPropsTest.java
@@ -18,19 +18,23 @@ package org.apache.logging.log4j.core.config.xml;
 
 import java.util.List;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.core.LoggerContext;
-import org.apache.logging.log4j.core.config.Configuration;
-import org.apache.logging.log4j.core.config.ConfigurationFactory;
-import org.apache.logging.log4j.status.StatusLogger;
+import org.apache.logging.log4j.junit.InitialLoggerContext;
 import org.apache.logging.log4j.test.appender.ListAppender;
-import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.junit.Rule;
 import org.junit.Test;
+
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.both;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
 
 /**
  *
@@ -38,49 +42,42 @@ import org.junit.Test;
 public class XmlLoggerPropsTest {
 
     private static final String CONFIG = "log4j-loggerprops.xml";
-    private static Configuration config;
-    private static ListAppender listAppender;
-    private static LoggerContext ctx;
+
+    @Rule
+    public final InitialLoggerContext context = new InitialLoggerContext(CONFIG);
 
     @BeforeClass
     public static void setupClass() {
-        System.setProperty(ConfigurationFactory.CONFIGURATION_FILE_PROPERTY, CONFIG);
         System.setProperty("test", "test");
-        ctx = (LoggerContext) LogManager.getContext(false);
-        config = ctx.getConfiguration();
-        listAppender = (ListAppender) config.getAppender("List");
-    }
-
-    @AfterClass
-    public static void cleanupClass() {
-        System.clearProperty(ConfigurationFactory.CONFIGURATION_FILE_PROPERTY);
-        ctx.reconfigure();
-        StatusLogger.getLogger().reset();
     }
 
     @Test
     public void testWithProps() {
+        final ListAppender listAppender = context.getListAppender("List");
         assertNotNull("No List Appender", listAppender);
 
         try {
-            assertTrue("Configuration is not an XmlConfiguration", config instanceof XmlConfiguration);
+            assertThat(context.getConfiguration(), is(instanceOf(XmlConfiguration.class)));
             Logger logger = LogManager.getLogger(XmlLoggerPropsTest.class);
             logger.debug("Test with props");
             logger = LogManager.getLogger("tiny.bubbles");
             logger.debug("Test on root");
             final List<String> events = listAppender.getMessages();
-            assertTrue("No events", events.size() > 0);
-            assertTrue("Incorrect number of events", events.size() == 2);
-            assertTrue("Incorrect value", events.get(0).contains("user="));
-            assertTrue("Incorrect value", events.get(0).contains("phrasex=****"));
-            assertTrue("Incorrect value", events.get(0).contains("test=test"));
-            assertTrue("Incorrect value", events.get(0).contains("test2=test2default"));
-            assertTrue("Incorrect value", events.get(0).contains("test3=Unknown"));
-            assertTrue("Incorrect value", events.get(1).contains("user="));
-            assertTrue("Incorrect value", events.get(1).contains("phrasex=****"));
-            assertTrue("Incorrect value", events.get(1).contains("test=test"));
-            assertTrue("Incorrect value", events.get(1).contains("test2=test2default"));
-            assertTrue("Incorrect value", events.get(1).contains("test3=Unknown"));
+            assertThat("Incorrect number of events", events, both(hasSize(greaterThan(0))).and(hasSize(equalTo(2))));
+            assertThat(events.get(0), allOf(
+                containsString("user="),
+                containsString("phrasex=****"),
+                containsString("test=test"),
+                containsString("test2=test2default"),
+                containsString("test3=Unknown")
+            ));
+            assertThat(events.get(1), allOf(
+                containsString("user="),
+                containsString("phrasex=****"),
+                containsString("test=test"),
+                containsString("test2=test2default"),
+                containsString("test3=Unknown")
+            ));
         } finally {
             System.clearProperty("test");
         }
