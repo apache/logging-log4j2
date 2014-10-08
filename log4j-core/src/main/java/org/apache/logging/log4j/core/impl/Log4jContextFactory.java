c23fe1c13eff4d25c507bd5a290e92b7da0acd79
Matt Sicker <mattsicker@apache.org>
2014-10-08 15:39:10 -0500

    Don't overwrite external context in Log4jContextFactory.
      - This is consistent with the other two getContext() methods below.

--- log4j-core/src/main/java/org/apache/logging/log4j/core/impl/Log4jContextFactory.java
+++ log4j-core/src/main/java/org/apache/logging/log4j/core/impl/Log4jContextFactory.java
@@ -140,7 +140,9 @@ public class Log4jContextFactory implements LoggerContextFactory, ShutdownCallba
     public LoggerContext getContext(final String fqcn, final ClassLoader loader, final Object externalContext,
                                     final boolean currentContext) {
         final LoggerContext ctx = selector.getContext(fqcn, loader, currentContext);
-        ctx.setExternalContext(externalContext);
+        if (externalContext != null && ctx.getExternalContext() == null) {
+            ctx.setExternalContext(externalContext);
+        }
         if (ctx.getState() == LifeCycle.State.INITIALIZED) {
             ctx.start();
         }
