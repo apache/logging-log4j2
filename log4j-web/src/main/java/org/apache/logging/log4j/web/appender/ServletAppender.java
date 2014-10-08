1a3033aef10dab01ac3b559377e6eaf4481b29fd
Matt Sicker <mattsicker@apache.org>
2014-10-07 21:16:04 -0500

    Update ServletAppender docs and plugin factory.
      - Use @Required.
      - Use boolean instead of String.
      - Use WebLoggerContextUtils.getServletContext().
      - Correct javadoc references to "Console" appender.

--- log4j-web/src/main/java/org/apache/logging/log4j/web/appender/ServletAppender.java
+++ log4j-web/src/main/java/org/apache/logging/log4j/web/appender/ServletAppender.java
@@ -16,30 +16,28 @@
  */
 package org.apache.logging.log4j.web.appender;
 
-import org.apache.logging.log4j.LogManager;
+import java.io.Serializable;
+import javax.servlet.ServletContext;
+
 import org.apache.logging.log4j.core.Filter;
 import org.apache.logging.log4j.core.Layout;
 import org.apache.logging.log4j.core.LogEvent;
-import org.apache.logging.log4j.core.LoggerContext;
 import org.apache.logging.log4j.core.appender.AbstractAppender;
 import org.apache.logging.log4j.core.config.plugins.Plugin;
 import org.apache.logging.log4j.core.config.plugins.PluginAttribute;
 import org.apache.logging.log4j.core.config.plugins.PluginElement;
 import org.apache.logging.log4j.core.config.plugins.PluginFactory;
-import org.apache.logging.log4j.core.impl.ContextAnchor;
+import org.apache.logging.log4j.core.config.plugins.validation.constraints.Required;
 import org.apache.logging.log4j.core.layout.AbstractStringLayout;
 import org.apache.logging.log4j.core.layout.PatternLayout;
-import org.apache.logging.log4j.core.util.Booleans;
-
-import javax.servlet.ServletContext;
-import java.io.Serializable;
+import org.apache.logging.log4j.web.WebLoggerContextUtils;
 
 /**
  * Logs using the ServletContext's log method
  */
 @Plugin(name = "Servlet", category = "Core", elementType = "appender", printObject = true)
 public class ServletAppender extends AbstractAppender {
-    
+
     private static final long serialVersionUID = 1L;
 
     private final ServletContext servletContext;
@@ -56,25 +54,23 @@ public class ServletAppender extends AbstractAppender {
     }
 
     /**
-     * Create a Console Appender.
-     * @param layout The layout to use (required).
+     * Create a Servlet Appender.
+     * @param layout The layout to use (required). Must extend {@link AbstractStringLayout}.
      * @param filter The Filter or null.
      * @param name The name of the Appender (required).
-     * @param ignore If {@code "true"} (default) exceptions encountered when appending events are logged; otherwise
-     *               they are propagated to the caller.
+     * @param ignoreExceptions If {@code true} (default) exceptions encountered when appending events are logged;
+     *                         otherwise they are propagated to the caller.
      * @return The ServletAppender.
      */
     @PluginFactory
     public static ServletAppender createAppender(
             @PluginElement("Layout") Layout<? extends Serializable> layout,
             @PluginElement("Filter") final Filter filter,
-            @PluginAttribute("name") final String name,
-            @PluginAttribute(value = "ignoreExceptions", defaultBoolean = true) final String ignore) {
-        if (name == null) {
-            LOGGER.error("No name provided for ConsoleAppender");
-            return null;
-        }
-        final ServletContext servletContext = getServletContext();
+            @PluginAttribute("name")
+            @Required(message = "No name provided for ServletAppender")
+            final String name,
+            @PluginAttribute(value = "ignoreExceptions", defaultBoolean = true) final boolean ignoreExceptions) {
+        final ServletContext servletContext = WebLoggerContextUtils.getServletContext();
         if (servletContext == null) {
             LOGGER.error("No servlet context is available");
             return null;
@@ -85,19 +81,7 @@ public class ServletAppender extends AbstractAppender {
             LOGGER.error("Layout must be a StringLayout to log to ServletContext");
             return null;
         }
-        final boolean ignoreExceptions = Booleans.parseBoolean(ignore, true);
         return new ServletAppender(name, (AbstractStringLayout) layout, filter, servletContext, ignoreExceptions);
     }
 
-    private static ServletContext getServletContext() {
-        LoggerContext lc = ContextAnchor.THREAD_CONTEXT.get();
-        if (lc == null) {
-            lc = (LoggerContext) LogManager.getContext(false);
-        }
-        if (lc != null) {
-            final Object obj = lc.getExternalContext();
-            return obj != null && obj instanceof ServletContext ? (ServletContext) obj : null;
-        }
-        return null;
-    }
 }
