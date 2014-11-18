2741e01c9728b21d0cd55ddd025f8bac2b2df073
Gary Gregory <garydgregory@gmail.com>
2014-11-17 21:54:28 -0500

    This test did not clean up after itself and caused other tests to fail in Eclipse.

--- log4j-1.2-api/src/test/java/org/apache/log4j/VelocityTest.java
+++ log4j-1.2-api/src/test/java/org/apache/log4j/VelocityTest.java
@@ -18,9 +18,15 @@ package org.apache.log4j;
 
 import java.io.StringWriter;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.LoggerContext;
+import org.apache.logging.log4j.core.config.Configurator;
+import org.apache.logging.log4j.status.StatusLogger;
 import org.apache.velocity.Template;
 import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.Velocity;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 /**
@@ -28,6 +34,19 @@ import org.junit.Test;
  */
 public class VelocityTest {
 
+private static LoggerContext context;
+    
+    @BeforeClass
+    public static void setupClass() {
+        context = (LoggerContext) LogManager.getContext(false);
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+        Configurator.shutdown(context);
+        StatusLogger.getLogger().reset();
+    }    
+    
     @Test
     public void testVelocity() {
         Velocity.init();
