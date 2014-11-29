0db4a578fc01253c42d7b965d2dda47a5c87edbc
Gary Gregory <garydgregory@gmail.com>
2014-11-29 12:34:13 -0500

    Adding a test for [LOG4J2-834] NoClassDefFoundError in ThrowableProxy. This is already fixed but we show with this test that the use case from the JIRA is handled.

--- log4j-core/src/test/java/org/apache/logging/log4j/core/impl/ThrowableProxyTest.java
+++ log4j-core/src/test/java/org/apache/logging/log4j/core/impl/ThrowableProxyTest.java
@@ -168,4 +168,25 @@ public class ThrowableProxyTest {
                 throwable.getStackTrace());
         assertNotNull("No package data returned", callerPackageData);
     }
+
+    /**
+     * Asserts that LOG4J2-834 is solved by constructing a ThrowableProxy over a RuntimeException object thrown at a
+     * unloaded known class (already compiled and available as a test resource:
+     * org.apache.logging.log4j.core.impl.ForceNoDefClassFoundError.class).
+     */
+    @Test
+    public void testStackWithUnloadableClass() throws Exception {
+        final Stack<Class<?>> stack = new Stack<Class<?>>();
+        final Map<String, ThrowableProxy.CacheEntry> map = new HashMap<String, ThrowableProxy.CacheEntry>();
+
+        final String runtimeExceptionThrownAtUnloadableClass_base64 = "rO0ABXNyABpqYXZhLmxhbmcuUnVudGltZUV4Y2VwdGlvbp5fBkcKNIPlAgAAeHIAE2phdmEubGFuZy5FeGNlcHRpb27Q/R8+GjscxAIAAHhyABNqYXZhLmxhbmcuVGhyb3dhYmxl1cY1Jzl3uMsDAANMAAVjYXVzZXQAFUxqYXZhL2xhbmcvVGhyb3dhYmxlO0wADWRldGFpbE1lc3NhZ2V0ABJMamF2YS9sYW5nL1N0cmluZztbAApzdGFja1RyYWNldAAeW0xqYXZhL2xhbmcvU3RhY2tUcmFjZUVsZW1lbnQ7eHBxAH4ABnB1cgAeW0xqYXZhLmxhbmcuU3RhY2tUcmFjZUVsZW1lbnQ7AkYqPDz9IjkCAAB4cAAAAAFzcgAbamF2YS5sYW5nLlN0YWNrVHJhY2VFbGVtZW50YQnFmiY23YUCAARJAApsaW5lTnVtYmVyTAAOZGVjbGFyaW5nQ2xhc3NxAH4ABEwACGZpbGVOYW1lcQB+AARMAAptZXRob2ROYW1lcQB+AAR4cAAAAAZ0ADxvcmcuYXBhY2hlLmxvZ2dpbmcubG9nNGouY29yZS5pbXBsLkZvcmNlTm9EZWZDbGFzc0ZvdW5kRXJyb3J0AB5Gb3JjZU5vRGVmQ2xhc3NGb3VuZEVycm9yLmphdmF0AARtYWlueA==";
+        final byte[] binaryDecoded = DatatypeConverter
+                .parseBase64Binary(runtimeExceptionThrownAtUnloadableClass_base64);
+        final ByteArrayInputStream inArr = new ByteArrayInputStream(binaryDecoded);
+        final ObjectInputStream in = new ObjectInputStream(inArr);
+        final Throwable throwable = (Throwable) in.readObject();
+        final ThrowableProxy subject = new ThrowableProxy(throwable);
+
+        subject.toExtendedStackTrace(stack, map, null, throwable.getStackTrace());
+    }
 }
