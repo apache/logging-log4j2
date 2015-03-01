a87c1e37b003f85ffe7f64df3c86120b0ace767f
Gary Gregory <garydgregory@gmail.com>
2015-03-01 14:40:34 -0800

    [LOG4J2-947] A new StatusLoggerAdmin listener is added to StatusLogger every time the log is reconfigured.

--- log4j-core/src/main/java/org/apache/logging/log4j/core/jmx/StatusLoggerAdmin.java
+++ log4j-core/src/main/java/org/apache/logging/log4j/core/jmx/StatusLoggerAdmin.java
@@ -63,7 +63,27 @@ public class StatusLoggerAdmin extends NotificationBroadcasterSupport implements
         } catch (final Exception e) {
             throw new IllegalStateException(e);
         }
+        removeListeners(contextName);
         StatusLogger.getLogger().registerListener(this);
+    }
+
+    /**
+     * Add listener to StatusLogger for this context, or replace it if it already exists.
+     *
+     * @param ctxName
+     */
+    private void removeListeners(final String ctxName) {
+        final StatusLogger logger = StatusLogger.getLogger();
+        final Iterable<StatusListener> listeners = logger.getListeners();
+        // Remove any StatusLoggerAdmin listeners already registered for this context
+        for (final StatusListener statusListener : listeners) {
+            if (statusListener instanceof StatusLoggerAdmin) {
+                final StatusLoggerAdmin adminListener = (StatusLoggerAdmin) statusListener;
+                if (ctxName != null && ctxName.equals(adminListener.contextName)) {
+                    logger.removeListener(adminListener);
+                }
+            }
+        }
     }
 
     private static MBeanNotificationInfo createNotificationInfo() {
