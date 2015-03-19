e4bca837b5d99c982e9126e869665e9c83cad1dd
Gary Gregory <garydgregory@gmail.com>
2015-03-19 11:28:43 -0700

    Javadoc tweak.

--- log4j-core/src/main/java/org/apache/logging/log4j/core/config/Reconfigurable.java
+++ log4j-core/src/main/java/org/apache/logging/log4j/core/config/Reconfigurable.java
@@ -17,7 +17,7 @@
 package org.apache.logging.log4j.core.config;
 
 /**
- *  Interface to be implemented by Configurations that can be reconfigured at runtime.
+ *  Implemented by Configurations that can be reconfigured at runtime.
  */
 public interface Reconfigurable {
 
