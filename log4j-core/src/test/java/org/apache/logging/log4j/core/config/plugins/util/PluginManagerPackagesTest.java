0c90563ff81381910bb6a36614d38d77d033d8e4
Gary Gregory <garydgregory@gmail.com>
2015-06-11 01:27:07 -0700

    [LOG4J2-1017] Update Java platform from Java 6 to 7. Use try-with-resources.

--- log4j-core/src/test/java/org/apache/logging/log4j/core/config/plugins/util/PluginManagerPackagesTest.java
+++ log4j-core/src/test/java/org/apache/logging/log4j/core/config/plugins/util/PluginManagerPackagesTest.java
@@ -93,22 +93,23 @@ public class PluginManagerPackagesTest {
         // set up compiler
         final JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
         final DiagnosticCollector<JavaFileObject> diagnostics = new DiagnosticCollector<>();
-        final StandardJavaFileManager fileManager = compiler.getStandardFileManager(diagnostics, null, null);
-        final Iterable<? extends JavaFileObject> compilationUnits = fileManager.getJavaFileObjectsFromFiles(Arrays.asList(f));
-
-        // compile generated source
-        // (switch off annotation processing: no need to create Log4j2Plugins.dat)
-        final List<String> options = Arrays.asList("-proc:none");
-        compiler.getTask(null, fileManager, diagnostics, options, null, compilationUnits).call();
-
-        // check we don't have any compilation errors
         final List<String> errors = new ArrayList<>();
-        for (final Diagnostic<? extends JavaFileObject> diagnostic : diagnostics.getDiagnostics()) {
-            if (diagnostic.getKind() == Diagnostic.Kind.ERROR) {
-                errors.add(String.format("Compile error: %s%n", diagnostic.getMessage(Locale.getDefault())));
+        try (final StandardJavaFileManager fileManager = compiler.getStandardFileManager(diagnostics, null, null)) {
+            final Iterable<? extends JavaFileObject> compilationUnits = fileManager.getJavaFileObjectsFromFiles(Arrays
+                    .asList(f));
+
+            // compile generated source
+            // (switch off annotation processing: no need to create Log4j2Plugins.dat)
+            final List<String> options = Arrays.asList("-proc:none");
+            compiler.getTask(null, fileManager, diagnostics, options, null, compilationUnits).call();
+
+            // check we don't have any compilation errors
+            for (final Diagnostic<? extends JavaFileObject> diagnostic : diagnostics.getDiagnostics()) {
+                if (diagnostic.getKind() == Diagnostic.Kind.ERROR) {
+                    errors.add(String.format("Compile error: %s%n", diagnostic.getMessage(Locale.getDefault())));
+                }
             }
         }
-        fileManager.close();
         assertTrue(errors.toString(), errors.isEmpty());
     }
 }
