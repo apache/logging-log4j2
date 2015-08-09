bfd73824a6bf5dcd2a18ac2b9b75a4c20a6ee2eb
Ralph Goers <rgoers@nextiva.com>
2015-08-08 17:00:53 -0700

    LOG4J2-1044 - Write pending events to Flume when the appender is stopped.

--- log4j-flume-ng/src/main/java/org/apache/logging/log4j/flume/appender/FlumeAvroManager.java
+++ log4j-flume-ng/src/main/java/org/apache/logging/log4j/flume/appender/FlumeAvroManager.java
@@ -258,6 +258,15 @@ public class FlumeAvroManager extends AbstractFlumeManager {
     protected void releaseSub() {
         if (rpcClient != null) {
             try {
+                synchronized(this) {
+                    try {
+                        if (batchSize > 1 && batchEvent.getEvents().size() > 0) {
+                            send(batchEvent);
+                        }
+                    } catch (final Exception ex) {
+                        LOGGER.error("Error sending final batch: {}", ex.getMessage());
+                    }
+                } 
                 rpcClient.close();
             } catch (final Exception ex) {
                 LOGGER.error("Attempt to close RPC client failed", ex);
