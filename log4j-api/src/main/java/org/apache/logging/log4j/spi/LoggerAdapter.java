146fa40dd6b6889277a86f6e5858ffacb672480c
ggregory <ggregory@apache.org>
2015-08-09 17:12:43 -0700

    Use the more common type name T instead of L.

--- log4j-api/src/main/java/org/apache/logging/log4j/spi/LoggerAdapter.java
+++ log4j-api/src/main/java/org/apache/logging/log4j/spi/LoggerAdapter.java
@@ -23,10 +23,10 @@ import java.io.Closeable;
  * Logger classes. This registry should not be used for Log4j Loggers; it is instead used for creating bridges to
  * other external log systems.
  *
- * @param <L> the external logger class for this registry (e.g., {@code org.slf4j.Logger})
+ * @param <T> the external logger class for this registry (e.g., {@code org.slf4j.Logger})
  * @since 2.1
  */
-public interface LoggerAdapter<L> extends Closeable {
+public interface LoggerAdapter<T> extends Closeable {
 
     /**
      * Gets a named logger. Implementations should defer to the abstract methods in {@link AbstractLoggerAdapter}.
@@ -34,5 +34,5 @@ public interface LoggerAdapter<L> extends Closeable {
      * @param name the name of the logger to get
      * @return the named logger
      */
-    L getLogger(String name);
+    T getLogger(String name);
 }
