a202ab08be2cf15ccc27e59ed22c355d28cf5f5f
ggregory <ggregory@apache.org>
2015-08-09 17:39:56 -0700

    Replace deprecated EasyMock usage.

--- log4j-web/src/test/java/org/apache/logging/log4j/web/Log4jServletContainerInitializerTest.java
+++ log4j-web/src/test/java/org/apache/logging/log4j/web/Log4jServletContainerInitializerTest.java
@@ -25,6 +25,7 @@ import javax.servlet.ServletContext;
 
 import org.apache.logging.log4j.util.Strings;
 import org.easymock.Capture;
+import org.easymock.EasyMock;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -104,8 +105,8 @@ public class Log4jServletContainerInitializerTest {
     public void testOnStartupWithServletVersion3_xEffectiveVersion3_x() throws Exception {
         final FilterRegistration.Dynamic registration = createStrictMock(FilterRegistration.Dynamic.class);
 
-        final Capture<EventListener> listenerCapture = new Capture<>();
-        final Capture<Class<? extends Filter>> filterCapture = new Capture<>();
+        final Capture<EventListener> listenerCapture = EasyMock.newCapture();
+        final Capture<Class<? extends Filter>> filterCapture = EasyMock.newCapture();
 
         expect(this.servletContext.getMajorVersion()).andReturn(3);
         expect(this.servletContext.getEffectiveMajorVersion()).andReturn(3);
