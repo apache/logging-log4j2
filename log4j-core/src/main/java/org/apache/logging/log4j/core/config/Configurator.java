e760529f681eff401601f234cc81bba09cb1a768
ggregory <ggregory@apache.org>
2015-08-22 13:51:13 -0700

    [LOG4J2-1090] Add Core Configurator APIs to change a logger's level.

--- log4j-core/src/main/java/org/apache/logging/log4j/core/config/Configurator.java
+++ log4j-core/src/main/java/org/apache/logging/log4j/core/config/Configurator.java
@@ -164,6 +164,30 @@ public final class Configurator {
         return initialize(name, null, configLocation);
     }
 
+    /**
+     * Sets the levels of <code>parentLogger</code> and all 'child' loggers to the given <code>level</level>.
+     * @param parentLogger the parent logger
+     * @param level the new level
+     */
+    public static void setChildren(final String parentLogger, final Level level) {
+        // 1) get logger config
+        // 2) if exact match, use it, if not, create it.
+        // 3) set level on logger config 
+        // 4) update child logger configs with level
+        // 5) update loggers
+        final LoggerContext loggerContext = LoggerContext.getContext(false);
+        final Configuration config = loggerContext.getConfiguration();
+        boolean set = setLevel(parentLogger, level, config);
+        for (final Map.Entry<String, LoggerConfig> entry : config.getLoggers().entrySet()) {
+            if (entry.getKey().startsWith(parentLogger)) {
+                set |= setLevel(entry.getValue(), level);
+            }
+        }
+        if (set) {
+            loggerContext.updateLoggers();
+        }
+    }
+
     private static boolean setLevel(final LoggerConfig loggerConfig, final Level level) {
         final boolean set = !loggerConfig.getLevel().equals(level);
         if (set) {
--- log4j-core/src/test/java/org/apache/logging/log4j/core/LoggerTest.java
+++ log4j-core/src/test/java/org/apache/logging/log4j/core/LoggerTest.java
@@ -133,6 +133,26 @@ public class LoggerTest {
     }
 
     @Test
+    public void debugChangeLevelAllChildrenLoggers() {
+        // Use logger AND child loggers
+        logger.debug("Debug message 1");
+        loggerChild.debug("Debug message 1 child");
+        loggerGrandchild.debug("Debug message 1 grandchild");
+        final List<LogEvent> events = app.getEvents();
+        assertEventCount(events, 3);
+        Configurator.setChildren(logger.getName(), Level.OFF);
+        logger.debug("Debug message 2");
+        loggerChild.warn("Warn message 2 child");
+        loggerGrandchild.fatal("Fatal message 2 grandchild");
+        assertEventCount(events, 3);
+        Configurator.setChildren(logger.getName(), Level.DEBUG);
+        logger.debug("Debug message 3");
+        loggerChild.warn("Trace message 3 child");
+        loggerGrandchild.trace("Fatal message 3 grandchild");
+        assertEventCount(events, 5);
+    }
+
+    @Test
     public void debugChangeLevelChildLogger() {
         // Use logger AND child loggers
         logger.debug("Debug message 1");
