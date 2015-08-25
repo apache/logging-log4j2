8b10560c320d2f60791b2239d80bc9cf5eedc95b
ggregory <ggregory@apache.org>
2015-08-25 16:15:29 -0700

    Add missing (simple) test for org.apache.logging.log4j.Level.forName(String, int).

--- log4j-api/src/test/java/org/apache/logging/log4j/LevelTest.java
+++ log4j-api/src/test/java/org/apache/logging/log4j/LevelTest.java
@@ -33,6 +33,17 @@ public class LevelTest {
     }
 
     @Test
+    public void testForNameEquals() {
+        final String name = "Foo";
+		final int intValue = 1;
+		final Level level = Level.forName(name, intValue);
+        assertNotNull(level);
+        assertEquals(level, Level.forName(name, intValue));
+        assertEquals(level, Level.getLevel(name));
+        assertEquals(intValue, Level.getLevel(name).intLevel());
+    }
+
+    @Test
     public void testGoodLevels() {
         final Level level = Level.toLevel("INFO");
         assertNotNull(level);
