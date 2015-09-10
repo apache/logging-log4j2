dfdd5811b4630e1cd56531946dc6ad5d16741cc2
ggregory <ggregory@apache.org>
2015-09-09 17:45:06 -0700

    Redundant specification of type arguments.

--- log4j-api/src/test/java/org/apache/logging/log4j/LambdaLoggerTest.java
+++ log4j-api/src/test/java/org/apache/logging/log4j/LambdaLoggerTest.java
@@ -56,7 +56,7 @@ public class LambdaLoggerTest {
         private static final long serialVersionUID = 1L;
 
         boolean enabled = true;
-        final List<LambdaLoggerTest.LogEvent> list = new ArrayList<LambdaLoggerTest.LogEvent>();
+        final List<LambdaLoggerTest.LogEvent> list = new ArrayList<>();
 
         @Override
         public boolean isEnabled(final Level level, final Marker marker, final Message message, final Throwable t) {
