5ac3a57074251e251d6fa6c7d9bdc1868b584413
Mikael Ståldal <mikael.staldal@magine.com>
2015-09-18 14:57:48 +0200

    Fix Javadoc issue

--- log4j-core/src/main/java/org/apache/logging/log4j/core/util/datetime/FastDateParser.java
+++ log4j-core/src/main/java/org/apache/logging/log4j/core/util/datetime/FastDateParser.java
@@ -313,7 +313,6 @@ public class FastDateParser implements DateParser, Serializable {
      * given by {@link ParsePosition#getIndex()} has been updated. If the input buffer has been fully
      * parsed, then the index will point to just after the end of the input buffer.
      *
-     * @see org.apache.commons.lang3.time.DateParser#parse(java.lang.String, java.text.ParsePosition)
      * {@inheritDoc}
      */
     @Override
