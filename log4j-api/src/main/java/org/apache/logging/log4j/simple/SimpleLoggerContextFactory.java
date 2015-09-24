b2e612f2733499a326a6daec43c7e6b68ed3a3f3
rpopma <rpopma@apache.org>
2015-09-24 09:16:07 +0200

    Checkstyle: 'context' hides field, IDE autoformatted

--- log4j-api/src/main/java/org/apache/logging/log4j/simple/SimpleLoggerContextFactory.java
+++ log4j-api/src/main/java/org/apache/logging/log4j/simple/SimpleLoggerContextFactory.java
@@ -30,17 +30,17 @@ public class SimpleLoggerContextFactory implements LoggerContextFactory {
 
     @Override
     public LoggerContext getContext(final String fqcn, final ClassLoader loader, final Object externalContext,
-                                    final boolean currentContext) {
+            final boolean currentContext) {
         return context;
     }
 
     @Override
     public LoggerContext getContext(final String fqcn, final ClassLoader loader, final Object externalContext,
-                                    final boolean currentContext, final URI configLocation, final String name) {
+            final boolean currentContext, final URI configLocation, final String name) {
         return context;
     }
 
     @Override
-    public void removeContext(final LoggerContext context) {
+    public void removeContext(final LoggerContext removeContext) {
     }
 }
