9434a82f249960c38483ebd4fa61afa7fd90111e
rpopma <rpopma@apache.org>
2015-09-24 08:50:34 +0200

    Checkstyle: FileTabCharacter, removed trailing space (IDE autoformatted), removed redundant 'public' keyword

--- log4j-api/src/main/java/org/apache/logging/log4j/Marker.java
+++ log4j-api/src/main/java/org/apache/logging/log4j/Marker.java
@@ -19,16 +19,17 @@ package org.apache.logging.log4j;
 import java.io.Serializable;
 
 /**
- *  Markers are objects that are used to add easily filterable information to log messages.
+ * Markers are objects that are used to add easily filterable information to log messages.
  *
- *  Markers can be hierarchical - each Marker may have a parent. This allows for broad categories
- *  being subdivided into more specific categories. An example might be a Marker named "Error" with
- *  children named "SystemError" and "ApplicationError".
+ * Markers can be hierarchical - each Marker may have a parent. This allows for broad categories being subdivided into
+ * more specific categories. An example might be a Marker named "Error" with children named "SystemError" and
+ * "ApplicationError".
  */
 public interface Marker extends Serializable {
 
-	/**
+    /**
      * Adds a Marker as a parent to this Marker.
+     * 
      * @param markers The parent markers to add.
      * @return The current Marker object, thus allowing multiple adds to be concatenated.
      * @throws IllegalArgumentException if the argument is {@code null}
@@ -36,44 +37,48 @@ public interface Marker extends Serializable {
     Marker addParents(Marker... markers);
 
     /**
-	 * Returns true if the given marker has the same name as this marker.
-	 *
-	 * @param obj the reference object with which to compare. 
-	 * @return true if the given marker has the same name as this marker.
-	 * @since 2.4
-	 */
+     * Returns true if the given marker has the same name as this marker.
+     *
+     * @param obj the reference object with which to compare.
+     * @return true if the given marker has the same name as this marker.
+     * @since 2.4
+     */
     @Override
-	public boolean equals(Object obj);
+    boolean equals(Object obj);
 
     /**
      * Returns the name of this Marker.
+     * 
      * @return The name of the Marker.
      */
     String getName();
 
     /**
      * Returns a list of parents of this Marker.
+     * 
      * @return The parent Markers or {@code null} if this Marker has no parents.
      */
     Marker[] getParents();
 
     /**
-     * Returns a hash code value based on the name of this marker.
-     * Markers are equal if they have the same name.
+     * Returns a hash code value based on the name of this marker. Markers are equal if they have the same name.
+     * 
      * @return the computed hash code
-	 * @since 2.4
+     * @since 2.4
      */
     @Override
-	public int hashCode();
+    int hashCode();
 
     /**
      * Indicates whether this Marker has references to any other Markers.
+     * 
      * @return {@code true} if the Marker has parent Markers
      */
     boolean hasParents();
 
     /**
      * Checks whether this Marker is an instance of the specified Marker.
+     * 
      * @param m The Marker to check.
      * @return {@code true} if this Marker or one of its ancestors is the specified Marker, {@code false} otherwise.
      * @throws IllegalArgumentException if the argument is {@code null}
@@ -82,6 +87,7 @@ public interface Marker extends Serializable {
 
     /**
      * Checks whether this Marker is an instance of the specified Marker.
+     * 
      * @param name The name of the Marker.
      * @return {@code true} if this Marker or one of its ancestors matches the specified name, {@code false} otherwise.
      * @throws IllegalArgumentException if the argument is {@code null}
@@ -90,6 +96,7 @@ public interface Marker extends Serializable {
 
     /**
      * Removes the specified Marker as a parent of this Marker.
+     * 
      * @param marker The marker to remove.
      * @return {@code true} if the marker was removed.
      * @throws IllegalArgumentException if the argument is {@code null}
@@ -98,6 +105,7 @@ public interface Marker extends Serializable {
 
     /**
      * Replaces the set of parent Markers with the provided Markers.
+     * 
      * @param markers The new set of parent Markers or {@code null} to clear the parents.
      * @return The current Marker object.
      */
