c1748d04b342186637bfd406acdf8c5a50476d2b
ggregory <ggregory@apache.org>
2015-10-20 15:33:55 -0700

    Remove unused imports.

--- log4j-core/src/test/java/org/apache/logging/log4j/core/util/WatchManagerTest.java
+++ log4j-core/src/test/java/org/apache/logging/log4j/core/util/WatchManagerTest.java
@@ -16,9 +16,7 @@
  */
 package org.apache.logging.log4j.core.util;
 
-import org.junit.Assume;
-import org.junit.Ignore;
-import org.junit.Test;
+import static org.junit.Assert.assertNotNull;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -33,7 +31,8 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
-import static org.junit.Assert.*;
+import org.junit.Assume;
+import org.junit.Test;
 
 /**
  * Test the WatchManager
