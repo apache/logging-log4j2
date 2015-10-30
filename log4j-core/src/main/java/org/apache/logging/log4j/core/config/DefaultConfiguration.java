40bb20755f6e7a0a26ba10bb2724a1bff6f9cb7d
ggregory <ggregory@apache.org>
2015-10-30 13:07:25 -0700

    Minor tweaks.

--- log4j-core/src/main/java/org/apache/logging/log4j/core/config/DefaultConfiguration.java
+++ log4j-core/src/main/java/org/apache/logging/log4j/core/config/DefaultConfiguration.java
@@ -53,7 +53,6 @@ public class DefaultConfiguration extends AbstractConfiguration {
      */
     public DefaultConfiguration() {
         super(ConfigurationSource.NULL_SOURCE);
-        
         setName(DEFAULT_NAME);
         final Layout<? extends Serializable> layout = PatternLayout.newBuilder()
             .withPattern(DEFAULT_PATTERN)
@@ -62,13 +61,13 @@ public class DefaultConfiguration extends AbstractConfiguration {
         final Appender appender = ConsoleAppender.createDefaultAppenderForLayout(layout);
         appender.start();
         addAppender(appender);
-        final LoggerConfig root = getRootLogger();
-        root.addAppender(appender, null, null);
+        final LoggerConfig rootLoggerConfig = getRootLogger();
+        rootLoggerConfig.addAppender(appender, null, null);
 
         final String levelName = PropertiesUtil.getProperties().getStringProperty(DEFAULT_LEVEL);
         final Level level = levelName != null && Level.valueOf(levelName) != null ?
             Level.valueOf(levelName) : Level.ERROR;
-        root.setLevel(level);
+        rootLoggerConfig.setLevel(level);
     }
 
     @Override
