0a0d14db2b3ebb358d5c926e7acf2d0b21cbc7e6
Mikael Ståldal <mikael.staldal@magine.com>
2015-11-09 13:27:12 +0100

    Make logger non-lazy

--- log4j-api-scala_2.11/src/main/scala/org/apache/logging/log4j/scala/Logging.scala
+++ log4j-api-scala_2.11/src/main/scala/org/apache/logging/log4j/scala/Logging.scala
@@ -20,7 +20,7 @@ import org.apache.logging.log4j.LogManager
 
 trait Logging {
 
-  protected lazy val logger: Logger =
+  protected val logger: Logger =
     new Logger(LogManager.getContext(getClass.getClassLoader, false).getLogger(getClass.getName))
 
 }
