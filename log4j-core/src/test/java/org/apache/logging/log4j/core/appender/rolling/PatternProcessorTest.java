f28d3d8b42d23109f400980ae5d93c5c953bab40
ggregory <ggregory@apache.org>
2015-12-31 18:51:06 -0800

    Add tests for [LOG4J2-1232] Incorrect log rotation in last week of year.

--- log4j-core/src/test/java/org/apache/logging/log4j/core/appender/rolling/PatternProcessorTest.java
+++ log4j-core/src/test/java/org/apache/logging/log4j/core/appender/rolling/PatternProcessorTest.java
@@ -171,6 +171,19 @@ public class PatternProcessorTest {
     }
 
     @Test
+    public void testGetNextTimeHourlyReturnsFirstMinuteOfNextYear() {
+        final PatternProcessor pp = new PatternProcessor("logs/app-%d{yyyy-MM-dd-HH}.log.gz");
+        final Calendar initial = Calendar.getInstance();
+        initial.set(2015, Calendar.DECEMBER, 31, 23, 31, 59);
+        final long actual = pp.getNextTime(initial.getTimeInMillis(), 1, false);
+
+        final Calendar expected = Calendar.getInstance();
+        expected.set(2016, Calendar.JANUARY, 1, 0, 0, 0);
+        expected.set(Calendar.MILLISECOND, 0);
+        assertEquals(format(expected.getTimeInMillis()), format(actual));
+    }
+
+    @Test
     public void testGetNextTimeHourlyReturnsFirstMinuteOfNextHour2() {
         final PatternProcessor pp = new PatternProcessor("logs/app-%d{yyyy-MM-dd-HH}.log.gz");
         final Calendar initial = Calendar.getInstance();
