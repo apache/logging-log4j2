23235449d5ddc776f4128c3ce082cb51bb9950a7
ggregory <ggregory@apache.org>
2016-01-15 16:17:52 -0800

    Statement unnecessarily nested within else clause.

--- log4j-core/src/main/java/org/apache/logging/log4j/core/util/FileUtils.java
+++ log4j-core/src/main/java/org/apache/logging/log4j/core/util/FileUtils.java
@@ -97,9 +97,8 @@ public final class FileUtils {
         String fileName = file.getName();
         if (fileName.lastIndexOf(".") != -1 && fileName.lastIndexOf(".") != 0) {
             return fileName.substring(fileName.lastIndexOf(".") + 1);
-        } else {
-            return null;
         }
+        return null;
     }
 
     /**
