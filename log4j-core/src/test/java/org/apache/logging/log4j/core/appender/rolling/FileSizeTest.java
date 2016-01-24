232043c1f88d202a29e147eac152fca06e74606e
Ralph Goers <rgoers@apache.org>
2016-01-23 22:38:36 -0700

    Verify that file sizes are parsed correctly.

---
+++ log4j-core/src/test/java/org/apache/logging/log4j/core/appender/rolling/FileSizeTest.java
@@ -0,0 +1,36 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache license, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the license for the specific language governing permissions and
+ * limitations under the license.
+ */
+package org.apache.logging.log4j.core.appender.rolling;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ */
+public class FileSizeTest {
+
+    private final static long EXPECTED = 10 * 1024;
+    @Test
+    public void testFileSize() throws Exception {
+
+        long value = FileSize.parse("10KB", 0);
+        assertTrue("unexpected value " + value, value == EXPECTED);
+        value = FileSize.parse("10 KB", 0);
+        assertTrue("unexpected value " + value, value == EXPECTED);
+    }
+}
