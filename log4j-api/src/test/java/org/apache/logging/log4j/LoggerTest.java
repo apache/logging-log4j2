01efb530e16407f557796d502011e24177075d12
ggregory <ggregory@apache.org>
2016-02-15 09:35:46 -0800

    Add missing test.

--- log4j-api/src/test/java/org/apache/logging/log4j/LoggerTest.java
+++ log4j-api/src/test/java/org/apache/logging/log4j/LoggerTest.java
@@ -109,6 +109,17 @@ public class LoggerTest {
     }
 
     @Test
+    public void flowTracingExitValueOnly() {
+        logger.traceEntry("doFoo(a={}, b={})", 1, 2);
+        logger.traceExit(3);
+        assertEquals(2, results.size());
+        assertThat("Incorrect Entry", results.get(0), startsWith("ENTRY[ FLOW ] TRACE entry"));
+        assertThat("Missing entry data", results.get(0), containsString("doFoo(a=1, b=2)"));
+        assertThat("Incorrect Exit", results.get(1), startsWith("EXIT[ FLOW ] TRACE exit"));
+        assertThat("Missing exit data", results.get(1), containsString("3"));
+    }
+
+    @Test
     public void flowTracingString_ObjectArray2() {
         EntryMessage msg = logger.traceEntry("doFoo(a={}, b={})", 1, 2);
         logger.traceExit(msg, 3);
