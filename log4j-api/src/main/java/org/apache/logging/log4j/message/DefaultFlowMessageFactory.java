bb1d0a584e9411a5fe1445a3f9430f0240d3fe36
ggregory <ggregory@apache.org>
2016-02-19 15:49:39 -0800

    Fix comments.

--- log4j-api/src/main/java/org/apache/logging/log4j/message/DefaultFlowMessageFactory.java
+++ log4j-api/src/main/java/org/apache/logging/log4j/message/DefaultFlowMessageFactory.java
@@ -173,7 +173,7 @@ public class DefaultFlowMessageFactory implements FlowMessageFactory, Serializab
     /*
      * (non-Javadoc)
      *
-     * @see org.apache.logging.log4j.message.MessageFactory#newExitMessage(org.apache.logging.log4j.message.EntryMessage)
+     * @see org.apache.logging.log4j.message.FlowMessageFactory#newExitMessage(org.apache.logging.log4j.message.EntryMessage)
      */
     @Override
     public ExitMessage newExitMessage(final EntryMessage message) {
@@ -183,7 +183,7 @@ public class DefaultFlowMessageFactory implements FlowMessageFactory, Serializab
     /*
      * (non-Javadoc)
      *
-     * @see org.apache.logging.log4j.message.MessageFactory#newExitMessage(java.lang.Object, org.apache.logging.log4j.message.EntryMessage)
+     * @see org.apache.logging.log4j.message.FlowMessageFactory#newExitMessage(java.lang.Object, org.apache.logging.log4j.message.EntryMessage)
      */
     @Override
     public ExitMessage newExitMessage(final Object result, final EntryMessage message) {
@@ -193,7 +193,7 @@ public class DefaultFlowMessageFactory implements FlowMessageFactory, Serializab
     /*
      * (non-Javadoc)
      *
-     * @see org.apache.logging.log4j.message.MessageFactory#newExitMessage(java.lang.Object, org.apache.logging.log4j.message.Message)
+     * @see org.apache.logging.log4j.message.FlowMessageFactory#newExitMessage(java.lang.Object, org.apache.logging.log4j.message.Message)
      */
     @Override
     public ExitMessage newExitMessage(final Object result, final Message message) {
