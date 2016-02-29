cd17eb10b3e3142a1a0a30b74a736a25217262b9
Matt Sicker <boards@gmail.com>
2016-02-28 18:42:04 -0600

    Move OSGi JUnit helpers into junit package.

--- log4j-api/src/test/java/org/apache/logging/log4j/junit/BundleTestInfo.java
+++ log4j-api/src/test/java/org/apache/logging/log4j/junit/BundleTestInfo.java
@@ -14,7 +14,8 @@
  * See the license for the specific language governing permissions and
  * limitations under the license.
  */
-package org.apache.logging.log4j.osgi;
+
+package org.apache.logging.log4j.junit;
 
 import java.io.FileReader;
 import java.io.IOException;
