3761dba60f3f498f8691f59e772a2cb4d9be2928
ggregory <ggregory@apache.org>
2016-03-11 17:05:21 -0800

    Re-implement CompositeFilter to be backed by a Filter[] instead of an ArrayList<Filter> for performance. Internally, we now traverse the array as a primitive with [] instead of using a new Iterator each time the filters need to be traversed. Note: I have a use case where the internal list was always empty, which meant an iterator was always created, when for example, Logger.isTraceEnabled() was called. After a couple of million times calling isTraceEnabled(), it adds up. Now, the array is not traversed since it is since of length 0. This is means one less object generated per call to isTraceEnabled() and thrown away (the iterator) when a config has an empty Filters section (because child in it is commented out).

--- log4j-core/src/test/java/org/apache/logging/log4j/core/CustomLevelsWithFiltersTest.java
+++ log4j-core/src/test/java/org/apache/logging/log4j/core/CustomLevelsWithFiltersTest.java
@@ -59,10 +59,10 @@ public class CustomLevelsWithFiltersTest {
         assertNotNull(appender);
         final CompositeFilter compFilter = (CompositeFilter) appender.getFilter();
         assertNotNull(compFilter);
-        final List<Filter> filterList = compFilter.getFilters();
-        assertNotNull(filterList);
+        final Filter[] filters = compFilter.getFiltersArray();
+        assertNotNull(filters);
         boolean foundLevel = false;
-        for (final Filter filter : filterList) {
+        for (final Filter filter : filters) {
             final ThresholdFilter tFilter = (ThresholdFilter) filter;
             if (infom1Level.equals(tFilter.getLevel())) {
                 foundLevel = true;
