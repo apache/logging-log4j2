5d1c542a78751e259c5e06b1d43e35a3aa0129c8
rpopma <rpopma@apache.org>
2016-03-17 01:13:25 +1100

    LOG4J2-1278 added explicit cast to disambiguate which method is being called

--- log4j-core/src/main/java/org/apache/logging/log4j/core/Logger.java
+++ log4j-core/src/main/java/org/apache/logging/log4j/core/Logger.java
@@ -416,7 +416,7 @@ public class Logger extends AbstractLogger implements Supplier<LoggerConfig> {
         boolean filter(final Level level, final Marker marker, final String msg, final Throwable t) {
             final Filter filter = config.getFilter();
             if (filter != null) {
-                final Filter.Result r = filter.filter(logger, level, marker, msg, t);
+                final Filter.Result r = filter.filter(logger, level, marker, (Object) msg, t);
                 if (r != Filter.Result.NEUTRAL) {
                     return r == Filter.Result.ACCEPT;
                 }
