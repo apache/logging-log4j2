d90306fc46543b9340ddfb96aa5a6eb4b1a3a9eb
rpopma <rpopma@apache.org>
2016-03-25 01:27:03 +0900

    LOG4J2-639 fix to handle context.stop() correctly when using the tryPublish API

--- log4j-core/src/main/java/org/apache/logging/log4j/core/async/AsyncLoggerDisruptor.java
+++ log4j-core/src/main/java/org/apache/logging/log4j/core/async/AsyncLoggerDisruptor.java
@@ -186,7 +186,13 @@ class AsyncLoggerDisruptor {
     }
 
     public boolean tryPublish(final RingBufferLogEventTranslator translator) {
-        return disruptor.getRingBuffer().tryPublishEvent(translator);
+        // LOG4J2-639: catch NPE if disruptor field was set to null in stop()
+        try {
+            return disruptor.getRingBuffer().tryPublishEvent(translator);
+        } catch (final NullPointerException npe) {
+            LOGGER.warn("[{}] Ignoring log event after log4j was shut down.", contextName);
+            return false;
+        }
     }
 
     void enqueueLogMessageInfo(final RingBufferLogEventTranslator translator) {
