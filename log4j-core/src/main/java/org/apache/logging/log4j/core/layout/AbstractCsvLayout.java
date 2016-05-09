8e85826a24cd4e1439edb747de6070fee0af4972
ggregory <ggregory@apache.org>
2016-05-09 11:07:37 -0700

    Sort 1 method.

--- log4j-core/src/main/java/org/apache/logging/log4j/core/layout/AbstractCsvLayout.java
+++ log4j-core/src/main/java/org/apache/logging/log4j/core/layout/AbstractCsvLayout.java
@@ -35,15 +35,6 @@ public abstract class AbstractCsvLayout extends AbstractStringLayout {
     protected static final String DEFAULT_FORMAT = "Default";
     private static final String CONTENT_TYPE = "text/csv";
 
-    private final CSVFormat format;
-
-    protected AbstractCsvLayout(final Configuration config, final Charset charset, final CSVFormat csvFormat,
-            final String header, final String footer) {
-        super(config, charset, PatternLayout.createSerializer(config, null, header, null, null, false, false),
-                PatternLayout.createSerializer(config, null, footer, null, null, false, false));
-        this.format = csvFormat;
-    }
-
     protected static CSVFormat createFormat(final String format, final Character delimiter, final Character escape,
             final Character quote, final QuoteMode quoteMode, final String nullString, final String recordSeparator) {
         CSVFormat csvFormat = CSVFormat.valueOf(format);
@@ -66,6 +57,15 @@ public abstract class AbstractCsvLayout extends AbstractStringLayout {
             csvFormat = csvFormat.withRecordSeparator(recordSeparator);
         }
         return csvFormat;
+    }
+
+    private final CSVFormat format;
+
+    protected AbstractCsvLayout(final Configuration config, final Charset charset, final CSVFormat csvFormat,
+            final String header, final String footer) {
+        super(config, charset, PatternLayout.createSerializer(config, null, header, null, null, false, false),
+                PatternLayout.createSerializer(config, null, footer, null, null, false, false));
+        this.format = csvFormat;
     }
 
     @Override
