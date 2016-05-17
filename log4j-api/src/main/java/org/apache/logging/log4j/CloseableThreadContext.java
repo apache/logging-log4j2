7fca94f59224aceb99ee833d2758e5a6ae4650ca
rpopma <rpopma@apache.org>
2016-05-18 00:32:49 +0900

    LOG4J2-1348 applied latest patch where CloseableThreadContext returns an instance that allows chained method calls like CloseableThreadContext.put(key, value).put(anotherKey, anotherValue);

--- log4j-api/src/main/java/org/apache/logging/log4j/CloseableThreadContext.java
+++ log4j-api/src/main/java/org/apache/logging/log4j/CloseableThreadContext.java
@@ -17,9 +17,8 @@
 package org.apache.logging.log4j;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
-
-import org.apache.logging.log4j.util.Strings;
 
 /**
  * Adds entries to the {@link ThreadContext stack or map} and them removes them when the object is closed, e.g. as part
@@ -27,32 +26,32 @@ import org.apache.logging.log4j.util.Strings;
  *
  * @since 2.6
  */
-public class CloseableThreadContext implements AutoCloseable {
+public class CloseableThreadContext {
 
-    /**
-     * Pushes new diagnostic context information on to the Thread Context Stack. The information will be popped off when
-     * the instance is closed.
-     *
-     * @param message
-     *            The new diagnostic context information.
-     * @return a new instance that will back out the changes when closed.
-     */
-    public static CloseableThreadContext push(final String message) {
-        return new CloseableThreadContext(message);
+    private CloseableThreadContext() {
     }
 
     /**
      * Pushes new diagnostic context information on to the Thread Context Stack. The information will be popped off when
      * the instance is closed.
      *
-     * @param message
-     *            The new diagnostic context information.
-     * @param args
-     *            Parameters for the message.
+     * @param message The new diagnostic context information.
      * @return a new instance that will back out the changes when closed.
      */
-    public static CloseableThreadContext push(final String message, final Object... args) {
-        return new CloseableThreadContext(message, args);
+    public static CloseableThreadContext.Instance push(final String message) {
+        return new CloseableThreadContext.Instance().push(message);
+    }
+
+    /**
+     * Pushes new diagnostic context information on to the Thread Context Stack. The information will be popped off when
+     * the instance is closed.
+     *
+     * @param message The new diagnostic context information.
+     * @param args    Parameters for the message.
+     * @return a new instance that will back out the changes when closed.
+     */
+    public static CloseableThreadContext.Instance push(final String message, final Object... args) {
+        return new CloseableThreadContext.Instance().push(message, args);
     }
 
     /**
@@ -60,113 +59,101 @@ public class CloseableThreadContext implements AutoCloseable {
      * {@link ThreadContext} will be replaced with the supplied values, and restored back to their original values when
      * the instance is closed.
      *
-     * @param firstKey
-     *            The first key to be added
-     * @param firstValue
-     *            The first value to be added
-     * @param subsequentKeyValuePairs
-     *            Any subsequent key/value pairs to be added. Note: If the last key does not have a corresponding value
-     *            then an empty String will be used as a value.
+     * @param key   The  key to be added
+     * @param value The value to be added
      * @return a new instance that will back out the changes when closed.
      */
-    public static CloseableThreadContext put(final String firstKey, final String firstValue,
-            final String... subsequentKeyValuePairs) {
-        return new CloseableThreadContext(firstKey, firstValue, subsequentKeyValuePairs);
+    public static CloseableThreadContext.Instance put(final String key, final String value) {
+        return new CloseableThreadContext.Instance().put(key, value);
     }
 
-    private final boolean isStack;
+    public static class Instance implements AutoCloseable {
 
-    private final Map<String, String> oldValues = new HashMap<>();
+        private int pushCount = 0;
+        private final Map<String, String> originalValues = new HashMap<>();
 
-    /**
-     * Creates an instance of a {@code CloseableThreadContext} that pushes new diagnostic context information on to the
-     * Thread Context Stack. The information will be popped off when the instance is closed.
-     *
-     * @param message
-     *            The new diagnostic context information.
-     */
-    public CloseableThreadContext(final String message) {
-        this.isStack = true;
-        ThreadContext.push(message);
-    }
-
-    /**
-     * Creates an instance of a {@code CloseableThreadContext} that pushes new diagnostic context information on to the
-     * Thread Context Stack. The information will be popped off when the instance is closed.
-     *
-     * @param message
-     *            The new diagnostic context information.
-     * @param args
-     *            Parameters for the message.
-     */
-    public CloseableThreadContext(final String message, final Object... args) {
-        this.isStack = true;
-        ThreadContext.push(message, args);
-    }
-
-    /**
-     * Creates an instance of a {@code CloseableThreadContext} that populates the Thread Context Map with the supplied
-     * key/value pairs. Any existing keys in the ThreadContext will be replaced with the supplied values, and restored
-     * back to their original values when the instance is closed.
-     *
-     * @param firstKey
-     *            The first key to be added
-     * @param firstValue
-     *            The first value to be added
-     * @param subsequentKeyValuePairs
-     *            Any subsequent key/value pairs to be added. Note: If the last key does not have a corresponding value
-     *            then an empty String will be used as a value.
-     */
-    public CloseableThreadContext(final String firstKey, final String firstValue,
-            final String... subsequentKeyValuePairs) {
-        this.isStack = false;
-        storeAndSet(firstKey, firstValue);
-        for (int i = 0; i < subsequentKeyValuePairs.length; i += 2) {
-            final String key = subsequentKeyValuePairs[i];
-            final String value = (i + 1) < subsequentKeyValuePairs.length ? subsequentKeyValuePairs[i + 1] : Strings.EMPTY;
-            storeAndSet(key, value);
+        private Instance() {
         }
-    }
 
-    /**
-     * Removes the values from the {@link ThreadContext}.
-     * <p>
-     * If this {@code CloseableThreadContext} was added to the {@link ThreadContext} <em>stack</em>, then this will pop
-     * the diagnostic information off the stack.
-     * </p>
-     * <p>
-     * If the {@code CloseableThreadContext} was added to the {@link ThreadContext} <em>map</em>, then this will either
-     * remove the values that were added, or restore them to their original values it they already existed.
-     * </p>
-     */
-    @Override
-    public void close() {
-        if (this.isStack) {
+        /**
+         * Pushes new diagnostic context information on to the Thread Context Stack. The information will be popped off when
+         * the instance is closed.
+         *
+         * @param message The new diagnostic context information.
+         * @return the instance that will back out the changes when closed.
+         */
+        public Instance push(final String message) {
+            ThreadContext.push(message);
+            pushCount++;
+            return this;
+        }
+
+        /**
+         * Pushes new diagnostic context information on to the Thread Context Stack. The information will be popped off when
+         * the instance is closed.
+         *
+         * @param message The new diagnostic context information.
+         * @param args    Parameters for the message.
+         * @return the instance that will back out the changes when closed.
+         */
+        public Instance push(final String message, final Object[] args) {
+            ThreadContext.push(message, args);
+            pushCount++;
+            return this;
+        }
+
+        /**
+         * Populates the Thread Context Map with the supplied key/value pairs. Any existing keys in the
+         * {@link ThreadContext} will be replaced with the supplied values, and restored back to their original values when
+         * the instance is closed.
+         *
+         * @param key   The  key to be added
+         * @param value The value to be added
+         * @return the instance that will back out the changes when closed.
+         */
+        public Instance put(final String key, final String value) {
+            // If there are no existing values, a null will be stored as an old value
+            if (!originalValues.containsKey(key)) {
+                originalValues.put(key, ThreadContext.get(key));
+            }
+            ThreadContext.put(key, value);
+            return this;
+        }
+
+        /**
+         * Removes the values from the {@link ThreadContext}.
+         * <p>
+         * Values pushed to the {@link ThreadContext} <em>stack</em> will be popped off.
+         * </p>
+         * <p>
+         * Values put on the {@link ThreadContext} <em>map</em> will be removed, or restored to their original values it they already existed.
+         * </p>
+         */
+        @Override
+        public void close() {
             closeStack();
-        } else {
             closeMap();
         }
-    }
 
-    private void closeMap() {
-        for (final Map.Entry<String, String> entry : oldValues.entrySet()) {
-            // If the old value was null, remove it from the ThreadContext
-            if (null == entry.getValue()) {
-                ThreadContext.remove(entry.getKey());
-            } else {
-                ThreadContext.put(entry.getKey(), entry.getValue());
+        private void closeMap() {
+            for (Iterator<Map.Entry<String, String>> it = originalValues.entrySet().iterator(); it.hasNext(); ) {
+                final Map.Entry<String, String> entry = it.next();
+                final String key = entry.getKey();
+                final String originalValue = entry.getValue();
+                if (null == originalValue) {
+                    ThreadContext.remove(key);
+                } else {
+                    ThreadContext.put(key, originalValue);
+                }
+                it.remove();
             }
         }
-    }
 
-    private String closeStack() {
-        return ThreadContext.pop();
+        private void closeStack() {
+            for (int i = 0; i < pushCount; i++) {
+                ThreadContext.pop();
+            }
+            pushCount = 0;
+        }
     }
-
-    private void storeAndSet(final String key, final String value) {
-        // If there are no existing values, a null will be stored as an old value
-        oldValues.put(key, ThreadContext.get(key));
-        ThreadContext.put(key, value);
-    }
-
 }
