6d80ddf7ed92d65174b52fe946ae73d74f437d8b
rpopma <rpopma@apache.org>
2016-06-04 01:30:33 +0900

    LOG4J2-1409 Fixed ArrayIndexOutOfBoundsException that may occur in ReusableParameterizedMessage.

--- log4j-api/src/main/java/org/apache/logging/log4j/message/ReusableParameterizedMessage.java
+++ log4j-api/src/main/java/org/apache/logging/log4j/message/ReusableParameterizedMessage.java
@@ -17,7 +17,6 @@
 package org.apache.logging.log4j.message;
 
 import java.util.Arrays;
-import java.util.Objects;
 
 import org.apache.logging.log4j.util.PerformanceSensitive;
 
@@ -62,10 +61,32 @@ public class ReusableParameterizedMessage implements ReusableMessage {
         Object[] result;
         if (varargs == null) {
             result = params;
-            params = Objects.requireNonNull(emptyReplacement);
+            if (emptyReplacement.length >= 10) {
+                params = emptyReplacement;
+            } else {
+                // Bad replacement! Too small, may blow up future 10-arg messages.
+                if (argCount <= emptyReplacement.length) {
+                    // copy params into the specified replacement array and return that
+                    System.arraycopy(params, 0, emptyReplacement, 0, argCount);
+                    result = emptyReplacement;
+                } else {
+                    // replacement array is too small for current content and future content: discard it
+                    params = new Object[10];
+                }
+            }
         } else {
-            result = varargs;
-            varargs = Objects.requireNonNull(emptyReplacement);
+            // The returned array will be reused by the caller in future swapParameter() calls.
+            // Therefore we want to avoid returning arrays with less than 10 elements.
+            // If the vararg array is less than 10 params we just copy its content into the specified array
+            // and return it. This helps the caller to retain a reusable array of at least 10 elements.
+            if (argCount <= emptyReplacement.length) {
+                // copy params into the specified replacement array and return that
+                System.arraycopy(varargs, 0, emptyReplacement, 0, argCount);
+                result = emptyReplacement;
+            } else {
+                result = varargs;
+                varargs = emptyReplacement;
+            }
         }
         return result;
     }
