e70e31d8a66c28e07e80342d9c667a5f5b538881
rpopma <rpopma@apache.org>
2016-06-11 01:29:13 +0900

    LOG4J2-1415 Avoid calling toString() on auto-boxed primitive message parameters

--- log4j-api/src/main/java/org/apache/logging/log4j/message/ParameterFormatter.java
+++ log4j-api/src/main/java/org/apache/logging/log4j/message/ParameterFormatter.java
@@ -442,6 +442,27 @@ final class ParameterFormatter {
         } else if (o instanceof StringBuilderFormattable) {
             ((StringBuilderFormattable) o).formatTo(str);
             return true;
+        } else if (o instanceof Integer) { // LOG4J2-1415 unbox auto-boxed primitives to avoid calling toString()
+            str.append(((Integer) o).intValue());
+            return true;
+        } else if (o instanceof Long) {
+            str.append(((Long) o).longValue());
+            return true;
+        } else if (o instanceof Double) {
+            str.append(((Double) o).doubleValue());
+            return true;
+        } else if (o instanceof Boolean) {
+            str.append(((Boolean) o).booleanValue());
+            return true;
+        } else if (o instanceof Character) {
+            str.append(((Character) o).charValue());
+            return true;
+        } else if (o instanceof Short) {
+            str.append(((Short) o).shortValue());
+            return true;
+        } else if (o instanceof Float) {
+            str.append(((Float) o).floatValue());
+            return true;
         }
         return appendDate(o, str);
     }
