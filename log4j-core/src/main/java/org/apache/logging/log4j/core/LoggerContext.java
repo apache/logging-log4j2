0cf1df05e01ef37a41da167f84fc46e26d592c8f
rpopma <rpopma@apache.org>
2016-06-14 00:32:08 +0900

    LOG4J2-1419 add documentation and use better system property name for triggering stacktrace on LoggerContext start

--- log4j-core/src/main/java/org/apache/logging/log4j/core/LoggerContext.java
+++ log4j-core/src/main/java/org/apache/logging/log4j/core/LoggerContext.java
@@ -206,7 +206,7 @@ public class LoggerContext extends AbstractLifeCycle implements org.apache.loggi
     @Override
     public void start() {
         LOGGER.debug("Starting LoggerContext[name={}, {}]...", getName(), this);
-        if (PropertiesUtil.getProperties().getBooleanProperty("log4j.LoggerContext.trace.start", false)) {
+        if (PropertiesUtil.getProperties().getBooleanProperty("log4j.LoggerContext.stacktrace.on.start", false)) {
             LOGGER.debug("Stack trace to locate invoker",
                     new Exception("Not a real error, showing stack trace to locate invoker"));
         }
