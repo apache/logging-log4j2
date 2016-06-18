68e8ecb810adf46819b7181b45512a111cfd32b4
Matt Sicker <boards@gmail.com>
2016-06-17 19:07:59 -0500

    Make SpinPolicy configurable in DisruptorBlockingQueueFactory

--- log4j-core/src/main/java/org/apache/logging/log4j/core/async/DisruptorBlockingQueueFactory.java
+++ log4j-core/src/main/java/org/apache/logging/log4j/core/async/DisruptorBlockingQueueFactory.java
@@ -3,8 +3,10 @@ package org.apache.logging.log4j.core.async;
 import java.util.concurrent.BlockingQueue;
 
 import com.conversantmedia.util.concurrent.DisruptorBlockingQueue;
+import com.conversantmedia.util.concurrent.SpinPolicy;
 import org.apache.logging.log4j.core.config.Node;
 import org.apache.logging.log4j.core.config.plugins.Plugin;
+import org.apache.logging.log4j.core.config.plugins.PluginAttribute;
 import org.apache.logging.log4j.core.config.plugins.PluginFactory;
 
 /**
@@ -14,13 +16,22 @@ import org.apache.logging.log4j.core.config.plugins.PluginFactory;
  */
 @Plugin(name = "DisruptorBlockingQueue", category = Node.CATEGORY, elementType = BlockingQueueFactory.ELEMENT_TYPE)
 public class DisruptorBlockingQueueFactory<E> implements BlockingQueueFactory<E> {
+
+    private final SpinPolicy spinPolicy;
+
+    private DisruptorBlockingQueueFactory(final SpinPolicy spinPolicy) {
+        this.spinPolicy = spinPolicy;
+    }
+
     @Override
     public BlockingQueue<E> create(int capacity) {
-        return new DisruptorBlockingQueue<>(capacity);
+        return new DisruptorBlockingQueue<>(capacity, spinPolicy);
     }
 
     @PluginFactory
-    public static <E> DisruptorBlockingQueueFactory<E> createFactory() {
-        return new DisruptorBlockingQueueFactory<>();
+    public static <E> DisruptorBlockingQueueFactory<E> createFactory(
+        @PluginAttribute(value = "SpinPolicy", defaultString = "WAITING") final SpinPolicy spinPolicy
+    ) {
+        return new DisruptorBlockingQueueFactory<>(spinPolicy);
     }
 }
