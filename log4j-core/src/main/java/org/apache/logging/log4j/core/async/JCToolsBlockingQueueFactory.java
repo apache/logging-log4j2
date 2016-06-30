67d78505afac27f3366fbc6939aacef92bc373c7
Matt Sicker <boards@gmail.com>
2016-06-30 12:48:02 -0500

    Improve JCTools usage in AsyncAppender
    This is from a patch from Anthony Maire (reformatted and final'd) on LOG4J2-1439.
    
    * Adds configurable wait strategies.
    * Always keep some empty space in the queue to avoid the producer thread to suffer from false sharing when the queue is full.
    * Add a queue latency test to check performance without using AsyncAppender which is often hiding the queue performance issues.
    * Upgrade to JCTool 1.2.1.
    * Use spin wait strategy in perf tests to match other perf tests.

--- log4j-core/src/main/java/org/apache/logging/log4j/core/async/JCToolsBlockingQueueFactory.java
+++ log4j-core/src/main/java/org/apache/logging/log4j/core/async/JCToolsBlockingQueueFactory.java
@@ -7,6 +7,7 @@ import java.util.concurrent.locks.LockSupport;
 
 import org.apache.logging.log4j.core.config.Node;
 import org.apache.logging.log4j.core.config.plugins.Plugin;
+import org.apache.logging.log4j.core.config.plugins.PluginAttribute;
 import org.apache.logging.log4j.core.config.plugins.PluginFactory;
 import org.jctools.queues.MpscArrayQueue;
 
@@ -18,17 +19,21 @@ import org.jctools.queues.MpscArrayQueue;
 @Plugin(name = "JCToolsBlockingQueue", category = Node.CATEGORY, elementType = BlockingQueueFactory.ELEMENT_TYPE)
 public class JCToolsBlockingQueueFactory<E> implements BlockingQueueFactory<E> {
 
-    private JCToolsBlockingQueueFactory() {
+    private final WaitStrategy waitStrategy;
+
+    private JCToolsBlockingQueueFactory(final WaitStrategy waitStrategy) {
+        this.waitStrategy = waitStrategy;
     }
 
     @Override
     public BlockingQueue<E> create(final int capacity) {
-        return new MpscBlockingQueue<>(capacity);
+        return new MpscBlockingQueue<>(capacity, waitStrategy);
     }
 
     @PluginFactory
-    public static <E> JCToolsBlockingQueueFactory<E> createFactory() {
-        return new JCToolsBlockingQueueFactory<>();
+    public static <E> JCToolsBlockingQueueFactory<E> createFactory(
+        @PluginAttribute(value = "WaitStrategy", defaultString = "PARK") final WaitStrategy waitStrategy) {
+        return new JCToolsBlockingQueueFactory<>(waitStrategy);
     }
 
     /**
@@ -36,8 +41,11 @@ public class JCToolsBlockingQueueFactory<E> implements BlockingQueueFactory<E> {
      */
     private static final class MpscBlockingQueue<E> extends MpscArrayQueue<E> implements BlockingQueue<E> {
 
-        MpscBlockingQueue(final int capacity) {
+        private final JCToolsBlockingQueueFactory.WaitStrategy waitStrategy;
+
+        MpscBlockingQueue(final int capacity, final JCToolsBlockingQueueFactory.WaitStrategy waitStrategy) {
             super(capacity);
+            this.waitStrategy = waitStrategy;
         }
 
         @Override
@@ -49,29 +57,59 @@ public class JCToolsBlockingQueueFactory<E> implements BlockingQueueFactory<E> {
         public int drainTo(final Collection<? super E> c, final int maxElements) {
             return drain(new Consumer<E>() {
                 @Override
-                public void accept(E arg0) {
-                    c.add(arg0);
+                public void accept(E e) {
+                    c.add(e);
                 }
             }, maxElements);
         }
 
         @Override
         public boolean offer(final E e, final long timeout, final TimeUnit unit) throws InterruptedException {
-            // TODO Auto-generated method stub
-            return offer(e);
+            int idleCounter = 0;
+            final long timeoutNanos = System.nanoTime() + unit.toNanos(timeout);
+            do {
+                if (offer(e)) {
+                    return true;
+                } else if (System.nanoTime() - timeoutNanos > 0) {
+                    return false;
+                }
+                idleCounter = waitStrategy.idle(idleCounter);
+            } while (!Thread.interrupted()); //clear interrupted flag
+            throw new InterruptedException();
         }
 
         @Override
         public E poll(final long timeout, final TimeUnit unit) throws InterruptedException {
-            // TODO Auto-generated method stub
-            return poll();
+            int idleCounter = 0;
+            final long timeoutNanos = System.nanoTime() + unit.toNanos(timeout);
+            do {
+                E result = poll();
+                if (result != null) {
+                    return result;
+                } else if (System.nanoTime() - timeoutNanos > 0) {
+                    return null;
+                }
+                idleCounter = waitStrategy.idle(idleCounter);
+            } while (!Thread.interrupted()); //clear interrupted flag
+            throw new InterruptedException();
         }
 
         @Override
         public void put(final E e) throws InterruptedException {
-            while (!relaxedOffer(e)) {
-                LockSupport.parkNanos(1L);
-            }
+            int idleCounter = 0;
+            do {
+                if (offer(e)) {
+                    return;
+                }
+                idleCounter = waitStrategy.idle(idleCounter);
+            } while (!Thread.interrupted()); //clear interrupted flag
+            throw new InterruptedException();
+        }
+
+        @Override
+        public boolean offer(final E e) {
+            //keep 2 cache lines empty to avoid false sharing that will slow the consumer thread when queue is full.
+            return offerIfBelowThreshold(e, capacity() - 32);
         }
 
         @Override
@@ -81,13 +119,64 @@ public class JCToolsBlockingQueueFactory<E> implements BlockingQueueFactory<E> {
 
         @Override
         public E take() throws InterruptedException {
-            for (; ; ) {
-                final E result = poll();
+            int idleCounter = 100;
+            do {
+                final E result = relaxedPoll();
                 if (result != null) {
                     return result;
                 }
-                LockSupport.parkNanos(1L);
-            }
+                idleCounter = waitStrategy.idle(idleCounter);
+            } while (!Thread.interrupted()); //clear interrupted flag
+            throw new InterruptedException();
         }
     }
+
+    public enum WaitStrategy {
+        SPIN(new Idle() {
+            @Override
+            public int idle(final int idleCounter) {
+                return idleCounter + 1;
+            }
+        }),
+        YIELD(new Idle() {
+            @Override
+            public int idle(final int idleCounter) {
+                Thread.yield();
+                return idleCounter + 1;
+            }
+        }),
+        PARK(new Idle() {
+            @Override
+            public int idle(final int idleCounter) {
+                LockSupport.parkNanos(1L);
+                return idleCounter + 1;
+            }
+        }),
+        PROGRESSIVE(new Idle() {
+            @Override
+            public int idle(final int idleCounter) {
+                if (idleCounter > 200) {
+                    LockSupport.parkNanos(1L);
+                } else if (idleCounter > 100) {
+                    Thread.yield();
+                }
+                return idleCounter + 1;
+            }
+        });
+
+        private final Idle idle;
+
+        private int idle(final int idleCounter) {
+            return idle.idle(idleCounter);
+        }
+
+        WaitStrategy(final Idle idle) {
+            this.idle = idle;
+        }
+    }
+
+    private interface Idle {
+        int idle(int idleCounter);
+    }
+
 }
