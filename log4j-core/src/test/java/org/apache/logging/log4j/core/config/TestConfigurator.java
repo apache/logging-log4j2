db36b47488e082d925ffdb6887965a8e1c9b2c4f
Gary Gregory <ggregory@apache.org>
2016-07-19 20:04:21 -0700

    Test API org.apache.logging.log4j.core.config.Configurator.initialize(String, ClassLoader, URI).

--- log4j-core/src/test/java/org/apache/logging/log4j/core/config/TestConfigurator.java
+++ log4j-core/src/test/java/org/apache/logging/log4j/core/config/TestConfigurator.java
@@ -109,6 +109,22 @@ public class TestConfigurator {
     }
 
     @Test
+    public void testInitialize_Name_ClassLoader_URI() throws Exception {
+        ctx = Configurator.initialize("Test1", null, new File("target/test-classes/log4j2-config.xml").toURI());
+        LogManager.getLogger("org.apache.test.TestConfigurator");
+        Configuration config = ctx.getConfiguration();
+        assertNotNull("No configuration", config);
+        assertEquals("Incorrect Configuration.", CONFIG_NAME, config.getName());
+        final Map<String, Appender> map = config.getAppenders();
+        assertNotNull("Appenders map should not be null.", map);
+        assertThat(map, hasSize(greaterThan(0)));
+        assertThat("Wrong configuration", map, hasKey("List"));
+        Configurator.shutdown(ctx);
+        config = ctx.getConfiguration();
+        assertEquals("Unexpected Configuration.", NullConfiguration.NULL_NAME, config.getName());
+    }
+
+    @Test
     public void testFromStream() throws Exception {
         final File file = new File("target/test-classes/log4j2-config.xml");
         final InputStream is = new FileInputStream(file);
