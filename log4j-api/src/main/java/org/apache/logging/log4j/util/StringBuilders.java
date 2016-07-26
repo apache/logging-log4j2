6a3cee062277610d4983452d5e0b8e6d26c78e91
rpopma <rpopma@apache.org>
2016-07-27 01:26:21 +0900

    LOG4J2-1270 added utility method StringBuilders::appendValue(StringBuilder, Object) that avoids creating temporary objects while appending an Object to a StringBuilder

--- log4j-api/src/main/java/org/apache/logging/log4j/util/StringBuilders.java
+++ log4j-api/src/main/java/org/apache/logging/log4j/util/StringBuilders.java
@@ -27,7 +27,7 @@ public final class StringBuilders {
 
     /**
      * Appends in the following format: double quoted value.
-     * 
+     *
      * @param sb a string builder
      * @param value a value
      * @return {@code "value"}
@@ -38,7 +38,7 @@ public final class StringBuilders {
 
     /**
      * Appends in the following format: key=double quoted value.
-     * 
+     *
      * @param sb a string builder
      * @param entry a map entry
      * @return {@code key="value"}
@@ -49,7 +49,7 @@ public final class StringBuilders {
 
     /**
      * Appends in the following format: key=double quoted value.
-     * 
+     *
      * @param sb a string builder
      * @param key a key
      * @param value a value
@@ -59,4 +59,36 @@ public final class StringBuilders {
         return sb.append(key).append(Chars.EQ).append(Chars.DQUOTE).append(value).append(Chars.DQUOTE);
     }
 
+    /**
+     * Appends a text representation of the specified object to the specified StringBuilder,
+     * if possible without allocating temporary objects.
+     *
+     * @param stringBuilder the StringBuilder to append the value to
+     * @param obj the object whose text representation to append to the StringBuilder
+     */
+    public static void appendValue(final StringBuilder stringBuilder, final Object obj) {
+        if (obj == null || obj instanceof String) {
+            stringBuilder.append((String) obj);
+        } else if (obj instanceof StringBuilderFormattable) {
+            ((StringBuilderFormattable) obj).formatTo(stringBuilder);
+        } else if (obj instanceof CharSequence) {
+            stringBuilder.append((CharSequence) obj);
+        } else if (obj instanceof Integer) { // LOG4J2-1437 unbox auto-boxed primitives to avoid calling toString()
+            stringBuilder.append(((Integer) obj).intValue());
+        } else if (obj instanceof Long) {
+            stringBuilder.append(((Long) obj).longValue());
+        } else if (obj instanceof Double) {
+            stringBuilder.append(((Double) obj).doubleValue());
+        } else if (obj instanceof Boolean) {
+            stringBuilder.append(((Boolean) obj).booleanValue());
+        } else if (obj instanceof Character) {
+            stringBuilder.append(((Character) obj).charValue());
+        } else if (obj instanceof Short) {
+            stringBuilder.append(((Short) obj).shortValue());
+        } else if (obj instanceof Float) {
+            stringBuilder.append(((Float) obj).floatValue());
+        } else {
+            stringBuilder.append(obj);
+        }
+    }
 }
