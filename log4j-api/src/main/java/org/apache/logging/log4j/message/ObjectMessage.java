f313f93b840813e5aae90e8a5d0978a128fe3e8a
rpopma <rpopma@apache.org>
2016-07-27 01:26:54 +0900

    LOG4J2-1270 delegate to utility method StringBuilders::appendValue(StringBuilder, Object)

--- log4j-api/src/main/java/org/apache/logging/log4j/message/ObjectMessage.java
+++ log4j-api/src/main/java/org/apache/logging/log4j/message/ObjectMessage.java
@@ -22,6 +22,7 @@ import java.io.ObjectOutputStream;
 import java.io.Serializable;
 
 import org.apache.logging.log4j.util.StringBuilderFormattable;
+import org.apache.logging.log4j.util.StringBuilders;
 
 /**
  * Handles messages that contain an Object.
@@ -58,29 +59,7 @@ public class ObjectMessage implements Message, StringBuilderFormattable {
 
     @Override
     public void formatTo(final StringBuilder buffer) {
-        if (obj == null || obj instanceof String) {
-            buffer.append((String) obj);
-        } else if (obj instanceof StringBuilderFormattable) {
-            ((StringBuilderFormattable) obj).formatTo(buffer);
-        } else if (obj instanceof CharSequence) {
-            buffer.append((CharSequence) obj);
-        } else if (obj instanceof Integer) { // LOG4J2-1437 unbox auto-boxed primitives to avoid calling toString()
-            buffer.append(((Integer) obj).intValue());
-        } else if (obj instanceof Long) {
-            buffer.append(((Long) obj).longValue());
-        } else if (obj instanceof Double) {
-            buffer.append(((Double) obj).doubleValue());
-        } else if (obj instanceof Boolean) {
-            buffer.append(((Boolean) obj).booleanValue());
-        } else if (obj instanceof Character) {
-            buffer.append(((Character) obj).charValue());
-        } else if (obj instanceof Short) {
-            buffer.append(((Short) obj).shortValue());
-        } else if (obj instanceof Float) {
-            buffer.append(((Float) obj).floatValue());
-        } else {
-            buffer.append(obj);
-        }
+        StringBuilders.appendValue(buffer, obj);
     }
 
     /**
