61f706fc0a811c3184e941f2e8d1e012b5cbdb39
Gary Gregory <ggregory@apache.org>
2016-08-08 08:50:11 -0700

    [LOG4J2-1501] FileAppender should be able to create files lazily.

--- log4j-core/src/main/java/org/apache/logging/log4j/core/appender/FileAppender.java
+++ log4j-core/src/main/java/org/apache/logging/log4j/core/appender/FileAppender.java
@@ -39,7 +39,7 @@ import org.apache.logging.log4j.core.util.Integers;
 @Plugin(name = "File", category = "Core", elementType = "appender", printObject = true)
 public final class FileAppender extends AbstractOutputStreamAppender<FileManager> {
 
-    private static final int DEFAULT_BUFFER_SIZE = 8192;
+    static final int DEFAULT_BUFFER_SIZE = 8192;
     private final String fileName;
     private final Advertiser advertiser;
     private Object advertisement;
@@ -96,8 +96,9 @@ public final class FileAppender extends AbstractOutputStreamAppender<FileManager
      * @param advertiseUri The advertised URI which can be used to retrieve the file contents.
      * @param config The Configuration
      * @return The FileAppender.
+     * @deprecated Use {@link #createAppender(String, boolean, boolean, String, String, String, boolean, String, Layout<? extends Serializable>, Filter, String, String, boolean, Configuration)}
      */
-    @PluginFactory
+    @Deprecated
     public static FileAppender createAppender(
             // @formatter:off
             @PluginAttribute("fileName") final String fileName,
@@ -144,13 +145,83 @@ public final class FileAppender extends AbstractOutputStreamAppender<FileManager
             layout = PatternLayout.createDefaultLayout();
         }
 
-        final FileManager manager = FileManager.getFileManager(fileName, isAppend, isLocking, isBuffered, advertiseUri,
-                layout, bufferSize, isFlush);
+        final FileManager manager = FileManager.getFileManager(fileName, isAppend, isLocking, isBuffered, false,
+                advertiseUri, layout, bufferSize, isFlush);
         if (manager == null) {
             return null;
         }
 
         return new FileAppender(name, layout, filter, manager, fileName, ignoreExceptions, !isBuffered || isFlush,
                 isAdvertise ? config.getAdvertiser() : null);
+    }
+
+    /**
+     * Create a File Appender.
+     * @param fileName The name and path of the file.
+     * @param append "True" if the file should be appended to, "false" if it should be overwritten.
+     * The default is "true".
+     * @param locking "True" if the file should be locked. The default is "false".
+     * @param name The name of the Appender.
+     * @param immediateFlush "true" if the contents should be flushed on every write, "false" otherwise. The default
+     * is "true".
+     * @param ignoreExceptions If {@code "true"} (default) exceptions encountered when appending events are logged; otherwise
+     *               they are propagated to the caller.
+     * @param bufferedIo "true" if I/O should be buffered, "false" otherwise. The default is "true".
+     * @param bufferSize buffer size for buffered IO (default is 8192).
+     * @param layout The layout to use to format the event. If no layout is provided the default PatternLayout
+     * will be used.
+     * @param filter The filter, if any, to use.
+     * @param advertise "true" if the appender configuration should be advertised, "false" otherwise.
+     * @param advertiseUri The advertised URI which can be used to retrieve the file contents.
+     * @param lazyCreate true if you want to lazy-create the file (a.k.a. on-demand.)
+     * @param config The Configuration
+     * @return The FileAppender.
+     * @since 2.7
+     */
+    @PluginFactory
+    public static FileAppender createAppender(
+            // @formatter:off
+            @PluginAttribute("fileName") final String fileName,
+            @PluginAttribute(value = "append", defaultBoolean = true) final boolean append,
+            @PluginAttribute("locking") final boolean locking,
+            @PluginAttribute("name") final String name,
+            @PluginAttribute(value = "immediateFlush", defaultBoolean = true) final boolean immediateFlush,
+            @PluginAttribute(value = "ignoreExceptions", defaultBoolean = true) final boolean ignoreExceptions,
+            @PluginAttribute(value = "bufferedIo", defaultBoolean = true) boolean bufferedIo,
+            @PluginAttribute(value = "bufferSize", defaultInt = DEFAULT_BUFFER_SIZE) final int bufferSize,
+            @PluginElement("Layout") Layout<? extends Serializable> layout,
+            @PluginElement("Filter") final Filter filter,
+            @PluginAttribute("advertise") final boolean advertise,
+            @PluginAttribute("advertiseUri") final String advertiseUri,
+            @PluginAttribute("lazyCreate") final boolean lazyCreate,
+            @PluginConfiguration final Configuration config) {
+             // @formatter:on
+        if (locking && bufferedIo) {
+            LOGGER.warn("Locking and buffering are mutually exclusive. No buffering will occur for {}", fileName);
+            bufferedIo = false;
+        }
+        if (!bufferedIo && bufferSize > 0) {
+            LOGGER.warn("The bufferSize is set to {} but bufferedIo is not true: {}", bufferSize, bufferedIo);
+        }
+        if (name == null) {
+            LOGGER.error("No name provided for FileAppender");
+            return null;
+        }
+        if (fileName == null) {
+            LOGGER.error("No filename provided for FileAppender with name {}", name);
+            return null;
+        }
+        if (layout == null) {
+            layout = PatternLayout.createDefaultLayout();
+        }
+
+        final FileManager manager = FileManager.getFileManager(fileName, append, locking, bufferedIo, lazyCreate,
+                advertiseUri, layout, bufferSize, immediateFlush);
+        if (manager == null) {
+            return null;
+        }
+
+        return new FileAppender(name, layout, filter, manager, fileName, ignoreExceptions, !bufferedIo || immediateFlush,
+                advertise ? config.getAdvertiser() : null);
     }
 }
