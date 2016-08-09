d4dca3f27643053ba53651b5e446fcf44157975f
Gary Gregory <ggregory@apache.org>
2016-08-08 17:19:42 -0700

    (GC) We know the length in advance, so use it.

--- log4j-core/src/main/java/org/apache/logging/log4j/core/config/AppendersPlugin.java
+++ log4j-core/src/main/java/org/apache/logging/log4j/core/config/AppendersPlugin.java
@@ -42,7 +42,7 @@ public final class AppendersPlugin {
     public static ConcurrentMap<String, Appender> createAppenders(
                 @PluginElement("Appenders") final Appender[] appenders) {
 
-        final ConcurrentMap<String, Appender> map =  new ConcurrentHashMap<>();
+        final ConcurrentMap<String, Appender> map =  new ConcurrentHashMap<>(appenders.length);
 
         for (final Appender appender : appenders) {
             map.put(appender.getName(), appender);
