5feca684bd8b003fdeeb7a7d767b29d8debb2f63
Gary Gregory <ggregory@apache.org>
2016-08-14 19:08:21 -0700

    Sort members.

--- log4j-api/src/main/java/org/apache/logging/log4j/spi/ThreadContextMap.java
+++ log4j-api/src/main/java/org/apache/logging/log4j/spi/ThreadContextMap.java
@@ -24,34 +24,6 @@ import java.util.Map;
 public interface ThreadContextMap {
     
     /**
-     * Puts a context value (the <code>o</code> parameter) as identified
-     * with the <code>key</code> parameter into the current thread's
-     * context map.
-     *
-     * <p>If the current thread does not have a context map it is
-     * created as a side effect.</p>
-     * @param key The key name.
-     * @param value The key value.
-     */
-    void put(final String key, final String value);
-
-    /**
-     * Gets the context identified by the <code>key</code> parameter.
-     *
-     * <p>This method has no side effects.</p>
-     * @param key The key to locate.
-     * @return The value associated with the key or null.
-     */
-    String get(final String key);
-
-    /**
-     * Removes the the context identified by the <code>key</code>
-     * parameter.
-     * @param key The key to remove.
-     */
-    void remove(final String key);
-
-    /**
      * Clears the context.
      */
     void clear();
@@ -62,6 +34,15 @@ public interface ThreadContextMap {
      * @return True if the key is in the context, false otherwise.
      */
     boolean containsKey(final String key);
+
+    /**
+     * Gets the context identified by the <code>key</code> parameter.
+     *
+     * <p>This method has no side effects.</p>
+     * @param key The key to locate.
+     * @return The value associated with the key or null.
+     */
+    String get(final String key);
 
     /**
      * Gets a non-{@code null} mutable copy of current thread's context Map.
@@ -80,4 +61,23 @@ public interface ThreadContextMap {
      * @return true if the Map is empty, false otherwise.
      */
     boolean isEmpty();
+
+    /**
+     * Puts a context value (the <code>o</code> parameter) as identified
+     * with the <code>key</code> parameter into the current thread's
+     * context map.
+     *
+     * <p>If the current thread does not have a context map it is
+     * created as a side effect.</p>
+     * @param key The key name.
+     * @param value The key value.
+     */
+    void put(final String key, final String value);
+
+    /**
+     * Removes the the context identified by the <code>key</code>
+     * parameter.
+     * @param key The key to remove.
+     */
+    void remove(final String key);
 }
