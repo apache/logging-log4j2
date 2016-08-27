736c8a557b3949f670980d1034738c1de7e55e4e
rpopma <rpopma@apache.org>
2016-08-27 16:54:37 +0900

    LOG4J2-1349 added CopyOnWrite marker interface

---
+++ log4j-api/src/main/java/org/apache/logging/log4j/spi/CopyOnWrite.java
@@ -0,0 +1,9 @@
+package org.apache.logging.log4j.spi;
+
+/**
+ * Marker interface indicating that the implementing class is a copy-on-write data structure.
+ *
+ * @since 2.7
+ */
+public interface CopyOnWrite {
+}
