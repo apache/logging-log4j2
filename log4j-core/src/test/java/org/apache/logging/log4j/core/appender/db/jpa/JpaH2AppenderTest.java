94072a1ec7802b493e1caeb57fd05b29df288afc
Gary Gregory <ggregory@apache.org>
2016-08-28 13:34:26 -0700

    Use try-with-resources for JDBC resources.

--- log4j-core/src/test/java/org/apache/logging/log4j/core/appender/db/jpa/JpaH2AppenderTest.java
+++ log4j-core/src/test/java/org/apache/logging/log4j/core/appender/db/jpa/JpaH2AppenderTest.java
@@ -35,21 +35,19 @@ public class JpaH2AppenderTest extends AbstractJpaAppenderTest {
     protected Connection setUpConnection() throws SQLException {
         final Connection connection = DriverManager.getConnection("jdbc:h2:mem:Log4j", USER_ID, PASSWORD);
 
-        Statement statement = connection.createStatement();
-        statement.executeUpdate("CREATE TABLE jpaBaseLogEntry ( " +
-                "id INTEGER IDENTITY, eventDate DATETIME, level NVARCHAR(10), logger NVARCHAR(255), " +
-                "message NVARCHAR(1024), exception NVARCHAR(1048576)" +
-                " )");
-        statement.close();
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate("CREATE TABLE jpaBaseLogEntry ( "
+                    + "id INTEGER IDENTITY, eventDate DATETIME, level NVARCHAR(10), logger NVARCHAR(255), "
+                    + "message NVARCHAR(1024), exception NVARCHAR(1048576)" + " )");
+        }
 
-        statement = connection.createStatement();
-        statement.executeUpdate("CREATE TABLE jpaBasicLogEntry ( " +
-                "id INTEGER IDENTITY, timemillis BIGINT, nanoTime BIGINT, level NVARCHAR(10), loggerName NVARCHAR(255), " +
-                "message NVARCHAR(1024), thrown NVARCHAR(1048576), contextMapJson NVARCHAR(1048576)," +
-                "loggerFQCN NVARCHAR(1024), contextStack NVARCHAR(1048576), marker NVARCHAR(255), source NVARCHAR(2048)," +
-                "threadId BIGINT, threadName NVARCHAR(255), threadPriority INTEGER" +
-                " )");
-        statement.close();
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate("CREATE TABLE jpaBasicLogEntry ( "
+                    + "id INTEGER IDENTITY, timemillis BIGINT, nanoTime BIGINT, level NVARCHAR(10), loggerName NVARCHAR(255), "
+                    + "message NVARCHAR(1024), thrown NVARCHAR(1048576), contextMapJson NVARCHAR(1048576),"
+                    + "loggerFQCN NVARCHAR(1024), contextStack NVARCHAR(1048576), marker NVARCHAR(255), source NVARCHAR(2048),"
+                    + "threadId BIGINT, threadName NVARCHAR(255), threadPriority INTEGER" + " )");
+        }
 
         return connection;
     }
