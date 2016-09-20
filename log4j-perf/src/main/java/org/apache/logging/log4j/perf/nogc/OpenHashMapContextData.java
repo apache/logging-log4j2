ce7e4983999799c22feedef457df60c7e6e8e31a
Gary Gregory <ggregory@apache.org>
2016-09-19 22:21:17 -0700

    Always use a {} block.

--- log4j-perf/src/main/java/org/apache/logging/log4j/perf/nogc/OpenHashMapContextData.java
+++ log4j-perf/src/main/java/org/apache/logging/log4j/perf/nogc/OpenHashMapContextData.java
@@ -651,8 +651,9 @@ public class OpenHashMapContextData<K, V> implements MutableContextData, ThreadC
      */
     public boolean trim(final int n) {
         final int l = HashCommon.nextPowerOfTwo((int) Math.ceil(n / loadFactor));
-        if (l >= n || size > HashCommon.maxFill(l, loadFactor))
-            return true;
+        if (l >= n || size > HashCommon.maxFill(l, loadFactor)) {
+			return true;
+		}
         try {
             rehash(l);
         } catch (OutOfMemoryError cantDoIt) { // unusual to catch OOME but in this case appropriate
