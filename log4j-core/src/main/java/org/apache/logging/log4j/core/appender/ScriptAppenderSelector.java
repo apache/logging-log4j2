33765882bddfd34f8d2913612d3bc2bda9f35e40
Gary Gregory <ggregory@apache.org>
2016-09-20 11:56:23 -0700

    [LOG4J2-1597] Fix NPEs.

--- log4j-core/src/main/java/org/apache/logging/log4j/core/appender/ScriptAppenderSelector.java
+++ log4j-core/src/main/java/org/apache/logging/log4j/core/appender/ScriptAppenderSelector.java
@@ -63,15 +63,19 @@ public class ScriptAppenderSelector extends AbstractAppender {
         public Appender build() {
             if (name == null) {
                 LOGGER.error("Name missing.");
+                return null;
             }
             if (script == null) {
                 LOGGER.error("Script missing for ScriptAppenderSelector appender {}", name);
+                return null;
             }
             if (appenderSet == null) {
                 LOGGER.error("AppenderSet missing for ScriptAppenderSelector appender {}", name);
+                return null;
             }
             if (configuration == null) {
                 LOGGER.error("Configuration missing for ScriptAppenderSelector appender {}", name);
+                return null;
             }
             final ScriptManager scriptManager = configuration.getScriptManager();
             scriptManager.addScript(script);
