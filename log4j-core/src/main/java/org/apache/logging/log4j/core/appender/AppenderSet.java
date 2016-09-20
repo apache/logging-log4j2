53ad0d7d32a8f042220c7be4744e3c0e5819c6a9
Gary Gregory <ggregory@apache.org>
2016-09-20 12:11:08 -0700

    [LOG4J2-1597] Fix FindBugs warnings.

--- log4j-core/src/main/java/org/apache/logging/log4j/core/appender/AppenderSet.java
+++ log4j-core/src/main/java/org/apache/logging/log4j/core/appender/AppenderSet.java
@@ -48,18 +48,23 @@ public class AppenderSet {
         @Override
         public AppenderSet build() {
             if (configuration == null) {
-                LOGGER.error("Configuration is missing from AppenderSet");
+                LOGGER.error("Configuration is missing from AppenderSet {}", this);
+                return null;                
             }
-            final List<Node> children = node.getChildren();
-            final Map<String, Node> map = new HashMap<>(node == null ? 0 : children.size());
-            if (children == null) {
-                LOGGER.error("No child node in the AppenderSet {}", this);
+            if (node == null) {
+                LOGGER.error("No node in AppenderSet {}", this);
                 return null;
             }
+            final List<Node> children = node.getChildren();
+            if (children == null) {
+                LOGGER.error("No children node in AppenderSet {}", this);
+                return null;
+            }
+            final Map<String, Node> map = new HashMap<>(children.size());
             for (final Node childNode : children) {
                 final String key = childNode.getAttributes().get("name");
                 if (key == null) {
-                    LOGGER.error("The attribute 'name' is missing from from the node {} in the AppenderSet {}",
+                    LOGGER.error("The attribute 'name' is missing from from the node {} in AppenderSet {}",
                             childNode, children);
                 } else {
                     map.put(key, childNode);
