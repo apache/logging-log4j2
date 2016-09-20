0aae2f16ae1c08230c4c428d1f451a8368a9d2a5
Gary Gregory <ggregory@apache.org>
2016-09-20 12:12:08 -0700

    [LOG4J2-1600] NPE due to org.apache.logging.log4j.core.layout.MarkerPatternSelector.createSelector(PatternMatch[], String, boolean, boolean, Configuration).

--- log4j-core/src/main/java/org/apache/logging/log4j/core/layout/MarkerPatternSelector.java
+++ log4j-core/src/main/java/org/apache/logging/log4j/core/layout/MarkerPatternSelector.java
@@ -98,7 +98,8 @@ public class MarkerPatternSelector implements PatternSelector {
             defaultPattern = PatternLayout.DEFAULT_CONVERSION_PATTERN;
         }
         if (properties == null || properties.length == 0) {
-            LOGGER.warn("No marker patterns were provided");
+            LOGGER.warn("No marker patterns were provided with PatternMatch");
+            return null;
         }
         return new MarkerPatternSelector(properties, defaultPattern, alwaysWriteExceptions,
                 noConsoleNoAnsi, config);
