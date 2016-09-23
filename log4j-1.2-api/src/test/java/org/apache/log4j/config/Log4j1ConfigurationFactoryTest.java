e58fab172feb3ad828f0b3fa7c399794088a2f7a
Gary Gregory <ggregory@apache.org>
2016-09-23 15:55:30 -0700

    Better assert message.

--- log4j-1.2-api/src/test/java/org/apache/log4j/config/Log4j1ConfigurationFactoryTest.java
+++ log4j-1.2-api/src/test/java/org/apache/log4j/config/Log4j1ConfigurationFactoryTest.java
@@ -47,19 +47,21 @@ import org.junit.Test;
 
 public class Log4j1ConfigurationFactoryTest {
 
-	private Layout<?> testConsole(final String configResource) throws Exception {
-		final Configuration configuration = configure(configResource);
-		final ConsoleAppender appender = configuration.getAppender("Console");
-		assertNotNull(appender);
-		assertEquals(Target.SYSTEM_ERR, appender.getTarget());
-		//
-		final LoggerConfig loggerConfig = configuration.getLoggerConfig("com.example.foo");
-		assertNotNull(loggerConfig);
-		assertEquals(Level.DEBUG, loggerConfig.getLevel());
-		configuration.start();
-		configuration.stop();
-		return appender.getLayout();
-	}
+    private Layout<?> testConsole(final String configResource) throws Exception {
+        final Configuration configuration = configure(configResource);
+        final String name = "Console";
+        final ConsoleAppender appender = configuration.getAppender(name);
+        assertNotNull("Missing appender '" + name + "' in configuration " + configResource + " → " + configuration,
+                appender);
+        assertEquals(Target.SYSTEM_ERR, appender.getTarget());
+        //
+        final LoggerConfig loggerConfig = configuration.getLoggerConfig("com.example.foo");
+        assertNotNull(loggerConfig);
+        assertEquals(Level.DEBUG, loggerConfig.getLevel());
+        configuration.start();
+        configuration.stop();
+        return appender.getLayout();
+    }
 
 	private Layout<?> testFile(final String configResource) throws Exception {
 		final Configuration configuration = configure(configResource);
