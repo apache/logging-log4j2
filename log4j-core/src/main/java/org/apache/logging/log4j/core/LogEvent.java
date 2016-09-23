49321dd3d620035c39448a050654b47db95f529b
rpopma <rpopma@apache.org>
2016-09-23 17:11:54 +0900

    LOG4J2-1010 better javadoc for LogEvent::getContextData

--- log4j-core/src/main/java/org/apache/logging/log4j/core/LogEvent.java
+++ log4j-core/src/main/java/org/apache/logging/log4j/core/LogEvent.java
@@ -59,7 +59,15 @@ public interface LogEvent extends Serializable {
 
     /**
      * Returns the {@code ReadOnlyStringMap} object holding context data key-value pairs.
+     * <p>
+     * Context data (also known as Mapped Diagnostic Context or MDC) is data that is set by the application to be
+     * included in all subsequent log events. The default source for context data is the {@link ThreadContext},
+     * but users can configure a custom {@link ContextDataInjector} to obtain key-value pairs from any arbitrary
+     * source.
+     *
      * @return the {@code ReadOnlyStringMap} object holding context data key-value pairs
+     * @see ContextDataInjector
+     * @see ThreadContext
      * @since 2.7
      */
     ReadOnlyStringMap getContextData();
