d16716a58e6bba87f7874f23dfc43db41edbf1b6
rpopma <rpopma@apache.org>
2016-09-24 22:37:53 +0900

    LOG4J2-1611 productionize JdkMapAdapterStringMap (null checks, toString/equals/hashCode); allow mutation; add unit tests

--- log4j-core/src/main/java/org/apache/logging/log4j/core/impl/JdkMapAdapterStringMap.java
+++ log4j-core/src/main/java/org/apache/logging/log4j/core/impl/JdkMapAdapterStringMap.java
@@ -16,7 +16,9 @@
  */
 package org.apache.logging.log4j.core.impl;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 import org.apache.logging.log4j.util.BiConsumer;
 import org.apache.logging.log4j.util.ReadOnlyStringMap;
@@ -28,15 +30,28 @@ import org.apache.logging.log4j.util.TriConsumer;
  */
 class JdkMapAdapterStringMap implements StringMap {
     private static final long serialVersionUID = -7348247784983193612L;
-    private Map<String, String> map;
+    private static final String FROZEN = "Frozen collection cannot be modified";
+
+    private final Map<String, String> map;
+    private boolean immutable = false;
+
+    public JdkMapAdapterStringMap() {
+        this(new HashMap<String, String>());
+    }
 
     public JdkMapAdapterStringMap(final Map<String, String> map) {
-        this.map = map;
+        this.map = Objects.requireNonNull(map, "map");
     }
 
     @Override
     public Map<String, String> toMap() {
         return map;
+    }
+
+    private void assertNotFrozen() {
+        if (immutable) {
+            throw new UnsupportedOperationException(FROZEN);
+        }
     }
 
     @Override
@@ -78,34 +93,70 @@ class JdkMapAdapterStringMap implements StringMap {
 
     @Override
     public void clear() {
-        fail();
+        if (map.isEmpty()) {
+            return;
+        }
+        assertNotFrozen();
+        map.clear();
     }
 
     @Override
     public void freeze() {
+        immutable = true;
     }
 
     @Override
     public boolean isFrozen() {
-        return true;
+        return immutable;
     }
 
     @Override
     public void putAll(final ReadOnlyStringMap source) {
-        fail();
+        assertNotFrozen();
+        source.forEach(PUT_ALL, map);
     }
+
+    private static TriConsumer<String, String, Map<String, String>> PUT_ALL = new TriConsumer<String, String, Map<String, String>>() {
+        @Override
+        public void accept(final String key, final String value, final Map<String, String> stringStringMap) {
+            stringStringMap.put(key, value);
+        }
+    };
 
     @Override
     public void putValue(final String key, final Object value) {
-        fail();
+        assertNotFrozen();
+        map.put(key, value == null ? null : String.valueOf(value));
     }
 
     @Override
     public void remove(final String key) {
-        fail();
+        if (!map.containsKey(key)) {
+            return;
+        }
+        assertNotFrozen();
+        map.remove(key);
     }
 
-    private void fail() {
-        throw new UnsupportedOperationException("This is a read-only data structure");
+    @Override
+    public String toString() {
+        return map.toString();
+    }
+
+    @Override
+    public boolean equals(final Object object) {
+        if (object == this) {
+            return true;
+        }
+        if (!(object instanceof JdkMapAdapterStringMap)) {
+            return false;
+        }
+        JdkMapAdapterStringMap other = (JdkMapAdapterStringMap) object;
+        return map.equals(other.map) && immutable == other.immutable;
+    }
+
+    @Override
+    public int hashCode() {
+        return map.hashCode() + (immutable ? 31 : 0);
     }
 }
