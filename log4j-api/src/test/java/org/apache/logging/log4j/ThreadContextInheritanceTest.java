60c38fbd3bec7a1f89b848eac7c86f423c22fcb6
prabh.simran <prabh@codenation.co.in>
2016-09-28 11:12:45 +0400

    Duplicate Code refactoring

--- log4j-api/src/test/java/org/apache/logging/log4j/ThreadContextInheritanceTest.java
+++ log4j-api/src/test/java/org/apache/logging/log4j/ThreadContextInheritanceTest.java
@@ -29,13 +29,13 @@ import static org.junit.Assert.*;
  * Tests {@link ThreadContext}.
  */
 public class ThreadContextInheritanceTest {
-    
+
     @BeforeClass
     public static void setupClass() {
         System.setProperty(DefaultThreadContextMap.INHERITABLE_MAP, "true");
         ThreadContext.init();
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
         System.clearProperty(DefaultThreadContextMap.INHERITABLE_MAP);
@@ -80,94 +80,42 @@ public class ThreadContextInheritanceTest {
 
     @Test
     public void perfTest() throws Exception {
-        ThreadContext.clearMap();
-        final Timer complete = new Timer("ThreadContextTest");
-        complete.start();
-        ThreadContext.put("Var1", "value 1");
-        ThreadContext.put("Var2", "value 2");
-        ThreadContext.put("Var3", "value 3");
-        ThreadContext.put("Var4", "value 4");
-        ThreadContext.put("Var5", "value 5");
-        ThreadContext.put("Var6", "value 6");
-        ThreadContext.put("Var7", "value 7");
-        ThreadContext.put("Var8", "value 8");
-        ThreadContext.put("Var9", "value 9");
-        ThreadContext.put("Var10", "value 10");
-        final int loopCount = 1000000;
-        final Timer timer = new Timer("ThreadContextCopy", loopCount);
-        timer.start();
-        for (int i = 0; i < loopCount; ++i) {
-            final Map<String, String> map = ThreadContext.getImmutableContext();
-            assertNotNull(map);
-        }
-        timer.stop();
-        complete.stop();
-        System.out.println(timer.toString());
-        System.out.println(complete.toString());
+        UtilityClass.perfTest();
     }
 
     @Test
     public void testGetContextReturnsEmptyMapIfEmpty() {
-        ThreadContext.clearMap();
-        assertTrue(ThreadContext.getContext().isEmpty());
+        UtilityClass.testGetContextReturnsEmptyMapIfEmpty();
     }
 
     @Test
     public void testGetContextReturnsMutableCopy() {
-        ThreadContext.clearMap();
-        final Map<String, String> map1 = ThreadContext.getContext();
-        assertTrue(map1.isEmpty());
-        map1.put("K", "val"); // no error
-        assertEquals("val", map1.get("K"));
-
-        // adding to copy does not affect thread context map
-        assertTrue(ThreadContext.getContext().isEmpty());
-
-        ThreadContext.put("key", "val2");
-        final Map<String, String> map2 = ThreadContext.getContext();
-        assertEquals(1, map2.size());
-        assertEquals("val2", map2.get("key"));
-        map2.put("K", "val"); // no error
-        assertEquals("val", map2.get("K"));
-
-        // first copy is not affected
-        assertNotSame(map1, map2);
-        assertEquals(1, map1.size());
+        UtilityClass.testGetContextReturnsMutableCopy();
     }
 
     @Test
     public void testGetImmutableContextReturnsEmptyMapIfEmpty() {
-        ThreadContext.clearMap();
-        assertTrue(ThreadContext.getImmutableContext().isEmpty());
+        UtilityClass.testGetImmutableContextReturnsEmptyMapIfEmpty();
     }
 
     @Test(expected = UnsupportedOperationException.class)
     public void testGetImmutableContextReturnsImmutableMapIfNonEmpty() {
-        ThreadContext.clearMap();
-        ThreadContext.put("key", "val");
-        final Map<String, String> immutable = ThreadContext.getImmutableContext();
-        immutable.put("otherkey", "otherval");
+        UtilityClass.testGetImmutableContextReturnsImmutableMapIfNonEmpty();
     }
 
     @Test(expected = UnsupportedOperationException.class)
     public void testGetImmutableContextReturnsImmutableMapIfEmpty() {
-        ThreadContext.clearMap();
-        final Map<String, String> immutable = ThreadContext.getImmutableContext();
-        immutable.put("otherkey", "otherval");
+        UtilityClass.testGetImmutableContextReturnsImmutableMapIfEmpty();
     }
 
     @Test
     public void testGetImmutableStackReturnsEmptyStackIfEmpty() {
-        ThreadContext.clearStack();
-        assertTrue(ThreadContext.getImmutableStack().asList().isEmpty());
+        UtilityClass.testGetImmutableStackReturnsEmptyStackIfEmpty();
     }
 
     @Test
     public void testPut() {
-        ThreadContext.clearMap();
-        assertNull(ThreadContext.get("testKey"));
-        ThreadContext.put("testKey", "testValue");
-        assertEquals("testValue", ThreadContext.get("testKey"));
+        UtilityClass.testPut();
     }
 
     @Test
