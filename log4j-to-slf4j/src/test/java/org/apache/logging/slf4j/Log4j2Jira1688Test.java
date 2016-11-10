abf29afc4a1cb7eda58ce1169f14933d1f4e48a8
Gary Gregory <ggregory@apache.org>
2016-11-10 13:34:38 -0800

    Add comments.

--- log4j-to-slf4j/src/test/java/org/apache/logging/slf4j/Log4j2Jira1688Test.java
+++ log4j-to-slf4j/src/test/java/org/apache/logging/slf4j/Log4j2Jira1688Test.java
@@ -37,7 +37,7 @@ public class Log4j2Jira1688Test {
         final Object[] args = createArray(limit);
         final Object[] originalArgs = Arrays.copyOf(args, args.length);
 
-        // System.out.println("args " + Arrays.toString(args));
+        System.out.println("args " + Arrays.toString(args));
 
         // Logger definition
         final String someFormat = "test {}";
@@ -45,12 +45,14 @@ public class Log4j2Jira1688Test {
 
         // First logging of args
         logger.error(someFormat, args); // Only the first element (args[0]) of args will be logged - why?
+        // GG: because the pattern {} picks up the 1 st argument, not the whole array
         Assert.assertArrayEquals(originalArgs, args);
-        // System.out.println("args are still ok: " + Arrays.toString(args));
+        System.out.println("args are still ok: " + Arrays.toString(args));
 
         // Bug: The second logging of args sets all elements of args to null
         logger.error(someFormat, args);
-        // System.out.println("args " + Arrays.toString(args));
+        // GG: All is well args is still intact
+        System.out.println("args " + Arrays.toString(args));
         Assert.assertArrayEquals(originalArgs, args);
     }
 
