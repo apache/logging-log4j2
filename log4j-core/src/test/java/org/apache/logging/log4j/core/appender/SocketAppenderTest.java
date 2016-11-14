06fe5e6d1326ef0b6aef9833904f75d40c9f17dd
Gary Gregory <ggregory@apache.org>
2016-11-13 23:32:09 -0800

    More precise ivar name.

--- log4j-core/src/test/java/org/apache/logging/log4j/core/appender/SocketAppenderTest.java
+++ log4j-core/src/test/java/org/apache/logging/log4j/core/appender/SocketAppenderTest.java
@@ -327,18 +327,18 @@ public class SocketAppenderTest {
 
     public static class TcpSocketTestServer extends Thread {
 
-        private final ServerSocket sock;
+        private final ServerSocket serverSocket;
         private volatile boolean shutdown = false;
         private volatile int count = 0;
         private final BlockingQueue<LogEvent> queue;
 
         public TcpSocketTestServer(final int port) throws IOException {
-            this.sock = new ServerSocket(port);
+            this.serverSocket = new ServerSocket(port);
             this.queue = new ArrayBlockingQueue<>(10);
         }
 
         public int getLocalPort() {
-            return sock.getLocalPort();
+            return serverSocket.getLocalPort();
         }
         
         public void reset() {
@@ -354,7 +354,7 @@ public class SocketAppenderTest {
         @Override
         public void run() {
             try {
-                try (final Socket socket = sock.accept()) {
+                try (final Socket socket = serverSocket.accept()) {
                     if (socket != null) {
                         final ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
                         while (!shutdown) {
