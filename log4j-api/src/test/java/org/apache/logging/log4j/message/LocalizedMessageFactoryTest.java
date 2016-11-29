2bc846a5067a7414194ac07be6ebf4956295271d
Gary Gregory <garydgregory@gmail.com>
2016-11-29 00:28:08 -0800

    [LOG4J2-1682]
    Logger using LocalizedMessageFactory prints key instead of messsage.

---
+++ log4j-api/src/test/java/org/apache/logging/log4j/message/LocalizedMessageFactoryTest.java
@@ -0,0 +1,37 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache license, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the license for the specific language governing permissions and
+ * limitations under the license.
+ */
+package org.apache.logging.log4j.message;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Tests {@link LocalizedMessageFactory}.
+ */
+public class LocalizedMessageFactoryTest {
+
+    @Test
+    public void testNewMessage() {
+        final LocalizedMessageFactory localizedMessageFactory = new LocalizedMessageFactory(
+                ResourceBundle.getBundle("MF", Locale.US));
+        final Message message = localizedMessageFactory.newMessage("hello_world");
+        Assert.assertEquals("Hello world.", message.getFormattedMessage());
+    }
+}
