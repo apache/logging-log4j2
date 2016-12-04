fce3294b738516c6f202a8a3033459b35ee23f8d
Ludovic HOCHET <lhochet@gmail.com>
2016-12-04 13:14:24 -0800

    [LOG4J2-1664] Improve OSGi unit tests.

--- log4j-osgi/src/test/java/org/apache/logging/log4j/osgi/tests/AbstractLoadBundleTest.java
+++ log4j-osgi/src/test/java/org/apache/logging/log4j/osgi/tests/AbstractLoadBundleTest.java
@@ -149,17 +149,19 @@ public abstract class AbstractLoadBundleTest {
             core.start();
         }
         catch (final BundleException ex) {
+            boolean shouldRethrow = true;
             final Throwable t = ex.getCause();
-            if (t != null)
-            {
+            if (t != null) {
                 final Throwable t2 = t.getCause();
-                if (t2 != null)
-                {
+                if (t2 != null) {
                     final String cause = t2.toString();
                     final boolean result = cause.equals("java.lang.ClassNotFoundException: org.apache.logging.log4j.Logger") // Equinox
                                   || cause.equals("java.lang.ClassNotFoundException: org.apache.logging.log4j.Logger not found by org.apache.logging.log4j.core [2]"); // Felix
                     Assert.assertFalse("org.apache.logging.log4j package is not properly imported in org.apache.logging.log4j.core bundle, check that the package is exported from api and is not split between api and core", result);
+                    shouldRethrow = !result;
                 }
+            }
+            if (shouldRethrow) {
                 throw ex; // rethrow if the cause of the exception is something else
             }
         }
@@ -175,7 +177,6 @@ public abstract class AbstractLoadBundleTest {
      * Tests LOG4J2-920.
      */
     @Test
-    @Ignore("org.osgi.framework.BundleException: The bundle \"org.apache.logging.log4j.core_2.7.1.SNAPSHOT [2]\" could not be resolved. Reason: Missing Constraint: Import-Package: javax.jms; version=\"[1.1.0,2.0.0)\"")
     public void testMissingImportOfCoreOsgiPackage() throws BundleException, ReflectiveOperationException {
 
         final Bundle api = getApiBundle();
@@ -206,7 +207,6 @@ public abstract class AbstractLoadBundleTest {
      * Tests the log of a simple message in an OSGi container
      */
     @Test
-    @Ignore("org.osgi.framework.BundleException: The bundle \"org.apache.logging.log4j.core_2.7.1.SNAPSHOT [2]\" could not be resolved. Reason: Missing Constraint: Import-Package: javax.jms; version=\"[1.1.0,2.0.0)\"")
     public void testSimpleLogInAnOsgiContext() throws BundleException, ReflectiveOperationException {
 
         final Bundle api = getApiBundle();
