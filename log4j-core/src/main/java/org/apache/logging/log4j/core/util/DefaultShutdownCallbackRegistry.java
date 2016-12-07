2ff6db4db30b2f0191282db66ff1d35345c16826
Gary Gregory <garydgregory@gmail.com>
2016-12-06 16:09:33 -0800

    [LOG4J2-1642]
    DefaultShutdownCallbackRegistry can throw a NoClassDefFoundError.

--- log4j-core/src/main/java/org/apache/logging/log4j/core/util/DefaultShutdownCallbackRegistry.java
+++ log4j-core/src/main/java/org/apache/logging/log4j/core/util/DefaultShutdownCallbackRegistry.java
@@ -76,7 +76,7 @@ public class DefaultShutdownCallbackRegistry implements ShutdownCallbackRegistry
                     try {
                         LOGGER.error(SHUTDOWN_HOOK_MARKER, "Caught exception executing shutdown hook {}", hook, t1);
                     } catch (final Throwable t2) {
-                        System.err.println("Caught exception logging exception");
+                        System.err.println("Caught exception " + t2.getClass() + " logging exception " + t1.getClass());
                         t1.printStackTrace();
                     }
                 }
