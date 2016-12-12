fc58fba3532e9413b64fb020abf6419b6775878f
Gary Gregory <ggregory@apache.org>
2016-12-12 13:32:23 -0800

    Remove unused import.

--- log4j-core/src/main/java/org/apache/logging/log4j/core/net/TcpSocketManager.java
+++ log4j-core/src/main/java/org/apache/logging/log4j/core/net/TcpSocketManager.java
@@ -33,7 +33,6 @@ import org.apache.logging.log4j.core.Layout;
 import org.apache.logging.log4j.core.appender.AppenderLoggingException;
 import org.apache.logging.log4j.core.appender.ManagerFactory;
 import org.apache.logging.log4j.core.appender.OutputStreamManager;
-import org.apache.logging.log4j.core.net.TcpSocketManager.TcpSocketManagerFactory;
 import org.apache.logging.log4j.core.util.Closer;
 import org.apache.logging.log4j.core.util.Log4jThread;
 import org.apache.logging.log4j.core.util.NullOutputStream;
