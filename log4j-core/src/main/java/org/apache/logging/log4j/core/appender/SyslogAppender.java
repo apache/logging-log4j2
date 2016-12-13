1009e7364aabf9384557213ebea98ef43ade3380
Gary Gregory <ggregory@apache.org>
2016-12-12 17:15:16 -0800

    [LOG4J2-1737]
    Add a Builder to SyslogLayout and deprecate
    SyslogLayout.createLayout(Facility, boolean, String, Charset).

--- log4j-core/src/main/java/org/apache/logging/log4j/core/appender/SyslogAppender.java
+++ log4j-core/src/main/java/org/apache/logging/log4j/core/appender/SyslogAppender.java
@@ -104,16 +104,24 @@ public class SyslogAppender extends SocketAppender {
         private LoggerFields[] loggerFields;
 
         @Override
-        public SyslogAppender build() {            
+        public SyslogAppender build() {
             final Protocol protocol = getProtocol();
             final SslConfiguration sslConfiguration = getSslConfiguration();
             final boolean useTlsMessageFormat = sslConfiguration != null || protocol == Protocol.SSL;
             final Configuration configuration = getConfiguration();
-            final Layout<? extends Serializable> layout = RFC5424.equalsIgnoreCase(format) ?
-                Rfc5424Layout.createLayout(facility, id, enterpriseNumber, includeMdc, mdcId, mdcPrefix, eventPrefix, newLine,
-                    escapeNL, appName, msgId, excludes, includes, required, exceptionPattern, useTlsMessageFormat, loggerFields,
-                    configuration) :
-                SyslogLayout.createLayout(facility, newLine, escapeNL, charsetName);
+            final Layout<? extends Serializable> layout = RFC5424.equalsIgnoreCase(format)
+                    ? Rfc5424Layout.createLayout(facility, id, enterpriseNumber, includeMdc, mdcId, mdcPrefix,
+                            eventPrefix, newLine, escapeNL, appName, msgId, excludes, includes, required,
+                            exceptionPattern, useTlsMessageFormat, loggerFields, configuration)
+                    :
+                    // @formatter:off
+                        SyslogLayout.newBuilder()
+                            .setFacility(facility)
+                            .setIncludeNewLine(newLine)
+                            .setEscapeNL(escapeNL)
+                            .setCharset(charsetName)
+                            .build();
+                        // @formatter:off
 
             final String name = getName();
             if (name == null) {
