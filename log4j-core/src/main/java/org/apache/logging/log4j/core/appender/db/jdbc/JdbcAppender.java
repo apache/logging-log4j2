80354c6d272a8975f129f8f40b848d6a389ea54d
Matt Sicker <matt.sicker@spr.com>
2017-01-06 23:34:09 -0600

    [LOG4J2-1770]: Add Builder to JdbcAppender

--- log4j-core/src/main/java/org/apache/logging/log4j/core/appender/db/jdbc/JdbcAppender.java
+++ log4j-core/src/main/java/org/apache/logging/log4j/core/appender/db/jdbc/JdbcAppender.java
@@ -16,15 +16,23 @@
  */
 package org.apache.logging.log4j.core.appender.db.jdbc;
 
+import java.io.Serializable;
+import java.nio.charset.Charset;
+import java.util.Arrays;
+import java.util.Objects;
+
 import org.apache.logging.log4j.core.Appender;
 import org.apache.logging.log4j.core.Core;
 import org.apache.logging.log4j.core.Filter;
+import org.apache.logging.log4j.core.Layout;
 import org.apache.logging.log4j.core.appender.AbstractAppender;
 import org.apache.logging.log4j.core.appender.db.AbstractDatabaseAppender;
 import org.apache.logging.log4j.core.config.plugins.Plugin;
-import org.apache.logging.log4j.core.config.plugins.PluginAttribute;
+import org.apache.logging.log4j.core.config.plugins.PluginBuilderAttribute;
+import org.apache.logging.log4j.core.config.plugins.PluginBuilderFactory;
 import org.apache.logging.log4j.core.config.plugins.PluginElement;
-import org.apache.logging.log4j.core.config.plugins.PluginFactory;
+import org.apache.logging.log4j.core.config.plugins.validation.constraints.Required;
+import org.apache.logging.log4j.core.util.Assert;
 import org.apache.logging.log4j.core.util.Booleans;
 
 /**
@@ -55,52 +63,132 @@ public final class JdbcAppender extends AbstractDatabaseAppender<JdbcDatabaseMan
     /**
      * Factory method for creating a JDBC appender within the plugin manager.
      *
-     * @param name The name of the appender.
-     * @param ignore If {@code "true"} (default) exceptions encountered when appending events are logged; otherwise
-     *               they are propagated to the caller.
-     * @param filter The filter, if any, to use.
-     * @param connectionSource The connections source from which database connections should be retrieved.
-     * @param bufferSize If an integer greater than 0, this causes the appender to buffer log events and flush whenever
-     *                   the buffer reaches this size.
-     * @param tableName The name of the database table to insert log events into.
-     * @param columnConfigs Information about the columns that log event data should be inserted into and how to insert
-     *                      that data.
-     * @return a new JDBC appender.
+     * @see Builder
+     * @deprecated use {@link #newBuilder()}
      */
-    @PluginFactory
-    public static JdbcAppender createAppender(
-            @PluginAttribute("name") final String name,
-            @PluginAttribute("ignoreExceptions") final String ignore,
-            @PluginElement("Filter") final Filter filter,
-            @PluginElement("ConnectionSource") final ConnectionSource connectionSource,
-            @PluginAttribute("bufferSize") final String bufferSize,
-            @PluginAttribute("tableName") final String tableName,
-            @PluginElement("ColumnConfigs") final ColumnConfig[] columnConfigs) {
+    @Deprecated
+    public static <B extends Builder<B>> JdbcAppender createAppender(final String name, final String ignore,
+                                                                     final Filter filter,
+                                                                     final ConnectionSource connectionSource,
+                                                                     final String bufferSize, final String tableName,
+                                                                     final ColumnConfig[] columnConfigs) {
+        Assert.requireNonEmpty(name, "Name cannot be empty");
+        Objects.requireNonNull(connectionSource, "ConnectionSource cannot be null");
+        Assert.requireNonEmpty(tableName, "Table name cannot be empty");
+        Assert.requireNonEmpty(columnConfigs, "ColumnConfigs cannot be empty");
 
         final int bufferSizeInt = AbstractAppender.parseInt(bufferSize, 0);
         final boolean ignoreExceptions = Booleans.parseBoolean(ignore, true);
 
-        final StringBuilder managerName = new StringBuilder("jdbcManager{ description=").append(name)
-                .append(", bufferSize=").append(bufferSizeInt).append(", connectionSource=")
-                .append(connectionSource.toString()).append(", tableName=").append(tableName).append(", columns=[ ");
+        return JdbcAppender.<B>newBuilder()
+            .setBufferSize(bufferSizeInt)
+            .setColumnConfigs(columnConfigs)
+            .setConnectionSource(connectionSource)
+            .setTableName(tableName)
+            .withName(name)
+            .withIgnoreExceptions(ignoreExceptions)
+            .withFilter(filter)
+            .build();
+    }
 
-        int i = 0;
-        for (final ColumnConfig column : columnConfigs) {
-            if (i++ > 0) {
-                managerName.append(", ");
+    @PluginBuilderFactory
+    public static <B extends Builder<B>> B newBuilder() {
+        return new Builder<B>().asBuilder();
+    }
+
+    public static class Builder<B extends Builder<B>> extends AbstractAppender.Builder<B>
+        implements org.apache.logging.log4j.core.util.Builder<JdbcAppender> {
+
+        @PluginElement("ConnectionSource")
+        @Required(message = "No ConnectionSource provided")
+        private ConnectionSource connectionSource;
+
+        @PluginBuilderAttribute
+        private int bufferSize;
+
+        @PluginBuilderAttribute
+        @Required(message = "No table name provided")
+        private String tableName;
+
+        @PluginElement("ColumnConfigs")
+        private ColumnConfig[] columnConfigs;
+
+        // TODO: LOG4J2-424
+//        @PluginElement("ColumnMappings")
+//        private ColumnMapping[] columnMappings;
+
+        /**
+         * The connections source from which database connections should be retrieved.
+         */
+        public B setConnectionSource(final ConnectionSource connectionSource) {
+            this.connectionSource = connectionSource;
+            return asBuilder();
+        }
+
+        /**
+         * If an integer greater than 0, this causes the appender to buffer log events and flush whenever the buffer
+         * reaches this size.
+         */
+        public B setBufferSize(final int bufferSize) {
+            this.bufferSize = bufferSize;
+            return asBuilder();
+        }
+
+        /**
+         * The name of the database table to insert log events into.
+         */
+        public B setTableName(final String tableName) {
+            this.tableName = tableName;
+            return asBuilder();
+        }
+
+        /**
+         * Information about the columns that log event data should be inserted into and how to insert that data.
+         */
+        public B setColumnConfigs(final ColumnConfig... columnConfigs) {
+            this.columnConfigs = columnConfigs;
+            return asBuilder();
+        }
+
+//        public B setColumnMappings(final ColumnMapping... columnMappings) {
+//            this.columnMappings = columnMappings;
+//            return asBuilder();
+//        }
+
+        @Override
+        public JdbcAppender build() {
+            final String managerName = "JdbcManager{name=" + getName() + ", bufferSize=" + bufferSize + ", tableName=" +
+                tableName + ", columnConfigs=" + Arrays.toString(columnConfigs) + '}';
+            final JdbcDatabaseManager manager = JdbcDatabaseManager.getJDBCDatabaseManager(managerName, bufferSize,
+                connectionSource, tableName, columnConfigs);
+            if (manager == null) {
+                return null;
             }
-            managerName.append(column.toString());
+            return new JdbcAppender(getName(), getFilter(), isIgnoreExceptions(), manager);
         }
 
-        managerName.append(" ] }");
-
-        final JdbcDatabaseManager manager = JdbcDatabaseManager.getJDBCDatabaseManager(
-                managerName.toString(), bufferSizeInt, connectionSource, tableName, columnConfigs
-        );
-        if (manager == null) {
-            return null;
+        @Override
+        @Deprecated
+        public Layout<? extends Serializable> getLayout() {
+            throw new UnsupportedOperationException();
         }
 
-        return new JdbcAppender(name, filter, ignoreExceptions, manager);
+        @Override
+        @Deprecated
+        public B withLayout(final Layout<? extends Serializable> layout) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        @Deprecated
+        public Layout<? extends Serializable> getOrCreateLayout() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        @Deprecated
+        public Layout<? extends Serializable> getOrCreateLayout(final Charset charset) {
+            throw new UnsupportedOperationException();
+        }
     }
 }
