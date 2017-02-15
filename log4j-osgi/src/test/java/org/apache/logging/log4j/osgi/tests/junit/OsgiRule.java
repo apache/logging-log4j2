f34bb43321e3ff2b22492e51d5a38709f4d9e1e9
Gary Gregory <ggregory@apache.org>
2017-02-14 21:45:17 -0800

    Fix build on Windows (as seen on my machine and the build VM).

--- log4j-osgi/src/test/java/org/apache/logging/log4j/osgi/tests/junit/OsgiRule.java
+++ log4j-osgi/src/test/java/org/apache/logging/log4j/osgi/tests/junit/OsgiRule.java
@@ -56,7 +56,9 @@ public class OsgiRule extends ExternalResource {
         // Cleans framework before first init. Subsequent init invocations do not clean framework.
         configMap.put("org.osgi.framework.storage.clean", "onFirstInit");
         configMap.put("felix.log.level", "4");
-        configMap.put("eclipse.log.level", "ALL");        
+        configMap.put("eclipse.log.level", "ALL");
+        // Hack to get the build working on Windows. Could try newer versions of Felix.
+        configMap.put("felix.cache.locking", "false");        
         // Delegates loading of endorsed libraries to JVM classloader
         // config.put("org.osgi.framework.bootdelegation", "javax.*,org.w3c.*,org.xml.*");
         framework = factory.newFramework(configMap);
