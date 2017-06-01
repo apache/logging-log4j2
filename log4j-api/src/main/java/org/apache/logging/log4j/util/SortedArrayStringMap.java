194a398d8f70d6680f8154168011171f0a12113b
rpopma <rpopma@apache.org>
2017-06-01 20:27:16 +0900

    LOG4J2-1926 removed dependency on java.rmi for Android compatibility

--- log4j-api/src/main/java/org/apache/logging/log4j/util/SortedArrayStringMap.java
+++ log4j-api/src/main/java/org/apache/logging/log4j/util/SortedArrayStringMap.java
@@ -16,9 +16,12 @@
  */
 package org.apache.logging.log4j.util;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InvalidObjectException;
-import java.rmi.MarshalledObject;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.Arrays;
 import java.util.ConcurrentModificationException;
 import java.util.HashMap;
@@ -485,12 +488,28 @@ public class SortedArrayStringMap implements IndexedStringMap {
             for (int i = 0; i < size; i++) {
                 s.writeObject(keys[i]);
                 try {
-                    s.writeObject(new MarshalledObject<>(values[i]));
+                    s.writeObject(marshall(values[i]));
                 } catch (final Exception e) {
                     handleSerializationException(e, i, keys[i]);
                     s.writeObject(null);
                 }
             }
+        }
+    }
+
+    private static byte[] marshall(Object obj) throws IOException {
+        ByteArrayOutputStream bout = new ByteArrayOutputStream();
+        try (ObjectOutputStream oos = new ObjectOutputStream(bout)) {
+            oos.writeObject(obj);
+            oos.flush();
+            return bout.toByteArray();
+        }
+    }
+
+    private static Object unmarshall(byte[] data) throws IOException, ClassNotFoundException {
+        ByteArrayInputStream bin = new ByteArrayInputStream(data);
+        try (ObjectInputStream ois = new ObjectInputStream(bin)) {
+            return ois.readObject();
         }
     }
 
@@ -542,8 +561,8 @@ public class SortedArrayStringMap implements IndexedStringMap {
         for (int i = 0; i < mappings; i++) {
             keys[i] = (String) s.readObject();
             try {
-                final MarshalledObject<Object> marshalledObject = (MarshalledObject<Object>) s.readObject();
-                values[i] = marshalledObject == null ? null : marshalledObject.get();
+                final byte[] marshalledObject = (byte[]) s.readObject();
+                values[i] = marshalledObject == null ? null : unmarshall(marshalledObject);
             } catch (final Exception | LinkageError error) {
                 handleSerializationException(error, i, keys[i]);
                 values[i] = null;
