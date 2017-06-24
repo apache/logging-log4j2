98eaaf9861156661f8d053adced0856fb7b54d6b
rpopma <rpopma@apache.org>
2017-06-24 19:55:49 +0900

    LOG4J2-1926 Facilitate Adroid use: Moved ExtendedThreadInformation from log4j-api to log4j-core to avoid all references to java.lang.management in log4j-api

--- log4j-core/src/main/java/org/apache/logging/log4j/core/message/ExtendedThreadInformation.java
+++ log4j-core/src/main/java/org/apache/logging/log4j/core/message/ExtendedThreadInformation.java
@@ -14,12 +14,13 @@
  * See the license for the specific language governing permissions and
  * limitations under the license.
  */
-package org.apache.logging.log4j.message;
+package org.apache.logging.log4j.core.message;
 
 import java.lang.management.LockInfo;
 import java.lang.management.MonitorInfo;
 import java.lang.management.ThreadInfo;
 
+import org.apache.logging.log4j.message.ThreadInformation;
 import org.apache.logging.log4j.util.StringBuilders;
 
 /**
