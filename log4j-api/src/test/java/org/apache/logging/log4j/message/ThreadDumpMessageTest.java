39fb971db3c276b4854254b5322b28e7165a8909
Gary Gregory <ggregory@apache.org>
2017-07-06 03:10:10 -0700

    Add final modifier to private fields.

--- log4j-api/src/test/java/org/apache/logging/log4j/message/ThreadDumpMessageTest.java
+++ log4j-api/src/test/java/org/apache/logging/log4j/message/ThreadDumpMessageTest.java
@@ -144,7 +144,7 @@ public class ThreadDumpMessageTest {
 
     private class ThreadWithCountDownLatch extends Thread {
         private final CountDownLatch started;
-        private CountDownLatch keepAlive;
+        private final CountDownLatch keepAlive;
         volatile boolean finished;
 
         public ThreadWithCountDownLatch(final CountDownLatch started, final CountDownLatch keepAlive) {
