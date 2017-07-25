9073618c94adbf68d41aedb21e58304780182856
Jerry xnslong <notifications@github.com>
2017-07-25 12:35:48 -0700

    [LOG4J2-1977] Consider the StringBuilder's capacity instead of content length when trimming. Applied patch with minor changes. Closes #92.

--- log4j-api/src/test/java/org/apache/logging/log4j/util/StringBuildersTest.java
+++ log4j-api/src/test/java/org/apache/logging/log4j/util/StringBuildersTest.java
@@ -34,4 +34,18 @@ public class StringBuildersTest {
         StringBuilders.trimToMaxSize(sb, Constants.MAX_REUSABLE_MESSAGE_SIZE);
         assertTrue("trimmed OK", sb.length() <= Constants.MAX_REUSABLE_MESSAGE_SIZE);
     }
+
+    @Test
+    public void trimToMaxSizeWithLargeCapacity() throws Exception {
+        final StringBuilder sb = new StringBuilder();
+        final char[] value = new char[4 * 1024];
+        sb.append(value);
+        sb.setLength(0);
+
+        assertTrue("needs trimming", sb.capacity() > Constants.MAX_REUSABLE_MESSAGE_SIZE);
+        StringBuilders.trimToMaxSize(sb, Constants.MAX_REUSABLE_MESSAGE_SIZE);
+        assertTrue("trimmed OK", sb.capacity() <= Constants.MAX_REUSABLE_MESSAGE_SIZE);
+    }
+
+
 }
