7f116d6ac099f64217cfe56b4588f6c6dec5aa95
Gary Gregory <garydgregory@gmail.com>
2017-08-13 19:44:27 -0600

    Javadoc.

--- log4j-core/src/main/java/org/apache/logging/log4j/core/DefaultLoggerContextAccessor.java
+++ log4j-core/src/main/java/org/apache/logging/log4j/core/DefaultLoggerContextAccessor.java
@@ -23,7 +23,7 @@ package org.apache.logging.log4j.core;
 public class DefaultLoggerContextAccessor implements LoggerContextAccessor {
 
     /**
-     * Singleton insstamce.
+     * Singleton instance.
      */
     public static DefaultLoggerContextAccessor INSTANCE = new DefaultLoggerContextAccessor();
 
