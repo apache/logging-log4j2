03a5e071bc66f0d8e771e5dbe1453b4eb6821bbb
Gary Gregory <garydgregory@gmail.com>
2017-08-14 13:44:09 -0600

    Allow for empty rule chains.

--- log4j-core/src/test/java/org/apache/logging/log4j/test/RuleChainFactory.java
+++ log4j-core/src/test/java/org/apache/logging/log4j/test/RuleChainFactory.java
@@ -33,11 +33,12 @@ public class RuleChainFactory {
      * @return a new rule chain.
      */
     public static RuleChain create(final TestRule... testRules) {
+        if (testRules == null || testRules.length == 0) {
+            return RuleChain.emptyRuleChain();
+        }
         RuleChain ruleChain = RuleChain.outerRule(testRules[0]);
-        for (int idx = 1; idx < testRules.length; idx++) {
-            if (ruleChain != null) {
-                ruleChain = ruleChain.around(testRules[idx]);
-            }
+        for (int i = 1; i < testRules.length; i++) {
+            ruleChain = ruleChain.around(testRules[i]);
         }
         return ruleChain;
     }
