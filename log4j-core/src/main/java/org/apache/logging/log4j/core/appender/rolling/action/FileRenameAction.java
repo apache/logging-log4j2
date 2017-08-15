4f2df221c96d047dd26f437b0d56ca4919def943
Benjamin Jaton <benjamin.jaton@gmail.com>
2017-08-15 08:44:26 -0700

    FileRenameAction marked as successful only if the delete of the source file is successful.

--- log4j-core/src/main/java/org/apache/logging/log4j/core/appender/rolling/action/FileRenameAction.java
+++ log4j-core/src/main/java/org/apache/logging/log4j/core/appender/rolling/action/FileRenameAction.java
@@ -129,9 +129,9 @@ public class FileRenameAction extends AbstractAction {
                         try {
                             Files.copy(Paths.get(source.getAbsolutePath()), Paths.get(destination.getAbsolutePath()),
                                     StandardCopyOption.REPLACE_EXISTING);
-                            result = true;
                             try {
                                 Files.delete(Paths.get(source.getAbsolutePath()));
+                                result = true;
                                 LOGGER.trace("Renamed file {} to {} using copy and delete",
                                         source.getAbsolutePath(), destination.getAbsolutePath());
                             } catch (final IOException exDelete) {
@@ -139,6 +139,7 @@ public class FileRenameAction extends AbstractAction {
                                         exDelete.getClass().getName(), exDelete.getMessage());
                                 try {
                                     new PrintWriter(source.getAbsolutePath()).close();
+                                    result = true;
                                     LOGGER.trace("Renamed file {} to {} with copy and truncation",
                                             source.getAbsolutePath(), destination.getAbsolutePath());
                                 } catch (final IOException exOwerwrite) {
@@ -180,3 +181,4 @@ public class FileRenameAction extends AbstractAction {
     }
 
 }
+
