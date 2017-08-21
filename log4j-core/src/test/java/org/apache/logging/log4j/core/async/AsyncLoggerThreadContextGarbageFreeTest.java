0029e5d081548787915e0cbd4b4e3ba1a4f22f05
Gary Gregory <garydgregory@gmail.com>
2017-08-21 09:08:25 -0600

    Better JUnit integration.

--- log4j-core/src/test/java/org/apache/logging/log4j/core/async/AsyncLoggerThreadContextGarbageFreeTest.java
+++ log4j-core/src/test/java/org/apache/logging/log4j/core/async/AsyncLoggerThreadContextGarbageFreeTest.java
@@ -30,7 +30,7 @@ import org.junit.runners.Parameterized;
 @RunWith(Parameterized.class)
 @Category(AsyncLoggers.class)
 public class AsyncLoggerThreadContextGarbageFreeTest extends AbstractAsyncThreadContextTestBase {
-    @Parameterized.Parameters
+    @Parameterized.Parameters(name = "{0} {1}")
     public static Collection<Object[]> data() {
         return Arrays.asList(new Object[][] {
                 { ContextImpl.GARBAGE_FREE, Mode.ALL_ASYNC},
