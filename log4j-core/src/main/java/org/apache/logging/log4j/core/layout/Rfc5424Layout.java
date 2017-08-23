ce1183629fe89625a77872c7153853e7774502a6
Paul Burrowes <paul.burrowes@metaswitch.com>
2017-08-23 09:38:36 -0600

    [LOG4J2-922] Parameter of mdcId in SyslogAppender has no default value.

--- log4j-core/src/main/java/org/apache/logging/log4j/core/layout/Rfc5424Layout.java
+++ log4j-core/src/main/java/org/apache/logging/log4j/core/layout/Rfc5424Layout.java
@@ -138,7 +138,7 @@ public final class Rfc5424Layout extends AbstractStringLayout {
         this.includeMdc = includeMDC;
         this.includeNewLine = includeNL;
         this.escapeNewLine = escapeNL == null ? null : Matcher.quoteReplacement(escapeNL);
-        this.mdcId = mdcId;
+        this.mdcId = id == null ? DEFAULT_MDCID : id;
         this.mdcSdId = new StructuredDataId(mdcId, enterpriseNumber, null, null);
         this.mdcPrefix = mdcPrefix;
         this.eventPrefix = eventPrefix;
