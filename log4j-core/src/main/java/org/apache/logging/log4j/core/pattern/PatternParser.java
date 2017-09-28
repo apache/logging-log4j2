36919e2c72cfb88af5aca1ea4fd629b4bf5bac6d
zsomborklara <zsombor.klara@cloudera.com>
2017-09-28 17:28:03 +0200

    LOG4J2-1216: Nested pattern layout options broken

--- log4j-core/src/main/java/org/apache/logging/log4j/core/pattern/PatternParser.java
+++ log4j-core/src/main/java/org/apache/logging/log4j/core/pattern/PatternParser.java
@@ -271,32 +271,27 @@ public final class PatternParser {
      */
     private static int extractOptions(final String pattern, final int start, final List<String> options) {
         int i = start;
-        while (i < pattern.length() && pattern.charAt(i) == '{') {
-            final int begin = i++;
-            int end;
-            int depth = 0;
-            do {
-                end = pattern.indexOf('}', i);
-                if (end == -1) {
-                    break;
+        while (i < pattern.length()  &&  pattern.charAt(i) == '{') {
+            i++;                      // skip opening "{"
+            final int begin = i;      // position of first real char
+            int depth = 1;            // already inside one level
+            while (depth > 0  &&  i < pattern.length()) {
+                char c = pattern.charAt(i);
+                if (c == '{') {
+                    depth++;
+                } else if (c == '}') {
+                    depth--;
+                    // TODO(?) maybe escaping of { and } with \ or %
                 }
-                final int next = pattern.indexOf("{", i);
-                if (next != -1 && next < end) {
-                    i = end + 1;
-                    ++depth;
-                } else if (depth > 0) {
-                    --depth;
-                }
-            } while (depth > 0);
+                i++;
+            } // while
 
-            if (end == -1) {
-                break;
+            if (depth > 0) {          // option not closed, continue with pattern after closing bracket
+                return pattern.indexOf('}', start) + 1;
             }
 
-            final String r = pattern.substring(begin + 1, end);
-            options.add(r);
-            i = end + 1;
-        }
+            options.add(pattern.substring(begin, i-1));
+        } // while
 
         return i;
     }
