d5d00d09ecf28edc9b926ab7a7af51ad406b09d6
Ralph Goers <rgoers@nextiva.com>
2017-10-14 22:34:09 -0700

    LOG4J-2056 - modularize log4j-api. Turn others into automatic modules

---
+++ log4j-api-java9/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module org.apache.logging.log4j {
+    exports org.apache.logging.log4j;
+    exports org.apache.logging.log4j.message;
+    exports org.apache.logging.log4j.simple;
+    exports org.apache.logging.log4j.spi;
+    exports org.apache.logging.log4j.status;
+    exports org.apache.logging.log4j.util;
+    uses org.apache.logging.log4j.spi.Provider;
+}
