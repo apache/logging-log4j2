9b33d931798be04aed2fa79d9207ecc36503b4e0
Carter Kozak <c4kofony@gmail.com>
2017-11-09 08:32:14 -0500

    LOG4J2-2109: Added "log4j.formatMsgNoLookups" global configuration
    This allows applications to globally disable message pattern
    from looking up replacements without specifying "%m{nolookups}".

--- log4j-core/src/test/java/org/apache/logging/log4j/core/pattern/MessagePatternConverterTest.java
+++ log4j-core/src/test/java/org/apache/logging/log4j/core/pattern/MessagePatternConverterTest.java
@@ -17,12 +17,15 @@
 package org.apache.logging.log4j.core.pattern;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.core.LogEvent;
 import org.apache.logging.log4j.core.config.Configuration;
 import org.apache.logging.log4j.core.config.DefaultConfiguration;
+import org.apache.logging.log4j.core.config.builder.impl.DefaultConfigurationBuilder;
 import org.apache.logging.log4j.core.impl.Log4jLogEvent;
+import org.apache.logging.log4j.core.util.Constants;
 import org.apache.logging.log4j.message.Message;
 import org.apache.logging.log4j.message.ParameterizedMessage;
 import org.apache.logging.log4j.message.SimpleMessage;
@@ -72,6 +75,44 @@ public class MessagePatternConverterTest {
         final StringBuilder sb = new StringBuilder();
         converter.format(event, sb);
         assertEquals("Unexpected result", "${date:now:buhu}", sb.toString());
+    }
+
+    @Test
+    public void testLookupEnabledByDefault() {
+        assertFalse("Expected lookups to be enabled", Constants.FORMAT_MESSAGES_PATTERN_DISABLE_LOOKUPS);
+    }
+
+    @Test
+    public void testLookup() {
+        final Configuration config = new DefaultConfigurationBuilder()
+                .addProperty("foo", "bar")
+                .build(true);
+        final MessagePatternConverter converter = MessagePatternConverter.newInstance(config, null);
+        final Message msg = new ParameterizedMessage("${foo}");
+        final LogEvent event = Log4jLogEvent.newBuilder() //
+                .setLoggerName("MyLogger") //
+                .setLevel(Level.DEBUG) //
+                .setMessage(msg).build();
+        final StringBuilder sb = new StringBuilder();
+        converter.format(event, sb);
+        assertEquals("Unexpected result", "bar", sb.toString());
+    }
+
+    @Test
+    public void testDisabledLookup() {
+        final Configuration config = new DefaultConfigurationBuilder()
+                .addProperty("foo", "bar")
+                .build(true);
+        final MessagePatternConverter converter = MessagePatternConverter.newInstance(
+                config, new String[] {"nolookups"});
+        final Message msg = new ParameterizedMessage("${foo}");
+        final LogEvent event = Log4jLogEvent.newBuilder() //
+                .setLoggerName("MyLogger") //
+                .setLevel(Level.DEBUG) //
+                .setMessage(msg).build();
+        final StringBuilder sb = new StringBuilder();
+        converter.format(event, sb);
+        assertEquals("Expected the raw pattern string without lookup", "${foo}", sb.toString());
     }
 
     @Test
