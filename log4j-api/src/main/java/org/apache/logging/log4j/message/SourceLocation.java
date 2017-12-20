d94a848e565dd983b613a438edc8380443a072fc
Jeff Shaw <shawjef3@gmail.com>
2017-12-19 23:54:40 -0500

    SourceLocation#toString is the same as StackTraceElement#toString

--- log4j-api/src/main/java/org/apache/logging/log4j/message/SourceLocation.java
+++ log4j-api/src/main/java/org/apache/logging/log4j/message/SourceLocation.java
@@ -6,21 +6,29 @@ import java.util.Objects;
  * An alternative to {@link java.lang.StackTraceElement} for describing source
  * code locations. It differs in that all fields are nullable.
  */
-public class SourceLocation implements java.io.Serializable {
+public final class SourceLocation implements java.io.Serializable {
     private static final long serialVersionUID = 4324770886316212646L;
 
     public static SourceLocation valueOf(StackTraceElement source) {
-        return new SourceLocation(source.getClassName(), source.getMethodName(), source.getFileName(), source.getLineNumber());
+        return new SourceLocation(source.getClassName(), source.getMethodName(), source.isNativeMethod(), source.getFileName(), source.getLineNumber());
     }
 
     final private String className;
+    final private boolean isNativeMethod;
     final private String methodName;
     final private String fileName;
     final private Integer lineNumber;
 
+    private String toStringMemo;
+
     public SourceLocation(String className, String methodName, String fileName, Integer lineNumber) {
+        this(className, methodName, false, fileName, lineNumber);
+    }
+
+    SourceLocation(String className, String methodName, boolean isNativeMethod, String fileName, Integer lineNumber) {
         this.className = className;
         this.methodName = methodName;
+        this.isNativeMethod = isNativeMethod;
         this.fileName = fileName;
         this.lineNumber = lineNumber;
     }
@@ -39,6 +47,35 @@ public class SourceLocation implements java.io.Serializable {
 
     public Integer getLineNumber() {
         return lineNumber;
+    }
+
+    @Override
+    public String toString() {
+        //Should mirror StackTraceElement#toString, for backwards compatibility.
+        if (toStringMemo == null) {
+            StringBuilder builder = new StringBuilder();
+            builder.append(className);
+            builder.append('.');
+            builder.append(methodName);
+            builder.append('(');
+            if (isNativeMethod) {
+                builder.append("Native Method");
+            } else {
+                if (fileName == null) {
+                    builder.append("Unknown Source");
+                } else {
+                    builder.append(fileName);
+                    if (lineNumber != null) {
+                        builder.append(':');
+                        builder.append(lineNumber);
+                    }
+                }
+            }
+            builder.append(')');
+
+            toStringMemo = builder.toString();
+        }
+        return toStringMemo;
     }
 
     @Override
