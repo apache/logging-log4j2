f408220999e079f8f3994684e84953987be68d6c
Mikael Ståldal <mikael@staldal.nu>
2018-01-09 17:37:56 +0100

    LOG4J2-1944 improve error message when no configuration file is found

--- log4j-core/src/main/java/org/apache/logging/log4j/core/config/ConfigurationFactory.java
+++ log4j-core/src/main/java/org/apache/logging/log4j/core/config/ConfigurationFactory.java
@@ -390,7 +390,8 @@ public abstract class ConfigurationFactory extends ConfigurationBuilderFactory {
                 return config;
             }
             LOGGER.error("No Log4j 2 configuration file found. " +
-                    "Using default configuration: logging only errors to the console. " +
+                    "Using default configuration (logging only errors to the console), " +
+                    "or user programmatically provided configurations. " +
                     "Set system property 'log4j2.debug' " +
                     "to show Log4j 2 internal initialization logging. " +
                     "See https://logging.apache.org/log4j/2.x/manual/configuration.html for instructions on how to configure Log4j 2");
