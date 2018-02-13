5b8338c0b79982feffe473188860436489971917
Carter Kozak <c4kofony@gmail.com>
2018-02-13 11:28:17 -0500

    Reusable LogEvents pass along the original format string
    This allows custom layouts to group logged messages parameterized
    values without creating new single-use messages for each event.
    
    This slightly modifies the getFormat implementation of some
    message types to avoid doing unnecessary work to load the
    format string.

--- log4j-api/src/main/java/org/apache/logging/log4j/message/ReusableObjectMessage.java
+++ log4j-api/src/main/java/org/apache/logging/log4j/message/ReusableObjectMessage.java
@@ -55,7 +55,7 @@ public class ReusableObjectMessage implements ReusableMessage {
      */
     @Override
     public String getFormat() {
-        return getFormattedMessage();
+        return obj instanceof String ? (String) obj : null;
     }
 
     /**
