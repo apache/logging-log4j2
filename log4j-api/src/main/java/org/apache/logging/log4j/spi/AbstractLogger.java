56d00d69f4adb76318e45d1c64321d38f59f874c
Carter Kozak <ckozak@apache.org>
2018-04-17 16:01:33 -0400

    AbstractLogger.handleLogMessageException uses parameterized logging

--- log4j-api/src/main/java/org/apache/logging/log4j/spi/AbstractLogger.java
+++ log4j-api/src/main/java/org/apache/logging/log4j/spi/AbstractLogger.java
@@ -2180,17 +2180,8 @@ public abstract class AbstractLogger implements ExtendedLogger, Serializable {
         if (exception instanceof LoggingException) {
             throw (LoggingException) exception;
         }
-        final String format = msg.getFormat();
-        final int formatLength = format == null ? 4 : format.length();
-        final StringBuilder sb = new StringBuilder(formatLength + 100);
-        sb.append(fqcn);
-        sb.append(" caught ");
-        sb.append(exception.getClass().getName());
-        sb.append(" logging ");
-        sb.append(msg.getClass().getSimpleName());
-        sb.append(": ");
-        sb.append(format);
-        StatusLogger.getLogger().warn(sb.toString(), exception);
+        StatusLogger.getLogger().warn("{} caught {} logging {}: {}", fqcn, exception.getClass().getName(),
+                msg.getClass().getSimpleName(), msg.getFormat(), exception);
     }
 
     @Override
