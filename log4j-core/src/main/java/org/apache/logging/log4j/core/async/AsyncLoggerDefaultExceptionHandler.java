90e61ccdc43d72cc9bc10b5b601af2aa4712cb43
Carter Kozak <ckozak@apache.org>
2018-05-03 12:37:19 -0400

    [LOG4J2-2333] Handle errors thrown in disruptor ExceptionHandler
    Previously this would kill the background thread and stop processing
    events.

--- log4j-core/src/main/java/org/apache/logging/log4j/core/async/AsyncLoggerDefaultExceptionHandler.java
+++ log4j-core/src/main/java/org/apache/logging/log4j/core/async/AsyncLoggerDefaultExceptionHandler.java
@@ -25,17 +25,22 @@ public class AsyncLoggerDefaultExceptionHandler implements ExceptionHandler<Ring
 
     @Override
     public void handleEventException(final Throwable throwable, final long sequence, final RingBufferLogEvent event) {
-        final StringBuilder sb = new StringBuilder(512);
-        sb.append("AsyncLogger error handling event seq=").append(sequence).append(", value='");
         try {
-            sb.append(event);
-        } catch (final Exception ignored) {
-            sb.append("[ERROR calling ").append(event.getClass()).append(".toString(): ");
-            sb.append(ignored).append("]");
+            final StringBuilder sb = new StringBuilder(512);
+            sb.append("AsyncLogger error handling event seq=").append(sequence).append(", value='");
+            try {
+                sb.append(event);
+            } catch (final Throwable ignored) {
+                sb.append("[ERROR calling ").append(event.getClass()).append(".toString(): ");
+                sb.append(ignored).append("]");
+            }
+            sb.append("':");
+            System.err.println(sb);
+            throwable.printStackTrace();
+        } catch (final Throwable ignored) {
+            // LOG4J2-2333: Not much we can do here without risking an OOM.
+            // Throwing an error here may kill the background thread.
         }
-        sb.append("':");
-        System.err.println(sb);
-        throwable.printStackTrace();
     }
 
     @Override
