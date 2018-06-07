d48b1d96ad0b28ee32945e59cddb210e502471a0
Carter Kozak <ckozak@apache.org>
2018-06-07 08:49:36 -0400

    Log4j 1.2 api tests do not leak MDC values.

--- log4j-1.2-api/src/test/java/org/apache/log4j/LogWithMDCTest.java
+++ log4j-1.2-api/src/test/java/org/apache/log4j/LogWithMDCTest.java
@@ -39,14 +39,19 @@ public class LogWithMDCTest {
     public void testMDC() throws Exception {
         MDC.put("Key1", "John");
         MDC.put("Key2", "Smith");
-        final Logger logger = Logger.getLogger("org.apache.test.logging");
-        logger.debug("This is a test");
-        final ListAppender listApp = (ListAppender) CTX.getAppender("List");
-        assertNotNull(listApp);
-        final List<String> msgs = listApp.getMessages();
-        assertNotNull("No messages received", msgs);
-        assertTrue(msgs.size() == 1);
-        assertTrue("Key1 is missing", msgs.get(0).contains("Key1=John"));
-        assertTrue("Key2 is missing", msgs.get(0).contains("Key2=Smith"));
+        try {
+            final Logger logger = Logger.getLogger("org.apache.test.logging");
+            logger.debug("This is a test");
+            final ListAppender listApp = (ListAppender) CTX.getAppender("List");
+            assertNotNull(listApp);
+            final List<String> msgs = listApp.getMessages();
+            assertNotNull("No messages received", msgs);
+            assertTrue(msgs.size() == 1);
+            assertTrue("Key1 is missing", msgs.get(0).contains("Key1=John"));
+            assertTrue("Key2 is missing", msgs.get(0).contains("Key2=Smith"));
+        } finally {
+            MDC.remove("Key1");
+            MDC.remove("Key2");
+        }
     }
 }
