45f89572d7994f45bf84ef7845a3095ca5a372e9
Carter Kozak <ckozak@apache.org>
2018-06-07 09:39:09 -0400

    [LOG4J2-2348] Ignore MongoDBTest (mongo 2 and 3)
    This test has a high flake rate.

--- log4j-mongodb3/src/test/java/org/apache/logging/log4j/mongodb3/MongoDbTest.java
+++ log4j-mongodb3/src/test/java/org/apache/logging/log4j/mongodb3/MongoDbTest.java
@@ -26,6 +26,7 @@ import org.apache.logging.log4j.test.RuleChainFactory;
 import org.bson.Document;
 import org.junit.Assert;
 import org.junit.ClassRule;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.rules.RuleChain;
@@ -37,6 +38,7 @@ import com.mongodb.client.MongoDatabase;
 /**
  * This class name does NOT end in "Test" in order to only be picked up by {@link Java8Test}.
  */
+@Ignore("LOG4J2-2348: MongoDB tests have high flake rate")
 @Category(Appenders.MongoDb.class)
 public class MongoDbTest {
 
