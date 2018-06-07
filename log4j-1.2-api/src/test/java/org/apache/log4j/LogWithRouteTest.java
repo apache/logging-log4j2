40d9b504a9cc2593a19ab9c487eb5c873937ca75
Carter Kozak <ckozak@apache.org>
2018-06-07 08:49:36 -0400

    Log4j 1.2 api tests do not leak MDC values.

--- log4j-1.2-api/src/test/java/org/apache/log4j/LogWithRouteTest.java
+++ log4j-1.2-api/src/test/java/org/apache/log4j/LogWithRouteTest.java
@@ -40,14 +40,19 @@ public class LogWithRouteTest {
     public void testMDC() throws Exception {
         MDC.put("Type", "Service");
         MDC.put("Name", "John Smith");
-        final Logger logger = Logger.getLogger("org.apache.test.logging");
-        logger.debug("This is a test");
-        final ListAppender listApp = (ListAppender) CTX.getAppender("List");
-        assertNotNull(listApp);
-        final List<String> msgs = listApp.getMessages();
-        assertNotNull("No messages received", msgs);
-        assertTrue(msgs.size() == 1);
-        assertTrue("Type is missing", msgs.get(0).contains("Type=Service"));
-        assertTrue("Name is missing", msgs.get(0).contains("Name=John Smith"));
+        try {
+            final Logger logger = Logger.getLogger("org.apache.test.logging");
+            logger.debug("This is a test");
+            final ListAppender listApp = (ListAppender) CTX.getAppender("List");
+            assertNotNull(listApp);
+            final List<String> msgs = listApp.getMessages();
+            assertNotNull("No messages received", msgs);
+            assertTrue(msgs.size() == 1);
+            assertTrue("Type is missing", msgs.get(0).contains("Type=Service"));
+            assertTrue("Name is missing", msgs.get(0).contains("Name=John Smith"));
+        } finally {
+            MDC.remove("Type");
+            MDC.remove("Name");
+        }
     }
 }
