627365bdb3674ae9a755ec221eac9ec275ed6341
kmeurer <kmeurer@palantir.com>
2018-08-09 15:46:19 -0400

    Refactor pool configuration to use BuilderFactory rather than Factory method.

--- log4j-redis/src/main/java/org/apache/logging/log4j/redis/appender/LoggingJedisPoolConfiguration.java
+++ log4j-redis/src/main/java/org/apache/logging/log4j/redis/appender/LoggingJedisPoolConfiguration.java
@@ -3,7 +3,7 @@ package org.apache.logging.log4j.redis.appender;
 import org.apache.logging.log4j.core.Core;
 import org.apache.logging.log4j.core.config.plugins.Plugin;
 import org.apache.logging.log4j.core.config.plugins.PluginAttribute;
-import org.apache.logging.log4j.core.config.plugins.PluginFactory;
+import org.apache.logging.log4j.core.config.plugins.PluginBuilderFactory;
 import redis.clients.jedis.JedisPoolConfig;
 
 @Plugin(name = "PoolConfiguration", category = Core.CATEGORY_NAME, printObject = true)
@@ -14,30 +14,114 @@ public class LoggingJedisPoolConfiguration extends JedisPoolConfig {
     }
 
     public static LoggingJedisPoolConfiguration defaultConfiguration() {
-        return new LoggingJedisPoolConfiguration();
+        return LoggingJedisPoolConfiguration.newBuilder().build();
     }
 
     /**
      * Creates a LoggingJedisPoolConfiguration from standard pool parameters.
      */
-    @PluginFactory
-    public static LoggingJedisPoolConfiguration createJedisPoolConfiguration(
-            @PluginAttribute(value = "minIdle", defaultInt = 1800000) int minIdle,
-            @PluginAttribute(value = "maxIdle", defaultInt = JedisPoolConfig.DEFAULT_MAX_IDLE) int maxIdle,
-            @PluginAttribute(value = "testOnBorrow") boolean testOnBorrow,
-            @PluginAttribute(value = "testOnReturn") boolean testOnReturn,
-            @PluginAttribute(value = "testWhileIdle") boolean testWhileIdle,
-            @PluginAttribute(value = "testsPerEvictionRun", defaultInt = JedisPoolConfig.DEFAULT_NUM_TESTS_PER_EVICTION_RUN) int testsPerEvictionRun,
-            @PluginAttribute(value = "timeBetweenEvictionRunsMillis", defaultLong = JedisPoolConfig.DEFAULT_TIME_BETWEEN_EVICTION_RUNS_MILLIS) long timeBetweenEvicationRunsMillis
-            ) {
-        LoggingJedisPoolConfiguration poolConfig = defaultConfiguration();
-        poolConfig.setMaxIdle(maxIdle);
-        poolConfig.setMinIdle(minIdle);
-        poolConfig.setTestOnBorrow(testOnBorrow);
-        poolConfig.setTestOnReturn(testOnReturn);
-        poolConfig.setTestWhileIdle(testWhileIdle);
-        poolConfig.setNumTestsPerEvictionRun(testsPerEvictionRun);
-        poolConfig.setTimeBetweenEvictionRunsMillis(timeBetweenEvicationRunsMillis);
-        return poolConfig;
+    @PluginBuilderFactory
+    public static Builder newBuilder() {
+        return new LoggingJedisPoolConfiguration.Builder();
+    }
+
+    private static class Builder implements org.apache.logging.log4j.core.util.Builder<LoggingJedisPoolConfiguration> {
+
+        @PluginAttribute(value = "minIdle", defaultInt = 1800000)
+        private int minIdle = JedisPoolConfig.DEFAULT_MIN_IDLE;
+
+        @PluginAttribute(value = "maxIdle")
+        private int maxIdle = JedisPoolConfig.DEFAULT_MAX_IDLE;
+
+        @PluginAttribute(value = "testOnBorrow")
+        private boolean testOnBorrow = JedisPoolConfig.DEFAULT_TEST_ON_BORROW;
+
+        @PluginAttribute(value = "testOnReturn")
+        boolean testOnReturn = JedisPoolConfig.DEFAULT_TEST_ON_RETURN;
+
+        @PluginAttribute(value = "testWhileIdle")
+        boolean testWhileIdle = JedisPoolConfig.DEFAULT_TEST_WHILE_IDLE;
+
+        @PluginAttribute(value = "testsPerEvictionRun")
+        int testsPerEvictionRun = JedisPoolConfig.DEFAULT_NUM_TESTS_PER_EVICTION_RUN;
+
+        @PluginAttribute(value = "timeBetweenEvictionRunsMillis")
+        long timeBetweenEvicationRunsMillis = JedisPoolConfig.DEFAULT_TIME_BETWEEN_EVICTION_RUNS_MILLIS;
+
+        @Override
+        public LoggingJedisPoolConfiguration build() {
+            LoggingJedisPoolConfiguration poolConfig = new LoggingJedisPoolConfiguration();
+            poolConfig.setMaxIdle(maxIdle);
+            poolConfig.setMinIdle(minIdle);
+            poolConfig.setTestOnBorrow(testOnBorrow);
+            poolConfig.setTestOnReturn(testOnReturn);
+            poolConfig.setTestWhileIdle(testWhileIdle);
+            poolConfig.setNumTestsPerEvictionRun(testsPerEvictionRun);
+            poolConfig.setTimeBetweenEvictionRunsMillis(timeBetweenEvicationRunsMillis);
+            return poolConfig;
+        }
+
+        public int getMinIdle() {
+            return minIdle;
+        }
+
+        public Builder setMinIdle(int minIdle) {
+            this.minIdle = minIdle;
+            return this;
+        }
+
+        public int getMaxIdle() {
+            return maxIdle;
+        }
+
+        public Builder setMaxIdle(int maxIdle) {
+            this.maxIdle = maxIdle;
+            return this;
+        }
+
+        public boolean isTestOnBorrow() {
+            return testOnBorrow;
+        }
+
+        public Builder setTestOnBorrow(boolean testOnBorrow) {
+            this.testOnBorrow = testOnBorrow;
+            return this;
+        }
+
+        public boolean isTestOnReturn() {
+            return testOnReturn;
+        }
+
+        public Builder setTestOnReturn(boolean testOnReturn) {
+            this.testOnReturn = testOnReturn;
+            return this;
+        }
+
+        public boolean isTestWhileIdle() {
+            return testWhileIdle;
+        }
+
+        public Builder setTestWhileIdle(boolean testWhileIdle) {
+            this.testWhileIdle = testWhileIdle;
+            return this;
+        }
+
+        public int getTestsPerEvictionRun() {
+            return testsPerEvictionRun;
+        }
+
+        public Builder setTestsPerEvictionRun(int testsPerEvictionRun) {
+            this.testsPerEvictionRun = testsPerEvictionRun;
+            return this;
+        }
+
+        public long getTimeBetweenEvicationRunsMillis() {
+            return timeBetweenEvicationRunsMillis;
+        }
+
+        public Builder setTimeBetweenEvicationRunsMillis(long timeBetweenEvicationRunsMillis) {
+            this.timeBetweenEvicationRunsMillis = timeBetweenEvicationRunsMillis;
+            return this;
+        }
     }
 }
