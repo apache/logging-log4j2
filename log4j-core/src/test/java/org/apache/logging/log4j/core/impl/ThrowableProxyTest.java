e3744ef99c8f07aa34f941b0c9275af5066eeb3c
Carter Kozak <ckozak@apache.org>
2018-08-21 11:58:20 -0400

    [LOG4J2-2391] Refactor as much logic as possible out of ThrowableProxy
    This change isolates initialization logic into a new
    ThrowableProxyInitializationUtils class, and rendering logic
    into ThrowableProxyRenderUtils.
    
    Both new utility classes are package private. ThrowableProxy public
    API has not changed.

--- log4j-core/src/test/java/org/apache/logging/log4j/core/impl/ThrowableProxyTest.java
+++ log4j-core/src/test/java/org/apache/logging/log4j/core/impl/ThrowableProxyTest.java
@@ -362,11 +362,11 @@ public class ThrowableProxyTest {
 
     @Test
     public void testStack() {
-        final Map<String, ThrowableProxy.CacheEntry> map = new HashMap<>();
+        final Map<String, ThrowableProxyInitializationUtils.CacheEntry> map = new HashMap<>();
         final Stack<Class<?>> stack = new Stack<>();
         final Throwable throwable = new IllegalStateException("This is a test");
         final ThrowableProxy proxy = new ThrowableProxy(throwable);
-        final ExtendedStackTraceElement[] callerPackageData = proxy.toExtendedStackTrace(stack, map, null,
+        final ExtendedStackTraceElement[] callerPackageData = ThrowableProxyInitializationUtils.toExtendedStackTrace(proxy, stack, map, null,
                 throwable.getStackTrace());
         assertNotNull("No package data returned", callerPackageData);
     }
@@ -379,7 +379,7 @@ public class ThrowableProxyTest {
     @Test
     public void testStackWithUnloadableClass() throws Exception {
         final Stack<Class<?>> stack = new Stack<>();
-        final Map<String, ThrowableProxy.CacheEntry> map = new HashMap<>();
+        final Map<String, ThrowableProxyInitializationUtils.CacheEntry> map = new HashMap<>();
 
         final String runtimeExceptionThrownAtUnloadableClass_base64 = "rO0ABXNyABpqYXZhLmxhbmcuUnVudGltZUV4Y2VwdGlvbp5fBkcKNIPlAgAAeHIAE2phdmEubGFuZy5FeGNlcHRpb27Q/R8+GjscxAIAAHhyABNqYXZhLmxhbmcuVGhyb3dhYmxl1cY1Jzl3uMsDAANMAAVjYXVzZXQAFUxqYXZhL2xhbmcvVGhyb3dhYmxlO0wADWRldGFpbE1lc3NhZ2V0ABJMamF2YS9sYW5nL1N0cmluZztbAApzdGFja1RyYWNldAAeW0xqYXZhL2xhbmcvU3RhY2tUcmFjZUVsZW1lbnQ7eHBxAH4ABnB1cgAeW0xqYXZhLmxhbmcuU3RhY2tUcmFjZUVsZW1lbnQ7AkYqPDz9IjkCAAB4cAAAAAFzcgAbamF2YS5sYW5nLlN0YWNrVHJhY2VFbGVtZW50YQnFmiY23YUCAARJAApsaW5lTnVtYmVyTAAOZGVjbGFyaW5nQ2xhc3NxAH4ABEwACGZpbGVOYW1lcQB+AARMAAptZXRob2ROYW1lcQB+AAR4cAAAAAZ0ADxvcmcuYXBhY2hlLmxvZ2dpbmcubG9nNGouY29yZS5pbXBsLkZvcmNlTm9EZWZDbGFzc0ZvdW5kRXJyb3J0AB5Gb3JjZU5vRGVmQ2xhc3NGb3VuZEVycm9yLmphdmF0AARtYWlueA==";
         final byte[] binaryDecoded = Base64Converter
@@ -389,7 +389,7 @@ public class ThrowableProxyTest {
         final Throwable throwable = (Throwable) in.readObject();
         final ThrowableProxy subject = new ThrowableProxy(throwable);
 
-        subject.toExtendedStackTrace(stack, map, null, throwable.getStackTrace());
+        ThrowableProxyInitializationUtils.toExtendedStackTrace(subject, stack, map, null, throwable.getStackTrace());
     }
 
     /**
