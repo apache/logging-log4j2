b9ad7d581104819997df2454d2b8a00134fe485c
Marco Herrn <marco@mherrn.de>
2018-10-24 09:25:00 +0200

    Separated SequenceNumberPatternConverterTests
    - Those tests can not be run sanely in the same test case. Therefore
      they are now split again

--- log4j-core/src/test/java/org/apache/logging/log4j/core/pattern/SequenceNumberPatternConverterZeroPaddedTest.java
+++ log4j-core/src/test/java/org/apache/logging/log4j/core/pattern/SequenceNumberPatternConverterZeroPaddedTest.java
@@ -21,22 +21,28 @@ import java.util.List;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.junit.LoggerContextRule;
 import org.apache.logging.log4j.test.appender.ListAppender;
-import org.junit.ClassRule;
 import org.junit.Test;
 
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.ClassRule;
 
 /**
  *
  */
-public class SequenceNumberPatternConverterTest {
+public class SequenceNumberPatternConverterZeroPaddedTest {
 
     @ClassRule
-    public static LoggerContextRule ctx = new LoggerContextRule("SequenceNumberPatternConverterTest.yml");
+    public static LoggerContextRule ctx = new LoggerContextRule("SequenceNumberPatternConverterZeroPaddedTest.yml");
+
+    @Before
+    public void before() {
+      ctx.getListAppender("Padded").clear();
+    }
 
     @Test
-    public void testSequenceIncreases() throws Exception {
+    public void testPaddedSequence() throws Exception {
         final Logger logger = ctx.getLogger();
         logger.info("Message 1");
         logger.info("Message 2");
@@ -44,8 +50,9 @@ public class SequenceNumberPatternConverterTest {
         logger.info("Message 4");
         logger.info("Message 5");
 
-        final ListAppender app = ctx.getListAppender("List");
+        final ListAppender app = ctx.getListAppender("Padded");
         final List<String> messages = app.getMessages();
-        assertThat(messages, contains("1", "2", "3", "4", "5"));
+        System.out.println("Written messages "+messages);
+        assertThat(messages, contains("001", "002", "003", "004", "005"));
     }
 }
