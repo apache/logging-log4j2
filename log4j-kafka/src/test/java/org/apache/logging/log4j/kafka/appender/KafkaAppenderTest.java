6ce22fcf6e07da11885d9f3f432d841025d2ae84
Gary Gregory <garydgregory@gmail.com>
2018-10-30 10:36:53 -0600

    [LOG4J2-2493] Remove deprecated code.

--- log4j-kafka/src/test/java/org/apache/logging/log4j/kafka/appender/KafkaAppenderTest.java
+++ log4j-kafka/src/test/java/org/apache/logging/log4j/kafka/appender/KafkaAppenderTest.java
@@ -103,20 +103,6 @@ public class KafkaAppenderTest {
     }
 
     @Test
-    public void testAppendWithSerializedLayout() throws Exception {
-        final Appender appender = ctx.getRequiredAppender("KafkaAppenderWithSerializedLayout");
-        final LogEvent logEvent = createLogEvent();
-        appender.append(logEvent);
-        final List<ProducerRecord<byte[], byte[]>> history = kafka.history();
-        assertEquals(1, history.size());
-        final ProducerRecord<byte[], byte[]> item = history.get(0);
-        assertNotNull(item);
-        assertEquals(TOPIC_NAME, item.topic());
-        assertNull(item.key());
-        assertEquals(LOG_MESSAGE, deserializeLogEvent(item.value()).getMessage().getFormattedMessage());
-    }
-
-    @Test
     public void testAsyncAppend() throws Exception {
         final Appender appender = ctx.getRequiredAppender("AsyncKafkaAppender");
         appender.append(createLogEvent());
