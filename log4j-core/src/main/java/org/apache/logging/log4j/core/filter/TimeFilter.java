cd04ac2be0d0a9614891d48e547772691c8ad250
Gary Gregory <garydgregory@gmail.com>
2018-11-04 09:41:06 -0700

    Fix typo in variable name.

--- log4j-core/src/main/java/org/apache/logging/log4j/core/filter/TimeFilter.java
+++ log4j-core/src/main/java/org/apache/logging/log4j/core/filter/TimeFilter.java
@@ -238,10 +238,10 @@ public final class TimeFilter extends AbstractFilter {
             @PluginAttribute("onMismatch") final Result mismatch) {
         final long s = parseTimestamp(start, 0);
         final long e = parseTimestamp(end, Long.MAX_VALUE);
-        final TimeZone timezone = tz == null ? TimeZone.getDefault() : TimeZone.getTimeZone(tz);
+        final TimeZone timeZone = tz == null ? TimeZone.getDefault() : TimeZone.getTimeZone(tz);
         final Result onMatch = match == null ? Result.NEUTRAL : match;
         final Result onMismatch = mismatch == null ? Result.DENY : mismatch;
-        return new TimeFilter(s, e, timezone, onMatch, onMismatch);
+        return new TimeFilter(s, e, timeZone, onMatch, onMismatch);
     }
 
     private static long parseTimestamp(final String timestamp, final long defaultValue) {
