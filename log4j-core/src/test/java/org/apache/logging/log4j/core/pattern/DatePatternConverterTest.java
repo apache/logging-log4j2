2c530f454a5193bf9a4d54ecec9415962c0c9e0f
Gary Gregory <garydgregory@gmail.com>
2018-12-05 11:32:05 -0700

    [LOG4J2-1246] PatternLayout %date conversion pattern should render time zone designator for ISO-ISO8601.
    - Add new format in pattern layout %d{ISO8601_OFFSET_DATE_TIME_X}
    - Add new format in pattern layout %d{ISO8601_OFFSET_DATE_TIME_XX}
    - Add new format in pattern layout %d{ISO8601_OFFSET_DATE_TIME_XXX}
    - Add new format in pattern layout %d{ISO8601_OFFSET_DATE_TIME_Z}

--- log4j-core/src/test/java/org/apache/logging/log4j/core/pattern/DatePatternConverterTest.java
+++ log4j-core/src/test/java/org/apache/logging/log4j/core/pattern/DatePatternConverterTest.java
@@ -440,7 +440,8 @@ public class DatePatternConverterTest {
         converter.format(event, sb);
 
         final SimpleDateFormat sdf = new SimpleDateFormat(converter.getPattern());
-        final String expected = sdf.format(new Date(event.getTimeMillis()));
+        final String format = sdf.format(new Date(event.getTimeMillis()));
+        final String expected = format.endsWith("Z") ? format.substring(0, format.length() - 1) + "+00:00" : format;
         assertEquals(expected, sb.toString());
     }
 
