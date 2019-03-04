502f9fc700820d0b97d5aaacea89535810f96eb2
ulrichenslin <uenslin@apple.com>
2019-03-04 15:19:25 +0200

    Cleaned up unused imports

--- log4j-api/src/test/java/org/apache/logging/log4j/util/ConstantsTest.java
+++ log4j-api/src/test/java/org/apache/logging/log4j/util/ConstantsTest.java
@@ -2,16 +2,12 @@ package org.apache.logging.log4j.util;
 
 import org.junit.Test;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.Properties;
-
 import static org.junit.Assert.*;
 
 public class ConstantsTest {
 
     @Test
-    public void testJdkVersionDetection() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+    public void testJdkVersionDetection() {
         assertEquals(1, Constants.getMajorVersion("1.1.2"));
         assertEquals(8, Constants.getMajorVersion("1.8.2"));
         assertEquals(9, Constants.getMajorVersion("9.1.1"));
