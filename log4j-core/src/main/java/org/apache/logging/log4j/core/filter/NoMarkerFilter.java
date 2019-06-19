2f2d7057583bc77c49094f9693f7f574c8748f72
Ralph Goers <rgoers@apache.org>
2019-06-19 08:08:15 -0700

    LOG4J2-2427 - Add NoMarker Filter

---
+++ log4j-core/src/main/java/org/apache/logging/log4j/core/filter/NoMarkerFilter.java
@@ -0,0 +1,151 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache license, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the license for the specific language governing permissions and
+ * limitations under the license.
+ */
+package org.apache.logging.log4j.core.filter;
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.Marker;
+import org.apache.logging.log4j.core.Filter;
+import org.apache.logging.log4j.core.LogEvent;
+import org.apache.logging.log4j.core.Logger;
+import org.apache.logging.log4j.core.config.Node;
+import org.apache.logging.log4j.core.config.plugins.Plugin;
+import org.apache.logging.log4j.core.config.plugins.PluginBuilderFactory;
+import org.apache.logging.log4j.message.Message;
+
+import org.apache.logging.log4j.util.PerformanceSensitive;
+
+/**
+ * This filter returns the onMatch result if there is no marker in the LogEvent.
+ */
+@Plugin(name = "NoMarkerFilter", category = Node.CATEGORY, elementType = Filter.ELEMENT_TYPE, printObject = true)
+@PerformanceSensitive("allocation")
+public final class NoMarkerFilter extends AbstractFilter {
+
+    private NoMarkerFilter(final Result onMatch, final Result onMismatch) {
+        super(onMatch, onMismatch);
+    }
+
+    @Override
+    public Result filter(final Logger logger, final Level level, final Marker marker, final String msg,
+                         final Object... params) {
+        return filter(marker);
+    }
+
+    @Override
+    public Result filter(final Logger logger, final Level level, final Marker marker, final Object msg,
+                         final Throwable t) {
+        return filter(marker);
+    }
+
+    @Override
+    public Result filter(final Logger logger, final Level level, final Marker marker, final Message msg,
+                         final Throwable t) {
+        return filter(marker);
+    }
+
+    @Override
+    public Result filter(final LogEvent event) {
+        return filter(event.getMarker());
+    }
+
+    @Override
+    public Result filter(final Logger logger, final Level level, final Marker marker, final String msg,
+                         final Object p0) {
+        return filter(marker);
+    }
+
+    @Override
+    public Result filter(final Logger logger, final Level level, final Marker marker, final String msg,
+                         final Object p0, final Object p1) {
+        return filter(marker);
+    }
+
+    @Override
+    public Result filter(final Logger logger, final Level level, final Marker marker, final String msg,
+                         final Object p0, final Object p1, final Object p2) {
+        return filter(marker);
+    }
+
+    @Override
+    public Result filter(final Logger logger, final Level level, final Marker marker, final String msg,
+                         final Object p0, final Object p1, final Object p2, final Object p3) {
+        return filter(marker);
+    }
+
+    @Override
+    public Result filter(final Logger logger, final Level level, final Marker marker, final String msg,
+                         final Object p0, final Object p1, final Object p2, final Object p3,
+                         final Object p4) {
+        return filter(marker);
+    }
+
+    @Override
+    public Result filter(final Logger logger, final Level level, final Marker marker, final String msg,
+                         final Object p0, final Object p1, final Object p2, final Object p3,
+                         final Object p4, final Object p5) {
+        return filter(marker);
+    }
+
+    @Override
+    public Result filter(final Logger logger, final Level level, final Marker marker, final String msg,
+                         final Object p0, final Object p1, final Object p2, final Object p3,
+                         final Object p4, final Object p5, final Object p6) {
+        return filter(marker);
+    }
+
+    @Override
+    public Result filter(final Logger logger, final Level level, final Marker marker, final String msg,
+                         final Object p0, final Object p1, final Object p2, final Object p3,
+                         final Object p4, final Object p5, final Object p6,
+                         final Object p7) {
+        return filter(marker);
+    }
+
+    @Override
+    public Result filter(final Logger logger, final Level level, final Marker marker, final String msg,
+                         final Object p0, final Object p1, final Object p2, final Object p3,
+                         final Object p4, final Object p5, final Object p6,
+                         final Object p7, final Object p8) {
+        return filter(marker);
+    }
+
+    @Override
+    public Result filter(final Logger logger, final Level level, final Marker marker, final String msg,
+                         final Object p0, final Object p1, final Object p2, final Object p3,
+                         final Object p4, final Object p5, final Object p6,
+                         final Object p7, final Object p8, final Object p9) {
+        return filter(marker);
+    }
+
+    private Result filter(final Marker marker) {
+        return null == marker ? onMatch : onMismatch;
+    }
+
+
+    @PluginBuilderFactory
+    public static Builder newBuilder() {
+        return new Builder();
+    }
+
+    public static class Builder extends AbstractFilterBuilder<Builder> implements org.apache.logging.log4j.core.util.Builder<NoMarkerFilter> {
+
+        @Override
+        public NoMarkerFilter build() {
+            return new NoMarkerFilter(this.getOnMatch(), this.getOnMismatch());
+        }
+    }
+}
---
+++ log4j-core/src/test/java/org/apache/logging/log4j/core/filter/NoMarkerFilterTest.java
@@ -0,0 +1,57 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache license, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the license for the specific language governing permissions and
+ * limitations under the license.
+ */
+package org.apache.logging.log4j.core.filter;
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.Marker;
+import org.apache.logging.log4j.MarkerManager;
+import org.apache.logging.log4j.core.Filter;
+import org.apache.logging.log4j.core.LogEvent;
+import org.apache.logging.log4j.core.impl.Log4jLogEvent;
+import org.apache.logging.log4j.message.SimpleMessage;
+import org.junit.Test;
+
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+
+public class NoMarkerFilterTest {
+
+    @Test
+    public void testMarkers() {
+        final Marker sampleMarker = MarkerManager.getMarker("SampleMarker");
+        NoMarkerFilter filter = NoMarkerFilter.newBuilder().build();
+        filter.start();
+        assertTrue(filter.isStarted());
+        assertSame(Filter.Result.DENY, filter.filter(null, null, sampleMarker, (Object) null, (Throwable) null));
+        assertSame(Filter.Result.NEUTRAL, filter.filter(null, null, null, (Object) null, (Throwable) null));
+        filter.stop();
+        LogEvent event = Log4jLogEvent.newBuilder() //
+                .setLevel(Level.DEBUG) //
+                .setMessage(new SimpleMessage("Hello, world!")).build();
+        assertSame(Filter.Result.NEUTRAL, filter.filter(event));
+
+        filter.start();
+        assertSame(Filter.Result.NEUTRAL, filter.filter(event));
+        event = Log4jLogEvent.newBuilder() //
+                .setMarker(sampleMarker) //
+                .setLevel(Level.DEBUG) //
+                .setMessage(new SimpleMessage("Hello, world!")).build();
+        assertSame(Filter.Result.DENY, filter.filter(event));
+        filter.stop();
+    }
+
+}
