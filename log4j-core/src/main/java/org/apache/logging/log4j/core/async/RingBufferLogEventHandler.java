a014c003e378965796bbf0be671e204b18f7aedf
Carter Kozak <ckozak@apache.org>
2019-06-20 11:47:52 -0400

    LOG4J2-2634: Break up large AsyncLogger methods for inlining

--- log4j-core/src/main/java/org/apache/logging/log4j/core/async/RingBufferLogEventHandler.java
+++ log4j-core/src/main/java/org/apache/logging/log4j/core/async/RingBufferLogEventHandler.java
@@ -44,10 +44,13 @@ public class RingBufferLogEventHandler implements
             final boolean endOfBatch) throws Exception {
         event.execute(endOfBatch);
         event.clear();
-
         // notify the BatchEventProcessor that the sequence has progressed.
         // Without this callback the sequence would not be progressed
         // until the batch has completely finished.
+        notifyCallback(sequence);
+    }
+
+    private void notifyCallback(long sequence) {
         if (++counter > NOTIFY_PROGRESS_THRESHOLD) {
             sequenceCallback.set(sequence);
             counter = 0;
