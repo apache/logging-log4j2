42a553ed349bc0e237b0f251bb09f7859e520ee8
Gary Gregory <gardgregory@gmail.com>
2019-08-06 17:57:20 -0400

    [LOG4J2-2667] "Values not bound to statement" when using JDBC appender, appender does not respect bufferSize="0".

--- log4j-jdbc/src/main/java/org/apache/logging/log4j/jdbc/appender/JdbcDatabaseManager.java
+++ log4j-jdbc/src/main/java/org/apache/logging/log4j/jdbc/appender/JdbcDatabaseManager.java
@@ -478,7 +478,7 @@ public final class JdbcDatabaseManager extends AbstractDatabaseManager {
         final boolean closed = true;
         try {
             if (this.connection != null && !this.connection.isClosed()) {
-                if (this.isBatchSupported && this.statement != null) {
+                if (isBuffered() && this.isBatchSupported && this.statement != null) {
                     logger().debug("Executing batch PreparedStatement {}", this.statement);
                     int[] result;
                     try {
@@ -777,7 +777,7 @@ public final class JdbcDatabaseManager extends AbstractDatabaseManager {
                 }
             }
 
-            if (this.isBatchSupported) {
+            if (isBuffered() && this.isBatchSupported) {
                 this.statement.addBatch();
             } else if (this.statement.executeUpdate() == 0) {
                 throw new AppenderLoggingException(
