290e19443f0a5bbe1e93e5437ab83701e344a1bc
Federico D'Ambrosio <fedexist@gmail.com>
2019-08-29 18:30:36 +0200

    Add test

--- log4j-kafka/src/test/java/org/apache/logging/log4j/kafka/appender/KafkaAppenderTest.java
+++ log4j-kafka/src/test/java/org/apache/logging/log4j/kafka/appender/KafkaAppenderTest.java
@@ -148,6 +148,22 @@ public class KafkaAppenderTest {
     }
 
 
+    @Test
+    public void testAppenderWithTimestamp() throws Exception {
+        final Appender appender = ctx.getRequiredAppender("KafkaAppenderWithTimestamp");
+        final LogEvent logEvent = createLogEvent();
+        appender.append(logEvent);
+        final List<ProducerRecord<byte[], byte[]>> history = kafka.history();
+        assertEquals(1, history.size());
+        final ProducerRecord<byte[], byte[]> item = history.get(0);
+        assertNotNull(item);
+        assertEquals(TOPIC_NAME, item.topic());
+        byte[] keyValue = "key".getBytes(StandardCharsets.UTF_8);
+        assertArrayEquals(item.key(), keyValue);
+        assertEquals(Long.valueOf(logEvent.getTimeMillis()), item.timestamp());
+        assertEquals(LOG_MESSAGE, new String(item.value(), StandardCharsets.UTF_8));
+    }
+
     private LogEvent deserializeLogEvent(final byte[] data) throws IOException, ClassNotFoundException {
         final ByteArrayInputStream bis = new ByteArrayInputStream(data);
         try (ObjectInput ois = new FilteredObjectInputStream(bis)) {
