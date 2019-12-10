5e063cf4a6c5ad252decab94aa2aa044355afa90
Ralph Goers <rgoers@apache.org>
2019-12-10 08:36:24 -0700

    Fix unit tests

--- log4j-api/src/test/java/org/apache/logging/log4j/LoggerTest.java
+++ log4j-api/src/test/java/org/apache/logging/log4j/LoggerTest.java
@@ -63,11 +63,13 @@ public class LoggerTest {
         logger.atError().withMarker(marker).log("Hello {}", "John");
         logger.atWarn().withThrowable(new Throwable("This is a test")).log((Message) new SimpleMessage("Log4j rocks!"));
         assertEquals(3, results.size());
-        assertThat("Incorrect message 1", results.get(0), equalTo(" DEBUG org.apache.logging.log4j.LoggerTest.builder(LoggerTest.java:62) Hello"));
+        assertThat("Incorrect message 1", results.get(0),
+                equalTo(" DEBUG org.apache.logging.log4j.LoggerTest.builder(LoggerTest.java:62) Hello"));
         assertThat("Incorrect message 2", results.get(1), equalTo("test ERROR Hello John"));
         assertThat("Incorrect message 3", results.get(2),
-                startsWith(" WARN Log4j rocks! java.lang.Throwable: This is a test" + System.lineSeparator()
-                        + "\tat org.apache.logging.log4j.LoggerTest.builder(LoggerTest.java:64)"));
+                startsWith(" WARN Log4j rocks! java.lang.Throwable: This is a test"));
+        assertThat("Throwable incorrect in message 3", results.get(2),
+                containsString("at org.apache.logging.log4j.LoggerTest.builder(LoggerTest.java:64)"));
     }
 
     @Test
