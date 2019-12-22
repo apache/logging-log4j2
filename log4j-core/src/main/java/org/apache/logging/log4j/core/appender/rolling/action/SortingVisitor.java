8cfae89e1ac754d600590746882f5eac4160a0f9
Carter Kozak <ckozak@apache.org>
2019-12-22 16:54:00 -0500

    LOG4J2-2677: Rollover and Deletion handle file deletions gracefully
    It's common for multiple appenders to write (and roll) log files
    into a single directory. When these appenders are also responsible
    for cleaning up after themselves, they must also search that
    directory for files to delete. These searches are executed in
    parallel by multiple appenders and have a chance to delete files
    the other is checking before the file can be checked against
    a regex or glob.
    Missing files can safely be assumed to have been deleted.

--- log4j-core/src/main/java/org/apache/logging/log4j/core/appender/rolling/action/SortingVisitor.java
+++ log4j-core/src/main/java/org/apache/logging/log4j/core/appender/rolling/action/SortingVisitor.java
@@ -17,8 +17,12 @@
 
 package org.apache.logging.log4j.core.appender.rolling.action;
 
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.status.StatusLogger;
+
 import java.io.IOException;
 import java.nio.file.FileVisitResult;
+import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
@@ -32,6 +36,7 @@ import java.util.Objects;
  */
 public class SortingVisitor extends SimpleFileVisitor<Path> {
 
+    private static final Logger LOGGER = StatusLogger.getLogger();
     private final PathSorter sorter;
     private final List<PathWithAttributes> collected = new ArrayList<>();
 
@@ -49,6 +54,18 @@ public class SortingVisitor extends SimpleFileVisitor<Path> {
     public FileVisitResult visitFile(final Path path, final BasicFileAttributes attrs) throws IOException {
         collected.add(new PathWithAttributes(path, attrs));
         return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult visitFileFailed(Path file, IOException ioException) throws IOException {
+        // LOG4J2-2677: Appenders may rollover and purge in parallel. SimpleVisitor rethrows exceptions from
+        // failed attempts to load file attributes.
+        if (ioException instanceof NoSuchFileException) {
+            LOGGER.info("File {} could not be accessed, it has likely already been deleted", file, ioException);
+            return FileVisitResult.CONTINUE;
+        } else {
+            return super.visitFileFailed(file, ioException);
+        }
     }
     
     public List<PathWithAttributes> getSortedPaths() {
