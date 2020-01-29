9abdf717e67730a74b6ba871a33aac51ac47b333
Marius Volkhart <marius@volkhart.com>
2020-01-29 13:25:34 -0500

    LOG4J2-2768 Add log(String, ...) overloads to LogBuilder
    The overloaded methods behave similarly to log(String, Object[]), but reduce the need for an Object[] allocation in certain code paths. The added methods and their Javadoc come from the Logger class. The Javadoc have been modified with the additional of the @see reference to the Unbox class. The Unbox class did not exist when the Logger Javadoc were originally written, and it seems pertinent to make users aware of the performance benefit when using these methods.

--- log4j-api/src/main/java/org/apache/logging/log4j/LogBuilder.java
+++ log4j-api/src/main/java/org/apache/logging/log4j/LogBuilder.java
@@ -65,4 +65,162 @@ public interface LogBuilder {
     default void log(Object message) {
 
     }
+
+    /**
+     * Logs a message with parameters.
+     *
+     * @param message the message to log; the format depends on the message factory.
+     * @param p0 parameter to the message.
+     *
+     * @see org.apache.logging.log4j.util.Unbox
+     */
+    default void log(String message, Object p0) {
+    }
+
+    /**
+     * Logs a message with parameters.
+     *
+     * @param message the message to log; the format depends on the message factory.
+     * @param p0 parameter to the message.
+     * @param p1 parameter to the message.
+     *
+     * @see org.apache.logging.log4j.util.Unbox
+     */
+    default void log(String message, Object p0, Object p1) {
+    }
+
+    /**
+     * Logs a message with parameters.
+     *
+     * @param message the message to log; the format depends on the message factory.
+     * @param p0 parameter to the message.
+     * @param p1 parameter to the message.
+     * @param p2 parameter to the message.
+     *
+     * @see org.apache.logging.log4j.util.Unbox
+     */
+    default void log(String message, Object p0, Object p1, Object p2) {
+    }
+
+    /**
+     * Logs a message with parameters.
+     *
+     * @param message the message to log; the format depends on the message factory.
+     * @param p0 parameter to the message.
+     * @param p1 parameter to the message.
+     * @param p2 parameter to the message.
+     * @param p3 parameter to the message.
+     *
+     * @see org.apache.logging.log4j.util.Unbox
+     */
+    default void log(String message, Object p0, Object p1, Object p2, Object p3) {
+    }
+
+    /**
+     * Logs a message with parameters.
+     *
+     * @param message the message to log; the format depends on the message factory.
+     * @param p0 parameter to the message.
+     * @param p1 parameter to the message.
+     * @param p2 parameter to the message.
+     * @param p3 parameter to the message.
+     * @param p4 parameter to the message.
+     *
+     * @see org.apache.logging.log4j.util.Unbox
+     */
+    default void log(String message, Object p0, Object p1, Object p2, Object p3, Object p4) {
+    }
+
+    /**
+     * Logs a message with parameters.
+     *
+     * @param message the message to log; the format depends on the message factory.
+     * @param p0 parameter to the message.
+     * @param p1 parameter to the message.
+     * @param p2 parameter to the message.
+     * @param p3 parameter to the message.
+     * @param p4 parameter to the message.
+     * @param p5 parameter to the message.
+     *
+     * @see org.apache.logging.log4j.util.Unbox
+     */
+    default void log(String message, Object p0, Object p1, Object p2, Object p3, Object p4, Object p5) {
+    }
+
+    /**
+     * Logs a message with parameters.
+     *
+     * @param message the message to log; the format depends on the message factory.
+     * @param p0 parameter to the message.
+     * @param p1 parameter to the message.
+     * @param p2 parameter to the message.
+     * @param p3 parameter to the message.
+     * @param p4 parameter to the message.
+     * @param p5 parameter to the message.
+     * @param p6 parameter to the message.
+     *
+     * @see org.apache.logging.log4j.util.Unbox
+     */
+    default void log(String message, Object p0, Object p1, Object p2, Object p3, Object p4, Object p5, Object p6) {
+    }
+
+    /**
+     * Logs a message with parameters.
+     *
+     * @param message the message to log; the format depends on the message factory.
+     * @param p0 parameter to the message.
+     * @param p1 parameter to the message.
+     * @param p2 parameter to the message.
+     * @param p3 parameter to the message.
+     * @param p4 parameter to the message.
+     * @param p5 parameter to the message.
+     * @param p6 parameter to the message.
+     * @param p7 parameter to the message.
+     *
+     * @see org.apache.logging.log4j.util.Unbox
+     */
+    default void log(String message, Object p0, Object p1, Object p2, Object p3, Object p4, Object p5, Object p6,
+            Object p7) {
+    }
+
+    /**
+     * Logs a message with parameters.
+     *
+     * @param message the message to log; the format depends on the message factory.
+     * @param p0 parameter to the message.
+     * @param p1 parameter to the message.
+     * @param p2 parameter to the message.
+     * @param p3 parameter to the message.
+     * @param p4 parameter to the message.
+     * @param p5 parameter to the message.
+     * @param p6 parameter to the message.
+     * @param p7 parameter to the message.
+     * @param p8 parameter to the message.
+     *
+     * @see org.apache.logging.log4j.util.Unbox
+     */
+    default void log(String message, Object p0, Object p1, Object p2, Object p3, Object p4, Object p5, Object p6,
+            Object p7, Object p8) {
+    }
+
+    /**
+     * Logs a message with parameters.
+     *
+     * @param message the message to log; the format depends on the message factory.
+     * @param p0 parameter to the message.
+     * @param p1 parameter to the message.
+     * @param p2 parameter to the message.
+     * @param p3 parameter to the message.
+     * @param p4 parameter to the message.
+     * @param p5 parameter to the message.
+     * @param p6 parameter to the message.
+     * @param p7 parameter to the message.
+     * @param p8 parameter to the message.
+     * @param p9 parameter to the message.
+     *
+     * @see org.apache.logging.log4j.util.Unbox
+     */
+    default void log(String message, Object p0, Object p1, Object p2, Object p3, Object p4, Object p5, Object p6,
+            Object p7, Object p8, Object p9) {
+    }
 }
