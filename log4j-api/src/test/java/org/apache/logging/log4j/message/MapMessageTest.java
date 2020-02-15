65565d1043033a15c57efd5bab27e0f88e595e8e
Volkan Yazıcı <volkan.yazici@gmail.com>
2020-02-15 20:27:56 +0100

    LOG4J2-2703 Complex data type support for the default MapMessage JSON formatter. (#339)

--- log4j-api/src/test/java/org/apache/logging/log4j/message/MapMessageTest.java
+++ log4j-api/src/test/java/org/apache/logging/log4j/message/MapMessageTest.java
@@ -16,10 +16,18 @@
  */
 package org.apache.logging.log4j.message;
 
+import com.google.common.base.Strings;
 import org.apache.logging.log4j.util.StringBuilderFormattable;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  *
@@ -79,7 +87,7 @@ public class MapMessageTest {
         msg.put("message", testMsg);
         msg.put("project", "Log4j");
         final String result = msg.getFormattedMessage(new String[]{"JSON"});
-        final String expected = "{\"message\":\"Test message {}\", \"project\":\"Log4j\"}";
+        final String expected = "{'message':'Test message {}','project':'Log4j'}".replace('\'', '"');
         assertEquals(expected, result);
     }
 
@@ -102,6 +110,87 @@ public class MapMessageTest {
         final String expected =
                 "{\"one\\ntwo\":\"hello\\tworld\\r\\nhh\\bere is it\\f\"}";
         assertEquals(expected, result);
+    }
+
+    @Test
+    public void testJsonFormatterNestedObjectSupport() {
+        final String actualJson = new ObjectMapMessage()
+                .with("key1", "val1")
+                .with("key2", Collections.singletonMap("key2.1", "val2.1"))
+                .with("key3", Arrays.asList(
+                        3,
+                        (byte) 127,
+                        4.5D,
+                        4.6F,
+                        Arrays.asList(true, false),
+                        new BigDecimal(30),
+                        Collections.singletonMap("key3.3", "val3.3")))
+                .with("key4", new LinkedHashMap<String, Object>() {{
+                    put("chars", new char[]{'a', 'b', 'c'});
+                    put("booleans", new boolean[]{true, false});
+                    put("bytes", new byte[]{1, 2});
+                    put("shorts", new short[]{3, 4});
+                    put("ints", new int[]{256, 257});
+                    put("longs", new long[]{2147483648L, 2147483649L});
+                    put("floats", new float[]{1.0F, 1.1F});
+                    put("doubles", new double[]{2.0D, 2.1D});
+                    put("objects", new Object[]{"foo", "bar"});
+                }})
+                .getFormattedMessage(new String[]{"JSON"});
+        final String expectedJson = ("{" +
+                "'key1':'val1'," +
+                "'key2':{'key2.1':'val2.1'}," +
+                "'key3':[3,127,4.5,4.6,[true,false],30,{'key3.3':'val3.3'}]," +
+                "'key4':{" +
+                "'chars':['a','b','c']," +
+                "'booleans':[true,false]," +
+                "'bytes':[1,2]," +
+                "'shorts':[3,4]," +
+                "'ints':[256,257]," +
+                "'longs':[2147483648,2147483649]," +
+                "'floats':[1.0,1.1]," +
+                "'doubles':[2.0,2.1]," +
+                "'objects':['foo','bar']" +
+                "}}").replace('\'', '"');
+        assertEquals(expectedJson, actualJson);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testJsonFormatterInfiniteRecursionPrevention() {
+        final List<Object> recursiveValue = Arrays.asList(1, null);
+        // noinspection CollectionAddedToSelf
+        recursiveValue.set(1, recursiveValue);
+        new ObjectMapMessage()
+                .with("key", recursiveValue)
+                .getFormattedMessage(new String[]{"JSON"});
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testJsonFormatterMaxDepthViolation() {
+        testJsonFormatterMaxDepth(MapMessageJsonFormatter.MAX_DEPTH - 1);
+    }
+
+    @Test
+    public void testJsonFormatterMaxDepthConformance() {
+        int depth = MapMessageJsonFormatter.MAX_DEPTH - 2;
+        String expectedJson = String
+                .format("{'key':%s1%s}",
+                        Strings.repeat("[", depth),
+                        Strings.repeat("]", depth))
+                .replace('\'', '"');
+        String actualJson = testJsonFormatterMaxDepth(depth);
+        assertEquals(expectedJson, actualJson);
+    }
+
+    public static String testJsonFormatterMaxDepth(int depth) {
+        List<Object> list = new LinkedList<>();
+        list.add(1);
+        while (--depth > 0) {
+            list = new LinkedList<>(Collections.singletonList(list));
+        }
+        return new ObjectMapMessage()
+                .with("key", list)
+                .getFormattedMessage(new String[]{"JSON"});
     }
 
     @Test
@@ -151,10 +240,9 @@ public class MapMessageTest {
 
     @Test
     public void testJSONFormatNonStringValue() {
-        final ObjectMapMessage msg = new ObjectMapMessage()
-                .with("key", 1L);
+        final ObjectMapMessage msg = new ObjectMapMessage().with("key", 1L);
         final String result = msg.getFormattedMessage(new String[]{"JSON"});
-        final String expected = "{\"key\":\"1\"}";
+        final String expected = "{'key':1}".replace('\'', '"');
         assertEquals(expected, result);
     }
 
