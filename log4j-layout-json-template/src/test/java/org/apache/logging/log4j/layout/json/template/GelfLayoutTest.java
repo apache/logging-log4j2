298350532053eee962608538c51a09599577695e
Volkan Yazıcı <volkan.yazici@gmail.com>
2020-03-27 21:04:32 +0100

    #335 Add tests for GelfLayout.

---
+++ log4j-layout-json-template/src/test/java/org/apache/logging/log4j/layout/json/template/GelfLayoutTest.java
@@ -0,0 +1,112 @@
+package org.apache.logging.log4j.layout.json.template;
+
+import org.apache.logging.log4j.core.Layout;
+import org.apache.logging.log4j.core.LogEvent;
+import org.apache.logging.log4j.core.config.Configuration;
+import org.apache.logging.log4j.core.config.DefaultConfiguration;
+import org.apache.logging.log4j.core.layout.GelfLayout;
+import org.apache.logging.log4j.core.util.KeyValuePair;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+
+public class GelfLayoutTest {
+
+    private static final Configuration CONFIGURATION = new DefaultConfiguration();
+
+    private static final String HOST_NAME = "localhost";
+
+    private static final JsonTemplateLayout JSON_TEMPLATE_LAYOUT = JsonTemplateLayout
+            .newBuilder()
+            .setConfiguration(CONFIGURATION)
+            .setEventTemplateUri("classpath:GelfLayout.json")
+            .setEventTemplateAdditionalFields(
+                    JsonTemplateLayout
+                            .EventTemplateAdditionalFields
+                            .newBuilder()
+                            .setAdditionalFields(
+                                    new KeyValuePair[]{
+                                            new KeyValuePair(
+                                                    "host",
+                                                    HOST_NAME)
+                                    })
+                            .build())
+            .build();
+
+    private static final GelfLayout GELF_LAYOUT = GelfLayout
+            .newBuilder()
+            .setConfiguration(CONFIGURATION)
+            .setHost(HOST_NAME)
+            .setCompressionType(GelfLayout.CompressionType.OFF)
+            .build();
+
+    @Test
+    public void test_lite_log_events() throws Exception {
+        final List<LogEvent> logEvents = LogEventFixture.createFullLogEvents(1_000);
+        test(logEvents);
+    }
+
+    @Test
+    public void test_full_log_events() throws Exception {
+        final List<LogEvent> logEvents = LogEventFixture.createFullLogEvents(1_000);
+        test(logEvents);
+    }
+
+    private static void test(final Collection<LogEvent> logEvents) throws Exception {
+        for (final LogEvent logEvent : logEvents) {
+            test(logEvent);
+        }
+    }
+
+    private static void test(final LogEvent logEvent) throws Exception {
+        final Map<String, Object> jsonTemplateLayoutMap = renderUsingJsonTemplateLayout(logEvent);
+        final Map<String, Object> gelfLayoutMap = renderUsingGelfLayout(logEvent);
+        Assertions.assertThat(jsonTemplateLayoutMap).isEqualTo(gelfLayoutMap);
+    }
+
+    private static Map<String, Object> renderUsingJsonTemplateLayout(
+            final LogEvent logEvent)
+            throws Exception {
+        final Map<String, Object> map = renderUsing(logEvent, JSON_TEMPLATE_LAYOUT);
+        return flattenKey(map, "_mdc", "_", String::valueOf);
+    }
+
+    @SuppressWarnings("SameParameterValue")
+    private static Map<String, Object> flattenKey(
+            final Map<String, Object> map,
+            final String key,
+            final String keyPrefix,
+            final Function<Object, Object> valueMapper) {
+        final Map<String, Object> flattenedMap = new LinkedHashMap<>(map);
+        @SuppressWarnings("unchecked")
+        final Map<String, Object> subMap = (Map<String, Object>) flattenedMap.remove(key);
+        if (subMap != null) {
+            subMap.forEach((subKey, subValue) -> {
+                final Object mappedSubValue = valueMapper.apply(subValue);
+                flattenedMap.put(keyPrefix + subKey, mappedSubValue);
+            });
+        }
+        return flattenedMap;
+    }
+
+    private static Map<String, Object> renderUsingGelfLayout(
+            final LogEvent logEvent)
+            throws Exception {
+        return renderUsing(logEvent, GELF_LAYOUT);
+    }
+
+    @SuppressWarnings("unchecked")
+    private static Map<String, Object> renderUsing(
+            final LogEvent logEvent,
+            final Layout<String> layout)
+            throws Exception {
+        final String json = layout.toSerializable(logEvent);
+        return JacksonFixture.getObjectMapper().readValue(json, Map.class);
+    }
+
+}
