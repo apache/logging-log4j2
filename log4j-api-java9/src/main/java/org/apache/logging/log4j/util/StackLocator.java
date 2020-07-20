fe57a508a305f896edfc979333273b829dd07f6d
Ralph Goers <rgoers@apache.org>
2020-07-19 22:16:43 -0700

    LOG4J2-2880 - Add StackWalker benchmark. Revert back to using StackWalker.walk based on performance results

--- log4j-api-java9/src/main/java/org/apache/logging/log4j/util/StackLocator.java
+++ log4j-api-java9/src/main/java/org/apache/logging/log4j/util/StackLocator.java
@@ -17,6 +17,7 @@
 package org.apache.logging.log4j.util;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.Stack;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -32,9 +33,6 @@ public class StackLocator {
     private final static StackWalker stackWalker = StackWalker.getInstance();
 
     private final static StackLocator INSTANCE = new StackLocator();
-
-    private final static ThreadLocal<String> FQCN = new ThreadLocal<>();
-    private final static FqcnCallerLocator LOCATOR = new FqcnCallerLocator();
 
     public static StackLocator getInstance() {
         return INSTANCE;
@@ -75,36 +73,14 @@ public class StackLocator {
     }
 
     public StackTraceElement calcLocation(final String fqcnOfLogger) {
-        FQCN.set(fqcnOfLogger);
-        final StackWalker.StackFrame walk = walker.walk(LOCATOR);
-        final StackTraceElement element = walk == null ? null : walk.toStackTraceElement();
-        FQCN.set(null);
-        return element;
+        return stackWalker.walk(
+                s -> s.dropWhile(f -> !f.getClassName().equals(fqcnOfLogger)) // drop the top frames until we reach the logger
+                        .dropWhile(f -> f.getClassName().equals(fqcnOfLogger)) // drop the logger frames
+                        .findFirst()).map(StackWalker.StackFrame::toStackTraceElement).orElse(null);
     }
 
     public StackTraceElement getStackTraceElement(final int depth) {
-        return stackWalker.walk(s -> s.skip(depth).findFirst()).get().toStackTraceElement();
-    }
-
-    static final class FqcnCallerLocator implements Function<Stream<StackWalker.StackFrame>, StackWalker.StackFrame> {
-
-        @Override
-        public StackWalker.StackFrame apply(Stream<StackWalker.StackFrame> stackFrameStream) {
-            String fqcn = FQCN.get();
-            boolean foundFqcn = false;
-            Object[] frames = stackFrameStream.toArray();
-            for (int i = 0; i < frames.length ; ++i) {
-                final String className = ((StackWalker.StackFrame) frames[i]).getClassName();
-                if (!foundFqcn) {
-                    // Skip frames until we find the FQCN
-                    foundFqcn = className.equals(fqcn);
-                } else if (!className.equals(fqcn)) {
-                    // The frame is no longer equal to the FQCN so it is the one we want.
-                    return (StackWalker.StackFrame) frames[i];
-                } // Otherwise it is equal to the FQCN so we need to skip it.
-            }
-            // Should never happen
-            return null;
-        }
+        return stackWalker.walk(s -> s.skip(depth).findFirst())
+                .map(StackWalker.StackFrame::toStackTraceElement).orElse(null);
     }
 }
