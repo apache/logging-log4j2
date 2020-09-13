1ca973b5572a56bbba402cb032d2cc393ffb170d
Matt Sicker <boards@gmail.com>
2020-09-13 16:30:59 -0500

    Migrate rewrite tests to JUnit 5
    Signed-off-by: Matt Sicker <boards@gmail.com>

--- log4j-core/src/test/java/org/apache/logging/log4j/core/appender/rewrite/RewriteAppenderTest.java
+++ log4j-core/src/test/java/org/apache/logging/log4j/core/appender/rewrite/RewriteAppenderTest.java
@@ -16,51 +16,33 @@
  */
 package org.apache.logging.log4j.core.appender.rewrite;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import org.apache.logging.log4j.EventLogger;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.LogEvent;
+import org.apache.logging.log4j.core.LoggerContext;
+import org.apache.logging.log4j.hamcrest.MapMatchers;
+import org.apache.logging.log4j.junit.LoggerContextSource;
+import org.apache.logging.log4j.junit.Named;
+import org.apache.logging.log4j.message.Message;
+import org.apache.logging.log4j.message.StructuredDataMessage;
+import org.apache.logging.log4j.test.appender.ListAppender;
+import org.junit.jupiter.api.Test;
 
 import java.util.List;
 import java.util.Map;
 
-import org.apache.logging.log4j.EventLogger;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.core.LogEvent;
-import org.apache.logging.log4j.junit.LoggerContextRule;
-import org.apache.logging.log4j.message.Message;
-import org.apache.logging.log4j.message.StructuredDataMessage;
-import org.apache.logging.log4j.test.appender.ListAppender;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.ClassRule;
-import org.junit.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.*;
 
-/**
- *
- */
+@LoggerContextSource("log4j-rewrite.xml")
 public class RewriteAppenderTest {
-    private ListAppender app;
-    private ListAppender app2;
+    private final ListAppender app;
+    private final ListAppender app2;
 
-    @ClassRule
-    public static LoggerContextRule init = new LoggerContextRule("log4j-rewrite.xml");
-
-    @Before
-    public void setUp() throws Exception {
-        app = init.getListAppender("List");
-        app2 = init.getListAppender("List2");
-    }
-
-    @After
-    public void tearDown() throws Exception {
-        if (app != null) {
-            app.clear();
-        }
-        if (app2 != null) {
-            app2.clear();
-        }
+    public RewriteAppenderTest(@Named("List") final ListAppender app, @Named("List2") final ListAppender app2) {
+        this.app = app.clear();
+        this.app2 = app2.clear();
     }
 
     @Test
@@ -70,44 +52,44 @@ public class RewriteAppenderTest {
         msg.put("Key2", "Value2");
         EventLogger.logEvent(msg);
         final List<LogEvent> list = app.getEvents();
-        assertNotNull("No events generated", list);
-        assertTrue("Incorrect number of events. Expected 1, got " + list.size(), list.size() == 1);
+        assertThat(list, hasSize(1));
         final LogEvent event = list.get(0);
         final Message m = event.getMessage();
-        assertTrue("Message is not a StringMapMessage: " + m.getClass(), m instanceof StructuredDataMessage);
+        assertThat(m, instanceOf(StructuredDataMessage.class));
         final StructuredDataMessage message = (StructuredDataMessage) m;
         final Map<String, String> map = message.getData();
-        assertNotNull("No Map", map);
-        assertTrue("Incorrect number of map entries, expected 3 got " + map.size(), map.size() == 3);
+        assertNotNull(map, "No Map");
+        assertThat(map, MapMatchers.hasSize(3));
         final String value = map.get("Key1");
         assertEquals("Apache", value);
     }
 
 
     @Test
-    public void testProperties() {
-        final Logger logger = LogManager.getLogger(RewriteAppenderTest.class);
+    public void testProperties(final LoggerContext context) {
+        final Logger logger = context.getLogger(RewriteAppenderTest.class);
         logger.debug("Test properties rewrite");
         final List<String> list = app2.getMessages();
-        assertNotNull("No events generated", list);
-        assertTrue("Incorrect number of events. Expected 1, got " + list.size(), list.size() == 1);
-        assertFalse("Did not resolve user name", list.get(0).contains("{user.dir}"));
+        assertThat(list, hasSize(1));
+        assertThat(list.get(0), not(containsString("{user.dir}")));
+        assertNotNull(list, "No events generated");
+        assertEquals(list.size(), 1, "Incorrect number of events. Expected 1, got " + list.size());
+        assertFalse(list.get(0).contains("{user."), "Did not resolve user name");
     }
 
 
     @Test
-    public void testFilter() {
+    public void testFilter(final LoggerContext context) {
         StructuredDataMessage msg = new StructuredDataMessage("Test", "This is a test", "Service");
         msg.put("Key1", "Value2");
         msg.put("Key2", "Value1");
-        final Logger logger = LogManager.getLogger("org.apache.logging.log4j.core.Logging");
+        final Logger logger = context.getLogger("org.apache.logging.log4j.core.Logging");
         logger.debug(msg);
         msg = new StructuredDataMessage("Test", "This is a test", "Service");
         msg.put("Key1", "Value1");
         msg.put("Key2", "Value2");
         logger.trace(msg);
 
-        final List<LogEvent> list = app.getEvents();
-        assertTrue("Events were generated", list == null || list.isEmpty());
+        assertThat(app.getEvents(), empty());
     }
 }
