933ba9ae4a023643e5186a22faed47bab4f5498f
Volkan Yazici <volkan.yazici@gmail.com>
2020-09-22 21:49:52 +0200

    Workarounds for Windows-specific "file being used by another process" failures.

--- log4j-core/src/test/java/org/apache/logging/log4j/core/appender/RandomAccessFileManagerTest.java
+++ log4j-core/src/test/java/org/apache/logging/log4j/core/appender/RandomAccessFileManagerTest.java
@@ -38,12 +38,11 @@ public class RandomAccessFileManagerTest {
 
     /**
      * Test method for
-     * {@link org.apache.logging.log4j.core.appender.RandomAccessFileManager#writeBytes(byte[], int, int)}
-     * .
+     * {@link org.apache.logging.log4j.core.appender.RandomAccessFileManager#writeBytes(byte[], int, int)}.
      */
     @Test
     public void testWrite_multiplesOfBufferSize() throws IOException {
-        final File file = new File(tempDir, "random-access-file.bin");
+        final File file = new File(tempDir, "testWrite_multiplesOfBufferSize.bin");
         try (final RandomAccessFile raf = new RandomAccessFile(file, "rw")) {
             final OutputStream os = NullOutputStream.getInstance();
             final RandomAccessFileManager manager = new RandomAccessFileManager(null, raf, file.getName(),
@@ -65,7 +64,7 @@ public class RandomAccessFileManagerTest {
      */
     @Test
     public void testWrite_dataExceedingBufferSize() throws IOException {
-        final File file = new File(tempDir, "random-access-file.bin");
+        final File file = new File(tempDir, "testWrite_dataExceedingBufferSize.bin");
         try (final RandomAccessFile raf = new RandomAccessFile(file, "rw")) {
             final OutputStream os = NullOutputStream.getInstance();
             final RandomAccessFileManager manager = new RandomAccessFileManager(null, raf, file.getName(),
@@ -84,7 +83,7 @@ public class RandomAccessFileManagerTest {
 
     @Test
     public void testConfigurableBufferSize() throws IOException {
-        final File file = new File(tempDir, "random-access-file.bin");
+        final File file = new File(tempDir, "testConfigurableBufferSize.bin");
         try (final RandomAccessFile raf = new RandomAccessFile(file, "rw")) {
             final OutputStream os = NullOutputStream.getInstance();
             final int bufferSize = 4 * 1024;
@@ -100,7 +99,7 @@ public class RandomAccessFileManagerTest {
 
     @Test
     public void testWrite_dataExceedingMinBufferSize() throws IOException {
-        final File file = new File(tempDir, "random-access-file.bin");
+        final File file = new File(tempDir, "testWrite_dataExceedingMinBufferSize.bin");
         try (final RandomAccessFile raf = new RandomAccessFile(file, "rw")) {
             final OutputStream os = NullOutputStream.getInstance();
             final int bufferSize = 1;
@@ -121,7 +120,7 @@ public class RandomAccessFileManagerTest {
     @Test
     public void testAppendDoesNotOverwriteExistingFile() throws IOException {
         final boolean isAppend = true;
-        final File file = new File(tempDir, "random-access-file.bin");
+        final File file = new File(tempDir, "testAppendDoesNotOverwriteExistingFile.bin");
         assertEquals(0, file.length());
 
         final byte[] bytes = new byte[4 * 1024];
