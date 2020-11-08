c94d5d0f30efa3af341543260635cfdcccdf3f92
Sandeep Barnwal <sandeep2960@gmail.com>
2020-11-08 19:42:05 +0530

    OG4J2-2850
    OG4J2-2850 : LocalizedMessageFactory creates new message with baseName as messagePattern

--- log4j-api/src/main/java/org/apache/logging/log4j/message/LocalizedMessageFactory.java
+++ log4j-api/src/main/java/org/apache/logging/log4j/message/LocalizedMessageFactory.java
@@ -70,7 +70,7 @@ public class LocalizedMessageFactory implements MessageFactory, Serializable {
     @Override
     public Message newMessage(final String key) {
         if (resourceBundle == null) {
-            return new LocalizedMessage(baseName,  key);
+            return new LocalizedMessage(baseName,  key, null);
         }
         return new LocalizedMessage(resourceBundle, key);
     }
