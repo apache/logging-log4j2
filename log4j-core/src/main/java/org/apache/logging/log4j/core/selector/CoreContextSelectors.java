702787e1d37f9becec1751b819013e687e588bb5
Carter Kozak <ckozak@apache.org>
2021-03-22 14:52:11 -0400

    LOG4J2-2940: Implement BasicAsyncLoggerContextSelector
    The BasicAsyncLoggerContextSelector is equivalent to the
    AsyncLoggerContextSelector without ClassLoader introspection
    and associated overhead.

--- log4j-core/src/main/java/org/apache/logging/log4j/core/selector/CoreContextSelectors.java
+++ log4j-core/src/main/java/org/apache/logging/log4j/core/selector/CoreContextSelectors.java
@@ -17,9 +17,15 @@
 package org.apache.logging.log4j.core.selector;
 
 import org.apache.logging.log4j.core.async.AsyncLoggerContextSelector;
+import org.apache.logging.log4j.core.async.BasicAsyncLoggerContextSelector;
 
 public class CoreContextSelectors {
 
-    public static final Class<?>[] CLASSES = new Class<?>[] { ClassLoaderContextSelector.class, BasicContextSelector.class, AsyncLoggerContextSelector.class };
+    public static final Class<?>[] CLASSES = new Class<?>[] {
+            ClassLoaderContextSelector.class,
+            BasicContextSelector.class,
+            AsyncLoggerContextSelector.class,
+            BasicAsyncLoggerContextSelector.class
+    };
 
 }
