apache/logging-log4j-flume

LOG4J2-3056 Refactor MD5 usage for sharing sensitive information (#484)

log4j-flume-ng/src/main/java/org/apache/logging/log4j/flume/appender/FlumeEmbeddedManager.java
@@ -32,16 +32,13 @@ import org.apache.logging.log4j.core.util.NameUtil;
 import org.apache.logging.log4j.util.PropertiesUtil;
 import org.apache.logging.log4j.util.Strings;
 
-/**
- *
- */
 public class FlumeEmbeddedManager extends AbstractFlumeManager {
 
     private static final String FILE_SEP = PropertiesUtil.getProperties().getStringProperty("file.separator");
 
     private static final String IN_MEMORY = "InMemory";
 
-    private static FlumeManagerFactory factory = new FlumeManagerFactory();
+    private static final FlumeManagerFactory FACTORY = new FlumeManagerFactory();
 
     private final EmbeddedAgent agent;
 
@@ -82,6 +79,17 @@ public class FlumeEmbeddedManager extends AbstractFlumeManager {
             throw new IllegalArgumentException("Cannot configure both Agents and Properties.");
         }
 
+        final String extendedName = extendManagerName(name, agents, properties);
+        return getManager(extendedName, FACTORY,
+                new FactoryData(name, agents, properties, batchSize, dataDir));
+
+    }
+
+    private static String extendManagerName(
+            final String name,
+            final Agent[] agents,
+            final Property[] properties) {
+
         final StringBuilder sb = new StringBuilder();
         boolean first = true;
 
@@ -106,8 +114,9 @@ public class FlumeEmbeddedManager extends AbstractFlumeManager {
             }
             sb.append(NameUtil.md5(props.toString()));
         }
-        return getManager(sb.toString(), factory,
-                new FactoryData(name, agents, properties, batchSize, dataDir));
+
+        return sb.toString();
+
     }
 
     @Override

apache/logging-log4j2

log4j-flume-ng/src/main/java/org/apache/logging/log4j/flume/appender/FlumeEmbeddedManager.java
@@ -82,6 +82,17 @@ public class FlumeEmbeddedManager extends AbstractFlumeManager {
             throw new IllegalArgumentException("Cannot configure both Agents and Properties.");
         }
 
+        final String managerName = createManagerName(name, agents, properties);
+        return getManager(managerName, factory,
+                new FactoryData(name, agents, properties, batchSize, dataDir));
+
+    }
+
+    private static String createManagerName(
+            final String name,
+            final Agent[] agents,
+            final Property[] properties) {
+
         final StringBuilder sb = new StringBuilder();
         boolean first = true;
 
@@ -106,8 +117,9 @@ public class FlumeEmbeddedManager extends AbstractFlumeManager {
             }
             sb.append(NameUtil.md5(props.toString()));
         }
-        return getManager(sb.toString(), factory,
-                new FactoryData(name, agents, properties, batchSize, dataDir));
+
+        return sb.toString();
+
     }
 
     @Override