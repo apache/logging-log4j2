391664b39ece1dd8645789a3baad1b39c0164236
Tim Perry <perry2of5@yahoo.com>
2021-05-05 11:21:50 -0700

    LOG4J2-3089 use ephemeral port to avoid locking.

--- log4j-layout-template-json/src/test/java/org/apache/logging/log4j/layout/template/json/JsonTemplateLayoutNullEventDelimiterTest.java
+++ log4j-layout-template-json/src/test/java/org/apache/logging/log4j/layout/template/json/JsonTemplateLayoutNullEventDelimiterTest.java
@@ -23,30 +23,25 @@ import org.assertj.core.api.Assertions;
 import org.awaitility.Awaitility;
 import org.junit.jupiter.api.Test;
 
+import java.io.BufferedWriter;
 import java.io.ByteArrayOutputStream;
 import java.io.EOFException;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStreamWriter;
+import java.nio.file.Files;
+import java.nio.charset.StandardCharsets;
 import java.net.BindException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.time.Duration;
 
 public class JsonTemplateLayoutNullEventDelimiterTest {
-
-    // Set the configuration.
-    static {
-        System.setProperty(
-                "log4j.configurationFile",
-                "nullEventDelimitedJsonTemplateLayoutLogging.xml");
-    }
-
-    // Note that this port is hardcoded in the configuration file too!
-    private static final int PORT = 50514;
-
     @Test
     public void test() throws Exception {
-
+        File tempConfig = File.createTempFile("nullEventDelimitedJsonTemplateLayoutLogging", ".xml");
         // Set the expected bytes.
         final byte[] expectedBytes = {
                 '"', 'f', 'o', 'o', '"', '\0',
@@ -54,7 +49,12 @@ public class JsonTemplateLayoutNullEventDelimiterTest {
         };
 
         // Start the TCP server.
-        try (final TcpServer server = new TcpServer(PORT)) {
+        try (final TcpServer server = new TcpServer(0)) {
+            makeTempConfig(server.getLocalPort(), tempConfig);
+            System.setProperty(
+                    "log4j.configurationFile",
+                    tempConfig.getAbsolutePath());
+
 
             // Produce log events.
             final Logger logger = LogManager.getLogger(JsonTemplateLayoutNullEventDelimiterTest.class);
@@ -71,11 +71,22 @@ public class JsonTemplateLayoutNullEventDelimiterTest {
             // Verify the received log events.
             final byte[] actualBytes = server.getReceivedBytes();
             Assertions.assertThat(actualBytes).startsWith(expectedBytes);
-
-        } catch (BindException bindException) {
-            System.err.println("Required port " + PORT + " not available, skipping " + getClass().getSimpleName());
+        } finally {
+            tempConfig.delete();
         }
 
+    }
+
+    public void makeTempConfig(final int serverPort, final File tempConfigFile) throws IOException {
+        try (FileOutputStream fos = new FileOutputStream(tempConfigFile);
+                OutputStreamWriter osw = new OutputStreamWriter(fos, StandardCharsets.UTF_8);
+                BufferedWriter bw = new BufferedWriter(osw);) {
+            for (String line : Files.readAllLines(
+                    new File("src/test/resources/nullEventDelimitedJsonTemplateLayoutLogging.xml").toPath())) {
+                bw.write(line.replace("TARGET_PORT_NUMBER", Integer.toString(serverPort)));
+                bw.newLine();
+            }
+        }
     }
 
     private static final class TcpServer extends Thread implements AutoCloseable {
@@ -95,6 +106,10 @@ public class JsonTemplateLayoutNullEventDelimiterTest {
             serverSocket.setSoTimeout(5_000);
             setDaemon(true);
             start();
+        }
+
+        public int getLocalPort() {
+            return serverSocket.getLocalPort();
         }
 
         @Override
