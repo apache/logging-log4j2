81aa8dbcef3b3999496dffd6ae605c6624e46fd1
Raman Gupta <rocketraman@gmail.com>
2021-07-07 10:27:50 -0400

    GCP layout test enhancement and stack trace fixes

--- log4j-layout-template-json/src/test/java/org/apache/logging/log4j/layout/template/json/GcpLayoutTest.java
+++ log4j-layout-template-json/src/test/java/org/apache/logging/log4j/layout/template/json/GcpLayoutTest.java
@@ -16,17 +16,22 @@
  */
 package org.apache.logging.log4j.layout.template.json;
 
+import com.google.common.base.Charsets;
 import org.apache.logging.log4j.core.LogEvent;
 import org.apache.logging.log4j.core.config.Configuration;
 import org.apache.logging.log4j.core.config.DefaultConfiguration;
+import org.apache.logging.log4j.core.impl.ThrowableProxy;
+import org.apache.logging.log4j.util.SortedArrayStringMap;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
+import java.time.Instant;
+import java.util.*;
 
 import static org.apache.logging.log4j.layout.template.json.TestHelpers.serializeUsingLayout;
 
@@ -63,9 +68,78 @@ class GcpLayoutTest {
         }
     }
 
+    @SuppressWarnings("rawtypes")
     private static void test(final LogEvent logEvent) {
         final Map<String, Object> jsonTemplateLayoutMap = renderUsingJsonTemplateLayout(logEvent);
-        // for now, we just validate there are no errors
+        Assertions.assertThat(Instant.parse(jsonTemplateLayoutMap.get("timestamp").toString()).toEpochMilli())
+          .isEqualTo(logEvent.getTimeMillis());
+        Assertions.assertThat(jsonTemplateLayoutMap.get("severity")).isEqualTo(logEvent.getLevel().toString());
+        Assertions.assertThat(jsonTemplateLayoutMap.get("_logger")).isEqualTo(logEvent.getLoggerName());
+        Assertions.assertThat(jsonTemplateLayoutMap.get("_thread").toString())
+          .isEqualTo(logEvent.getThreadName());
+
+        if (logEvent.getContextData().size() == 0) {
+            Assertions.assertThat(jsonTemplateLayoutMap.get("logging.googleapis.com/labels"))
+              .isNull();
+        } else {
+            Assertions.assertThat(logEvent.getContextData().toMap().keySet())
+              .isEqualTo(((Map)jsonTemplateLayoutMap.get("logging.googleapis.com/labels")).keySet());
+            logEvent.getContextData().toMap().keySet().forEach(k -> {
+                Assertions.assertThat((Object) logEvent.getContextData().getValue(k))
+                  .isEqualTo(((Map)jsonTemplateLayoutMap.get("logging.googleapis.com/labels")).get(k));
+            });
+        }
+
+        if (logEvent.getSource() == null) {
+            // not quite sure why the output has "?." in sourceLocation.function
+            //Assertions.assertThat(jsonTemplateLayoutMap.get("logging.googleapis.com/sourceLocation"))
+            //  .isNull();
+        } else {
+            if (logEvent.getSource().getFileName() == null || !logEvent.isIncludeLocation()) {
+                Assertions.assertThat(((Map) jsonTemplateLayoutMap.get("logging.googleapis.com/sourceLocation")).get("file"))
+                  .isNull();
+            } else {
+                Assertions.assertThat(((Map) jsonTemplateLayoutMap.get("logging.googleapis.com/sourceLocation")).get("file"))
+                  .isEqualTo(logEvent.getSource().getFileName());
+            }
+            if (logEvent.getSource().getLineNumber() < 0 || !logEvent.isIncludeLocation()) {
+                Assertions.assertThat(((Map) jsonTemplateLayoutMap.get("logging.googleapis.com/sourceLocation")).get("line"))
+                  .isNull();
+            } else {
+                Assertions.assertThat(((Map) jsonTemplateLayoutMap.get("logging.googleapis.com/sourceLocation")).get("line"))
+                  .isEqualTo(logEvent.getSource().getLineNumber());
+            }
+            Assertions.assertThat(((Map) jsonTemplateLayoutMap.get("logging.googleapis.com/sourceLocation")).get("function"))
+              .isEqualTo(logEvent.getSource().getClassName() + "." + logEvent.getSource().getMethodName());
+        }
+
+        // NOTE: no access to serial number in logEvent
+        if (logEvent.getThrown() == null) {
+            Assertions.assertThat(jsonTemplateLayoutMap.get("_exception"))
+              .isNull();
+            Assertions.assertThat(jsonTemplateLayoutMap.get("message"))
+              .isEqualTo(logEvent.getMessage().toString());
+        } else {
+            // the message field includes class packaging information as it uses the %xEx pattern,
+            // this fails as extendedStackTrace is truncated for some reason
+            //String extendedStackTrace = logEvent.getThrownProxy().getExtendedStackTraceAsString();
+            //Assertions.assertThat(jsonTemplateLayoutMap.get("message"))
+            //  .isEqualTo(logEvent.getMessage().toString() + " " + extendedStackTrace);
+
+            // for now, just compare the first line and that we have multiple stack trace lines
+            String[] messageLines = jsonTemplateLayoutMap.get("message").toString().split(System.lineSeparator());
+            Assertions.assertThat(messageLines[0])
+              .isEqualTo(logEvent.getMessage().toString() + " " + logEvent.getThrown().getClass().getName() + ": " + logEvent.getThrown().getMessage());
+            Assertions.assertThat(messageLines.length)
+              .isGreaterThan(1);
+
+            Assertions.assertThat(((Map) jsonTemplateLayoutMap.get("_exception")).get("exception_class"))
+              .isEqualTo(logEvent.getThrown().getClass().getName());
+            Assertions.assertThat(((Map) jsonTemplateLayoutMap.get("_exception")).get("exception_message"))
+              .isEqualTo(logEvent.getThrown().getMessage());
+            Assertions.assertThat(((Map) jsonTemplateLayoutMap.get("_exception")).get("stacktrace"))
+              .isEqualTo(serializeStackTrace(Charsets.UTF_8, logEvent.getThrown()));
+        }
     }
 
     private static Map<String, Object> renderUsingJsonTemplateLayout(
@@ -73,4 +147,17 @@ class GcpLayoutTest {
         return serializeUsingLayout(logEvent, JSON_TEMPLATE_LAYOUT);
     }
 
+    private static String serializeStackTrace(
+      final Charset charset,
+      final Throwable exception) {
+        final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        final String charsetName = charset.name();
+        try (final PrintStream printStream =
+               new PrintStream(outputStream, false, charsetName)) {
+            exception.printStackTrace(printStream);
+            return outputStream.toString(charsetName);
+        }  catch (final UnsupportedEncodingException error) {
+            throw new RuntimeException("failed converting the stack trace to string", error);
+        }
+    }
 }
