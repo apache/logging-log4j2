33be70406043ad436773500829c148e00a4fb1a6
Volkan Yazici <volkan@yazi.ci>
2021-10-20 16:31:31 +0200

    LOG4J2-2829 SocketAppender should propagate failures when reconnection fails.

--- log4j-core/src/main/java/org/apache/logging/log4j/core/net/TcpSocketManager.java
+++ log4j-core/src/main/java/org/apache/logging/log4j/core/net/TcpSocketManager.java
@@ -218,8 +218,8 @@ public class TcpSocketManager extends AbstractSocketManager {
             try {
                 writeAndFlush(bytes, offset, length, immediateFlush);
             } catch (final IOException causeEx) {
+                final String config = inetAddress + ":" + port;
                 if (retry && reconnector == null) {
-                    final String config = inetAddress + ":" + port;
                     reconnector = createReconnector();
                     try {
                         reconnector.reconnect();
@@ -238,7 +238,10 @@ public class TcpSocketManager extends AbstractSocketManager {
                                         config),
                                 causeEx);
                     }
+                    return;
                 }
+                final String message = String.format("Error writing to %s for connection %s", getName(), config);
+                throw new AppenderLoggingException(message);
             }
         }
     }
