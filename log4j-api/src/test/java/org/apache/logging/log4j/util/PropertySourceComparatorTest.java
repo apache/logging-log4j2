7d9f2a466cba6a4a84cfd453e993dea5c5c30842
Dave Burke <david.burke@securian.com>
2021-11-22 15:21:27 -0600

    LOG4J2-3193 Reverse the order of PropertySource.Comparator

---
+++ log4j-api/src/test/java/org/apache/logging/log4j/util/PropertySourceComparatorTest.java
@@ -0,0 +1,44 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache license, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the license for the specific language governing permissions and
+ * limitations under the license.
+ */
+package org.apache.logging.log4j.util;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import org.junit.jupiter.api.Test;
+
+public class PropertySourceComparatorTest {
+
+	@Test
+    public void testPriorityOrder() {
+		List<PropertySource> sources = new ArrayList<>();
+		sources.add(new EnvironmentPropertySource());
+		sources.add(new PropertiesPropertySource(new Properties()));
+		sources.add(new SystemPropertiesPropertySource());
+		
+		sources.sort(new PropertySource.Comparator());
+		
+		// When sources are applied, higher (later) priority sources override
+		// lower (earlier) priority sources.
+		assertTrue(sources.get(0) instanceof SystemPropertiesPropertySource);
+		assertTrue(sources.get(1) instanceof PropertiesPropertySource);
+		assertTrue(sources.get(2) instanceof EnvironmentPropertySource);
+    }
+}
