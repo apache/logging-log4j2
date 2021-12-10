ce7a3420840db12ae7fb3725ef297556f7943bc6
Josh Soref <jsoref@users.noreply.github.com>
2021-12-09 23:38:16 -0500

    spelling: contains
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- log4j-1.2-api/src/main/java/org/apache/log4j/helpers/OptionConverter.java
+++ log4j-1.2-api/src/main/java/org/apache/log4j/helpers/OptionConverter.java
@@ -306,7 +306,7 @@ public class OptionConverter {
                 // no more variables
                 if (i == 0) { // this is a simple string
                     return val;
-                } else { // add the tail string which contails no variables and return the result.
+                } else { // add the tail string which contains no variables and return the result.
                     sbuf.append(val.substring(i, val.length()));
                     return sbuf.toString();
                 }
--- log4j-core/src/main/java/org/apache/logging/log4j/core/util/OptionConverter.java
+++ log4j-core/src/main/java/org/apache/logging/log4j/core/util/OptionConverter.java
@@ -362,7 +362,7 @@ public final class OptionConverter {
                 if (i == 0) { // this is a simple string
                     return val;
                 }
-                // add the tail string which contails no variables and return the result.
+                // add the tail string which contains no variables and return the result.
                 sbuf.append(val.substring(i, val.length()));
                 return sbuf.toString();
             }
