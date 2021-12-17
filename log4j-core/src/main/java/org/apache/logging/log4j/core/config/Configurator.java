2322f197df2238a3c53372d3bce1fc5a0f0c0f17
Gary Gregory <garydgregory@gmail.com>
2021-12-16 21:16:09 -0500

    No need to nest.

--- log4j-core/src/main/java/org/apache/logging/log4j/core/config/Configurator.java
+++ log4j-core/src/main/java/org/apache/logging/log4j/core/config/Configurator.java
@@ -46,14 +46,14 @@ public final class Configurator {
         final LoggerContextFactory factory = LogManager.getFactory();
         if (factory instanceof Log4jContextFactory) {
             return (Log4jContextFactory) factory;
-        } else if (factory != null) {
+        }
+        if (factory != null) {
             LOGGER.error("LogManager returned an instance of {} which does not implement {}. Unable to initialize Log4j.",
                     factory.getClass().getName(), Log4jContextFactory.class.getName());
-            return null;
         } else {
             LOGGER.fatal("LogManager did not return a LoggerContextFactory. This indicates something has gone terribly wrong!");
-            return null;
         }
+        return null;
     }
 
     /**
