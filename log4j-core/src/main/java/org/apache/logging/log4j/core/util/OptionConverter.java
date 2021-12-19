6bd519e26d53317ef8597fe439272a26e063149d
Gary Gregory <garydgregory@gmail.com>
2021-12-19 10:33:42 -0500

    Reuse StrSubstitutor.

--- log4j-core/src/main/java/org/apache/logging/log4j/core/util/OptionConverter.java
+++ log4j-core/src/main/java/org/apache/logging/log4j/core/util/OptionConverter.java
@@ -22,9 +22,8 @@ import java.util.Properties;
 
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.lookup.StrSubstitutor;
 import org.apache.logging.log4j.status.StatusLogger;
-import org.apache.logging.log4j.util.PropertiesUtil;
-import org.apache.logging.log4j.util.Strings;
 
 /**
  * A convenience class to convert property values to specific types.
@@ -33,10 +32,6 @@ public final class OptionConverter {
 
     private static final Logger LOGGER = StatusLogger.getLogger();
 
-    private static final String DELIM_START = "${";
-    private static final char DELIM_STOP = '}';
-    private static final int DELIM_START_LEN = 2;
-    private static final int DELIM_STOP_LEN = 1;
     private static final int ONE_K = 1024;
 
     /**
@@ -347,50 +342,6 @@ public final class OptionConverter {
      */
     public static String substVars(final String val, final Properties props) throws
         IllegalArgumentException {
-
-        final StringBuilder sbuf = new StringBuilder();
-
-        int i = 0;
-        int j;
-        int k;
-
-        while (true) {
-            j = val.indexOf(DELIM_START, i);
-            if (j == -1) {
-                // no more variables
-                if (i == 0) { // this is a simple string
-                    return val;
-                }
-                // add the tail string which contails no variables and return the result.
-                sbuf.append(val.substring(i, val.length()));
-                return sbuf.toString();
-            }
-            sbuf.append(val.substring(i, j));
-            k = val.indexOf(DELIM_STOP, j);
-            if (k == -1) {
-                throw new IllegalArgumentException(Strings.dquote(val)
-                    + " has no closing brace. Opening brace at position " + j
-                    + '.');
-            }
-            j += DELIM_START_LEN;
-            final String key = val.substring(j, k);
-            // first try in System properties
-            String replacement = PropertiesUtil.getProperties().getStringProperty(key, null);
-            // then try props parameter
-            if (replacement == null && props != null) {
-                replacement = props.getProperty(key);
-            }
-
-            if (replacement != null) {
-                // Do variable substitution on the replacement string
-                // such that we can solve "Hello ${x2}" as "Hello p1"
-                // the where the properties are
-                // x1=p1
-                // x2=${x1}
-                final String recursiveReplacement = substVars(replacement, props);
-                sbuf.append(recursiveReplacement);
-            }
-            i = k + DELIM_STOP_LEN;
-        }
+        return StrSubstitutor.replace(val, props);
     }
 }
