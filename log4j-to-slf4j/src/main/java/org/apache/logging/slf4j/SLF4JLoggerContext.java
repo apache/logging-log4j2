06c61ddc60b24610fda694aa54f4c1dd2e29ff07
Michael Vorburger <vorburger@google.com>
2021-12-23 17:20:43 +0100

    Fix MessageFactory support bug in SLF4JLoggerContext
    as well as a related typo in LoggerTest

--- log4j-to-slf4j/src/main/java/org/apache/logging/slf4j/SLF4JLoggerContext.java
+++ log4j-to-slf4j/src/main/java/org/apache/logging/slf4j/SLF4JLoggerContext.java
@@ -43,17 +43,11 @@ public class SLF4JLoggerContext implements LoggerContext {
 
     @Override
     public ExtendedLogger getLogger(final String name, final MessageFactory messageFactory) {
-        // FIXME according to LOG4J2-1180, the below line should be:
-        // FIXME if (!loggerRegistry.hasLogger(name, messageFactory)) {
-        if (!loggerRegistry.hasLogger(name)) {
-            // FIXME: should be loggerRegistry.putIfAbsent(name, messageFactory,
-            loggerRegistry.putIfAbsent(name, null,
+        if (!loggerRegistry.hasLogger(name, messageFactory)) {
+            loggerRegistry.putIfAbsent(name, messageFactory,
                     new SLF4JLogger(name, messageFactory, LoggerFactory.getLogger(name)));
         }
-        // FIXME should be return loggerRegistry.getLogger(name, messageFactory);
-        return loggerRegistry.getLogger(name);
-
-        // TODO applying the above fixes causes (log4j-to-slf4j) LoggerTest to fail
+        return loggerRegistry.getLogger(name, messageFactory);
     }
 
     @Override
