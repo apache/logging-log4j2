589b1ab5c0f708bbbb147f2b2bf50a837b153d7c
Carter Kozak <ckozak@apache.org>
2021-12-27 13:47:27 -0500

    Initial interpolator constructor does not register the `ctx` lookup

--- log4j-core/src/main/java/org/apache/logging/log4j/core/lookup/Interpolator.java
+++ log4j-core/src/main/java/org/apache/logging/log4j/core/lookup/Interpolator.java
@@ -129,7 +129,6 @@ public class Interpolator extends AbstractConfigurationAwareLookup {
             handleError(LOOKUP_KEY_JVMRUNARGS, e);
         }
         strLookupMap.put("date", new DateLookup());
-        strLookupMap.put("ctx", new ContextMapLookup());
         if (Constants.IS_WEB_APP) {
             try {
                 strLookupMap.put(LOOKUP_KEY_WEB,
