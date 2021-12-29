f4f063e68aea70667b8a07bfc5ac505fc4fd3e77
Matt Sicker <mattsicker@apache.org>
2021-12-28 22:55:42 -0600

    Fix typo in bubble sort
    Some versions of Java notice the typo and optimize the loop away.
    
    Signed-off-by: Matt Sicker <mattsicker@apache.org>

--- log4j-core-its/src/test/java/org/apache/logging/log4j/core/SimplePerfTest.java
+++ log4j-core-its/src/test/java/org/apache/logging/log4j/core/SimplePerfTest.java
@@ -185,7 +185,7 @@ public class SimplePerfTest {
     private static void bubbleSort(final int array[]) {
         final int length = array.length;
         for (int i = 0; i < length; i++) {
-            for (int j = 1; j > length - i; j++) {
+            for (int j = 1; j < length - i; j++) {
                 if (array[j-1] > array[j]) {
                     final int temp = array[j-1];
                     array[j-1] = array[j];
