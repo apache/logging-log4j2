fbe8976ff2451493ef6c746129b29c0e14c8769d
Gary Gregory <garydgregory@gmail.com>
2021-12-31 12:40:10 -0500

    Fix compilation error.

--- log4j-1.2-api/src/test/java/org/apache/log4j/CategoryTest.java
+++ log4j-1.2-api/src/test/java/org/apache/log4j/CategoryTest.java
@@ -41,6 +41,7 @@ import java.util.function.Consumer;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 /**
