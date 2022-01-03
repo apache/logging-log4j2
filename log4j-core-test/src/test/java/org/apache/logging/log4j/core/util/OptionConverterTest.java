69932f0a9bf5c3688c0b62aec3e6625e5a28cb15
Ralph Goers <rgoers@apache.org>
2022-01-03 15:30:22 -0700

    LOG4J2-3306 - OptionConverter could cause a StackOverflowError

--- log4j-core-test/src/test/java/org/apache/logging/log4j/core/util/OptionConverterTest.java
+++ log4j-core-test/src/test/java/org/apache/logging/log4j/core/util/OptionConverterTest.java
@@ -16,11 +16,11 @@
  */
 package org.apache.logging.log4j.core.util;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import java.util.Properties;
 
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * Tests {@link OptionConverter}.
