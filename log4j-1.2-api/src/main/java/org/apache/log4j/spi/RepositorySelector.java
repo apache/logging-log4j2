84b3082308bc95ea41955049861baa1bfffcddf4
Gary Gregory <garydgregory@gmail.com>
2022-01-07 10:53:06 -0500

    Javadoc cleanup.

--- log4j-1.2-api/src/main/java/org/apache/log4j/spi/RepositorySelector.java
+++ log4j-1.2-api/src/main/java/org/apache/log4j/spi/RepositorySelector.java
@@ -17,26 +17,26 @@
 package org.apache.log4j.spi;
 
 /**
-
- The <code>LogManager</code> uses one (and only one)
- <code>RepositorySelector</code> implementation to select the
- {@link org.apache.log4j.spi.LoggerRepository} for a particular application context.
-
- <p>It is the responsibility of the <code>RepositorySelector</code>
- implementation to track the application context. Log4j makes no
- assumptions about the application context or on its management.
-
- <p>See also {@link org.apache.log4j.LogManager LogManager}.
-
- @since 1.2
-
+ * 
+ * The <code>LogManager</code> uses one (and only one) <code>RepositorySelector</code> implementation to select the
+ * {@link LoggerRepository} for a particular application context.
+ * 
+ * <p>
+ * It is the responsibility of the <code>RepositorySelector</code> implementation to track the application context.
+ * Log4j makes no assumptions about the application context or on its management.
+ * </p>
+ * 
+ * <p>
+ * See also {@link org.apache.log4j.LogManager LogManager}.
+ * 
+ * @since 1.2
  */
 public interface RepositorySelector {
 
     /**
-     * Returns a {@link org.apache.log4j.spi.LoggerRepository} depending on the
-     * context. Implementers must make sure that a valid (non-null)
+     * Gets a {@link LoggerRepository} depending on the context. Implementers must make sure that a valid (non-null)
      * LoggerRepository is returned.
+     * 
      * @return a LoggerRepository.
      */
     LoggerRepository getLoggerRepository();
