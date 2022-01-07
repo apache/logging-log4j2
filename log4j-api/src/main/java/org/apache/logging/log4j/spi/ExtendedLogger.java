9602f7a1c86ab4830be9ca5f643b3f927a2e2b56
Gary Gregory <garydgregory@gmail.com>
2022-01-07 17:44:05 -0500

    Fix indentation.

--- log4j-api/src/main/java/org/apache/logging/log4j/spi/ExtendedLogger.java
+++ log4j-api/src/main/java/org/apache/logging/log4j/spi/ExtendedLogger.java
@@ -498,17 +498,17 @@ public interface ExtendedLogger extends Logger {
     void logIfEnabled(String fqcn, Level level, Marker marker, String message, Object p0, Object p1, Object p2,
             Object p3, Object p4, Object p5, Object p6, Object p7, Object p8, Object p9);
 
-        /**
-         * Always logs a message at the specified level. It is the responsibility of the caller to ensure the specified
-         * level is enabled.
-         *
-         * @param fqcn The fully qualified class name of the logger entry point, used to determine the caller class and
-         *            method when location information needs to be logged.
-         * @param level The logging Level to check.
-         * @param marker A Marker or null.
-         * @param message The Message.
-         * @param t the exception to log, including its stack trace.
-         */
+    /**
+     * Always logs a message at the specified level. It is the responsibility of the caller to ensure the specified
+     * level is enabled.
+     *
+     * @param fqcn The fully qualified class name of the logger entry point, used to determine the caller class and
+     *            method when location information needs to be logged.
+     * @param level The logging Level to check.
+     * @param marker A Marker or null.
+     * @param message The Message.
+     * @param t the exception to log, including its stack trace.
+     */
     void logMessage(String fqcn, Level level, Marker marker, Message message, Throwable t);
 
     /**
