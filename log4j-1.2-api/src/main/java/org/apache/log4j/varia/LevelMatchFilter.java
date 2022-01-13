444e38cec537cd74288e10c25e52a2596bd7d4a1
Gary Gregory <garydgregory@gmail.com>
2022-01-12 19:12:01 -0500

    Add classes from version 1.2.17.
    - Binary compatibility.
    - Parts may be reimplemented later.
    - Some are used in forthcoming tests.

---
+++ log4j-1.2-api/src/main/java/org/apache/log4j/varia/LevelMatchFilter.java
@@ -0,0 +1,91 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.log4j.varia;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.helpers.OptionConverter;
+import org.apache.log4j.spi.Filter;
+import org.apache.log4j.spi.LoggingEvent;
+
+/**
+ * Simple filter based on level matching.
+ *
+ * <p>
+ * The filter admits two options <b>LevelToMatch</b> and <b>AcceptOnMatch</b>. If there is an exact match between the
+ * value of the <b>LevelToMatch</b> option and the level of the {@link LoggingEvent}, then the {@link #decide} method
+ * returns {@link Filter#ACCEPT} in case the <b>AcceptOnMatch</b> option value is set to <code>true</code>, if it is
+ * <code>false</code> then {@link Filter#DENY} is returned. If there is no match, {@link Filter#NEUTRAL} is returned.
+ * </p>
+ *
+ * @since 1.2
+ */
+public class LevelMatchFilter extends Filter {
+
+    /**
+     * Do we return ACCEPT when a match occurs. Default is <code>true</code>.
+     */
+    boolean acceptOnMatch = true;
+
+    /**
+     */
+    Level levelToMatch;
+
+    /**
+     * Return the decision of this filter.
+     *
+     * Returns {@link Filter#NEUTRAL} if the <b>LevelToMatch</b> option is not set or if there is not match. Otherwise, if
+     * there is a match, then the returned decision is {@link Filter#ACCEPT} if the <b>AcceptOnMatch</b> property is set to
+     * <code>true</code>. The returned decision is {@link Filter#DENY} if the <b>AcceptOnMatch</b> property is set to false.
+     *
+     */
+    @Override
+    public int decide(final LoggingEvent event) {
+        if (this.levelToMatch == null) {
+            return Filter.NEUTRAL;
+        }
+
+        boolean matchOccured = false;
+        if (this.levelToMatch.equals(event.getLevel())) {
+            matchOccured = true;
+        }
+
+        if (matchOccured) {
+            if (this.acceptOnMatch) {
+                return Filter.ACCEPT;
+            }
+            return Filter.DENY;
+        }
+        return Filter.NEUTRAL;
+    }
+
+    public boolean getAcceptOnMatch() {
+        return acceptOnMatch;
+    }
+
+    public String getLevelToMatch() {
+        return levelToMatch == null ? null : levelToMatch.toString();
+    }
+
+    public void setAcceptOnMatch(final boolean acceptOnMatch) {
+        this.acceptOnMatch = acceptOnMatch;
+    }
+
+    public void setLevelToMatch(final String level) {
+        levelToMatch = OptionConverter.toLevel(level, null);
+    }
+}
---
+++ log4j-1.2-api/src/main/java/org/apache/log4j/varia/StringMatchFilter.java
@@ -0,0 +1,106 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.log4j.varia;
+
+import org.apache.log4j.helpers.OptionConverter;
+import org.apache.log4j.spi.Filter;
+import org.apache.log4j.spi.LoggingEvent;
+
+/**
+ * Simple filter based on string matching.
+ *
+ * <p>
+ * The filter admits two options <b>StringToMatch</b> and <b>AcceptOnMatch</b>. If there is a match between the value of
+ * the StringToMatch option and the message of the {@link org.apache.log4j.spi.LoggingEvent}, then the
+ * {@link #decide(LoggingEvent)} method returns {@link org.apache.log4j.spi.Filter#ACCEPT} if the <b>AcceptOnMatch</b>
+ * option value is true, if it is false then {@link org.apache.log4j.spi.Filter#DENY} is returned. If there is no match,
+ * {@link org.apache.log4j.spi.Filter#NEUTRAL} is returned.
+ * </p>
+ *
+ * @since 0.9.0
+ */
+public class StringMatchFilter extends Filter {
+
+    /**
+     * @deprecated Options are now handled using the JavaBeans paradigm. This constant is not longer needed and will be
+     *             removed in the <em>near</em> term.
+     */
+    @Deprecated
+    public static final String STRING_TO_MATCH_OPTION = "StringToMatch";
+
+    /**
+     * @deprecated Options are now handled using the JavaBeans paradigm. This constant is not longer needed and will be
+     *             removed in the <em>near</em> term.
+     */
+    @Deprecated
+    public static final String ACCEPT_ON_MATCH_OPTION = "AcceptOnMatch";
+
+    boolean acceptOnMatch = true;
+    String stringToMatch;
+
+    /**
+     * Returns {@link Filter#NEUTRAL} is there is no string match.
+     */
+    @Override
+    public int decide(final LoggingEvent event) {
+        final String msg = event.getRenderedMessage();
+        if (msg == null || stringToMatch == null) {
+            return Filter.NEUTRAL;
+        }
+        if (msg.indexOf(stringToMatch) == -1) {
+            return Filter.NEUTRAL;
+        }
+        return acceptOnMatch ? Filter.ACCEPT : Filter.DENY;
+    }
+
+    public boolean getAcceptOnMatch() {
+        return acceptOnMatch;
+    }
+
+    /**
+     * @deprecated We now use JavaBeans introspection to configure components. Options strings are no longer needed.
+     */
+    @Deprecated
+    public String[] getOptionStrings() {
+        return new String[] {STRING_TO_MATCH_OPTION, ACCEPT_ON_MATCH_OPTION};
+    }
+
+    public String getStringToMatch() {
+        return stringToMatch;
+    }
+
+    public void setAcceptOnMatch(final boolean acceptOnMatch) {
+        this.acceptOnMatch = acceptOnMatch;
+    }
+
+    /**
+     * @deprecated Use the setter method for the option directly instead of the generic <code>setOption</code> method.
+     */
+    @Deprecated
+    public void setOption(final String key, final String value) {
+        if (key.equalsIgnoreCase(STRING_TO_MATCH_OPTION)) {
+            stringToMatch = value;
+        } else if (key.equalsIgnoreCase(ACCEPT_ON_MATCH_OPTION)) {
+            acceptOnMatch = OptionConverter.toBoolean(value, acceptOnMatch);
+        }
+    }
+
+    public void setStringToMatch(final String s) {
+        stringToMatch = s;
+    }
+}
