327089b7d3b78449b458b244d5be0d228598809f
Gary Gregory <garydgregory@gmail.com>
2022-01-16 09:42:02 -0500

    Use some symmetry in naming: We already have 'configurationStrSubstitutor', so rename 'subst' to 'runtimeStrSubstitutor'.

--- log4j-1.2-api/src/main/java/org/apache/log4j/config/Log4j1Configuration.java
+++ log4j-1.2-api/src/main/java/org/apache/log4j/config/Log4j1Configuration.java
@@ -16,15 +16,23 @@
  */
 package org.apache.log4j.config;
 
+import java.util.Map;
+import java.util.Objects;
+import java.util.Properties;
+
 import org.apache.log4j.builders.BuilderManager;
 import org.apache.logging.log4j.core.LoggerContext;
 import org.apache.logging.log4j.core.config.AbstractConfiguration;
 import org.apache.logging.log4j.core.config.Configuration;
 import org.apache.logging.log4j.core.config.ConfigurationSource;
 import org.apache.logging.log4j.core.config.Reconfigurable;
+import org.apache.logging.log4j.core.lookup.Interpolator;
+import org.apache.logging.log4j.core.lookup.PropertiesLookup;
+import org.apache.logging.log4j.core.lookup.StrLookup;
+import org.apache.logging.log4j.core.lookup.StrSubstitutor;
 
 /**
- * Class Description goes here.
+ * Log4j 1 Configuration.
  */
 public class Log4j1Configuration extends AbstractConfiguration implements Reconfigurable {
 
@@ -38,8 +46,7 @@ public class Log4j1Configuration extends AbstractConfiguration implements Reconf
 
     protected final BuilderManager manager = new BuilderManager();
 
-    public Log4j1Configuration(final LoggerContext loggerContext, final ConfigurationSource configurationSource,
-            int monitorIntervalSeconds) {
+    public Log4j1Configuration(final LoggerContext loggerContext, final ConfigurationSource configurationSource, final int monitorIntervalSeconds) {
         super(loggerContext, configurationSource);
         initializeWatchers(this, configurationSource, monitorIntervalSeconds);
     }
@@ -54,7 +61,24 @@ public class Log4j1Configuration extends AbstractConfiguration implements Reconf
     @Override
     public void initialize() {
         getStrSubstitutor().setConfiguration(this);
-        getConfigurationStrSubstitutor().setConfiguration(this);
+        final StrSubstitutor strSubstitutor = getConfigurationStrSubstitutor();
+        strSubstitutor.setConfiguration(this);
+        // Start: Load system properties as default properties.
+        final StrLookup variableResolver = strSubstitutor.getVariableResolver();
+        if (variableResolver instanceof Interpolator) {
+//            final Interpolator interpolator = (Interpolator) variableResolver;
+//            final StrLookup defaultLookup = interpolator.getDefaultLookup();
+//            if (defaultLookup instanceof PropertiesLookup) {
+//                final Map<String, String> properties = ((PropertiesLookup) defaultLookup).getProperties();
+//                final Properties systemProperties = System.getProperties();
+//                // Must lock to avoid edits from other threads
+//                synchronized (systemProperties) {
+//                    // Merge, don't replace!
+//                    systemProperties.forEach((k, v) -> properties.merge(Objects.toString(k), Objects.toString(v), (o, n) -> o));
+//                }
+//            }
+        }
+        // End: Load system properties as default properties.
         super.getScheduler().start();
         doConfigure();
         setState(State.INITIALIZED);
