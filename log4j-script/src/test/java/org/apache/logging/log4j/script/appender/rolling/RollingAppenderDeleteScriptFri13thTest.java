f1c82a111faa8d2bbc6e94b9a652b796e48aec7e
Matt Sicker <mattsicker@apache.org>
2022-05-01 20:54:53 -0500

    Refactor test
    Signed-off-by: Matt Sicker <mattsicker@apache.org>

--- log4j-script/src/test/java/org/apache/logging/log4j/script/appender/rolling/RollingAppenderDeleteScriptFri13thTest.java
+++ log4j-script/src/test/java/org/apache/logging/log4j/script/appender/rolling/RollingAppenderDeleteScriptFri13thTest.java
@@ -16,19 +16,26 @@
  */
 package org.apache.logging.log4j.script.appender.rolling;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-
-import java.io.File;
-
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.core.test.junit.LoggerContextRule;
 import org.apache.logging.log4j.script.factory.ScriptManagerFactoryImpl;
+import org.assertj.core.api.ThrowingConsumer;
 import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.RuleChain;
+
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.locks.LockSupport;
+import java.util.function.LongSupplier;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  *
@@ -50,31 +57,38 @@ public class RollingAppenderDeleteScriptFri13thTest {
 
     @Test
     public void testAppender() throws Exception {
-        final File dir = new File(DIR);
-        dir.mkdirs();
+        final var dir = Path.of(DIR);
+        Files.createDirectories(dir);
         for (int i = 1; i <= 30; i++) {
-            final String day = i < 10 ? "0" + i : "" + i;
-            new File(dir, "test-201511" + day + "-0.log").createNewFile();
+            Files.createFile(dir.resolve(String.format("test-201511%02d-0.log", i)));
         }
-        assertEquals("Dir " + DIR + " filecount", 30, dir.listFiles().length);
+        final LongSupplier calculateDirectorySize = () -> {
+            final var size = new AtomicLong();
+            doWithDirectoryListing(dir, stream -> size.set(stream.count()));
+            return size.get();
+        };
+
+        doWithDirectoryListing(dir, stream -> assertThat(stream).hasSize(30));
 
         final Logger logger = loggerContextRule.getLogger();
         // Trigger the rollover
-        while (dir.listFiles().length < 32) {
+        while (calculateDirectorySize.getAsLong() < 32) {
             // 60+ chars per message: each message should trigger a rollover
             logger.debug("This is a very, very, very, very long test message............."); // 60+ chars:
-            Thread.sleep(100); // Allow time for rollover to complete
+            LockSupport.parkNanos(TimeUnit.MILLISECONDS.toNanos(100)); // Allow time for rollover to complete
         }
 
-        final File[] files = dir.listFiles();
-        for (final File file : files) {
-            System.out.println(file);
+        try (final var stream = Files.list(dir)) {
+            assertThat(stream.map(path -> path.getFileName().toString())).allSatisfy(name ->
+                    assertThat(name).startsWith("test-").endsWith(".log").doesNotContain("20151113"));
         }
-        for (final File file : files) {
-            assertTrue(file.getName() + " starts with 'test-'", file.getName().startsWith("test-"));
-            assertTrue(file.getName() + " ends with '.log'", file.getName().endsWith(".log"));
-            final String strDate = file.getName().substring(5, 13);
-            assertFalse(file + " is not Fri 13th", strDate.endsWith("20151113"));
+    }
+
+    private static void doWithDirectoryListing(final Path directory, final ThrowingConsumer<Stream<Path>> consumer) {
+        try (final var directoryStream = Files.list(directory)) {
+            consumer.accept(directoryStream);
+        } catch (final IOException e) {
+            throw new UncheckedIOException(e);
         }
     }
 }
