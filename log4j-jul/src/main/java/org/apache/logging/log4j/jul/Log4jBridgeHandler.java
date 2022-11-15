3a2f67a56c22d2b63e3527bfecd8a45f9f2e8424
Marcel Koch <m@rcelko.ch>
2022-11-15 18:09:01 +0100

    execute spotless:apply, again

--- log4j-jul/src/main/java/org/apache/logging/log4j/jul/Log4jBridgeHandler.java
+++ log4j-jul/src/main/java/org/apache/logging/log4j/jul/Log4jBridgeHandler.java
@@ -93,7 +93,7 @@ public class Log4jBridgeHandler extends java.util.logging.Handler implements Pro
     private boolean doDebugOutput = false;
     private String julSuffixToAppend = null;
     private transient boolean installAsLevelPropagator = false;
-    
+
     /**
      * Adds a new Log4jBridgeHandler instance to JUL's root logger.
      * This is a programmatic alternative to specify
