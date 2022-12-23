ef9d78427d9d7bbb25f692fde0d10ce8bc1d4181
Thorsten Heit <theit@gmx.de>
2022-12-23 10:03:13 +0100

    Fixed another warning from spotless-maven-plugin (tabs vs. spaces)
    Signed-off-by: Thorsten Heit <theit@gmx.de>

--- log4j-core-test/src/test/java/org/apache/logging/log4j/core/pattern/ExtendedThrowablePatternConverterTest.java
+++ log4j-core-test/src/test/java/org/apache/logging/log4j/core/pattern/ExtendedThrowablePatternConverterTest.java
@@ -139,11 +139,11 @@ public class ExtendedThrowablePatternConverterTest {
     }
 
     @Test
-	public void testDeserializedLogEventWithThrowableProxyButNoThrowable() {
-		final ExtendedThrowablePatternConverter converter = ExtendedThrowablePatternConverter.newInstance(null, null);
-		final Throwable originalThrowable = new Exception("something bad happened");
-		final ThrowableProxy throwableProxy = new ThrowableProxy(originalThrowable);
-		final Throwable deserializedThrowable = null;
+    public void testDeserializedLogEventWithThrowableProxyButNoThrowable() {
+        final ExtendedThrowablePatternConverter converter = ExtendedThrowablePatternConverter.newInstance(null, null);
+        final Throwable originalThrowable = new Exception("something bad happened");
+        final ThrowableProxy throwableProxy = new ThrowableProxy(originalThrowable);
+        final Throwable deserializedThrowable = null;
         final Log4jLogEvent event = Log4jLogEvent.newBuilder() //
                 .setLoggerName("testLogger") //
                 .setLoggerFqcn(this.getClass().getName()) //
@@ -152,12 +152,12 @@ public class ExtendedThrowablePatternConverterTest {
                 .setThrown(deserializedThrowable) //
                 .setThrownProxy(throwableProxy) //
                 .setTimeMillis(0).build();
-		final StringBuilder sb = new StringBuilder();
-		converter.format(event, sb);
-		final String result = sb.toString();
-		assertTrue(result.contains(originalThrowable.getMessage()), result);
-		assertTrue(result.contains(originalThrowable.getStackTrace()[0].getMethodName()), result);
-	}
+        final StringBuilder sb = new StringBuilder();
+        converter.format(event, sb);
+        final String result = sb.toString();
+        assertTrue(result.contains(originalThrowable.getMessage()), result);
+        assertTrue(result.contains(originalThrowable.getStackTrace()[0].getMethodName()), result);
+    }
 
     @Test
     public void testFiltering() {
