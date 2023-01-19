cf0bf168eb4b0630eb88dfbe15346c88465f51d4
Piotr P. Karwasz <piotr.github@karwasz.org>
2023-01-19 09:10:32 +0100

    Removes dependency on log4j-samples-configuration
    Depending on an unpublished artifact causes problems with `mvn site`.

--- log4j-osgi/src/test/java/org/apache/logging/log4j/osgi/tests/AbstractLoadBundleTest.java
+++ log4j-osgi/src/test/java/org/apache/logging/log4j/osgi/tests/AbstractLoadBundleTest.java
@@ -16,7 +16,6 @@
  */
 package org.apache.logging.log4j.osgi.tests;
 
-import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
@@ -25,6 +24,9 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Tests a basic Log4J 'setup' in an OSGi container.
@@ -47,10 +49,6 @@ public abstract class AbstractLoadBundleTest extends AbstractOsgiTest {
 
     private Bundle getCoreBundle() throws BundleException {
         return installBundle("org.apache.logging.log4j.core");
-    }
-
-    private Bundle getDummyBundle() throws BundleException {
-        return installBundle("org.apache.logging.log4j.samples.log4j-samples-configuration");
     }
 
     private Bundle get12ApiBundle() throws BundleException {
@@ -214,71 +212,6 @@ public abstract class AbstractLoadBundleTest extends AbstractOsgiTest {
         plugins.uninstall();
         api.uninstall();
     }
-
-    /**
-     * Tests LOG4J2-920.
-     */
-    @Test
-    public void testLoadingOfConfigurableCoreClasses() throws BundleException, ReflectiveOperationException {
-
-        final Bundle api = getApiBundle();
-        final Bundle plugins = getPluginsBundle();
-        final Bundle core = getCoreBundle();
-        final Bundle dummy = getDummyBundle();
-
-        start(api, plugins, core, dummy);
-
-        final ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        final PrintStream logStream = new PrintStream(baos);
-
-        final PrintStream bakStream = setupStream(api, logStream);
-
-        log(dummy);
-
-        setupStream(api, bakStream);
-
-        // org.apache.logging.log4j.core.osgi.BundleContextSelector cannot be found by org.apache.logging.log4j.api
-        final boolean result = baos.toString().contains("BundleContextSelector cannot be found");
-        Assert.assertFalse("Core class BundleContextSelector cannot be loaded in OSGI setup", result);
-
-        stop(api, plugins, core, dummy);
-        uninstall(api, plugins, core, dummy);
-    }
-
-    /**
-     * Tests the log of a simple message in an OSGi container
-     */
-    @Test
-    public void testSimpleLogInAnOsgiContext() throws BundleException, ReflectiveOperationException {
-
-        final Bundle api = getApiBundle();
-        final Bundle plugins = getPluginsBundle();
-        final Bundle core = getCoreBundle();
-        final Bundle dummy = getDummyBundle();
-
-        start(api, plugins, core, dummy);
-
-        final PrintStream bakStream = System.out;
-        try {
-            final ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            final PrintStream logStream = new PrintStream(baos);
-            System.setOut(logStream);
-
-            log(dummy);
-
-            final String result = baos.toString().substring(
-                12).trim(); // remove the instant then the spaces at start and end, that are non constant
-            String expected = "[main] ERROR org.apache.logging.log4j.configuration.CustomConfiguration - Test OK";
-            Assert.assertTrue("Incorrect string. Expected string ends with: " + expected + " Actual: " + result,
-                    result.endsWith(expected));
-        } finally {
-            System.setOut(bakStream);
-        }
-
-        stop(api, plugins, core, dummy);
-        uninstall(api, plugins, core, dummy);
-    }
-
 
     /**
      * Tests the loading of the 1.2 Compatibility API bundle, its classes should be loadable from the Core bundle,
