9c4b8e054534172908795e70bd264388158c3354
Piotr P. Karwasz <piotr.github@karwasz.org>
2023-01-24 09:25:09 +0100

    Fix JPA tests (switch to Postgres compatibility)

--- log4j-jpa/src/test/java/org/apache/logging/log4j/jpa/appender/JpaH2AppenderTest.java
+++ log4j-jpa/src/test/java/org/apache/logging/log4j/jpa/appender/JpaH2AppenderTest.java
@@ -40,13 +40,13 @@ public class JpaH2AppenderTest extends AbstractJpaAppenderTest {
 
         try (Statement statement = connection.createStatement()) {
             statement.executeUpdate("CREATE TABLE jpaBaseLogEntry ( "
-                    + "id INTEGER IDENTITY, eventDate DATETIME, instant NVARCHAR(64), level NVARCHAR(10), "
+                    + "id INTEGER GENERATED ALWAYS AS IDENTITY, eventDate DATETIME, instant NVARCHAR(64), level NVARCHAR(10), "
                     + "logger NVARCHAR(255), message NVARCHAR(1024), exception NVARCHAR(1048576)" + " )");
         }
 
         try (Statement statement = connection.createStatement()) {
             statement.executeUpdate("CREATE TABLE jpaBasicLogEntry ( "
-                    + "id INTEGER IDENTITY, timemillis BIGINT, instant NVARCHAR(64), nanoTime BIGINT, "
+                    + "id INTEGER GENERATED ALWAYS AS IDENTITY, timemillis BIGINT, instant NVARCHAR(64), nanoTime BIGINT, "
                     + "level NVARCHAR(10), loggerName NVARCHAR(255), message NVARCHAR(1024), "
                     + "thrown NVARCHAR(1048576), contextMapJson NVARCHAR(1048576), loggerFQCN NVARCHAR(1024), "
                     + "contextStack NVARCHAR(1048576), marker NVARCHAR(255), source NVARCHAR(2048),"
