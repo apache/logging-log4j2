d18cf9480637ff9f6ac6858910357123ba04854a
Piotr P. Karwasz <piotr.github@karwasz.org>
2023-07-21 16:58:00 +0200

    Add a `TestPropertyLookup`
    The lookup uses the same per-test source as `TestPropertySource`.

---
+++ log4j-core-test/src/main/java/org/apache/logging/log4j/core/test/junit/TestPropertyLookup.java
@@ -0,0 +1,36 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to you under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.logging.log4j.core.test.junit;
+
+import org.apache.logging.log4j.core.LogEvent;
+import org.apache.logging.log4j.core.config.plugins.Plugin;
+import org.apache.logging.log4j.core.lookup.AbstractLookup;
+import org.apache.logging.log4j.core.lookup.StrLookup;
+import org.apache.logging.log4j.test.junit.TestPropertySource;
+
+/**
+ * Resolves properties using {@link TestPropertySource}.
+ */
+@Plugin(name = "test", category = StrLookup.CATEGORY)
+public class TestPropertyLookup extends AbstractLookup {
+
+    @Override
+    public String lookup(LogEvent event, String key) {
+        return TestPropertySource.getProperties().getProperty(key);
+    }
+
+}
