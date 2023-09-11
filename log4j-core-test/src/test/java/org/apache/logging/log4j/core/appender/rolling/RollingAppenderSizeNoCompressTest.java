acad6a7e8fbd0e50c5cbb73db07e00ab0b8a7df6
Piotr P. Karwasz <piotr.github@karwasz.org>
2023-09-11 20:34:20 +0200

    Improves tests writing to `target/rolling1`

--- log4j-core-test/src/test/java/org/apache/logging/log4j/core/appender/rolling/RollingAppenderSizeNoCompressTest.java
+++ log4j-core-test/src/test/java/org/apache/logging/log4j/core/appender/rolling/RollingAppenderSizeNoCompressTest.java
@@ -17,81 +17,61 @@
 package org.apache.logging.log4j.core.appender.rolling;
 
 import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
 import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
-import org.apache.commons.compress.utils.IOUtils;
 import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.core.test.junit.LoggerContextRule;
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.RuleChain;
+import org.apache.logging.log4j.core.LoggerContext;
+import org.apache.logging.log4j.core.test.junit.LoggerContextSource;
+import org.apache.logging.log4j.status.StatusLogger;
+import org.apache.logging.log4j.test.junit.TempLoggingDir;
+import org.apache.logging.log4j.test.junit.UsingStatusListener;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * LOG4J2-1804.
  */
+@UsingStatusListener
 public class RollingAppenderSizeNoCompressTest {
 
-  private static final String CONFIG = "log4j-rolling-size.xml";
+    private static final Logger LOGGER = StatusLogger.getLogger();
 
-    private static final String DIR = "target/rolling1";
-
-    public static LoggerContextRule loggerContextRule = LoggerContextRule.createShutdownTimeoutLoggerContextRule(CONFIG);
-
-    @Rule
-    public RuleChain chain = loggerContextRule.withCleanFoldersRule(DIR);
-
-    private Logger logger;
-
-    @Before
-    public void setUp() throws Exception {
-        this.logger = loggerContextRule.getLogger(RollingAppenderSizeNoCompressTest.class.getName());
-    }
+    @TempLoggingDir
+    private static Path loggingPath;
 
     @Test
-    public void testAppender() throws Exception {
+    @LoggerContextSource
+    public void testAppender(final LoggerContext context) throws Exception {
+        final Logger logger = context.getLogger(getClass());
       final List<String> messages = new ArrayList<>();
         for (int i=0; i < 1000; ++i) {
           final String message = "This is test message number " + i;
           messages.add(message);
             logger.debug(message);
-            if (i % 100 == 0) {
-              Thread.sleep(500);
-            }
         }
-        if (!loggerContextRule.getLoggerContext().stop(30, TimeUnit.SECONDS)) {
-            System.err.println("Could not stop cleanly " + loggerContextRule + " for " + this);
+        if (!context.stop(30, TimeUnit.SECONDS)) {
+            LOGGER.error("Could not stop cleanly logger context {}.", context);
         }
-        final File dir = new File(DIR);
-        assertTrue("Directory not created", dir.exists());
-        final File[] files = dir.listFiles();
-        assertNotNull(files);
-        for (final File file : files) {
+        final List<Path> files = StreamSupport.stream(Files.newDirectoryStream(loggingPath).spliterator(), false)
+                .collect(Collectors.toList());
+        for (final Path file : files) {
           final ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            try (final FileInputStream fis = new FileInputStream(file)) {
-              try {
-                    IOUtils.copy(fis, baos);
-                } catch (final Exception ex) {
-                    ex.printStackTrace();
-                    fail("Unable to read " + file.getAbsolutePath());
-                }
-          }
+            Files.copy(file, baos);
             final String text = new String(baos.toByteArray(), Charset.defaultCharset());
             final String[] lines = text.split("[\\r\\n]+");
             for (final String line : lines) {
               messages.remove(line);
             }
         }
-        assertTrue("Log messages lost : " + messages.size(), messages.isEmpty());
-        assertTrue("Files not rolled : " + files.length, files.length > 2);
+        assertThat(messages).as("Lost messages").isEmpty();
+        assertThat(files).as("Log files").hasSizeGreaterThan(31);
     }
 }
