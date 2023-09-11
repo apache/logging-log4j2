1d6627b33ef030829cc34d7f30200791becc7ffd
Piotr P. Karwasz <piotr.github@karwasz.org>
2023-09-11 20:34:20 +0200

    Improves tests writing to `target/rolling1`

--- log4j-core-test/src/test/java/org/apache/logging/log4j/core/appender/rolling/RollingAppenderSizeNoCompressTest.java
+++ log4j-core-test/src/test/java/org/apache/logging/log4j/core/appender/rolling/RollingAppenderSizeNoCompressTest.java
@@ -17,70 +17,60 @@
 package org.apache.logging.log4j.core.appender.rolling;
 
 import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
 import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
-import org.apache.commons.compress.utils.IOUtils;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.core.LoggerContext;
 import org.apache.logging.log4j.core.test.junit.LoggerContextSource;
-import org.apache.logging.log4j.test.junit.CleanUpDirectories;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Tag;
+import org.apache.logging.log4j.status.StatusLogger;
+import org.apache.logging.log4j.test.junit.TempLoggingDir;
+import org.apache.logging.log4j.test.junit.UsingStatusListener;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * LOG4J2-1804.
  */
-@Tag("sleepy")
+@UsingStatusListener
 public class RollingAppenderSizeNoCompressTest {
 
-  private static final String CONFIG = "log4j-rolling-size.xml";
+    private static final Logger LOGGER = StatusLogger.getLogger();
 
-    private static final String DIR = "target/rolling1";
+    @TempLoggingDir
+    private static Path loggingPath;
 
     @Test
-    @CleanUpDirectories(DIR)
-    @LoggerContextSource(CONFIG)
+    @LoggerContextSource
     public void testAppender(final Logger logger, final LoggerContext context) throws Exception {
       final List<String> messages = new ArrayList<>();
         for (int i=0; i < 1000; ++i) {
           final String message = "This is test message number " + i;
           messages.add(message);
             logger.debug(message);
-            if (i % 100 == 0) {
-              Thread.sleep(500);
-            }
         }
-        assertTrue(context.stop(30, TimeUnit.SECONDS), () -> "Could not stop cleanly " + context + " for " + this);
-        final File dir = new File(DIR);
-        assertTrue(dir.exists(), "Directory not created");
-        final File[] files = dir.listFiles();
-        assertNotNull(files);
-        for (final File file : files) {
+        if (!context.stop(30, TimeUnit.SECONDS)) {
+            LOGGER.error("Could not stop cleanly logger context {}.", context);
+        }
+        final List<Path> files = StreamSupport.stream(Files.newDirectoryStream(loggingPath).spliterator(), false)
+                .collect(Collectors.toList());
+        for (final Path file : files) {
           final ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            try (final FileInputStream fis = new FileInputStream(file)) {
-              try {
-                    IOUtils.copy(fis, baos);
-                } catch (final Exception ex) {
-                    ex.printStackTrace();
-                    Assertions.fail("Unable to read " + file.getAbsolutePath());
-                }
-          }
-            final String text = baos.toString(Charset.defaultCharset());
+            Files.copy(file, baos);
+            final String text = new String(baos.toByteArray(), Charset.defaultCharset());
             final String[] lines = text.split("[\\r\\n]+");
             for (final String line : lines) {
               messages.remove(line);
             }
         }
-        assertTrue(messages.isEmpty(), "Log messages lost : " + messages.size());
-        assertTrue(files.length > 2, "Files not rolled : " + files.length);
+        assertThat(messages).as("Lost messages").isEmpty();
+        assertThat(files).as("Log files").hasSizeGreaterThan(31);
     }
 }
