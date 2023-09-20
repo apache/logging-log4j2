da4c7831167c774162d4933d35b3df712f5bf534
Piotr P. Karwasz <piotr.github@karwasz.org>
2023-09-20 16:03:36 +0200

    Make @SetTestProperty annotation inheritable

--- log4j-api-test/src/main/java/org/apache/logging/log4j/test/junit/TestPropertyResolver.java
+++ log4j-api-test/src/main/java/org/apache/logging/log4j/test/junit/TestPropertyResolver.java
@@ -16,12 +16,15 @@
  */
 package org.apache.logging.log4j.test.junit;
 
+import java.util.List;
+
 import org.apache.logging.log4j.test.TestProperties;
 import org.junit.jupiter.api.extension.BeforeAllCallback;
 import org.junit.jupiter.api.extension.BeforeEachCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.extension.ParameterContext;
 import org.junit.jupiter.api.extension.ParameterResolutionException;
+import org.junit.platform.commons.support.AnnotationSupport;
 import org.junit.platform.commons.support.HierarchyTraversalMode;
 import org.junit.platform.commons.support.ModifierSupport;
 import org.junit.platform.commons.support.ReflectionSupport;
@@ -36,9 +39,10 @@ public class TestPropertyResolver extends TypeBasedParameterResolver<TestPropert
     @Override
     public void beforeEach(final ExtensionContext context) throws Exception {
         final TestProperties props = TestPropertySource.createProperties(context);
-        final SetTestProperty[] setProperties = context.getRequiredTestMethod()
-                .getAnnotationsByType(SetTestProperty.class);
-        if (setProperties.length > 0) {
+        final List<SetTestProperty> setProperties = AnnotationSupport.findRepeatableAnnotations(
+                context.getRequiredTestMethod(),
+                SetTestProperty.class);
+        if (setProperties.size() > 0) {
             for (final SetTestProperty setProperty : setProperties) {
                 props.setProperty(setProperty.key(), setProperty.value());
             }
@@ -63,9 +67,10 @@ public class TestPropertyResolver extends TypeBasedParameterResolver<TestPropert
     @Override
     public void beforeAll(final ExtensionContext context) throws Exception {
         final TestProperties props = TestPropertySource.createProperties(context);
-        final SetTestProperty[] setProperties = context.getRequiredTestClass()
-                .getAnnotationsByType(SetTestProperty.class);
-        if (setProperties.length > 0) {
+        final List<SetTestProperty> setProperties = AnnotationSupport.findRepeatableAnnotations(
+                context.getRequiredTestClass(),
+                SetTestProperty.class);
+        if (setProperties.size() > 0) {
             for (final SetTestProperty setProperty : setProperties) {
                 props.setProperty(setProperty.key(), setProperty.value());
             }
