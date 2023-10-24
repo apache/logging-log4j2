2182b226891bef13582b70c3b25651e28b612517
Volkan Yazıcı <volkan@yazi.ci>
2023-10-24 10:47:48 +0200

    Export missing OSGi & JPMS modules (#1895)

--- log4j-1.2-api/src/main/java/org/apache/log4j/builders/package-info.java
+++ log4j-1.2-api/src/main/java/org/apache/log4j/builders/package-info.java
@@ -14,11 +14,11 @@
  * See the license for the specific language governing permissions and
  * limitations under the license.
  */
-@ExportTo("org.apache.logging.log4j.core")
+@Export
 @Open("org.apache.logging.log4j.core")
 @Version("2.20.1")
 package org.apache.log4j.builders;
 
-import aQute.bnd.annotation.jpms.ExportTo;
 import aQute.bnd.annotation.jpms.Open;
+import org.osgi.annotation.bundle.Export;
 import org.osgi.annotation.versioning.Version;
--- log4j-layout-template-json/src/main/java/org/apache/logging/log4j/layout/template/json/util/package-info.java
+++ log4j-layout-template-json/src/main/java/org/apache/logging/log4j/layout/template/json/util/package-info.java
@@ -14,4 +14,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+@Export
+@Version("2.22.0")
+@Open("org.apache.logging.log4j.core")
 package org.apache.logging.log4j.layout.template.json.util;
+
+import aQute.bnd.annotation.jpms.Open;
+import org.osgi.annotation.bundle.Export;
+import org.osgi.annotation.versioning.Version;
