fa0c52828ce7d639737a0a076e13eb743df8b4ec
Piotr P. Karwasz <piotr.github@karwasz.org>
2023-11-14 13:06:46 +0100

    Use @BaselineIgnore instead of packages exclusions

--- log4j-core/src/main/java/org/apache/logging/log4j/core/util/datetime/package-info.java
+++ log4j-core/src/main/java/org/apache/logging/log4j/core/util/datetime/package-info.java
@@ -18,8 +18,10 @@
  * Log4j 2 date formatting classes.
  */
 @Export
-@Version("2.21.0")
+@Version("2.21.1")
+@BaselineIgnore("2.22.0")
 package org.apache.logging.log4j.core.util.datetime;
 
+import aQute.bnd.annotation.baseline.BaselineIgnore;
 import org.osgi.annotation.bundle.Export;
 import org.osgi.annotation.versioning.Version;
