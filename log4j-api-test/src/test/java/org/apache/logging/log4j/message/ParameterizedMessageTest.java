66d8cc713030a515289bcf38b2e77c62156ccaf0
SeasonPan <244014926@qq.com>
2024-03-12 22:15:25 +0800

    Fix issue #2363: Extract Throwable should be irrelevant to the count of patternAnalysis

--- log4j-api-test/src/test/java/org/apache/logging/log4j/message/ParameterizedMessageTest.java
+++ log4j-api-test/src/test/java/org/apache/logging/log4j/message/ParameterizedMessageTest.java
@@ -17,7 +17,6 @@
 package org.apache.logging.log4j.message;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.math.BigDecimal;
