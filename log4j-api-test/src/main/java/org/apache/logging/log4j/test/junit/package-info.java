9f576e137b569b600cd086dd7522bc879bf16ff7
Piotr P. Karwasz <piotr.github@karwasz.org>
2024-03-27 16:51:35 +0100

    Fix BND failures

--- log4j-api-test/src/main/java/org/apache/logging/log4j/test/junit/package-info.java
+++ log4j-api-test/src/main/java/org/apache/logging/log4j/test/junit/package-info.java
@@ -16,7 +16,9 @@
  */
 @Export
 @Version("2.24.0")
+@BaselineIgnore("2.24.0")
 package org.apache.logging.log4j.test.junit;
 
+import aQute.bnd.annotation.baseline.BaselineIgnore;
 import org.osgi.annotation.bundle.Export;
 import org.osgi.annotation.versioning.Version;
