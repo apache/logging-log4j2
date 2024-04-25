490fa6082c6eada0d1239b5cb44973b55b1bff08
Michael Braun <michaelbraun@netflix.com>
2024-04-25 11:43:40 -0400

    Review feedback

--- log4j-api/src/main/java/org/apache/logging/log4j/status/StatusData.java
+++ log4j-api/src/main/java/org/apache/logging/log4j/status/StatusData.java
@@ -38,7 +38,6 @@ public class StatusData implements Serializable {
 
     private final Instant instant;
 
-    @Nullable
     private final DateTimeFormatter instantFormatter;
 
     @Nullable
@@ -79,7 +78,8 @@ public class StatusData implements Serializable {
             @Nullable final String threadName,
             @Nullable final DateTimeFormatter instantFormatter,
             final Instant instant) {
-        this.instantFormatter = instantFormatter;
+        // DateTimeFormatter.ISO_INSTANT is the default used in instant.toString()
+        this.instantFormatter = instantFormatter != null ? instantFormatter : DateTimeFormatter.ISO_INSTANT;
         this.instant = instant;
         this.caller = caller;
         this.level = requireNonNull(level, "level");
@@ -167,9 +167,7 @@ public class StatusData implements Serializable {
     @SuppressWarnings("DefaultCharset")
     public String getFormattedStatus() {
         final StringBuilder sb = new StringBuilder();
-        // DateTimeFormatter.ISO_INSTANT is the default used in instant.toString()
-        DateTimeFormatter formatterToUse = instantFormatter != null ? instantFormatter : DateTimeFormatter.ISO_INSTANT;
-        formatterToUse.formatTo(instant, sb);
+        instantFormatter.formatTo(instant, sb);
         sb.append(SPACE);
         sb.append(getThreadName());
         sb.append(SPACE);
