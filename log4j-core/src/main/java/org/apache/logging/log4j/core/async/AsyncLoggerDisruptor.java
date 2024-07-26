089601576f94198d9edde51a311a09139cc67eb9
jackshirazi <jacks@fasterj.com>
2024-07-26 20:05:12 +0100

    Fix `ClassCastException` in LMAX Disruptor 3 initialization (#2768)
    Co-authored-by: Volkan Yazıcı <volkan@yazi.ci>
    Co-authored-by: Piotr P. Karwasz <piotr.github@karwasz.org>

--- log4j-core/src/main/java/org/apache/logging/log4j/core/async/AsyncLoggerDisruptor.java
+++ log4j-core/src/main/java/org/apache/logging/log4j/core/async/AsyncLoggerDisruptor.java
@@ -36,7 +36,6 @@ import org.apache.logging.log4j.core.util.Log4jThread;
 import org.apache.logging.log4j.core.util.Log4jThreadFactory;
 import org.apache.logging.log4j.core.util.Throwables;
 import org.apache.logging.log4j.message.Message;
-import org.apache.logging.log4j.util.LoaderUtil;
 
 /**
  * Helper class for async loggers: AsyncLoggerDisruptor handles the mechanics of working with the LMAX Disruptor, and
@@ -54,7 +53,11 @@ class AsyncLoggerDisruptor extends AbstractLifeCycle {
     private static EventHandler<RingBufferLogEvent> createEventHandler() {
         if (DisruptorUtil.DISRUPTOR_MAJOR_VERSION == 3) {
             try {
-                return LoaderUtil.newInstanceOf("org.apache.logging.log4j.core.async.RingBufferLogEventHandler");
+                return (EventHandler<RingBufferLogEvent>)
+                        // Avoid using `LoaderUtil`, which might choose an incorrect class loader – see #2768.
+                        Class.forName("org.apache.logging.log4j.core.async.RingBufferLogEventHandler")
+                                .getConstructor()
+                                .newInstance();
             } catch (final ReflectiveOperationException | LinkageError e) {
                 LOGGER.warn("Failed to create event handler for LMAX Disruptor 3.x, trying version 4.x.", e);
             }
