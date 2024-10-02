6f95e5123b777a890d3d19d0b7cbd118704d2017
hulkoba <jacoba@neighbourhood.ie>
2024-10-02 12:15:58 +0200

    refactor log4j-jpa to use junit5

--- log4j-jpa/src/test/java/org/apache/logging/log4j/core/appender/db/jpa/converter/ContextStackAttributeConverterTest.java
+++ log4j-jpa/src/test/java/org/apache/logging/log4j/core/appender/db/jpa/converter/ContextStackAttributeConverterTest.java
@@ -16,22 +16,22 @@
  */
 package org.apache.logging.log4j.core.appender.db.jpa.converter;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.Arrays;
 import org.apache.logging.log4j.ThreadContext;
-import org.apache.logging.log4j.core.test.categories.Appenders;
 import org.apache.logging.log4j.spi.MutableThreadContextStack;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.experimental.categories.Category;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
 
-@Category(Appenders.Jpa.class)
+@Tag("Appenders.Jpa")
 public class ContextStackAttributeConverterTest {
     private ContextStackAttributeConverter converter;
 
-    @Before
+    @BeforeEach
     public void setUp() {
         this.converter = new ContextStackAttributeConverter();
     }
@@ -41,9 +41,9 @@ public class ContextStackAttributeConverterTest {
         final ThreadContext.ContextStack stack = new MutableThreadContextStack(Arrays.asList("value1", "another2"));
 
         assertEquals(
-                "The converted value is not correct.",
                 "value1\nanother2",
-                this.converter.convertToDatabaseColumn(stack));
+                this.converter.convertToDatabaseColumn(stack),
+                "The converted value is not correct.");
     }
 
     @Test
@@ -51,18 +51,18 @@ public class ContextStackAttributeConverterTest {
         final ThreadContext.ContextStack stack = new MutableThreadContextStack(Arrays.asList("key1", "value2", "my3"));
 
         assertEquals(
-                "The converted value is not correct.",
                 "key1\nvalue2\nmy3",
-                this.converter.convertToDatabaseColumn(stack));
+                this.converter.convertToDatabaseColumn(stack),
+                "The converted value is not correct.");
     }
 
     @Test
     public void testConvertNullToDatabaseColumn() {
-        assertNull("The converted value should be null.", this.converter.convertToDatabaseColumn(null));
+        assertNull(this.converter.convertToDatabaseColumn(null), "The converted value should be null.");
     }
 
-    @Test(expected = UnsupportedOperationException.class)
+    @Test
     public void testConvertToEntityAttribute() {
-        this.converter.convertToEntityAttribute(null);
+        assertThrows(UnsupportedOperationException.class, () -> this.converter.convertToEntityAttribute(null));
     }
 }
