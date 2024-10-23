e749fa135f78dcdf5952fe51c8191a57250c1614
Alba Herrerías <albaherreriasdev@gmail.com>
2024-10-23 10:03:32 +0100

    Migrate `log4j-slf4j2-impl` to JUnit 5 (#3080)
    Migrate `log4j-slf4j2-impl` to JUnit 5.

--- log4j-slf4j2-impl/src/test/java/org/apache/logging/slf4j/LoggerContextTest.java
+++ log4j-slf4j2-impl/src/test/java/org/apache/logging/slf4j/LoggerContextTest.java
@@ -16,12 +16,13 @@
  */
 package org.apache.logging.slf4j;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Set;
 import org.apache.logging.log4j.core.LifeCycle;
 import org.apache.logging.log4j.spi.LoggerContext;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.slf4j.LoggerFactory;
 
 /**
@@ -35,9 +36,9 @@ public class LoggerContextTest {
         factory.getLogger("test");
         Set<LoggerContext> set = factory.getLoggerContexts();
         final LoggerContext ctx1 = set.toArray(LoggerContext.EMPTY_ARRAY)[0];
-        assertTrue("LoggerContext is not enabled for shutdown", ctx1 instanceof LifeCycle);
+        assertInstanceOf(LifeCycle.class, ctx1, "LoggerContext is not enabled for shutdown");
         ((LifeCycle) ctx1).stop();
         set = factory.getLoggerContexts();
-        assertTrue("Expected no LoggerContexts", set.isEmpty());
+        assertTrue(set.isEmpty(), "Expected no LoggerContexts");
     }
 }
