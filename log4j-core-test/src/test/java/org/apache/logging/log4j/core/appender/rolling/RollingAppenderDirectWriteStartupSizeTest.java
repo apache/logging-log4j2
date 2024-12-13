2e0ce989da0708ed46ee709a7a8528e58081dd3e
Alba Herrerias <albaherreriasdev@gmail.com>
2024-12-13 12:50:35 +0000

    Migrate `RollingAppenderDirectWriteStartupSizeTest` to JUnit 5

--- log4j-core-test/src/test/java/org/apache/logging/log4j/core/appender/rolling/RollingAppenderDirectWriteStartupSizeTest.java
+++ log4j-core-test/src/test/java/org/apache/logging/log4j/core/appender/rolling/RollingAppenderDirectWriteStartupSizeTest.java
@@ -16,16 +16,19 @@
  */
 package org.apache.logging.log4j.core.appender.rolling;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import org.apache.logging.log4j.core.appender.RollingFileAppender;
-import org.apache.logging.log4j.core.test.junit.CleanFolders;
-import org.apache.logging.log4j.core.test.junit.LoggerContextRule;
-import org.junit.Assert;
-import org.junit.BeforeClass;
-import org.junit.Rule;
-import org.junit.Test;
+import org.apache.logging.log4j.core.test.junit.CleanFoldersRuleExtension;
+import org.apache.logging.log4j.core.test.junit.LoggerContextSource;
+import org.apache.logging.log4j.core.test.junit.Named;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
 
 /**
  * Test LOG4J2-2485.
@@ -40,14 +43,18 @@ public class RollingAppenderDirectWriteStartupSizeTest {
 
     private static final String MESSAGE = "test message";
 
-    @Rule
-    public LoggerContextRule loggerContextRule = LoggerContextRule.createShutdownTimeoutLoggerContextRule(CONFIG);
+    @RegisterExtension
+    private CleanFoldersRuleExtension cleanFolders = new CleanFoldersRuleExtension(
+            DIR,
+            CONFIG,
+            RollingAppenderDirectWriteStartupSizeTest.class.getName(),
+            this.getClass().getClassLoader(),
+            false,
+            true,
+            10);
 
-    @Rule
-    public CleanFolders cleanFolders = new CleanFolders(false, true, 10, DIR);
-
-    @BeforeClass
-    public static void beforeClass() throws Exception {
+    @BeforeAll
+    public static void beforeAll() throws Exception {
         final Path log = Paths.get(DIR, FILE);
         if (Files.exists(log)) {
             Files.delete(log);
@@ -59,12 +66,11 @@ public class RollingAppenderDirectWriteStartupSizeTest {
     }
 
     @Test
-    public void testRollingFileAppenderWithReconfigure() throws Exception {
-        final RollingFileAppender rfAppender =
-                loggerContextRule.getRequiredAppender("RollingFile", RollingFileAppender.class);
+    @LoggerContextSource(value = CONFIG, timeout = 10)
+    public void testRollingFileAppenderWithReconfigure(@Named("RollingFile") RollingFileAppender rfAppender) {
         final RollingFileManager manager = rfAppender.getManager();
 
-        Assert.assertNotNull(manager);
-        Assert.assertEquals("Existing file size not preserved on startup", MESSAGE.getBytes().length, manager.size);
+        assertNotNull(manager);
+        assertEquals(MESSAGE.getBytes().length, manager.size, "Existing file size not preserved on startup");
     }
 }
