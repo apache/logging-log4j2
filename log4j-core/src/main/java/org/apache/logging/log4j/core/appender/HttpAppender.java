f977274400126b2c6c172bb2083fcb309bd7f8aa
suvrat1629 <suvrat1629@gmail.com>
2025-02-05 15:48:01 +0530

    http appender and http appender build test file

--- log4j-core/src/main/java/org/apache/logging/log4j/core/appender/HttpAppender.java
+++ log4j-core/src/main/java/org/apache/logging/log4j/core/appender/HttpAppender.java
@@ -1,19 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to you under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.apache.logging.log4j.core.appender;
 
 import java.io.Serializable;
@@ -32,16 +16,15 @@ import org.apache.logging.log4j.core.config.plugins.PluginBuilderFactory;
 import org.apache.logging.log4j.core.config.plugins.PluginElement;
 import org.apache.logging.log4j.core.config.plugins.validation.constraints.Required;
 import org.apache.logging.log4j.core.net.ssl.SslConfiguration;
+import org.apache.logging.log4j.status.StatusLogger;
 
-/**
- * Sends log events over HTTP.
- */
 @Plugin(name = "Http", category = Node.CATEGORY, elementType = Appender.ELEMENT_TYPE, printObject = true)
 public final class HttpAppender extends AbstractAppender {
 
+    private static final StatusLogger LOGGER = StatusLogger.getLogger();
+
     /**
      * Builds HttpAppender instances.
-     * @param <B> The type to build
      */
     public static class Builder<B extends Builder<B>> extends AbstractAppender.Builder<B>
             implements org.apache.logging.log4j.core.util.Builder<HttpAppender> {
@@ -70,6 +53,18 @@ public final class HttpAppender extends AbstractAppender {
 
         @Override
         public HttpAppender build() {
+            // Validate URL presence
+            if (url == null) {
+                LOGGER.error("HttpAppender requires URL to be set.");
+                return null; // Return null if URL is missing
+            }
+
+            // Validate layout presence
+            if (getLayout() == null) {
+                LOGGER.error("HttpAppender requires a layout to be set.");
+                return null; // Return null if layout is missing
+            }
+
             final HttpManager httpManager = new HttpURLConnectionManager(
                     getConfiguration(),
                     getConfiguration().getLoggerContext(),
@@ -81,10 +76,12 @@ public final class HttpAppender extends AbstractAppender {
                     headers,
                     sslConfiguration,
                     verifyHostname);
+
             return new HttpAppender(
                     getName(), getLayout(), getFilter(), isIgnoreExceptions(), httpManager, getPropertyArray());
         }
 
+        // Getter and Setter methods
         public URL getUrl() {
             return url;
         }
@@ -149,9 +146,6 @@ public final class HttpAppender extends AbstractAppender {
         }
     }
 
-    /**
-     * @return a builder for a HttpAppender.
-     */
     @PluginBuilderFactory
     public static <B extends Builder<B>> B newBuilder() {
         return new Builder<B>().asBuilder();
