94d2fb86735728763b73ce98f59d06fc72e86730
ASF Logging Services RM <private@logging.apache.org>
2025-04-15 08:04:25 +0000

    Add website content generated from `1b544d38c9238a0039a99e296fe93da43b8f7ace`

--- javadoc/log4j-jakarta-web/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.apache.logging.log4j.web"}];updateSearchResults();
--- javadoc/log4j-jpa/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.apache.logging.log4j.core.appender.db.jpa"},{"l":"org.apache.logging.log4j.core.appender.db.jpa.converter"}];updateSearchResults();
