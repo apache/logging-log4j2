30e649e59cfe7a127aa632361175eabe44ae9938
Jan Luehe <jluehe@jluehe-ltmyrnq.internal.salesforce.com>
2025-05-24 11:16:34 +0200

    Have org.apache.logging.log4j.util.Base64Util invoke java.util.Base64 directly instead of reflectively (#3686)

--- log4j-api/src/main/java/org/apache/logging/log4j/util/Base64Util.java
+++ log4j-api/src/main/java/org/apache/logging/log4j/util/Base64Util.java
@@ -16,11 +16,8 @@
  */
 package org.apache.logging.log4j.util;
 
-import java.lang.reflect.Method;
 import java.nio.charset.Charset;
-import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.LoggingException;
-import org.apache.logging.log4j.status.StatusLogger;
+import java.util.Base64;
 
 /**
  * Base64 encodes Strings. This utility is only necessary because the mechanism to do this changed in Java 8 and
@@ -30,27 +27,7 @@ import org.apache.logging.log4j.status.StatusLogger;
  */
 public final class Base64Util {
 
-    private static final Logger LOGGER = StatusLogger.getLogger();
-
-    private static Method encodeMethod = null;
-    private static Object encoder = null;
-
-    static {
-        try {
-            final Class<?> clazz = LoaderUtil.loadClass("java.util.Base64");
-            final Class<?> encoderClazz = LoaderUtil.loadClass("java.util.Base64$Encoder");
-            final Method method = clazz.getMethod("getEncoder");
-            encoder = method.invoke(null);
-            encodeMethod = encoderClazz.getMethod("encodeToString", byte[].class);
-        } catch (Exception ex) {
-            try {
-                final Class<?> clazz = LoaderUtil.loadClass("javax.xml.bind.DataTypeConverter");
-                encodeMethod = clazz.getMethod("printBase64Binary");
-            } catch (Exception ex2) {
-                LOGGER.error("Unable to create a Base64 Encoder", ex2);
-            }
-        }
-    }
+    private static final Base64.Encoder ENCODER = Base64.getEncoder();
 
     private Base64Util() {}
 
@@ -60,17 +37,6 @@ public final class Base64Util {
      */
     @Deprecated
     public static String encode(final String str) {
-        if (str == null) {
-            return null;
-        }
-        final byte[] data = str.getBytes(Charset.defaultCharset());
-        if (encodeMethod != null) {
-            try {
-                return (String) encodeMethod.invoke(encoder, data);
-            } catch (Exception ex) {
-                throw new LoggingException("Unable to encode String", ex);
-            }
-        }
-        throw new LoggingException("No Encoder, unable to encode string");
+        return str != null ? ENCODER.encodeToString(str.getBytes(Charset.defaultCharset())) : null;
     }
 }
