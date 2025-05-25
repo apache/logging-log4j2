f8b135387c44d9779fa63419d6050eeec93cd32a
Piotr P. Karwasz <pkarwasz-github@apache.org>
2025-05-25 15:04:47 +0200

    fix: add since info to `RootLogger.Builder`

--- log4j-core/src/main/java/org/apache/logging/log4j/core/config/LoggerConfig.java
+++ log4j-core/src/main/java/org/apache/logging/log4j/core/config/LoggerConfig.java
@@ -943,13 +943,14 @@ public class LoggerConfig extends AbstractFilterable implements LocationAware {
             }
 
             /**
-             * @deprecated Use {@link #setFilter(Filter)} instead
+             * @deprecated since 2.25.0. Use {@link #setFilter(Filter)} instead.
              */
             @Deprecated
             public B withtFilter(final Filter filter) {
                 return setFilter(filter);
             }
 
+            /** @since 2.25.0 */
             public B setFilter(final Filter filter) {
                 this.filter = filter;
                 return asBuilder();
