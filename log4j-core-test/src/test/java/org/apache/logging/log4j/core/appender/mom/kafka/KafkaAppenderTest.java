apache/logging-log4j2

commit b967cc0d2f67707123d9f074a6ae47f33e7eabea

added retryCount

log4j-kafka/src/main/java/org/apache/logging/log4j/kafka/appender/KafkaAppender.java
@@ -49,6 +49,9 @@ public final class KafkaAppender extends AbstractAppender {
      */
     public static class Builder<B extends Builder<B>> extends AbstractAppender.Builder<B>
             implements org.apache.logging.log4j.plugins.util.Builder<KafkaAppender> {
+    	
+    	@PluginAttribute
+    	private String retryCount;
 
         @PluginAttribute
         private String topic;
@@ -68,7 +71,7 @@ public final class KafkaAppender extends AbstractAppender {
                 return null;
             }
             final KafkaManager kafkaManager = KafkaManager.getManager(getConfiguration().getLoggerContext(),
-                    getName(), topic, syncSend, getPropertyArray(), key);
+                    getName(), topic, syncSend, getPropertyArray(), key, retryCount);
             return new KafkaAppender(getName(), layout, getFilter(), isIgnoreExceptions(), getPropertyArray(), kafkaManager);
         }
 

log4j-kafka/src/main/java/org/apache/logging/log4j/kafka/appender/KafkaManager.java
@@ -58,13 +58,25 @@ public class KafkaManager extends AbstractManager {
      * The Constructor should have been declared private as all Managers are create by the internal factory;
      */
     private KafkaManager(final LoggerContext loggerContext, final String name, final String topic, final boolean syncSend,
-                        final Property[] properties, final String key) {
+                        final Property[] properties, final String key, final String retryCount) {
         super(loggerContext, name);
         this.topic = Objects.requireNonNull(topic, "topic");
         this.syncSend = syncSend;
         config.setProperty("key.serializer", "org.apache.kafka.common.serialization.ByteArraySerializer");
         config.setProperty("value.serializer", "org.apache.kafka.common.serialization.ByteArraySerializer");
         config.setProperty("batch.size", "0");
+        
+        if(retryCount!=null) {
+        	try {
+        		Integer.parseInt(retryCount);
+        		config.setProperty("retries", retryCount);
+        	}catch(NumberFormatException numberFormatException) {
+        		
+        	}
+        	
+        	
+        }
+        
         for (final Property property : properties) {
             config.setProperty(property.getName(), property.getValue());
         }
@@ -142,12 +154,12 @@ public class KafkaManager extends AbstractManager {
     }
 
     public static KafkaManager getManager(final LoggerContext loggerContext, final String name, final String topic,
-            final boolean syncSend, final Property[] properties, final String key) {
+            final boolean syncSend, final Property[] properties, final String key, final String retryCount) {
         StringBuilder sb = new StringBuilder(name);
         for (Property prop: properties) {
             sb.append(" ").append(prop.getName()).append("=").append(prop.getValue());
         }
-        return getManager(sb.toString(), factory, new FactoryData(loggerContext, topic, syncSend, properties, key));
+        return getManager(sb.toString(), factory, new FactoryData(loggerContext, topic, syncSend, properties, key, retryCount));
     }
 
     private static class FactoryData {
@@ -156,14 +168,16 @@ public class KafkaManager extends AbstractManager {
         private final boolean syncSend;
         private final Property[] properties;
         private final String key;
+        private final String retryCount;
 
         public FactoryData(final LoggerContext loggerContext, final String topic, final boolean syncSend,
-                final Property[] properties, final String key) {
+                final Property[] properties, final String key, final String retryCount) {
             this.loggerContext = loggerContext;
             this.topic = topic;
             this.syncSend = syncSend;
             this.properties = properties;
             this.key = key;
+            this.retryCount = retryCount;
         }
 
     }
@@ -171,7 +185,7 @@ public class KafkaManager extends AbstractManager {
     private static class KafkaManagerFactory implements ManagerFactory<KafkaManager, FactoryData> {
         @Override
         public KafkaManager createManager(String name, FactoryData data) {
-            return new KafkaManager(data.loggerContext, name, data.topic, data.syncSend, data.properties, data.key);
+            return new KafkaManager(data.loggerContext, name, data.topic, data.syncSend, data.properties, data.key, data.retryCount);
         }
     }
 

commit ac53a9f1c7816a2035e87b020910b231264aaba4

Fix KafkaAppender reporting error after successful retry
When retryCount is configured and the initial tryAppend() fails, the
retry loop uses break to exit on success. However, break only exits the
while loop and execution always reaches the error() call afterward. This
causes spurious error notifications for transient Kafka failures that
were successfully recovered by a retry.

Replace break with return so that a successful retry exits append()
without reporting an error. Retry exceptions are now logged at DEBUG
level for diagnostics instead of being silently discarded.

Also remove dead code in Builder.getRetryCount() where Integer.valueOf(int)
was wrapped in a NumberFormatException catch that can never fire.

The bug was introduced in #315.

Signed-off-by: Sebastien Tardif <sebtardif@ncf.ca>

log4j-core/src/main/java/org/apache/logging/log4j/core/appender/mom/kafka/KafkaAppender.java
@@ -94,13 +94,7 @@ public final class KafkaAppender extends AbstractAppender {
         }
 
         public Integer getRetryCount() {
-            Integer intRetryCount = null;
-            try {
-                intRetryCount = Integer.valueOf(retryCount);
-            } catch (NumberFormatException e) {
-
-            }
-            return intRetryCount;
+            return retryCount;
         }
 
         public String getTopic() {
@@ -216,16 +210,20 @@ public final class KafkaAppender extends AbstractAppender {
             try {
                 tryAppend(event);
             } catch (final Exception e) {
-
                 if (this.retryCount != null) {
                     int currentRetryAttempt = 0;
                     while (currentRetryAttempt < this.retryCount) {
                         currentRetryAttempt++;
                         try {
                             tryAppend(event);
-                            break;
-                        } catch (Exception e1) {
-
+                            return;
+                        } catch (final Exception retryException) {
+                            LOGGER.debug(
+                                    "Unable to write to Kafka in appender [{}], retry attempt [{}/{}].",
+                                    getName(),
+                                    currentRetryAttempt,
+                                    this.retryCount,
+                                    retryException);
                         }
                     }
                 }

log4j-core-test/src/test/java/org/apache/logging/log4j/core/appender/mom/kafka/KafkaAppenderTest.java
@@ -18,6 +18,7 @@ package org.apache.logging.log4j.core.appender.mom.kafka;
 
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
@@ -30,6 +31,8 @@ import java.util.List;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 import org.apache.kafka.clients.producer.MockProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.clients.producer.RecordMetadata;
@@ -38,6 +41,7 @@ import org.apache.kafka.common.serialization.Serializer;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.ThreadContext;
 import org.apache.logging.log4j.core.Appender;
+import org.apache.logging.log4j.core.ErrorHandler;
 import org.apache.logging.log4j.core.LogEvent;
 import org.apache.logging.log4j.core.impl.Log4jLogEvent;
 import org.apache.logging.log4j.core.test.categories.Appenders;
@@ -55,6 +59,8 @@ public class KafkaAppenderTest {
 
     private static final Serializer<byte[]> SERIALIZER = new ByteArraySerializer();
 
+    private static final AtomicInteger retrySendCount = new AtomicInteger(0);
+
     private static final MockProducer<byte[], byte[]> kafka =
             new MockProducer<byte[], byte[]>(true, SERIALIZER, SERIALIZER) {
 
@@ -65,12 +71,13 @@ public class KafkaAppenderTest {
 
                     final boolean isRetryTest = "true".equals(ThreadContext.get("KafkaAppenderWithRetryCount"));
                     if (isRetryTest) {
-                        try {
-                            throw new TimeoutException();
-                        } catch (TimeoutException e) {
-                            // TODO Auto-generated catch block
-                            throw new RuntimeException(e);
-                        }
+                        throw new RuntimeException(new TimeoutException());
+                    }
+
+                    final boolean isRetrySuccessTest =
+                            "true".equals(ThreadContext.get("KafkaAppenderRetrySuccessTest"));
+                    if (isRetrySuccessTest && retrySendCount.getAndIncrement() == 0) {
+                        throw new RuntimeException(new TimeoutException());
                     }
 
                     return retVal;
@@ -203,6 +210,40 @@ public class KafkaAppenderTest {
             e.printStackTrace();
         } finally {
             ThreadContext.clearMap();
+        }
+    }
+
+    @Test
+    public void testRetrySuccessDoesNotReportError() {
+        retrySendCount.set(0);
+        final AtomicBoolean errorReported = new AtomicBoolean(false);
+        final Appender appender = ctx.getRequiredAppender("KafkaAppenderWithRetryCount");
+        final ErrorHandler originalHandler = ((KafkaAppender) appender).getHandler();
+        try {
+            ThreadContext.put("KafkaAppenderRetrySuccessTest", "true");
+            ((KafkaAppender) appender).setHandler(new ErrorHandler() {
+                @Override
+                public void error(final String msg) {
+                    errorReported.set(true);
+                }
+
+                @Override
+                public void error(final String msg, final Throwable t) {
+                    errorReported.set(true);
+                }
+
+                @Override
+                public void error(final String msg, final LogEvent event, final Throwable t) {
+                    errorReported.set(true);
+                }
+            });
+            appender.append(createLogEvent());
+            final List<ProducerRecord<byte[], byte[]>> history = kafka.history();
+            assertEquals(2, history.size());
+            assertFalse("Error should not be reported when retry succeeds", errorReported.get());
+        } finally {
+            ThreadContext.clearMap();
+            ((KafkaAppender) appender).setHandler(originalHandler);
         }
     }
 